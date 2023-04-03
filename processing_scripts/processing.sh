6451c598438a6c4bca61bbab1926d95dd7f385f1
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-03 13:21:50 -0400

    added support for sourcing clasqadb directly inside processing scripts

--- processing_scripts/processing.sh
+++ processing_scripts/processing.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-# Check if the current shell is csh
+# Run the appropriate commands based on the user's shell
 if echo $SHELL | grep -q "csh"; then
-    # Run csh commands
-    cd clasqaDB
-    source env.csh
-    cd ..
-    coatjava/bin/run-groovy $1 $2 $3 $4
+    # Run csh commands in a subshell
+    ( 
+        exec csh -c '
+        cd clasqaDB
+        source env.csh
+        cd ..
+        coatjava/bin/run-groovy '"\$1 \$2 \$3 \$4"'
+        '
+    )
 else
     # Run sh commands
-    # Assuming you have an env.sh file for sh shell
-    cd clasqaDB
-    . env.sh
-    cd ..
-    coatjava/bin/run-groovy $1 $2 $3 $4
+    (
+        cd clasqaDB
+        . env.sh
+        cd ..
+        coatjava/bin/run-groovy "$1" "$2" "$3" "$4"
+    )
 fi
