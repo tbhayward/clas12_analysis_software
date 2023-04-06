1152dd1f08f1079ab3fe1eba4e8712dfae9d68b3
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-06 09:37:21 -0400

    added mean PTPT

--- analysis_scripts/BSA_fits.C
+++ analysis_scripts/BSA_fits.C
@@ -372,6 +372,8 @@ void performChi2Fits(const char *filename, const char* output_file, const std::s
       double sumVariable = 0;
       double sumb2b = 0;
       double numEvents = 0;
+      double sumPTPT = 0;
+      double sumDepolarization_factor = 0;
       for (const eventData& event : gData) {
         double currentVariable = getEventProperty(event, currentFits);
         if (applyKinematicCuts(event, currentFits) && currentVariable >= allBins[currentFits][i] && 
@@ -379,11 +381,20 @@ void performChi2Fits(const char *filename, const char* output_file, const std::s
             sumVariable += currentVariable;
             sumb2b += event.b2b_factor;
             numEvents += 1;
+            sumPTPT += event.PTPT;
+            float gamma = (2 * M * data.x) / sqrt(data.Q2);
+            float epsilon = (1 - data.y - 0.25 * gamma * sqrt(1 - data.y)) / (1 - data.y);
+            float depolarization_factor = sqrt(1-epsilon*epsilon);
+            sumDepolarization_factor += depolarization_factor;
         }
       }
-      cout << numEvents << endl;
+      
       double meanVariable = numEvents > 0 ? sumVariable / numEvents : 0.0;
       double meanb2b = numEvents > 0 ? sumb2b / numEvents : 0.0;
+      double meanPTPT = numEvents > 0 ? sumPTPT / numEvents : 0.0;
+      double meanDepolarization_factor = numEvents > 0 ? sumDepolarization_factor / numEvents : 0.0;
+
+      cout << numEvents << " " << meanPTPT << " " << " " << meanDepolarization_factor << " " << meanb2b << endl;
 
       double A = fitFunction->GetParameter(0);
       double A_error = fitFunction->GetParError(0);
