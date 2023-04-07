fc86054b07aa0cfe60820b66c9ca4f956416d0c8
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-07 09:21:53 -0400

    fixed further typos in neutron fit

--- analysis_scripts/BSA_neutron_fits.cpp
+++ analysis_scripts/BSA_neutron_fits.cpp
@@ -256,10 +256,10 @@ TH1D* createHistogramForBin(const std::vector<eventData>& proton_data, const std
     histAsymmetry->SetBinError(iBin, error);
   }
   TCanvas *canvas = new TCanvas("canvas","My Histogram",800,600);
-  deuterium_histPos->SetLineColor(kRed);
-  deuterium_histPos->Draw(); // draw the histogram on the canvas
-  proton_histPos->SetLineColor(kBlue);
-  proton_histPos->Draw("same"); // draw the histogram on the canvas
+  histPos->SetLineColor(kRed);
+  histPos->Draw(); // draw the histogram on the canvas
+  histNeg->SetLineColor(kBlue);
+  histNeg->Draw("same"); // draw the histogram on the canvas
   canvas->Update(); // update the canvas to show the histogram
   canvas->SaveAs("/u/home/thayward/output.png");
 
