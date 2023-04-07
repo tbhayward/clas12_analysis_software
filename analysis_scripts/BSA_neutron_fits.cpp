1dbcab01d24a97d784463263070cb3fd67e20975
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-07 10:03:45 -0400

    fixed further typos in neutron fit

--- analysis_scripts/BSA_neutron_fits.cpp
+++ analysis_scripts/BSA_neutron_fits.cpp
@@ -365,18 +365,18 @@ void performChi2Fits(const char *proton_filename, const char *deuterium_filename
   }
 
 void plotDistribution(const char *proton_filename, const char *deuterium_filename) {
-  proton_gData = readData(proton_filename);
-  deuterium_gData = readData(deuterium_filename);
+  proton_data = readData(proton_filename);
+  deuterium_data = readData(deuterium_filename);
 
   TH1D *proton_hist = new TH1D("proton_hist", "Mx1 Distribution;Mx1;Counts", 100, 0, 2);
   TH1D *deuterium_hist = new TH1D("deuterium_hist", "Mx1 Distribution;Mx1;Counts", 100, 0, 2);
 
   for (const auto &event : proton_data) {
-    proton_hist->Fill(event.Mx1);
+    proton_hist->Fill(event.Mx2);
   }
 
   for (const auto &event : deuterium_data) {
-    deuterium_hist->Fill(event.Mx1);
+    deuterium_hist->Fill(event.Mx2);
   }
 
   proton_hist->SetLineColor(kRed);
