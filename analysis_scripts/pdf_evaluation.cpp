d3d9f1f8032deff8a1d1e90ac3c575996b6aa80c
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-10 14:31:50 -0400

    changed pdf_evaluation.cpp to accept command line arguments

--- analysis_scripts/pdf_evaluation.cpp
+++ analysis_scripts/pdf_evaluation.cpp
@@ -9,6 +9,8 @@ int main() {
 
     LHAPDF::PDF* pdf = LHAPDF::mkPDF("NNPDF31_nnlo_as_0118", 0);
 
+    std::vector<double> R_u_p_values, R_d_p_values, R_u_d_values, R_d_d_values;
+
     for (size_t i = 0; i < meanX.size(); i++) {
         double x = meanX[i];
         double Q2 = meanQ2[i];
@@ -24,14 +26,29 @@ int main() {
         double R_u_d = 0.5 * (u_p + u_n) / (u_p + d_p + u_n + d_n);
         double R_d_d = 0.5 * (d_p + d_n) / (u_p + d_p + u_n + d_n);
 
-        std::cout << "x = " << x << ", Q2 = " << Q2 << std::endl;
-        std::cout << "R_u_p: " << R_u_p << std::endl;
-        std::cout << "R_d_p: " << R_d_p << std::endl;
-        std::cout << "R_u_d: " << R_u_d << std::endl;
-        std::cout << "R_d_d: " << R_d_d << std::endl;
-        std::cout << std::endl;
+        R_u_p_values.push_back(R_u_p);
+        R_d_p_values.push_back(R_d_p);
+        R_u_d_values.push_back(R_u_d);
+        R_d_d_values.push_back(R_d_d);
     }
 
     delete pdf;
+
+    auto print_vector = [](const std::vector<double>& v, const std::string& name) {
+        std::cout << name << " = {";
+        for (size_t i = 0; i < v.size(); i++) {
+            std::cout << v[i];
+            if (i < v.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "}" << std::endl;
+    };
+
+    print_vector(R_u_p_values, "R_u_p");
+    print_vector(R_d_p_values, "R_d_p");
+    print_vector(R_u_d_values, "R_u_d");
+    print_vector(R_d_d_values, "R_d_d");
+
     return 0;
 }
