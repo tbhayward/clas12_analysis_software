2b56dc33c41fdef7fff85fb3af9d3286d2bdff66
Timothy Hayward <tbhayward@email.wm.edu>
2023-04-10 14:38:30 -0400

    changed pdf_evaluation.cpp to accept command line arguments

--- analysis_scripts/pdf_evaluation.cpp
+++ analysis_scripts/pdf_evaluation.cpp
@@ -7,6 +7,8 @@ int main() {
     // ppip
     std::vector<double> meanXRGA = {0.294096, 0.282596, 0.260506, 0.238092, 0.215682, 0.192768};
     std::vector<double> meanQ2RGA = {2.99703, 2.88093, 2.72367, 2.54051, 2.37417, 2.22758};
+    std::vector<double> meanXRGB = {0.281038,0.274911,0.258215,0.24149,0.224762,0.208944}
+    std::vector<double> meanQ2RGB = {2.98496,2.9152,2.79621,2.66012,2.52836,2.43014}
 
     LHAPDF::PDF* pdf = LHAPDF::mkPDF("NNPDF31_nnlo_as_0118", 0);
 
@@ -20,8 +22,8 @@ int main() {
         double R_u_p = u_p / (u_p + d_p);
         double R_d_p = d_p / (u_p + d_p);
 
-        double u_n = pdf->xfxQ(-1, x, std::sqrt(Q2));
-        double d_n = pdf->xfxQ(-2, x, std::sqrt(Q2));
+        double u_n = pdf->xfxQ(-1, meanXRGA[i], std::sqrt(meanQ2RGB));
+        double d_n = pdf->xfxQ(-2, meanXRGA[i], std::sqrt(meanQ2RGB));
         double R_u_d = 0.5 * (u_p + u_n) / (u_p + d_p + u_n + d_n);
         double R_d_d = 0.5 * (d_p + d_n) / (u_p + d_p + u_n + d_n);
 
