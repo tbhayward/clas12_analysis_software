7cb21dfadca0838046d46b562ac1467b595223ca
tbhayward <timothybhayward@gmail.com>
2023-04-12 13:18:46 -0400

    added import of data structure to csv file

--- analysis_scripts/test.cpp
+++ analysis_scripts/test.cpp
@@ -245,8 +245,8 @@ void test(const char* data_file, const char* output_file) {
   cout<< "Found " << variable_names.size() << " variables: " << endl;
   for (size_t i = 0; i < variable_names.size(); ++i) {
     cout << i << " " << variable_names[i];
-    if (i == variable_names.size() - 1) { cout << ","; }
-    else { cout << ", "; }
+    if (i == variable_names.size() - 1) { cout << "." << endl; }
+    else { cout << ", " << endl; }
   }
   std::cout << std::endl;
 
