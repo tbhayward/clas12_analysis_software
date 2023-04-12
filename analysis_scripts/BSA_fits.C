e1857871eaa5e71a4e03a846b39be79c6e69fa8e
tbhayward <timothybhayward@gmail.com>
2023-04-11 21:38:06 -0400

    added import of data structure to csv file

--- analysis_scripts/BSA_fits.C
+++ analysis_scripts/BSA_fits.C
@@ -292,7 +292,7 @@ TH1D* createHistogramForBin(const std::vector<eventData>& data, const char* hist
 
   for (const eventData& event : data) {
     cout << "Hey we made it this far" << endl;
-    // cout << event.data.at("Delta_phi") << endl;
+    cout << event.data.at("Delta_phi") << endl;
     double currentVariable = getEventProperty(event, currentFits);
     if (applyKinematicCuts(event, currentFits) && currentVariable >= varMin && 
       currentVariable < varMax) {
