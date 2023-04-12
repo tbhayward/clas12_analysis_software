a800c869b3bf0f17ba6813dde1d0c9a7388bf8e7
tbhayward <timothybhayward@gmail.com>
2023-04-12 19:59:39 -0400

    added import of data structure to csv file

--- analysis_scripts/BSA_fits.C
+++ analysis_scripts/BSA_fits.C
@@ -263,7 +263,7 @@ void performMLMFits(const char *filename, const char* output_file, const std::st
         if (applyKinematicCuts(event, currentFits) && currentVariable >= 
           allBins[currentFits][i] && currentVariable < allBins[currentFits][i + 1]) {
             sumVariable += currentVariable;
-            sumb2b += data.data["b2b_factor"];
+            sumb2b += event.data["b2b_factor"];
             numEvents += 1;
         }
     }
