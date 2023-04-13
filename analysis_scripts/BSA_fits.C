2642ae202a203aa368324705a8a1e1fc2a6bb276
tbhayward <timothybhayward@gmail.com>
2023-04-12 20:34:06 -0400

    added import of data structure to csv file

--- analysis_scripts/BSA_fits.C
+++ analysis_scripts/BSA_fits.C
@@ -17,26 +17,26 @@ std::vector<std::string> binNames;
 std::vector<std::string> propertyNames;
 std::vector<std::string> variable_names;
 
-std::vector<float> xBins = {0.05, 0.12, 0.20, 0.28, 0.36, 0.44, 0.60};
-std::vector<float> zetaBins = {0.30, 0.400, 0.475, 0.550, 0.625, 0.70, 0.80};
-std::vector<float> PT1Bins = {0, 0.18, 0.36, 0.54, 0.72, 0.90, 1.30};
-std::vector<float> PT2Bins = {0, 0.18, 0.36, 0.54, 0.72, 0.90, 1.30};
-std::vector<float> PTPTBins = {0, 0.09, 0.18, 0.27, 0.36, 0.45, 0.6};
-std::vector<float> zeta00Bins = {0.40, 0.49, 0.58, 0.67, 0.80};
-std::vector<float> zeta20Bins = {0.40, 0.49, 0.58, 0.67, 0.80};
-std::vector<float> zeta32Bins = {0.30, 0.37, 0.44, 0.51, 0.60};
-std::vector<float> Q200Bins = {1.00, 1.50, 2.00, 2.50, 3.00};
-std::vector<float> Q220Bins = {1.00, 2.00, 3.00, 4.00, 5.00};
-std::vector<float> Q232Bins = {2.00, 3.50, 5.00, 6.50, 8.00};
-std::vector<float> z1Bins = {0.10, 0.20, 0.28, 0.36, 0.44, 0.52, 0.7};
-std::vector<float> xF1Bins = {-0.10, 0.00, 0.08, 0.16, 0.26, 0.36, 0.50};
-std::vector<float> xF2Bins = {-0.82, -0.60, -0.38, -0.16, 0.06, 0.28, 0.50};
+// std::vector<float> xBins = {0.05, 0.12, 0.20, 0.28, 0.36, 0.44, 0.60};
+// std::vector<float> zetaBins = {0.30, 0.400, 0.475, 0.550, 0.625, 0.70, 0.80};
+// std::vector<float> PT1Bins = {0, 0.18, 0.36, 0.54, 0.72, 0.90, 1.30};
+// std::vector<float> PT2Bins = {0, 0.18, 0.36, 0.54, 0.72, 0.90, 1.30};
+// std::vector<float> PTPTBins = {0, 0.09, 0.18, 0.27, 0.36, 0.45, 0.6};
+// std::vector<float> zeta00Bins = {0.40, 0.49, 0.58, 0.67, 0.80};
+// std::vector<float> zeta20Bins = {0.40, 0.49, 0.58, 0.67, 0.80};
+// std::vector<float> zeta32Bins = {0.30, 0.37, 0.44, 0.51, 0.60};
+// std::vector<float> Q200Bins = {1.00, 1.50, 2.00, 2.50, 3.00};
+// std::vector<float> Q220Bins = {1.00, 2.00, 3.00, 4.00, 5.00};
+// std::vector<float> Q232Bins = {2.00, 3.50, 5.00, 6.50, 8.00};
+// std::vector<float> z1Bins = {0.10, 0.20, 0.28, 0.36, 0.44, 0.52, 0.7};
+// std::vector<float> xF1Bins = {-0.10, 0.00, 0.08, 0.16, 0.26, 0.36, 0.50};
+// std::vector<float> xF2Bins = {-0.82, -0.60, -0.38, -0.16, 0.06, 0.28, 0.50};
 
-std::vector<std::vector<float>> allBins = {xBins, zetaBins, PT1Bins, PT2Bins, PTPTBins,
-  zeta00Bins, zeta20Bins, zeta32Bins, Q200Bins, Q220Bins, Q232Bins, z1Bins, xF1Bins, xF2Bins};
-size_t currentFits = 0;
-std::vector<std::string> binNames = {"x", "zeta", "PT1", "PT2", "PTPT", "zeta00", "zeta20", 
-  "zeta32", "Q200", "Q220", "Q232", "z1", "xF1", "xF2"};
+// std::vector<std::vector<float>> allBins = {xBins, zetaBins, PT1Bins, PT2Bins, PTPTBins,
+//   zeta00Bins, zeta20Bins, zeta32Bins, Q200Bins, Q220Bins, Q232Bins, z1Bins, xF1Bins, xF2Bins};
+// size_t currentFits = 0;
+// std::vector<std::string> binNames = {"x", "zeta", "PT1", "PT2", "PTPT", "zeta00", "zeta20", 
+//   "zeta32", "Q200", "Q220", "Q232", "z1", "xF1", "xF2"};
 
 string trim_newline(const string &str) {
   if (!str.empty() && str.back() == '\n') {
