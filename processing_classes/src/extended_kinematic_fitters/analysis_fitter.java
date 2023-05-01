009b72b4beb62f958a399d538f47eba8e53499ff
Timothy Hayward <tbhayward@email.wm.edu>
2023-05-01 13:56:35 -0400

    fixed processing dihadrons filter

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -660,14 +660,14 @@ public class analysis_fitter extends GenericKinematicFitter {
     public boolean electron_test(int particle_Index, double p, float vz, HipoDataBank rec_Bank, HipoDataBank cal_Bank, 
             HipoDataBank track_Bank, HipoDataBank traj_Bank, HipoDataBank run_Bank, HipoDataBank cc_Bank) {
         return true
-//            && p > 2.0 // higher cut ultimately enforced when we cut on y < 0.8 or y < 0.75
+            && p > 2.0 // higher cut ultimately enforced when we cut on y < 0.8 or y < 0.75
                 // this is just to speed up processing
             && forward_detector_cut(particle_Index, rec_Bank)
-//            && calorimeter_energy_cut(particle_Index, cal_Bank) 
-//            && calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
-//            && calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
-//            && electron_z_vertex_cut(vz)
-//            && pcal_fiducial_cut(particle_Index, rec_Bank, cal_Bank)
+            && calorimeter_energy_cut(particle_Index, cal_Bank) 
+            && calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
+            && calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
+            && electron_z_vertex_cut(vz)
+            && pcal_fiducial_cut(particle_Index, rec_Bank, cal_Bank)
            // && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
 //            && nphe_cut(particle_Index, cc_Bank) // legacy cut used in the analysis note to check the effect
                 ;
@@ -685,8 +685,8 @@ public class analysis_fitter extends GenericKinematicFitter {
 //            && p > 1.20
 //            && p < 5.00 // this wasn't used in the dihadron publication but was used in the submitted single pion
             && forward_detector_cut(particle_Index, rec_Bank)
-//            && pion_z_vertex_cut(vz, trigger_electron_vz)
-//            && pion_chi2pid_cut(particle_Index, rec_Bank)
+            && pion_z_vertex_cut(vz, trigger_electron_vz)
+            && pion_chi2pid_cut(particle_Index, rec_Bank)
 //            && hadron_chi2pid_cut(particle_Index, rec_Bank)
 //            && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
               ;
