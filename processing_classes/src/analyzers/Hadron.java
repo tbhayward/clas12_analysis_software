2b826fe4ecbd107673362345c5416fab77a5bedc
Timothy Hayward <tbhayward@email.wm.edu>
2023-05-06 10:40:59 -0400

    fixed processing dihadrons filter

--- processing_classes/src/analyzers/Hadron.java
+++ processing_classes/src/analyzers/Hadron.java
@@ -67,11 +67,10 @@ public class Hadron {
         if (variables.W()<2) { return false; } 
 //        if (variables.xF()<0.0) { return false; } 
         else if (variables.y()>0.75) { return false; } 
-//        else if (variables.z()<0.2 || variables.z() > 0.95) { return false; } 
 //        else if (variables.p_p()<1.25) { return false; } 
 //        else if (variables.Mx()<1.5) { return false; } 
         else if (variables.x() < 0.30 || variables.x() > 0.36) { return false; }
-        else if (variables.Q2() < 2.4 || variables.Q2() > 3.0) { return false; }
+        else if (variables.Q2() < 2.4 || variables.Q2() > 9.0) { return false; }
         else if (variables.z() < 0.2 || variables.z() > 0.7) { return false; }
         else if (variables.pT() < 0.2 || variables.pT() > 1.0) { return false; }
 	return true;
