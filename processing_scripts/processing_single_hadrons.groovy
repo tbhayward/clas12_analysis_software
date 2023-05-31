c85bdf231f0518fcbcd82302d5484731a2d8b85e
Timothy Hayward <tbhayward@email.wm.edu>
2023-05-30 22:17:08 -0400

    added minimum momentum cuts again

--- processing_scripts/processing_single_hadrons.groovy
+++ processing_scripts/processing_single_hadrons.groovy
@@ -134,7 +134,7 @@ public static void main(String[] args) {
 		    // do not use the qa if the run is from RGC (until QA is produced!)
 		    boolean process_event = filter.isValid(research_Event) && 
 		    	(runnum == 11 || runnum >= 11571 || qa.OkForAsymmetry(runnum, evnum));
-		    println("test");
+		    println(research_Event);
 		    if (process_event) {
 
 		        // get # of particles w/ pid1
