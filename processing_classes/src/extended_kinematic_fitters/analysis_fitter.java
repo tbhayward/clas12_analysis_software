46c0d2b344b12360be0af300247268179f98642a
Timothy Hayward <tbhayward@email.wm.edu>
2023-05-30 21:04:38 -0400

    added minimum momentum cuts again

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -683,8 +683,8 @@ public class analysis_fitter extends GenericKinematicFitter {
         
         return true
 //            && p > 1.20
-            && p < 5.00 // this wasn't used in the dihadron publication but was used in the single pion
-            && forward_detector_cut(particle_Index, rec_Bank)
+//            && p < 5.00 // this wasn't used in the dihadron publication but was used in the single pion
+//            && forward_detector_cut(particle_Index, rec_Bank)
 //            && pion_z_vertex_cut(vz, trigger_electron_vz)
 //            && pion_chi2pid_cut(particle_Index, rec_Bank)
 //            && hadron_chi2pid_cut(particle_Index, rec_Bank)
@@ -702,10 +702,10 @@ public class analysis_fitter extends GenericKinematicFitter {
         
         return true
 //            && p > 1.20
-            && p < 3.50 
-            && forward_detector_cut(particle_Index, rec_Bank)
+//            && p < 3.50 
+//            && forward_detector_cut(particle_Index, rec_Bank)
 //            && pion_z_vertex_cut(vz, trigger_electron_vz)
-            && hadron_chi2pid_cut(particle_Index, rec_Bank)
+//            && hadron_chi2pid_cut(particle_Index, rec_Bank)
 //            && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
               ;
     }
