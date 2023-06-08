6d34087cea21e4f38e976a7c9df7df58189c42d8
Timothy Hayward <tbhayward@email.wm.edu>
2023-06-08 12:05:59 -0400

    changed sign of polarization

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -472,6 +472,7 @@ TH1D* createHistogramForBin(const std::vector<eventData>& data, const char* hist
     float cmm = total_charge_neg_neg;
     float Ptp = meanTargetPosPol;
     float Ptm = -meanTargetNegPol;
+    cout << cpp << " " << cpm << " " << cmp << " " << cmm << " " << Ptp << " " << Ptm << endl;
     // Calculate the asymmetry and error for the current bin
     float asymmetry = (1 / meanPol) * (Ptm*(Npp-Nmp)+Ptp*(Npm-Nmm)) / 
       (Ptm*(Npp+Nmp)+Ptp*(Npm+Nmm));
