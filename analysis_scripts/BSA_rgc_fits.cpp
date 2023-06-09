61a4e2b0b4ae4bce8ab2d4114896bb6448f07ad0
Timothy Hayward <tbhayward@email.wm.edu>
2023-06-09 12:35:06 -0400

    adding in TSA and DSA for chi2 fits

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -512,9 +512,9 @@ TH1D* createHistogramForBin(const std::vector<eventData>& data, const char* hist
     float Nmm = histNegNeg->GetBinContent(iBin);
 
     // Calculate the asymmetry and error for the current bin
-    // float asymmetry = (1 / meanPol) * (Ptm*(Npp-Nmp)+Ptp*(Npm-Nmm)) / 
-    //   (Ptm*(Npp+Nmp)+Ptp*(Npm+Nmm));
-    float asymmetry = asymmetry_value_calculation(Npp, Npm, Nmp, Nmm, meanPol, Ptp, Ptm, 0);
+    float asymmetry = (1 / meanPol) * (Ptm*(Npp-Nmp)+Ptp*(Npm-Nmm)) / 
+      (Ptm*(Npp+Nmp)+Ptp*(Npm+Nmm));
+    // float asymmetry = asymmetry_value_calculation(Npp, Npm, Nmp, Nmm, meanPol, Ptp, Ptm, 0);
     float error = (2 / meanPol) * std::sqrt(
         ((cmm*cpm*cpp*Nmp*std::pow(Ptm,2)*std::pow(Npp*Ptm+Npm*Ptp,2))+
         (cmp*cpm*cpp*Nmm*std::pow(Ptp,2)*std::pow(Npp*Ptm+Npm*Ptp,2))+
