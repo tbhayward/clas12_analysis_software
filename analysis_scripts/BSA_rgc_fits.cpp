809cea3431f2b5ecd6cff2d16ec7890d48aeb961
Timothy Hayward <tbhayward@email.wm.edu>
2023-06-11 12:22:00 -0400

    adding in TSA and DSA for chi2 fits

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -324,7 +324,7 @@ void negLogLikelihood(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, In
     float maxCharge = std::max({cpp, cpm, cmp, cmm});
     cout << (maxCharge/cpp) << " " << (maxCharge/cpm) << " " << (maxCharge/cmp) << " " << (maxCharge/cmm) << endl;
     // Calculate the negative log-likelihood value and store it in the output variable f
-    f = N * log(N) - ()*(sum_PP/cpp - sum_PM/cpm - sum_MP/cmp - sum_MM/cmm);
+    f = N * log(N) - (maxCharge)*(sum_PP/cpp - sum_PM/cpm - sum_MP/cmp - sum_MM/cmm);
 }
 
 void performMLMFits(const char *filename, const char* output_file, const std::string& prefix) {
