f1af95e52540ab7c24655624f03129fbec26460f
Timothy Hayward <tbhayward@email.wm.edu>
2023-07-31 13:53:14 -0400

    added in summing over MC to constrain unpolarized modulations in the MLM

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -278,7 +278,6 @@ bool applyKinematicCuts(const eventData& data, int currentFits, bool isMC) {
 
   bool goodEvent = 0;
   std::string property = binNames[currentFits];
-  cout << "The property is named " << property << endl;
   if (property == "xF") {
     goodEvent = data.data.at("Q2")>1 && data.data.at("W")>2 && data.data.at("Mx")>1.4 &&
       data.data.at("y")<0.75;
@@ -308,6 +307,7 @@ bool applyKinematicCuts(const eventData& data, int currentFits, bool isMC) {
 }
 
 float dilution_factor(float currentVariable, const std::string& prefix) {
+  cout << "The pefix is named " << prefix << endl;
   if (prefix == "xF") {
     return 0.186121-0.0263337*currentVariable-0.175587*std::pow(currentVariable,2)+
       0.0522814*std::pow(currentVariable,3);
