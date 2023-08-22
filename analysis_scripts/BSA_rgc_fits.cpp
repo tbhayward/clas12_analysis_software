4a6fe67e64ff29edd14a404d0d2937ac6cb41e0c
Timothy Hayward <tbhayward@email.wm.edu>
2023-08-22 19:20:51 -0400

    updated FC charges basd on Tony Forest's recommendation

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -1066,7 +1066,8 @@ void performChi2Fits(const char *filename, const char* output_file, const char* 
     // outputs of mean kinematic variables
     mean_variables << i << " & " << meanQ2 << " & " << meanW << " & " << meanx << " & ";
     mean_variables << meanpT << " & " << meanz << " & " << meanzeta << " & " << meanxF << 
-    " \\\\ \\hline ";
+    std::string(" \\\\ \\hline ");
+
 
   }
 
