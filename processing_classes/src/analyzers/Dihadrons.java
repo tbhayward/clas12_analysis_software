f68f8bb1e8a73c368b9b2b5ef6775db130b4f26e
Timothy Hayward <tbhayward@email.wm.edu>
2023-08-23 08:59:24 -0400

    updated FC charges basd on Tony Forest's recommendation

--- processing_classes/src/analyzers/Dihadrons.java
+++ processing_classes/src/analyzers/Dihadrons.java
@@ -82,10 +82,10 @@ public class Dihadrons {
         if (variables.Q2()<1) { return false; } 
         else if (variables.W()<2) { return false; } 
         else if (variables.y()>0.75) { return false; } 
-//        else if (variables.Mx() < 0.80 || variables.Mx() > 1.00) { return false; }
-//        else if (variables.Mx1()<1.50) { return false; } 
-//        else if (variables.Mx2()<1.50) { return false; } 
-//        else if (variables.Mh()<0.60 || variables.Mh() > 0.90) { return false; }
+        else if (variables.Mx() < 0.80 || variables.Mx() > 1.00) { return false; }
+        else if (variables.Mx1()<1.50) { return false; } 
+        else if (variables.Mx2()<1.50) { return false; } 
+        else if (variables.Mh()<0.60 || variables.Mh() > 0.90) { return false; }
 //        else if (variables.xF1()>0.0) { return false; } 
 //        else if (variables.xF2()<0.0) { return false; } 
 //        else if (variables.z2()<0.2) { return false; } 
