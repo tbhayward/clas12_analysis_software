d2f9d5c830ee2ec7ad335caca8d5f14a7faf8e6b
Timothy Hayward <tbhayward@email.wm.edu>
2023-08-25 13:38:29 -0400

    updated FC charges basd on Tony Forest's recommendation

--- processing_scripts/injection_single_hadrons.groovy
+++ processing_scripts/injection_single_hadrons.groovy
@@ -59,19 +59,19 @@ def helicity_assignment(double Q2, double x, double PT, double z, double zeta, d
 	// double ALL = 0;
 	// double ALLcosphi = 0;
 
-	// TEST 3
-	double ALUsinphi = -0.1; 
-	double AULsinphi = 0;
-	double AULsin2phi = 0;
-	double ALL = 0.3;
-	double ALLcosphi = 0.01;
-
-	// // TEST 4
-	// double ALUsinphi = 0; 
-	// double AULsinphi = -0.1;
+	// // TEST 3
+	// double ALUsinphi = -0.1; 
+	// double AULsinphi = 0;
 	// double AULsin2phi = 0;
 	// double ALL = 0.3;
-	// double ALLcosphi = 0;
+	// double ALLcosphi = 0.01;
+
+	// TEST 4
+	double ALUsinphi = 0; 
+	double AULsinphi = -0.1;
+	double AULsin2phi = 0;
+	double ALL = 0.3;
+	double ALLcosphi = 0;
 
 	// // TEST 5
 	// double ALUsinphi = -0.22; 
