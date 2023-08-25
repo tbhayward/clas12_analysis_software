53e199f7942c52f9883f34213cf2ca7ca5c39e37
Timothy Hayward <tbhayward@email.wm.edu>
2023-08-25 18:11:37 -0400

    updated FC charges basd on Tony Forest's recommendation

--- processing_scripts/injection_single_hadrons.groovy
+++ processing_scripts/injection_single_hadrons.groovy
@@ -39,10 +39,10 @@ public static double theta_calculation (double x, double y, double z) {
 
 def helicity_assignment(double Q2, double x, double PT, double z, double zeta, double phi,
 	double A, double B, double C, double W, double V) {
-	// double Pb = 0.83534; // injected beam polarization
+	double Pb = 0.83534; // injected beam polarization
 	// double Pt = 0.76200; // injected target polarization
 	// double Df = 0.1158; // injected dilution factor
-	double Pb = 1; // injected beam polarization
+	// double Pb = 1; // injected beam polarization
 	double Pt = 1; // injected target polarization
 	double Df = 1; // injected dilution factor
 
@@ -94,7 +94,7 @@ def helicity_assignment(double Q2, double x, double PT, double z, double zeta, d
 		// 	ht*Pt*Df*(B/A)*AULsin2phi*Math.sin(2*phi) + 
 		// 	hb*Pb*ht*Pt*Df*(C/A)*ALL + 
 		// 	hb*Pb*ht*Pt*Df*(W/A)*ALLcosphi*Math.cos(phi); 
-		double weight = 1 + hb*ALUsinphi*Math.sin(phi);
+		double weight = 1 + hb*Pb*ALUsinphi*Math.sin(phi);
 		def randomValue = new Random().nextDouble() * 2;
 		if (weight > randomValue) { weight_check = false; }
 	}
