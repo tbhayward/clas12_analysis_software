338a1825aea4f8514df8e570a30f011af280a25b
Timothy Hayward <tbhayward@email.wm.edu>
2023-08-28 09:57:55 -0400

    updated FC charges basd on Tony Forest's recommendation

--- processing_scripts/injection_single_hadrons.groovy
+++ processing_scripts/injection_single_hadrons.groovy
@@ -106,7 +106,8 @@ def helicity_assignment(double Q2, double x, double PT, double z, double zeta, d
 		// 	ht*Pt*Df*(A/B)*AULsin2phi*Math.sin(2*phi) + 
 		// 	hb*Pb*ht*Pt*Df*(A/C)*ALL + 
 		// 	hb*Pb*ht*Pt*Df*(A/W)*ALLcosphi*Math.cos(phi); 
-		double weight = 1 + hb*Pb*ht*Pt*Df*(A/W)*ALLcosphi*Math.cos(phi); 
+		// double weight = 1 + hb*Pb*ht*Pt*Df*(A/W)*ALLcosphi*Math.cos(phi); 
+		double weight = 1 + Pb*ht*Pt*Df*(A/W)*ALLcosphi*Math.cos(phi); 
 		def randomValue = new Random().nextDouble() * 2;
 		if (weight > randomValue) { weight_check = false; }
 	}
