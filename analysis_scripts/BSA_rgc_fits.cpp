a5539f1dbeeffb69c535a57cbb1e9073d146c203
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-03 12:43:33 -0400

    added injection study scripts

--- analysis_scripts/BSA_rgc_fits.cpp
+++ analysis_scripts/BSA_rgc_fits.cpp
@@ -234,8 +234,8 @@ eventData parseLine(const std::string& line, const std::vector<std::string>& var
   float E = 10.55; // beam energy
   float me = 0.000511; // electron mass in GeV
   float k = TMath::sqrt(E*E-me*me);
-  data.data["tmin"] = 2*me*(E-TMath::sqrt(me*me+data.data["e_p"]*data.data["e_p"]));
-  data.data["t"] = -2*k*data.data["e_p"]*(cos(data.data["e_theta"]-1))-tmin;
+  data.data["tmin"] = 2*me*(E-sqrt(me*me+data.data["e_p"]*data.data["e_p"]));
+  data.data["t"] = -2*k*data.data["e_p"]*(cos(data.data["e_theta"]-1))-data.data["tmin"];
 
   // Return the populated eventData object
   return data;
