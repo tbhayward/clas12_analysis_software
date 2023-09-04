0ed50a73e71bdacaeba1eb7b67cad4cad25288ba
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-03 20:04:39 -0400

    added injection study scripts

--- processing_classes/src/analyzers/Hadron.java
+++ processing_classes/src/analyzers/Hadron.java
@@ -64,7 +64,7 @@ public class Hadron {
 //            return false; }
         if (variables.Q2()<1) { return false; } 
         if (variables.W()<2) { return false; } 
-        if (variables.xF()<0.0) { return false; } 
+//        if (variables.xF()<0.0) { return false; } 
         else if (variables.y()>0.75) { return false; } 
 //        else if (variables.z()<0.2 || variables.z() > 0.95) { return false; } 
 //        else if (variables.p_p()<1.25) { return false; } 
