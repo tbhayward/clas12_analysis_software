6f4f1e72e9a91a8c836ecf400049f5f2e67456f1
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 17:20:01 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -251,7 +251,7 @@ void createHistograms(TTree* tree1, TTree* tree2,
             tree1->GetEntry(entry);
             int dyn_bin = (branch_var - min_val) / ((max_val - min_val) / 6);
             int phi_bin = phi / (2 * TMath::Pi() / 12);
-            cout << beam_pol << endl;
+            cout << dyn_bin << " " << phi_bin << endl;
             if (helicity > 0) {
                 N_pos[phi_bin]++;
             } else if (helicity < 0) {
