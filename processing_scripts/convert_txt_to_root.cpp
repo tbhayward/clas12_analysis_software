54cf487ebda531bc95ba3a35e24f9d62e97328bf
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-05 22:52:03 -0400

    added injection study scripts

--- processing_scripts/convert_txt_to_root.cpp
+++ processing_scripts/convert_txt_to_root.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     // Check for correct number of command line arguments
-    if (argc != 3) {
-        cout << "Usage: " << argv[0] << " <input_text_file> <output_root_file>" << endl;
+    if (argc != 4) {
+        cout << "Usage: " << argv[0];
+        cout << " <input_text_file> <output_root_file> <hadron_count>" << endl;
         return 1;
     }
     
@@ -24,47 +25,158 @@ int main(int argc, char *argv[]) {
     TFile *outfile = new TFile(argv[2], "RECREATE");
     TTree *tree = new TTree("PhysicsEvents", "Physics Events Tree");
 
-    // Declare variables to store data for each event
+    // Determine the hadron count from the command line argument
+    int hadron_count = atoi(argv[3]);
+
+    // Declare common variables
     int runnum, evnum, helicity;
-    double e_p, e_theta, e_phi, vz_e, p_p, p_theta, p_phi, vz_p, Q2, W, Mx, Mx2;
-    double x, y, z, xF, pT, zeta, eta, phi, DepA, DepB, DepC, DepV, DepW;
+    double e_p, e_theta, e_phi, vz_e, Q2, W, Mx, Mx2, x, y;
 
-    // Link TTree branches to variables
-    tree->Branch("runnum", &runnum, "runnum/I");
-    tree->Branch("evnum", &evnum, "evnum/I");
-    tree->Branch("helicity", &helicity, "helicity/I");
-    tree->Branch("e_p", &e_p, "e_p/D");
-    tree->Branch("e_theta", &e_theta, "e_theta/D");
-    tree->Branch("e_phi", &e_phi, "e_phi/D");
-    tree->Branch("vz_e", &vz_e, "vz_e/D");
-    tree->Branch("p_p", &p_p, "p_p/D");
-    tree->Branch("p_theta", &p_theta, "p_theta/D");
-    tree->Branch("p_phi", &p_phi, "p_phi/D");
-    tree->Branch("vz_p", &vz_p, "vz_p/D");
-    tree->Branch("Q2", &Q2, "Q2/D");
-    tree->Branch("W", &W, "W/D");
-    tree->Branch("Mx", &Mx, "Mx/D");
-    tree->Branch("Mx2", &Mx2, "Mx2/D");
-    tree->Branch("x", &x, "x/D");
-    tree->Branch("y", &y, "y/D");
-    tree->Branch("z", &z, "z/D");
-    tree->Branch("xF", &xF, "xF/D");
-    tree->Branch("pT", &pT, "pT/D");
-    tree->Branch("zeta", &zeta, "zeta/D");
-    tree->Branch("eta", &eta, "eta/D");
-    tree->Branch("phi", &phi, "phi/D");
-    tree->Branch("DepA", &DepA, "DepA/D");
-    tree->Branch("DepB", &DepB, "DepB/D");
-    tree->Branch("DepC", &DepC, "DepC/D");
-    tree->Branch("DepV", &DepV, "DepV/D");
-    tree->Branch("DepW", &DepW, "DepW/D");
-
-    // Loop to read each line from the text file and fill the TTree
-    while (infile >> runnum >> evnum >> helicity >> e_p >> e_theta >> e_phi >> vz_e >> 
-        p_p >> p_theta >> p_phi >> vz_p >> Q2 >> W >> Mx >> Mx2 >> x >> y >> z >> xF >> pT >> 
-        zeta >> eta >> phi >> DepA >> DepB >> DepC >> DepV >> DepW) {
-        tree->Fill(); // Fill the tree with the read data
+    // Case for zero hadrons (inclusive)
+    if (hadron_count == 0) {
+        // Link TTree branches to variables for zero hadrons
+        tree->Branch("runnum", &runnum, "runnum/I");
+        tree->Branch("evnum", &evnum, "evnum/I");
+        tree->Branch("helicity", &helicity, "helicity/I");
+        tree->Branch("e_p", &e_p, "e_p/D");
+        tree->Branch("e_theta", &e_theta, "e_theta/D");
+        tree->Branch("e_phi", &e_phi, "e_phi/D");
+        tree->Branch("vz_e", &vz_e, "vz_e/D");
+        tree->Branch("Q2", &Q2, "Q2/D");
+        tree->Branch("W", &W, "W/D");
+        tree->Branch("Mx", &Mx, "Mx/D");
+        tree->Branch("Mx2", &Mx2, "Mx2/D");
+        tree->Branch("x", &x, "x/D");
+        tree->Branch("y", &y, "y/D");
     }
+
+    // Case for one hadron
+    else if (hadron_count == 1) {
+        double z, xF, pT, zeta, eta, phi, DepA, DepB, DepC, DepV, DepW;
+        double p_p, p_theta, p_phi, vz_p;
+        // Link TTree branches to variables for one hadron
+        tree->Branch("runnum", &runnum, "runnum/I");
+        tree->Branch("evnum", &evnum, "evnum/I");
+        tree->Branch("helicity", &helicity, "helicity/I");
+        tree->Branch("e_p", &e_p, "e_p/D");
+        tree->Branch("e_theta", &e_theta, "e_theta/D");
+        tree->Branch("e_phi", &e_phi, "e_phi/D");
+        tree->Branch("vz_e", &vz_e, "vz_e/D");
+        tree->Branch("p_p", &p_p, "p_p/D");
+        tree->Branch("p_theta", &p_theta, "p_theta/D");
+        tree->Branch("p_phi", &p_phi, "p_phi/D");
+        tree->Branch("vz_p", &vz_p, "vz_p/D");
+        tree->Branch("Q2", &Q2, "Q2/D");
+        tree->Branch("W", &W, "W/D");
+        tree->Branch("Mx", &Mx, "Mx/D");
+        tree->Branch("Mx2", &Mx2, "Mx2/D");
+        tree->Branch("x", &x, "x/D");
+        tree->Branch("y", &y, "y/D");
+        tree->Branch("z", &z, "z/D");
+        tree->Branch("xF", &xF, "xF/D");
+        tree->Branch("pT", &pT, "pT/D");
+        tree->Branch("zeta", &zeta, "zeta/D");
+        tree->Branch("eta", &eta, "eta/D");
+        tree->Branch("phi", &phi, "phi/D");
+        tree->Branch("DepA", &DepA, "DepA/D");
+        tree->Branch("DepB", &DepB, "DepB/D");
+        tree->Branch("DepC", &DepC, "DepC/D");
+        tree->Branch("DepV", &DepV, "DepV/D");
+        tree->Branch("DepW", &DepW, "DepW/D");
+    }
+
+    // Case for two hadrons (dihadrons)
+    else if (hadron_count == 2) {
+        // Additional variables for two hadrons
+        double p1_p, p1_theta, p1_phi, vz_p1, p2_p, p2_theta, p2_phi, vz_p2;
+        double z1, z2, Mh, xF1, xF2, pT1, pT2, pTpT, zeta1, zeta2;
+        double eta1, eta2, Delta_eta, eta1_gN, eta2_gN;
+        double phi1, phi2, Delta_phi, phih, phiR, theta;
+        double DepA, DepB, DepC, DepV, DepW;
+
+        // Link TTree branches to variables for two hadrons
+        tree->Branch("runnum", &runnum, "runnum/I");
+        tree->Branch("evnum", &evnum, "evnum/I");
+        tree->Branch("helicity", &helicity, "helicity/I");
+        tree->Branch("e_p", &e_p, "e_p/D");
+        tree->Branch("e_theta", &e_theta, "e_theta/D");
+        tree->Branch("e_phi", &e_phi, "e_phi/D");
+        tree->Branch("vz_e", &vz_e, "vz_e/D");
+        tree->Branch("p1_p", &p1_p, "p1_p/D");
+        tree->Branch("p1_theta", &p1_theta, "p1_theta/D");
+        tree->Branch("p1_phi", &p1_phi, "p1_phi/D");
+        tree->Branch("vz_p1", &vz_p1, "vz_p1/D");
+        tree->Branch("p2_p", &p2_p, "p2_p/D");
+        tree->Branch("p2_theta", &p2_theta, "p2_theta/D");
+        tree->Branch("p2_phi", &p2_phi, "p2_phi/D");
+        tree->Branch("vz_p2", &vz_p2, "vz_p2/D");
+        tree->Branch("Q2", &Q2, "Q2/D");
+        tree->Branch("W", &W, "W/D");
+        tree->Branch("Mx", &Mx, "Mx/D");
+        tree->Branch("Mx2", &Mx2, "Mx2/D");
+        tree->Branch("x", &x, "x/D");
+        tree->Branch("y", &y, "y/D");
+        tree->Branch("z1", &z1, "z1/D");
+        tree->Branch("z2", &z2, "z2/D");
+        tree->Branch("Mh", &Mh, "Mh/D");
+        tree->Branch("xF1", &xF1, "xF1/D");
+        tree->Branch("xF2", &xF2, "xF2/D");
+        tree->Branch("pT1", &pT1, "pT1/D");
+        tree->Branch("pT2", &pT2, "pT2/D");
+        tree->Branch("pTpT", &pTpT, "pTpT/D");
+        tree->Branch("zeta1", &zeta1, "zeta1/D");
+        tree->Branch("zeta2", &zeta2, "zeta2/D");
+        tree->Branch("eta1", &eta1, "eta1/D");
+        tree->Branch("eta2", &eta2, "eta2/D");
+        tree->Branch("Delta_eta", &Delta_eta, "Delta_eta/D");
+        tree->Branch("eta1_gN", &eta1_gN, "eta1_gN/D");
+        tree->Branch("eta2_gN", &eta2_gN, "eta2_gN/D");
+        tree->Branch("phi1", &phi1, "phi1/D");
+        tree->Branch("phi2", &phi2, "phi2/D");
+        tree->Branch("Delta_phi", &Delta_phi, "Delta_phi/D");
+        tree->Branch("phih", &phih, "phih/D");
+        tree->Branch("phiR", &phiR, "phiR/D");
+        tree->Branch("theta", &theta, "theta/D");
+        tree->Branch("DepA", &DepA, "DepA/D");
+        tree->Branch("DepB", &DepB, "DepB/D");
+        tree->Branch("DepC", &DepC, "DepC/D");
+        tree->Branch("DepV", &DepV, "DepV/D");
+        tree->Branch("DepW", &DepW, "DepW/D");
+    }
+
+    // Loop to read each line from the text file and fill the TTree based on hadron_count
+    if (hadron_count == 0) {
+        while (infile >> runnum >> evnum >> helicity >> e_p >> e_theta >> e_phi >> vz_e >> 
+            Q2 >> W >> Mx >> Mx2 >> x >> y) {
+            tree->Fill(); // Fill the tree with the read data
+        }
+    } 
+    else if (hadron_count == 1) {
+        double z, xF, pT, zeta, eta, phi, DepA, DepB, DepC, DepV, DepW;
+        double p_p, p_theta, p_phi, vz_p;
+        while (infile >> runnum >> evnum >> helicity >> e_p >> e_theta >> e_phi >> vz_e >> 
+            p_p >> p_theta >> p_phi >> vz_p >> Q2 >> W >> Mx >> Mx2 >> x >> y >> z >> xF >> 
+            pT >> zeta >> eta >> phi >> DepA >> DepB >> DepC >> DepV >> DepW) {
+            tree->Fill(); // Fill the tree with the read data
+        }
+    } 
+    else if (hadron_count == 2) {
+        // Additional variables for two hadrons
+        double p1_p, p1_theta, p1_phi, vz_p1, p2_p, p2_theta, p2_phi, vz_p2;
+        double z1, z2, Mh, xF1, xF2, pT1, pT2, pTpT, zeta1, zeta2;
+        double eta1, eta2, Delta_eta, eta1_gN, eta2_gN;
+        double phi1, phi2, Delta_phi, phih, phiR, theta;
+        double DepA, DepB, DepC, DepV, DepW;
+        while (infile >> runnum >> evnum >> helicity >> e_p >> e_theta >> e_phi >> vz_e >> 
+            p1_p >> p1_theta >> p1_phi >> vz_p1 >> p2_p >> p2_theta >> p2_phi >> vz_p2 >> 
+            Q2 >> W >> Mx >> Mx2 >> x >> y >> z1 >> z2 >> Mh >> xF1 >> xF2 >> pT1 >> pT2 >> 
+            pTpT >> zeta1 >> zeta2 >> eta1 >> eta2 >> Delta_eta >> eta1_gN >> eta2_gN >> 
+            phi1 >> phi2 >> Delta_phi >> phih >> phiR >> theta >> 
+            DepA >> DepB >> DepC >> DepV >> DepW) {
+            tree->Fill(); // Fill the tree with the read data
+        }
+    }
+
 
     // Write the TTree to the ROOT file and close it
     tree->Write();
