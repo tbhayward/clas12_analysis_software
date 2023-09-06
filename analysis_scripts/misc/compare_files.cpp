0b8190524ea877a1b3f61fe0ff15870111b19b5d
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 14:15:14 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -1,4 +1,6 @@
-#include <TFile.h>
+// Created 9/6/23
+// Created to compare epi+X and epX nSidis distributions between pass-1 and preliminary pass-2 
+
 #include <TTree.h>
 #include <TCanvas.h>
 #include <TH1F.h>
@@ -6,47 +8,12 @@
 #include <TPaveText.h>
 #include <TAxis.h>
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <map>
-#include <cmath>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
-
-// Created 9/6/23
-// Created to compare epi+X and epX nSidis distributions between pass-1 and preliminary pass-2 
-
-struct HistConfig {
-    int bins;
-    double min;
-    double max;
-};
-
-std::map<std::string, HistConfig> histConfigs = {
-    {"beam_pol", {20, 0.80, 1.00}},
-    {"e_p", {200, 2, 9}},
-    {"e_phi", {200, 0, 2 * M_PI}},
-    {"eta", {200, -1, 3}},
-    {"e_theta", {200, 0, 40 * (M_PI / 180.0)}}, // Convert degree to radian
-    {"Mx", {200, -4, 3}},
-    {"Mx2", {200, -10, 10}},
-    {"phi", {200, 0, 2 * M_PI}},
-    {"p_p", {200, 0, 6}},
-    {"p_phi", {200, 0, 2 * M_PI}},
-    {"pT", {200, 0, 1.3}},
-    {"p_theta", {200, 0, 90 * (M_PI / 180.0)}}, // Convert degree to radian
-    {"Q2", {200, 0, 9}},
-    {"t", {200, -10, 1}},
-    {"tmin", {200, -0.5, 0}},
-    {"vze", {200, -15, 15}},
-    {"vzp", {200, -15, 15}},
-    {"W", {200, 2, 4}},
-    {"x", {200, 0, 1}},
-    {"xF", {200, -1, 1}},
-    {"y", {200, 0, 1}},
-    {"z", {200, 0, 1}},
-    {"zeta", {200, 0, 1}}
-};
 
 std::string formatBranchName(const std::string& original) {
     std::map<std::string, std::string> specialLabels = {
@@ -95,40 +62,20 @@ void createHistograms(TTree* tree1, TTree* tree2, const char* outDir) {
     TObjArray* branches2 = tree2->GetListOfBranches();
 
     if (branches1->GetEntries() != branches2->GetEntries()) {
-        cout << "Number of branches mismatch. Exiting." << endl;
+        std::cout << "Number of branches mismatch. Exiting." << std::endl;
         return;
     }
 
     for (int i = 0; i < branches1->GetEntries(); ++i) {
         const char* branchName = branches1->At(i)->GetName();
-        string formattedBranchName = formatBranchName(branchName);
-        
-        // Define your bin configurations here
-        int bins = 200; // default
-        double min = 0.0; // default
-        double max = 0.0; // default
+        std::string formattedBranchName = formatBranchName(branchName);
+        TCanvas canvas(branchName, "Canvas", 800, 600);
 
-        if (strcmp(branchName, "beam_pol") == 0) {
-            bins = 20;
-            min = 0.80;
-            max = 1.00;
-        } else if (strcmp(branchName, "e_p") == 0) {
-            min = 2;
-            max = 9;
-        } else if (strcmp(branchName, "e_phi") == 0) {
-            min = 0;
-            max = 2 * M_PI;
-        } else if (strcmp(branchName, "eta") == 0) {
-            min = -1;
-            max = 3;
-        } 
+        TH1F hist1(Form("%s_1", branchName), "", 200, 0, 0);
+        TH1F hist2(Form("%s_2", branchName), "", 200, 0, 0);
 
-
-        TH1F hist1(Form("%s_1", branchName), "", bins, min, max);
-        TH1F hist2(Form("%s_2", branchName), "", bins, min, max);
-
-        string cutCondition = "";
-        if (strcmp(branchName, "Mx") != 0 && strcmp(branchName, "Mx2") != 0) {
+        std::string cutCondition = "";
+        if (std::strcmp(branchName, "Mx") != 0 && std::strcmp(branchName, "Mx2") != 0) {
             cutCondition = "Mx > 1.5";
         }
 
@@ -138,8 +85,8 @@ void createHistograms(TTree* tree1, TTree* tree2, const char* outDir) {
         hist1.SetLineColor(kRed);
         hist2.SetLineColor(kBlue);
 
-        hist1.Draw();
-        hist2.Draw("same");
+        hist1.Draw(""); 
+        hist2.Draw("same"); 
 
         hist1.SetStats(0);
         hist2.SetStats(0);
@@ -151,38 +98,21 @@ void createHistograms(TTree* tree1, TTree* tree2, const char* outDir) {
         hist1.SetMaximum(max_value * 1.1);
         hist2.SetMaximum(max_value * 1.1);
 
-        TPaveText* stats = new TPaveText(0.65, 0.85, 0.85, 0.95, "NDC");
-        stats->AddText(Form("pass-1: %d", int(hist1.GetEntries())));
-        stats->AddText(Form("pass-2: %d", int(hist2.GetEntries())));
-        stats->SetTextAlign(12);
-        stats->Draw("same");
+        TPaveText* stats1 = new TPaveText(0.15, 0.85, 0.35, 0.95, "NDC");
+        TPaveText* stats2 = new TPaveText(0.65, 0.85, 0.85, 0.95, "NDC");
 
-        TCanvas canvas(Form("%s_Canvas", branchName), "Canvas", 800, 600);
+        stats1->AddText("pass-1");
+        stats1->AddText(Form("Counts: %d", int(hist1.GetEntries())));
+        stats1->SetTextAlign(12);
+        stats1->SetTextColor(kRed);
+        stats1->Draw("same");
+
+        stats2->AddText("pass-2");
+        stats2->AddText(Form("Counts: %d", int(hist2.GetEntries())));
+        stats2->SetTextAlign(12);
+        stats2->SetTextColor(kBlue);
+        stats2->Draw("same");
+
         canvas.SaveAs(Form("%s/%s.png", outDir, branchName));
     }
-}
-
-
-
-
-void compare_files(std::string root_file1_path, std::string root_file2_path) {
-
-    TFile* file1 = new TFile(root_file1_path.c_str(), "READ");
-    TFile* file2 = new TFile(root_file2_path.c_str(), "READ");
-
-    if (!file1->IsOpen() || !file2->IsOpen()) {
-        cout << "Error opening ROOT files (is the location correct?). Exiting." << endl;
-    }
-
-    TTree* tree1 = (TTree*)file1->Get("PhysicsEvents");
-    TTree* tree2 = (TTree*)file2->Get("PhysicsEvents");
-
-    if (!tree1 || !tree2) {
-        cout << "Error getting trees from ROOT files." << endl;
-    }
-
-    createHistograms(tree1, tree2, "output");
-
-    file1->Close();
-    file2->Close();
 }
