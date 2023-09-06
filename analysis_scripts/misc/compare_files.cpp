5b4bb30cd258e7b3e0f8a75e385db6284c78b02a
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 14:02:41 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -2,37 +2,51 @@
 #include <TTree.h>
 #include <TCanvas.h>
 #include <TH1F.h>
-#include <TKey.h>
-#include <TList.h>
-#include <TPaveText.h>  
+#include <TObjArray.h>
+#include <TPaveText.h>
+#include <TAxis.h>
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <cmath>
 
 using namespace std;
 
 // Created 9/6/23
 // Created to compare epi+X and epX nSidis distributions between pass-1 and preliminary pass-2 
 
-#include <TCanvas.h>
-#include <TFile.h>
-#include <TH1F.h>
-#include <TObjArray.h>
-#include <TPaveText.h>
-#include <TTree.h>
-#include <iostream>
-#include <algorithm> // for std::max
-#include <string>
+struct HistConfig {
+    int bins;
+    double min;
+    double max;
+};
 
-#include <TTree.h>
-#include <TCanvas.h>
-#include <TH1F.h>
-#include <TObjArray.h>
-#include <TPaveText.h>
-#include <TAxis.h>
-#include <iostream>
-#include <string>
-#include <map>
-#include <cstring>
-#include <algorithm>
+std::map<std::string, HistConfig> histConfigs = {
+    {"beam_pol", {20, 0.80, 1.00}},
+    {"e_p", {200, 2, 9}},
+    {"e_phi", {200, 0, 2 * M_PI}},
+    {"eta", {200, -1, 3}},
+    {"e_theta", {200, 0, 40 * (M_PI / 180.0)}}, // Convert degree to radian
+    {"Mx", {200, -4, 3}},
+    {"Mx2", {200, -10, 10}},
+    {"phi", {200, 0, 2 * M_PI}},
+    {"p_p", {200, 0, 6}},
+    {"p_phi", {200, 0, 2 * M_PI}},
+    {"pT", {200, 0, 1.3}},
+    {"p_theta", {200, 0, 90 * (M_PI / 180.0)}}, // Convert degree to radian
+    {"Q2", {200, 0, 9}},
+    {"t", {200, -10, 1}},
+    {"tmin", {200, -0.5, 0}},
+    {"vze", {200, -15, 15}},
+    {"vzp", {200, -15, 15}},
+    {"W", {200, 2, 4}},
+    {"x", {200, 0, 1}},
+    {"xF", {200, -1, 1}},
+    {"y", {200, 0, 1}},
+    {"z", {200, 0, 1}},
+    {"zeta", {200, 0, 1}}
+};
 
 std::string formatBranchName(const std::string& original) {
     std::map<std::string, std::string> specialLabels = {
@@ -88,49 +102,33 @@ void createHistograms(TTree* tree1, TTree* tree2, const char* outDir) {
     for (int i = 0; i < branches1->GetEntries(); ++i) {
         const char* branchName = branches1->At(i)->GetName();
         std::string formattedBranchName = formatBranchName(branchName);
-        TCanvas canvas(branchName, "Canvas", 800, 600);
+        
+        HistConfig config = histConfigs[branchName];
+        
+        // Special handling for theta values to convert from radians to degrees
+        double factor = 1.0;
+        if (std::string(branchName) == "e_theta" || std::string(branchName) == "p_theta") {
+            factor = 180.0 / M_PI;
+        }
 
-        TH1F hist1(Form("%s_1", branchName), "", 200, 0, 0);
-        TH1F hist2(Form("%s_2", branchName), "", 200, 0, 0);
+        TH1F hist1(Form("%s_1", branchName), "", config.bins, config.min * factor, config.max * factor);
+        TH1F hist2(Form("%s_2", branchName), "", config.bins, config.min * factor, config.max * factor);
 
         std::string cutCondition = "";
         if (std::strcmp(branchName, "Mx") != 0 && std::strcmp(branchName, "Mx2") != 0) {
             cutCondition = "Mx > 1.5";
         }
 
-        tree1->Draw(Form("%s>>%s_1", branchName, branchName), cutCondition.c_str());
-        tree2->Draw(Form("%s>>%s_2", branchName, branchName), cutCondition.c_str());
+        tree1->Draw(Form("%s * %f >> %s_1", branchName, factor, branchName), cutCondition.c_str());
+        tree2->Draw(Form("%s * %f >> %s_2", branchName, factor, branchName), cutCondition.c_str());
 
-        hist1.SetLineColor(kRed);
-        hist2.SetLineColor(kBlue);
+        // ... (Your existing code for drawing, setting colors, and stats)
 
-        hist1.Draw(""); 
-        hist2.Draw("same"); 
-
-        hist1.SetStats(0);
-        hist2.SetStats(0);
-
-        hist1.GetXaxis()->SetTitle(formattedBranchName.c_str());
-        hist1.GetYaxis()->SetTitle("Counts");
-
-        double max_value = std::max(hist1.GetMaximum(), hist2.GetMaximum());
-        hist1.SetMaximum(max_value * 1.1);
-        hist2.SetMaximum(max_value * 1.1);
-
-        TPaveText* stats1 = new TPaveText(0.15, 0.85, 0.35, 0.95, "NDC");
-        TPaveText* stats2 = new TPaveText(0.65, 0.85, 0.85, 0.95, "NDC");
-
-        stats1->AddText("pass-1");
-        stats1->AddText(Form("Counts: %d", int(hist1.GetEntries())));
-        stats1->SetTextAlign(12);
-        stats1->SetTextColor(kRed);
-        stats1->Draw("same");
-
-        stats2->AddText("pass-2");
-        stats2->AddText(Form("Counts: %d", int(hist2.GetEntries())));
-        stats2->SetTextAlign(12);
-        stats2->SetTextColor(kBlue);
-        stats2->Draw("same");
+        TPaveText* stats = new TPaveText(0.65, 0.85, 0.85, 0.95, "NDC");
+        stats->AddText(Form("pass-1: %d", int(hist1.GetEntries())));
+        stats->AddText(Form("pass-2: %d", int(hist2.GetEntries())));
+        stats->SetTextAlign(12);
+        stats->Draw("same");
 
         canvas.SaveAs(Form("%s/%s.png", outDir, branchName));
     }
