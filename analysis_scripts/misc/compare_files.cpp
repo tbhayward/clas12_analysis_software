2124567b44a397370129de0c5c2e174f4257231e
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-07 11:17:46 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -50,7 +50,7 @@ std::tuple<std::vector<double>, std::vector<double>, std::vector<double>> calcul
         tree1->GetEntry(entry);
 
 
-        if (runnum != 4903) { continue; }
+        if (runnum != 4984) { continue; }
         // if (runnum == 4984) {
         //     helicity=-1*helicity;
         // }
