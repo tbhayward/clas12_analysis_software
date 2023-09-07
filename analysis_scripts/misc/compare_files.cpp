7cee3d2c0f0c7322cb0fee2a7381f69ed241f77e
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 21:13:50 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -46,7 +46,7 @@ std::pair<std::vector<double>, std::vector<double>> calculateAndPlotALU(
     for (int entry = 0; entry < tree1->GetEntries(); ++entry) {
         tree1->GetEntry(entry);
 
-        cout << branchVar << " " << min_val << " " << max_val << " " << phi << " ";
+        cout << branch_var << " " << min_val << " " << max_val << " " << phi << " ";
         if(branch_var < min_val || branch_var > max_val) continue;  
         // Skip entries out of range
         if(phi < 0 || phi > 2 * TMath::Pi()) continue;  
