fb9073f864fdca199b23f811d1fcfe943c996817
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 22:55:12 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -227,9 +227,7 @@ void createHistograms(TTree* tree1, TTree* tree2,
         std::string cutCondition = "";
         if (std::strcmp(branchName, "Mx") != 0 && std::strcmp(branchName, "Mx2") != 0) {
             cutCondition = "Mx > 1.5";
-        } else {
-            cutCondition = "Mx > -100";
-        }
+        } 
 
         cout << "passed the cut condition" << endl;
         // Draw a temporary histogram to get statistics
