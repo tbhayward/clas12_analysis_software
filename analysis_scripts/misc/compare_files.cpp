5d03f7dc011e876f2d48acf9fb405d868ea4c1be
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-06 21:31:07 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/misc/compare_files.cpp
+++ analysis_scripts/misc/compare_files.cpp
@@ -208,7 +208,7 @@ void createHistograms(TTree* tree1, TTree* tree2,
         return;
     }
 
-    for (int i = 11; i < branches1->GetEntries(); ++i) {
+    for (int i = 12; i < branches1->GetEntries(); ++i) {
 
         const char* branchName = branches1->At(i)->GetName();
         if (std::strcmp(branchName, "runnum") == 0 || std::strcmp(branchName, "evnum") == 0 || 
