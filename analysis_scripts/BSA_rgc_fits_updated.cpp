4f6e9062b9a41c5b66c0b961f033eebd3714218c
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-08 10:05:55 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -440,17 +440,17 @@ TH1D* createHistogramForBin(TTree* data, const char* histName, int binIndex,
   double currentVariable; 
   data->SetBranchAddress(propertyNames[currentFits].c_str(), &currentVariable); 
 
-  // for (int entry = 0; entry < 10; ++entry) {  // Just read the first 10 entries for debugging
-  //   data->GetEntry(entry);
-  //   cout << "Entry " << entry << " : " << propertyNames[currentFits].c_str() << " " << currentVariable << " " << phi << " " << helicity << endl;
-  // }
+  for (int entry = 0; entry < 10; ++entry) {  // Just read the first 10 entries for debugging
+    data->GetEntry(entry);
+    cout << "Entry " << entry << " : " << propertyNames[currentFits].c_str() << " " << currentVariable << " " << phi << " " << helicity << endl;
+  }
 
 
   // for (int entry = 0; entry < data->GetEntries(); ++entry) {
   for (int entry = 0; entry < 10; ++entry) {
     data->GetEntry(entry);
     
-    cout << currentVariable << " " << " " << phi << " " << helicity << endl;
+    // cout << currentVariable << " " << " " << phi << " " << helicity << endl;
     if (applyKinematicCuts(data, entry, currentFits, 0) && currentVariable >= varMin && 
       currentVariable < varMax) {
       sumVariable+=currentVariable;
