48d887848c59dc835d81775ee3e3265848875e6c
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-07 21:59:26 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -8,7 +8,9 @@
 #include <vector>
 #include <map>
 #include <TSystem.h>
-#include <iomanip> // Include this header for std::fixed and std::setprecision
+#include <iomanip> 
+using namespace std;
+
 
 size_t currentFits = 0;
 size_t currentBin = 0;
@@ -157,56 +159,56 @@ int main(int argc, char *argv[]) {
 
   // Check for correct number of command line arguments
     if (argc != 5) {
-        std::cout << "Usage: " << argv[0];
-        std::cout << " <data_root_file> <mc_root_file> ";
-        std::cout << " <output_asymmetry_file> <output_kinematic_file>" << std::endl;
+        cout << "Usage: " << argv[0];
+        cout << " <data_root_file> <mc_root_file> ";
+        cout << " <output_asymmetry_file> <output_kinematic_file>" << endl;
         return 1;
     }
 
   // Clear the contents of the output_file
-  std::ofstream ofs(output_file, std::ios::trunc);
+  std::ofstream ofs(argv[3], std::ios::trunc);
   ofs.close();
 
   // Clear the contents of the kinematic_file
-  std::ofstream ofs2(kinematic_file, std::ios::trunc);
+  std::ofstream ofs2(argv[4], std::ios::trunc);
   ofs2.close();
 
   // load bins from external csv file
   load_bins_from_csv("bins_single_hadron.csv");
-  std::cout<< std::endl <<"-- Loaded information from bins.csv. " << std::endl;
+  cout<< endl <<"-- Loaded information from bins.csv. " << endl;
 
-  std::cout<< "Found " << allBins.size() << " sets of bins: " << std::endl;
+  cout<< "Found " << allBins.size() << " sets of bins: " << endl;
   for (size_t i = 0; i < binNames.size(); ++i) {
-    std::cout << binNames[i];
-    if (i == binNames.size() - 1) { std::cout << "."; }
-    else { std::cout << ", "; }
+    cout << binNames[i];
+    if (i == binNames.size() - 1) { cout << "."; }
+    else { cout << ", "; }
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout<< "Found " << allBins[currentFits].size() << " bin indices for: " << std::endl;
+  cout<< "Found " << allBins[currentFits].size() << " bin indices for: " << endl;
   for (size_t i = 0; i < allBins[currentFits].size(); ++i) {
-    std::cout << allBins[currentFits][i];
-    if (i == allBins[currentFits].size() - 1) { std::cout << "."; }
-    else { std::cout << ", "; }
+    cout << allBins[currentFits][i];
+    if (i == allBins[currentFits].size() - 1) { cout << "."; }
+    else { cout << ", "; }
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout << "Found " << variable_names.size() << " variables: " << std::endl;
+  cout << "Found " << variable_names.size() << " variables: " << endl;
   for (size_t i = 0; i < variable_names.size(); ++i) {
-    std::cout << i << ":" << variable_names[i] << std::flush;
+    cout << i << ":" << variable_names[i] << std::flush;
     if (i == variable_names.size() - 1) {
-      // std::cout << ". ";
+      // cout << ". ";
     } else {
-      std::cout << ", ";
+      cout << ", ";
     }
   }
-  std::cout << std::endl;
+  cout << endl;
 
   // load run infrom from external csv file
   load_run_info_from_csv("run_info_rgc.csv");
-  std::cout<< std::endl << std::endl <<"-- Loaded information from run_info_rgc.csv" << std::endl;
+  cout<< endl << endl <<"-- Loaded information from run_info_rgc.csv" << endl;
 
-  std::cout << "Found " << run_info_list.size() << " runs." << std::endl;
+  cout << "Found " << run_info_list.size() << " runs." << endl;
   cpp = 0; // total accumulated charge of positive beam - positive target
   cpm = 0; // total accumulated charge of positive beam - negative target
   cmp = 0; // total accumulated charge of negative beam - positive target
@@ -224,18 +226,18 @@ int main(int argc, char *argv[]) {
       }
   }
 
-  std::cout << "Total pos-pos (beam-target) charge: " << cpp << " (nc). ";
-  std::cout << "Total pos-neg charge: " << cpm << " (nc). ";
-  std::cout << "Total neg-pos charge: " << cmp << " (nc). ";
-  std::cout << "Total neg-neg charge: " << cmm << " (nc). ";
-  std::cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nc)."<<std::endl;
+  cout << "Total pos-pos (beam-target) charge: " << cpp << " (nc). ";
+  cout << "Total pos-neg charge: " << cpm << " (nc). ";
+  cout << "Total neg-pos charge: " << cmp << " (nc). ";
+  cout << "Total neg-neg charge: " << cmm << " (nc). ";
+  cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nc)."<<endl;
 
   // Load data and mc root files
   TFile* gData = new TFile(argv[1], "READ");
   TFile* gMC = new TFile(argv[2], "READ");
 
   if (!gData->IsOpen() || !gMC->IsOpen()) {
-        std::cout << "Error opening ROOT files (is the location correct?). Exiting." << std::endl;
+        cout << "Error opening ROOT files (is the location correct?). Exiting." << endl;
         return 2;
     }
 
