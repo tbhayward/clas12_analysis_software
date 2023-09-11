1f17ce084566a142d7aca8b46c11d0c87071aa42
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-11 11:37:08 -0400

    added misc directory for analysis scripts; created script to compare pass-1 and pass-2 nSidis files

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -441,6 +441,7 @@ TH1D* createHistogramForBin(TTree* data, const char* histName, int binIndex,
   double currentVariable; 
   data->SetBranchAddress(propertyNames[currentFits].c_str(), &currentVariable); 
 
+  cout << endl;
   for (int entry = 0; entry < 10; ++entry) {  // Just read the first 10 entries for debugging
     data->GetEntry(entry);
     cout << "Entry " << entry << " : " << propertyNames[currentFits].c_str();
