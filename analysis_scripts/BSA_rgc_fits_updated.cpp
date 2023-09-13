f4aadf1e22fe35794cc4d62d2e5853cb2615cd38
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-13 14:12:26 -0400

    working on rgc fitting program that accepts root tree input instead of text

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -255,42 +255,42 @@ public:
             goodEvent = *Q2 > 1 && *W > 2 && *Mx > 1.4 && *y < 0.75;
         }
         if (property == "Q2bin") {
-            goodEvent = Q2>1 && W>2 && Mx>1.4 && y<0.75 && x>0.2 && x<0.3 && pT>0.25 && pT<0.35 && 
-              xF<0;
+            goodEvent = *Q2>1 && *W>2 && *Mx>1.4 && *y<0.75 && *x>0.2 && *x<0.3 && *pT>0.25 && 
+            *pT<0.35 && *xF<0;
         }
         if (property == "PTTFR" || property ==  "xTFR" || property == "zetaTFR" || 
           property == "Q2TFR" || property ==  "x") {
-          goodEvent = Q2>1 && W>2 && Mx>1.4 && y<0.75 && xF<0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.4 && *y<0.75 && *xF<0;
         }
         if (property == "PTCFR" || property == "xCFR" || property == "zetaCFR" ||
           property == "Q2TFR") {
-          goodEvent = Q2>1 && W>2 && Mx>1.4 && y<0.75 && xF>0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.4 && *y<0.75 && *xF>0;
         } 
         //
         // epi+X
         if (property == "xFpip") { 
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75;
         }
         if (property == "PTTFRpip" || property ==  "xTFRpip" || property == "zTFRpip" || 
           property == "Q2TFRpip" || property ==  "xpip") {
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75 && xF<0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75 && *xF<0;
         }
         if (property == "PTCFRpip" || property == "xCFRpip" || property == "zCFRpip" ||
           property == "Q2TFRpip") {
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75 && xF>0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75 && *xF>0;
         }
         //
         // epi-X
         if (property == "xFpim") { 
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75;
         }
         if (property == "PTTFRpim" || property ==  "xTFRpim" || property == "zTFRpim" || 
           property == "Q2TFRpim" || property ==  "xpim") {
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75 && xF<0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75 && *xF<0;
         }
         if (property == "PTCFRpim" || property == "xCFRpim" || property == "zCFRpim" ||
           property == "Q2TFRpim") {
-          goodEvent = Q2>1 && W>2 && Mx>1.5 && y<0.75 && xF>0;
+          goodEvent = *Q2>1 && *W>2 && *Mx>1.5 && *y<0.75 && *xF>0;
         }
 
         if (isMC) {
