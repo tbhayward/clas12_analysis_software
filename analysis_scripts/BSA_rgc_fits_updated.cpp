04e617d15037e091f800d48232e4c235efc29028
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-13 16:13:42 -0400

    working on rgc fitting program that accepts root tree input instead of text

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -350,11 +350,11 @@ void plotHistogramAndFit(TH1D* histogram, TF1* fitFunction, int binIndex, int as
   graph->GetYaxis()->SetTitleSize(0.05);
 
   // Create the legend
-  TLegend *leg = new TLegend(0.161725, 0.7, 0.4, 0.9);  // Adjusted to the upper-left corner
+  TLegend *leg = new TLegend(0.16172, 0.7, 0.4, 0.9);  // Adjusted to the upper-left corner
   leg->SetBorderSize(1);
   leg->SetFillColor(0);
   leg->SetTextSize(0.025);  // Reduced text size
-  leg->SetTextAlign(12);  // Left-align text
+  // leg->SetTextAlign(12);  // Left-align text
 
   // Add fit parameters as legend entries based on the value of 'asymmetry'.
   const char* paramName;
