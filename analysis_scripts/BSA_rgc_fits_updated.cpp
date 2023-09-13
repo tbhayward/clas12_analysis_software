a49da73adbf80ea18725b3a9797dbe26b3439e0d
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-13 15:27:23 -0400

    working on rgc fitting program that accepts root tree input instead of text

--- analysis_scripts/BSA_rgc_fits_updated.cpp
+++ analysis_scripts/BSA_rgc_fits_updated.cpp
@@ -809,6 +809,9 @@ void performChi2Fits(TTreeReader &dataReader, const char* output_file, const cha
     TTreeReaderValue<double> DepW(dataReader, "DepW");
     TTreeReaderValue<double> currentVariable(dataReader, propertyNames[currentFits].c_str());
 
+    // Determine the variable range for the specified bin
+    double varMin = allBins[currentFits][i];
+    double varMax = allBins[currentFits][i + 1];
     KinematicCuts kinematicCuts(dataReader);  // Create an instance of the KinematicCuts class
     while (dataReader.Next()) {
       // Apply kinematic cuts (this function will need to be adapted)
