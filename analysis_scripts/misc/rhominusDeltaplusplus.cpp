429cccc7daedb585d3c744abdc857f8ff1518eaf
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-18 13:39:22 -0400

    working on rgc fitting program that accepts root tree input instead of text

--- analysis_scripts/misc/rhominusDeltaplusplus.cpp
+++ analysis_scripts/misc/rhominusDeltaplusplus.cpp
@@ -217,7 +217,7 @@ void createHistograms(TTreeReader &dataReader, const char* outDir) {
 
         	histMh12.Fill(*Mh12);
 
-        	histMh1x.Fill(*Mh1x);
+        	histMh1x.Fill(Mh1x);
 
         	histMh13vsDelta_E2EX.Fill(p1.E()-p2.E(), *Mh13);
         }
