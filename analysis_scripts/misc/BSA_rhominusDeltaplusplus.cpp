49f84c013dc0fce7ad87759ee8e52613eb1a0b4f
Timothy Hayward <tbhayward@email.wm.edu>
2023-09-25 11:11:35 -0400

    working on rgc fitting program that accepts root tree input instead of text

---
+++ analysis_scripts/misc/BSA_rhominusDeltaplusplus.cpp
@@ -0,0 +1,99 @@
+// Created 9/25/23
+// get BSA of rho- Delta++ 
+
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TH1F.h>
+#include <TObjArray.h>
+#include <TPaveText.h>
+#include <TAxis.h>
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+
+void createBSAPlot(TTreeReader &dataReader, const char* outDir) {
+    // Declare derived variables to read from the tree
+    double z2x, t13, t2x;
+    // Declare reader locations
+    TTreeReaderValue<int> helicity(dataReader, "helicity");
+    TTreeReaderValue<double> beam_pol(dataReader, "beam_pol");
+    TTreeReaderValue<double> e_p(dataReader, "e_p");
+    TTreeReaderValue<double> e_theta(dataReader, "e_theta");
+    TTreeReaderValue<double> e_phi(dataReader, "e_phi");
+    TTreeReaderValue<double> p1_p(dataReader, "p1_p");
+    TTreeReaderValue<double> p1_theta(dataReader, "p1_theta");
+    TTreeReaderValue<double> p1_phi(dataReader, "p1_phi");
+    TTreeReaderValue<double> p2_p(dataReader, "p2_p");
+    TTreeReaderValue<double> p2_theta(dataReader, "p2_theta");
+    TTreeReaderValue<double> p2_phi(dataReader, "p2_phi");
+    TTreeReaderValue<double> p3_p(dataReader, "p3_p");
+    TTreeReaderValue<double> p3_theta(dataReader, "p3_theta");
+    TTreeReaderValue<double> p3_phi(dataReader, "p3_phi");
+    TTreeReaderValue<double> phi1(dataReader, "phi1");
+    TTreeReaderValue<double> phi2(dataReader, "phi2");
+    TTreeReaderValue<double> phi3(dataReader, "phi3");
+    TTreeReaderValue<double> phi12(dataReader, "phi12");
+    TTreeReaderValue<double> phi13(dataReader, "phi13");
+    TTreeReaderValue<double> phi23(dataReader, "phi23");
+    TTreeReaderValue<double> DepW(dataReader, "DepW");
+    TTreeReaderValue<double> DepA(dataReader, "DepA");
+    TTreeReaderValue<double> x(dataReader, "x");
+    TTreeReaderValue<double> z1(dataReader, "z1");
+    TTreeReaderValue<double> z13(dataReader, "z13");
+    TTreeReaderValue<double> Mx(dataReader, "Mx");
+    TTreeReaderValue<double> Mx12(dataReader, "Mx12");
+    TTreeReaderValue<double> Mx13(dataReader, "Mx13");
+    TTreeReaderValue<double> Mx23(dataReader, "Mx23");
+    TTreeReaderValue<double> Mh12(dataReader, "Mh12");
+    TTreeReaderValue<double> Mh13(dataReader, "Mh13");
+    TTreeReaderValue<double> Mh23(dataReader, "Mh23");
+    TTreeReaderValue<double> xF13(dataReader, "xF13");
+    TTreeReaderValue<double> Delta_phi13(dataReader, "Delta_phi13");
+
+    // Declare new variables to store missing particle information
+    float px_p, px_theta, px_phi, Mx1x, Mx2x, Mx3x, Mh1x, Mh2x, Mh3x;
+
+    // Define initial state 4-momentum (10.1998 GeV electron beam and stationary proton)
+    TLorentzVector p_initial(0, 0, 10.1998, 10.1998 + 0.938); // (px, py, pz, E)
+
+    // Create canvas and set its style
+    TCanvas canvas("Invariant Masses", "Canvas", 1600, 1000);
+    setCanvasStyle(canvas, 3, 2);
+
+    // Create test canvas and set its style
+    TCanvas test_canvas("Cuts Test", "Canvas", 1600, 1000);
+    setCanvasStyle(test_canvas, 3, 2);
+
+    int counter = 0;
+    while (dataReader.Next()) {
+        counter++;
+        if (*Mx < 0 || *Mx12 < 0 || *Mx13 < 0 || *Mx23 < 0) { continue; }
+
+    }
+
+
+}
+
+void BSA_rhominusDeltaplusplus(std::string root_file1_path) {
+    gStyle->SetCanvasColor(0);
+
+    TFile* file1 = new TFile(root_file1_path.c_str(), "READ");
+
+    if (!file1->IsOpen()) {
+        cout << "Error opening ROOT file (is the location correct?). Exiting." << endl;
+    }
+
+    TTree* tree1 = (TTree*)file1->Get("PhysicsEvents");
+
+    if (!tree1) {
+        cout << "Error getting tree from ROOT files." << endl;
+    }
+
+    createBSAPlot(tree1, "output");
+
+    file1->Close(); delete file1;
+}
