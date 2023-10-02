10763dc4f0163088715fe99ea8829a78f35c4e83
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-02 11:12:33 -0400

    removed extraneous files from development merge

--- LHAPDF/LHAPDF-6.5.4/src/yamlcpp/depthguard.cpp
+++
@@ -1,9 +0,0 @@
-#include "yaml-cpp/depthguard.h"
-
-namespace LHAPDF_YAML {
-
-DeepRecursion::DeepRecursion(int depth, const Mark& mark_,
-                             const std::string& msg_)
-    : ParserException(mark_, msg_), m_depth(depth) {}
-
-}  // namespace LHAPDF_YAML
