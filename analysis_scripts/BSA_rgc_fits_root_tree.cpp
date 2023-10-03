ad6386fa37e80ff8fe6d2fbc0a40764cff9c371a
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-03 15:43:49 -0400

    added debugstream output for the comparison between text based and root based fitting

--- analysis_scripts/BSA_rgc_fits_root_tree.cpp
+++ analysis_scripts/BSA_rgc_fits_root_tree.cpp
@@ -703,6 +703,7 @@ TH1D* createHistogramForBin(TTreeReader &dataReader, const char* histName, int b
   }
   dataReader.Restart();  // Reset the TTreeReader at the end of the function
 
+  cout << sumPol << endl;
   // Calculate the mean polarization
   double meanVariable = numEvents > 0 ? sumVariable / numEvents : 0.0;
   double meanPol = sumPol / numEvents; // mean beam polarization for data 
