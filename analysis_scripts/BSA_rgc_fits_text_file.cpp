1468301caefd7e5b886e359599e320e82230047c
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-03 12:05:12 -0400

    added debugstream output for the comparison between text based and root based fitting

--- analysis_scripts/BSA_rgc_fits_text_file.cpp
+++ analysis_scripts/BSA_rgc_fits_text_file.cpp
@@ -883,7 +883,7 @@ TH1D* createHistogramForBin(const std::vector<eventData>& data, const char* hist
       numEvents++;
     }
   }
-  cout << histPosPos.getMean() << " " << histPosNeg.getMean() << " " << histNegPos.getMean() << " " << histNegNeg.getMean() << endl;
+  cout << histPosPos->GetMean() << " " << histPosNeg->GetMean() << " " << histNegPos->GetMean() << " " << histNegNeg->GetMean() << endl;
   
   // Calculate the mean polarization
   float meanVariable = numEvents > 0 ? sumVariable / numEvents : 0.0;
