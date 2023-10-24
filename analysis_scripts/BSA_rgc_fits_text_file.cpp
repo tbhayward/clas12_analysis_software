6d3fea4748ba029ee312b0bc75bef242ee55e124
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-24 12:04:55 -0400

    set up for rgb pass-2 check

--- analysis_scripts/BSA_rgc_fits_text_file.cpp
+++ analysis_scripts/BSA_rgc_fits_text_file.cpp
@@ -553,7 +553,7 @@ void negLogLikelihood(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, In
               + Pb*((DepW/DepA)*ALU_sinphi*sin(phi)) // BSA
               + Df*Pt*((DepV/DepA)*AUL_sinphi*sin(phi) + (DepB/DepA)*AUL_sin2phi*sin(2*phi)) // TSA
               + Df*Pb*Pt*((DepC/DepA)*ALL + (DepW/DepA)*ALL_cosphi*cos(phi)) ); // DSA
-          } else if (event.data.at("helicity") > 0 && event.data.at("target_pol") < 0 ) { phisum+=phi;
+          } else if (event.data.at("helicity") > 0 && event.data.at("target_pol") < 0 ) { phisum+= *phi;
             sum_PM += log(1 
               + (DepV/DepA)*AUU_cosphi*cos(phi) + (DepB/DepA)*AUU_cos2phi*cos(2*phi) // UU
               + Pb*((DepW/DepA)*ALU_sinphi*sin(phi)) // BSA
