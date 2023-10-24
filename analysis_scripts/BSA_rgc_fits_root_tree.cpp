6c55456b8e998e4bc3641f8cbe4e34942533c98c
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-24 16:01:44 -0400

    set up for rgb pass-2 check

--- analysis_scripts/BSA_rgc_fits_root_tree.cpp
+++ analysis_scripts/BSA_rgc_fits_root_tree.cpp
@@ -697,37 +697,33 @@ void negLogLikelihood(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, In
       double Pt = *target_pol;
 
       if (*helicity > 0 && *target_pol > 0) { 
-        // sum_PP += log(1 
-        //   + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
-        //   + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
-        //   + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-        //     (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
-        //   + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-        sum_PP = sum_PP+1.00;
+        sum_PP = sum_PP + log(1 
+          + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
+          + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
+          + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
+          + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       } else if (*helicity > 0 && *target_pol < 0) { 
-        // sum_PM += log(1 
-        //   + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU
-        //   + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
-        //   - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-        //     (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi)) // TSA
-        //   - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-        sum_PM = sum_PM+1.00;
+        sum_PM = sum_PM + log(1 
+          + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU
+          + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
+          - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi)) // TSA
+          - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       } else if (*helicity < 0 && *target_pol > 0) { 
-        // sum_MP += log(1 
-        //   + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
-        //   - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
-        //   + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-        //     (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
-        //   - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-        sum_MP = sum_MP+1.00;
+        sum_MP = sum_MP + log(1 
+          + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
+          - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
+          + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
+          - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       } else if (*helicity < 0 && *target_pol < 0) { 
-        // sum_MM += log(1 
-        //   + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
-        //   - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
-        //   - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-        //     (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
-        //   + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-        sum_MM = sum_MM+1.00;
+        sum_MM = sum_MM + log(1 
+          + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
+          - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
+          - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
+          + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       }
     }
   }
