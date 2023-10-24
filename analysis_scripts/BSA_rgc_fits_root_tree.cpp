7fb144d434303eef3104b2020c28c7cbd31df9b5
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-24 11:20:07 -0400

    set up for rgb pass-2 check

--- analysis_scripts/BSA_rgc_fits_root_tree.cpp
+++ analysis_scripts/BSA_rgc_fits_root_tree.cpp
@@ -696,33 +696,33 @@ void negLogLikelihood(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, In
       double Pb = *beam_pol;
       double Pt = *target_pol;
 
-      if (*helicity > 0 && Pt > 0) { 
+      if (*helicity > 0 && *target_pol > 0) { 
         sum_PP += log(1 
           + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
           + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
           + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
             (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
           + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-      } else if (*helicity > 0 && Pt < 0) { 
+      } else if (*helicity > 0 && *target_pol < 0) { 
         sum_PM += log(1 
           + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
           + Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
           - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi)) // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
           - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
-      } else if (*helicity < 0 && Pt > 0) { 
+      } else if (*helicity < 0 && *target_pol > 0) { 
         sum_MP += log(1 
           + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
           - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
           + Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi)) // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
           - Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       } else if (*helicity < 0 && Pt < 0) { 
         sum_MM += log(1 
           + (*DepV / *DepA)*AUU_cosphi*cos(*phi) + (*DepB / *DepA)*AUU_cos2phi*cos(2 * *phi) // UU 
           - Pb*((*DepW / *DepA)*ALU_sinphi*sin(*phi)) // BSA
           - Df*Pt*((*DepV / *DepA)*AUL_sinphi*sin(*phi)+ // TSA
-            (* DepB / *DepA)*AUL_sin2phi*sin(2 * *phi)) // TSA
+            (*DepB / *DepA)*AUL_sin2phi*sin(2 * *phi))//TSA
           + Df*Pb*Pt*((*DepC / *DepA)*ALL + (*DepW / *DepA)*ALL_cosphi*cos(*phi)) ); // DSA
       }
     }
