e1343bd20469209ce6a5c79cd23e92b6bdd3c603
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-31 18:13:52 -0400

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

---
+++ analysis_scripts/asymmetry_extraction/load_bins_from_csv.cpp
@@ -0,0 +1,69 @@
+void load_bins_from_csv(const std::string& filename) {
+  // Open the input file with the given filename
+  std::ifstream file(filename);
+
+  // Declare a string to store each line read from the file
+  std::string line;
+
+  // Declare a boolean flag to check if we have reached the bin declarations in the file
+  bool reached_bins = false;
+
+  // Loop through each line in the file until there are no more lines left to read
+  while (std::getline(file, line)) {
+    // If the line is empty or starts with a '#' (comment), skip to the next line
+    if (line.empty() || line[0] == '#') { continue; }
+
+    // If we have not reached the bin declarations yet
+    if (!reached_bins) {
+      // If the line contains a '-', it marks the start of bin declarations; 
+      // set the flag to true and continue to the next line
+      if (line.find("-") != std::string::npos) {
+        reached_bins = true;
+        continue;
+      }
+      // Use a stringstream to split the line by commas and store variable names
+      std::stringstream ss_var(line);
+      std::string var_name;
+      while (std::getline(ss_var, var_name, ',')) {
+        variable_names.push_back(var_name);
+      }
+    } else {
+      // If we have reached the bin declarations, use a stringstream to split the line by commas
+      std::stringstream ss(line);
+
+      // Declare strings to store the bin name and property
+      std::string bin_name, property;
+
+      // Read the bin name and property from the stringstream
+      std::getline(ss, bin_name, ',');
+      binNames.push_back(bin_name);
+      std::getline(ss, property, ',');
+      propertyNames.push_back(property);
+
+      // Declare a vector to store the bin values
+      std::vector<double> bin_values;
+
+      // Declare a string to store each value read from the stringstream
+      std::string value;
+
+      // Read the values from the stringstream and store them in the bin_values vector
+      while (std::getline(ss, value, ',')) {
+        bin_values.push_back(std::stof(value));
+      }
+
+      // Add the bin_values vector to the bins_map and allBins containers
+      bins_map[bin_name] = bin_values;
+      allBins.push_back(bin_values);
+    }
+  }
+
+  // Loop through each variable name to remove newline and carriage return characters
+  for (size_t i = 0; i < variable_names.size(); ++i) {
+    // Remove newline characters from the variable name
+    variable_names[i].erase(std::remove(variable_names[i].begin(),
+      variable_names[i].end(), '\n'), variable_names[i].end());
+    // Remove carriage return characters from the variable name
+    variable_names[i].erase(std::remove(variable_names[i].begin(),
+      variable_names[i].end(), '\r'), variable_names[i].end());
+  }
+}
---
+++ analysis_scripts/asymmetry_extraction/load_bins_from_csv.h
@@ -0,0 +1,9 @@
+// load_bins_from_csv.h
+#ifndef LOAD_BINS_FROM_CSV_H
+#define LOAD_BINS_FROM_CSV_H
+
+#include <string>
+
+void load_bins_from_csv(const std::string& filename);
+
+#endif
