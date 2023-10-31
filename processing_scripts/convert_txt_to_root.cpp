8bebe5008a80c3eecf0bcec70357797e7aefdc8c
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-31 12:44:49 -0400

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

--- processing_scripts/convert_txt_to_root.cpp
+++ processing_scripts/convert_txt_to_root.cpp
@@ -501,7 +501,8 @@ int main(int argc, char *argv[]) {
             mc_vz_p >> Q2 >> mc_Q2 >> W >> mc_W >> Mx >> mc_Mx >> Mx2 >> mc_Mx2 >> x >> mc_x >> 
             y >> mc_y >> z >> mc_z >> xF >> mc_xF >> pT >> mc_pT >> zeta >> mc_zeta >> eta >> 
             mc_eta >> phi >> mc_phi >> DepA >> mc_DepA >> DepB >> mc_DepB >> DepC >> mc_DepC >> 
-            DepV >> mc_DepV >> DepW >> mc_DepW >> matching_e_pid) {
+            DepV >> mc_DepV >> DepW >> mc_DepW >> matching_e_pid >> matching_p1_pid >>
+            mc_p1_parent) {
 
             cout << e_p << endl;
             runnum = 11;
