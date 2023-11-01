990bd595f228f75b3a212cdcf42e026f88dc221f
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-01 15:39:03 -0400

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

---
+++ analysis_scripts/asymmetry_extraction/formatLabelName.cpp
@@ -0,0 +1,52 @@
+#include "formatLabelName.h"
+
+std::string formatLabelName(const std::string& original) {
+    std::map<std::string, std::string> specialLabels = {
+        {"Q2", "Q^{2} (GeV^{2})"},
+        {"W", "W (GeV)"},
+        {"pT", "P_{T} (GeV)"},
+        {"t", "t (GeV^{2})"},
+        {"tmin", "t_{min} (GeV^{2})"},
+        {"e_p", "e_{p} (GeV)"},
+        {"Mx", "M_{x} (GeV)"},
+        {"Mx2", "M_{x}^{2} (GeV)"},
+        {"p_p", "p_{p} (GeV)"},
+        {"xF", "x_{F}"},
+    };
+  
+    if (specialLabels.find(original) != specialLabels.end()) {
+        return specialLabels[original];
+    }
+
+    std::string formatted = original;
+    size_t pos = 0;
+    while ((pos = formatted.find('_', pos)) != std::string::npos) {
+        formatted.replace(pos, 1, "_{");
+        size_t closing = formatted.find('_', pos + 2);
+        if (closing == std::string::npos) {
+            closing = formatted.length();
+        }
+        formatted.insert(closing, "}");
+        pos = closing + 1;
+    }
+
+    if (formatted.find("theta") != std::string::npos) {
+        formatted.replace(formatted.find("theta"), 5, "#theta");
+    }
+
+    if (formatted.find("zeta") != std::string::npos) {
+        formatted.replace(formatted.find("zeta"), 5, "#zeta");
+    }
+
+    if (formatted.find("phi") != std::string::npos) {
+        formatted.replace(formatted.find("phi"), 3, "#phi");
+    }
+
+    if (formatted.find("eta") != std::string::npos && 
+        formatted.find("theta") == std::string::npos && 
+        formatted.find("zeta") == std::string::npos) {
+        formatted.replace(formatted.find("eta"), 3, "#eta");
+    }
+  
+    return formatted;
+}
---
+++ analysis_scripts/asymmetry_extraction/formatLabelName.h
@@ -0,0 +1,9 @@
+#ifndef formatLabelName_H
+#define formatLabelName_H
+
+#include <string>
+#include <map>
+
+std::string formatLabelName(const std::string& original);
+
+#endif // formatLabelName_H
