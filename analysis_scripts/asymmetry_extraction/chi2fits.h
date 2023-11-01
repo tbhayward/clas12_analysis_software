e73197e2340cf669a38c5aaddad14881313f2f51
Timothy Hayward <tbhayward@email.wm.edu>
2023-10-31 21:02:36 -0400

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

--- analysis_scripts/asymmetry_extraction/chi2fits.h
+++ analysis_scripts/asymmetry_extraction/chi2fits.h
@@ -5,8 +5,12 @@
 #include <cmath>
 #include <iostream>
 
-double asymmetry_value_calculation(double currentVariable, const std::string& prefix, ...);
-double asymmetry_error_calculation(double currentVariable, const std::string& prefix, ...);
+double asymmetry_value_calculation(double currentVariable, const std::string& prefix, 
+  double Npp, double Npm, double Nmp, double Nmm, double meanPol, double Ptp, double Ptm, 
+  int asymmetry_index);
+double asymmetry_error_calculation(double currentVariable, const std::string& prefix, 
+  double Npp, double Npm, double Nmp, double Nmm, double meanPol, double Ptp, double Ptm, 
+  int asymmetry_index);
 double BSA_funcToFit(double* x, double* par);
 double TSA_funcToFit(double* x, double* par);
 double DSA_funcToFit(double* x, double* par);
