03e9b20f0eb6628ec98e4901e67a7e01df7211e7
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-06 10:33:30 -0500

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

--- analysis_scripts/asymmetry_extraction/histConfigs.cpp
+++ analysis_scripts/asymmetry_extraction/histConfigs.cpp
@@ -2,6 +2,8 @@
 #include "TMath.h" 
 
 std::map<std::string, HistConfig> histConfigs = {
+      {"Delta_eta", {200, -3, 3}},
+      {"Delta_phi", {200, 0, 0, TMath::Pi()}},
       {"DepA", {200, 0, 1}},
       {"DepB", {200, 0, 1}},
       {"DepC", {200, 0, 1}},
@@ -9,27 +11,45 @@ std::map<std::string, HistConfig> histConfigs = {
       {"DepW", {200, 0, 1}},
       {"e_p", {200, 2, 8}},
       {"e_phi", {200, 0, 2 * TMath::Pi()}},
-      {"eta", {200, -1, 3}},
+      {"eta", {200, -3, 3}},
+      {"eta1", {200, -3, 3}},
+      {"eta2", {200, -3, 3}},
+      {"eta1_gN", {200, -3, 3}},
+      {"eta2_gN", {200, -3, 3}},
       {"e_theta", {200, 0, 2 * TMath::Pi() / 180 * 30}}, // Convert degree to radian
       {"evnum", {200, 0, 0}},
       {"helicity", {2, -2, 2}},
       {"Mx", {200, 0., 3.}},
       {"Mx2", {200, -4, 10}},
       {"phi", {200, 0, 2 * TMath::Pi()}},
+      {"phi1", {200, 0, 2 * TMath::Pi()}},
+      {"phi2", {200, 0, 2 * TMath::Pi()}},
+      {"phih", {200, 0, 2 * TMath::Pi()}},
+      {"phiR", {200, 0, 2 * TMath::Pi()}},
       {"p_p", {200, 0, 6}},
       {"p_phi", {200, 0, 2 * TMath::Pi()}},
+      {"p1_phi", {200, 0, 2 * TMath::Pi()}},
+      {"p2_phi", {200, 0, 2 * TMath::Pi()}},
       {"pT", {200, 0, 1.2}},
       {"p_theta", {200, 0, 2 * TMath::Pi() / 180 * 30}}, // Convert degree to radian
+      {"p1_theta", {200, 0, 2 * TMath::Pi() / 180 * 30}}, // Convert degree to radian
+      {"p2_theta", {200, 0, 2 * TMath::Pi() / 180 * 30}}, // Convert degree to radian
       {"Q2", {200, 0, 9}},
       {"runnum", {200, 0, 0}},
+      {"theta", {200, 0, TMath::Pi()}},
       {"t", {200, -10, 1}},
       {"tmin", {200, -0.5, 0}},
       {"vz_e", {200, -15, 15}},
       {"vz_p", {200, -15, 15}},
+      {"vz_p1", {200, -15, 15}},
+      {"vz_p2", {200, -15, 15}},
       {"W", {200, 2, 4}},
       {"x", {200, 0, 0.6}},
       {"xF", {200, -1, 1}},
+      {"xF1", {200, -1, 1}},
+      {"xF2", {200, -1, 1}},
       {"y", {200, 0.0, 1.00}},
       {"z", {200, 0, 1}},
+      {"Mh", {200, 0, 2}},
       {"zeta", {200, 0.0, 1}}
     };
