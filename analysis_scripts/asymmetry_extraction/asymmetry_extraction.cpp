c5769b424718cb7fc6772e54d1da847fc6352d88
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-06 11:05:16 -0500

    incorporated processing_mc_single_hadrons, added to processing and convert_txt_to_root

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -1352,7 +1352,7 @@ int main(int argc, char *argv[]) {
   mcReader.SetTree(mc);  // Initialize the global variable
 
   // createCorrelationPlots();
-  createIntegratedKinematicPlots();
+  // createIntegratedKinematicPlots();
   // createIntegratedKinematicPlotsForBinsAndFits();
   currentFits=0;
 
