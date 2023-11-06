a3b22dab575489864d451362b154194597a5a836
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-06 13:25:34 -0500

    attempting to make it work with dihadrons

--- analysis_scripts/asymmetry_extraction/KinematicCuts.h
+++ analysis_scripts/asymmetry_extraction/KinematicCuts.h
@@ -13,8 +13,10 @@ private:
     TTreeReaderValue<double> Q2;
     TTreeReaderValue<double> W;
     TTreeReaderValue<double> Mx;
+    TTreeReaderValue<double> Mh;
     TTreeReaderValue<double> x;
     TTreeReaderValue<double> y;
+    TTreeReaderValue<double> z;
     TTreeReaderValue<double> pT;
     TTreeReaderValue<double> xF;
     TTreeReaderValue<double> target_pol;
