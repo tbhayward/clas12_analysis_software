dd248df7cdf6f05bc0b46444291e545f95dad40f
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-06 14:40:09 -0500

    attempting to make it work with dihadrons

--- analysis_scripts/asymmetry_extraction/KinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/KinematicCuts.cpp
@@ -74,12 +74,12 @@ bool KinematicCuts::applyCuts(int currentFits, bool isMC) {
         }
         //
         // epi+pi+X, exclusive rho
-        if (property == "exclusiveRhoIntegrated" || property == "exclusiveRhoIntegratedx" ||
-            property == "exclusiveRhoIntegratedt") {
+        // if (property == "exclusiveRhoIntegrated" || property == "exclusiveRhoIntegratedx" ||
+        //     property == "exclusiveRhoIntegratedt") {
 
-          goodEvent = *Q2>1 && *W>2 && *y<0.75 && fabs(*Mx-0.95)<0.15 && 
-            fabs(*Mh-0.78)<0.10 && *z>0.80;
-        }
+        //   goodEvent = *Q2>1 && *W>2 && *y<0.75 && fabs(*Mx-0.95)<0.15 && 
+        //     fabs(*Mh-0.78)<0.10 && *z>0.80;
+        // }
         // if (property == "exclusiveRhoTransversex" || property == "exclusiveRhoTransverset") {
         //   // Convert spherical coordinates to Cartesian for p1
         //   double p1_x, p1_y, p1_z;
