4b2ec6079914e34b199da652e4f6118b687dfcc9
Timothy Hayward <tbhayward@email.wm.edu>
2023-11-18 10:51:45 -0500

    attempting to make it work with dihadrons

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -675,7 +675,8 @@ void performChi2Fits(const char* output_file, const char* kinematic_file,
     TTreeReaderValue<double> pT(dataReader, "pT");
     TTreeReaderValue<double> xF(dataReader, "xF");
     TTreeReaderValue<double> Mx(dataReader, "Mx");
-    TTreeReaderValue<double> t(dataReader, "t");
+    // TTreeReaderValue<double> t(dataReader, "t");
+    TTreeReaderValue<double> t1(dataReader, "t1");
     TTreeReaderValue<double> tmin(dataReader, "tmin");
     TTreeReaderValue<double> DepA(dataReader, "DepA");
     TTreeReaderValue<double> DepB(dataReader, "DepB");
