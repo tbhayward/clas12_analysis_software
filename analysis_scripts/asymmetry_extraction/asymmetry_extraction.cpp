54d858698da2f0ce13488d93dbfa001991ef8d51
Timothy Hayward <tbhayward@email.wm.edu>
2023-12-08 11:53:20 -0500

    added processing_mc_dihadrons for comparison of generated and reconstructed dihadron events

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -934,11 +934,9 @@ void createIntegratedKinematicPlots() {
         // Loop over dataReader to fill the histogram
         KinematicCuts kinematicCuts(dataReader);
         while (dataReader.Next()) {
-            cout << *xF << endl;
             bool passedKinematicCuts = kinematicCuts.applyCuts(0, false);
             if (*dataVal >= config.xMin && *dataVal < config.xMax && passedKinematicCuts) {
                 dataHist->Fill(*dataVal);
-                cout << *xF << " " << config.xMin << " " << config.xMax << endl;
             }
         }
 
