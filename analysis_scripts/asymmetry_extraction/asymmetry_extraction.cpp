445513cf51619c8253fddcb0b1fc9b059666be07
Timothy Hayward <tbhayward@email.wm.edu>
2023-12-12 11:24:57 -0500

    added in RICH detector pid to analysis_fitter; set it so it won't accept pid=0

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -936,6 +936,7 @@ void createIntegratedKinematicPlots() {
         while (dataReader.Next()) {
             bool passedKinematicCuts = kinematicCuts.applyCuts(0, false);
             if (*dataVal >= config.xMin && *dataVal < config.xMax && passedKinematicCuts) {
+                cout << *xF << endl;
                 dataHist->Fill(*dataVal);
             }
         }
