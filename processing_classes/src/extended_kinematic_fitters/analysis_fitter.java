93002b122878cd3ee131e532a60e09452c614602
Timothy Hayward <tbhayward@email.wm.edu>
2023-12-12 21:44:03 -0500

    added in RICH detector pid to analysis_fitter; set it so it won't accept pid=0

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -703,8 +703,8 @@ public class analysis_fitter extends GenericKinematicFitter {
         double p = Math.sqrt(Math.pow(px,2)+Math.pow(py,2)+Math.pow(pz,2));
         
         return true
-            && p > 1.20
-            && p < 5.00 // this wasn't used in the dihadron publication but was used in the submitted single pion
+            && p > 1.00
+//            && p < 5.00 // this wasn't used in the dihadron publication but was used in the submitted single pion
             && forward_detector_cut(particle_Index, rec_Bank)
             && pion_z_vertex_cut(vz, trigger_electron_vz)
 //            && pion_chi2pid_cut(particle_Index, rec_Bank)
@@ -723,7 +723,7 @@ public class analysis_fitter extends GenericKinematicFitter {
         
         return true
             && p > 1.00
-            && p < 3.5 
+//            && p < 3.5 
             && forward_detector_cut(particle_Index, rec_Bank)
             && pion_z_vertex_cut(vz, trigger_electron_vz)
             && hadron_chi2pid_cut(particle_Index, rec_Bank)
