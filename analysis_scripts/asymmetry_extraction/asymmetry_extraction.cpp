d8a097e53690def653a2e4307b6bbe299cd4d713
Timothy Hayward <tbhayward@email.wm.edu>
2023-12-23 14:38:24 -0500

    added in RICH detector pid to analysis_fitter; set it so it won't accept pid=0

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -1367,9 +1367,9 @@ int main(int argc, char *argv[]) {
   cout << "Total neg-neg charge: " << cmm << " (nC). ";
   cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nC)."<< endl << endl;
 
-  createCorrelationPlots();
+  // createCorrelationPlots();
   createIntegratedKinematicPlots();
-  // createIntegratedKinematicPlotsForBinsAndFits();
+  createIntegratedKinematicPlotsForBinsAndFits();
   currentFits=0;
   dataReader.Restart(); mcReader.Restart();
 
