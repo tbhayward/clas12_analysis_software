cd44b7364d7cc5ea29ca27dc21a3a4e656f89d3e
Timothy Hayward <tbhayward@email.wm.edu>
2023-12-27 12:35:37 -0500

    finalized pass-2 hdron pid cuts

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -774,7 +774,7 @@ public class analysis_fitter extends GenericKinematicFitter {
 //            && p < 5.00 // this wasn't used in the dihadron publication but was used in the submitted single pion
             && forward_detector_cut(particle_Index, rec_Bank)
             && pion_z_vertex_cut(vz, trigger_electron_vz)
-//            && hadron_pass2_cut(particle_Index, rec_Bank)
+            && hadron_pass2_cut(particle_Index, rec_Bank)
 //            && pion_chi2pid_cut(particle_Index, rec_Bank)
 //            && hadron_chi2pid_cut(particle_Index, rec_Bank)
             && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
@@ -794,7 +794,7 @@ public class analysis_fitter extends GenericKinematicFitter {
 //            && p < 3.5 
             && forward_detector_cut(particle_Index, rec_Bank)
             && pion_z_vertex_cut(vz, trigger_electron_vz)
-//            && hadron_pass2_cut(particle_Index, rec_Bank)
+            && hadron_pass2_cut(particle_Index, rec_Bank)
 //            && hadron_chi2pid_cut(particle_Index, rec_Bank)
             && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
               ;
@@ -814,7 +814,7 @@ public class analysis_fitter extends GenericKinematicFitter {
             && proton_z_vertex_cut(vz, pion_vz)
             && forward_detector_cut(particle_Index, rec_Bank)
             && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
-//            && hadron_pass2_cut(particle_Index, rec_Bank)
+            && hadron_pass2_cut(particle_Index, rec_Bank)
 //            && hadron_chi2pid_cut(particle_Index, rec_Bank)
               ;
     }
