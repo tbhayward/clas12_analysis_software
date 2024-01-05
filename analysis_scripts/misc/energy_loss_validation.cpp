8ace4778e702fad00e685d4424ceb546d506d8a3
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-04 20:20:22 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/energy_loss_validation.cpp
+++ analysis_scripts/misc/energy_loss_validation.cpp
@@ -136,11 +136,11 @@ void compareTrees(const char* file1, const char* file2, const char* output, doub
         // Set initial parameter estimates for the fit function
         fitFunc1->SetParameters(100, lineValue, 0.1, 0, 1); // Example values, adjust as needed
         fitFunc1->SetParLimits(0, 0, 10e6); // Limit the mean around lineValue
-        fitFunc1->SetParLimits(1, 0, lineValue + 0.2); // Limit the mean around lineValue
+        fitFunc1->SetParLimits(1, 0, lineValue + 0.1); // Limit the mean around lineValue
 
         fitFunc2->SetParameters(100, lineValue, 0.1, 0, 1); // Example values, adjust as needed
         fitFunc2->SetParLimits(0, 0, 10e6); // Limit the mean around lineValue
-        fitFunc2->SetParLimits(1, 0, lineValue + 0.2); // Limit the mean around lineValue
+        fitFunc2->SetParLimits(1, 0, lineValue + 0.1); // Limit the mean around lineValue
 
         // Perform the fit on the first histogram
         hist1[i]->Fit(fitFunc1, "R");
