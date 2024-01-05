50f3feeef7b3d13ecb159f20a54cd363a969bb58
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-05 13:04:08 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/energy_loss_validation.cpp
+++ analysis_scripts/misc/energy_loss_validation.cpp
@@ -40,8 +40,8 @@ void compareTrees(const char* file1, const char* file2, const char* output, doub
         // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 0.6, 1.2));
 
         // pi-
-        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.1, 1.8));
-        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.1, 1.8));
+        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.1, 2.0));
+        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.1, 2.0));
 
         // kaon 
         // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 0.8, 1.6));
@@ -89,7 +89,7 @@ void compareTrees(const char* file1, const char* file2, const char* output, doub
 
         // Setting the title of the histogram to include the bin range
         char title[100];
-        sprintf(title, "%.1f < p (GeV) < %.1f; M_{x}^{2} (GeV^{2}); Counts", 
+        sprintf(title, "%.3f < p (GeV) < %.3f; M_{x}^{2} (GeV^{2}); Counts", 
             binEdges[i], binEdges[i+1]);
         hist1[i]->SetTitle(title);
 
