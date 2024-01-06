e057c1ae5781b6edec5b48421d7061929b1afea4
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-05 23:29:39 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/pair_production_rate.cpp
+++ analysis_scripts/misc/pair_production_rate.cpp
@@ -15,10 +15,10 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     TTree* tree2 = (TTree*)f2->Get("PhysicsEvents");
 
     // Define histograms for each variable
-    TH1D* h_e_p1 = new TH1D("h_e_p1", ";e_p (GeV);Normalized Counts", 100, 2.6, 10);
-    TH1D* h_e_p2 = new TH1D("h_e_p2", ";e_p (GeV);Normalized Counts", 100, 2.6, 10);
-    TH1D* h_Q21 = new TH1D("h_Q21", ";Q^2 (GeV^2);Normalized Counts", 100, 0, 10); 
-    TH1D* h_Q22 = new TH1D("h_Q22", ";Q^2 (GeV^2);Normalized Counts", 100, 0, 10);
+    TH1D* h_e_p1 = new TH1D("h_e_p1", ";e_{p} (GeV);Normalized Counts", 100, 2.6, 10);
+    TH1D* h_e_p2 = new TH1D("h_e_p2", ";e_{p} (GeV);Normalized Counts", 100, 2.6, 10);
+    TH1D* h_Q21 = new TH1D("h_Q21", ";Q^{2} (GeV^{2});Normalized Counts", 100, 0, 10); 
+    TH1D* h_Q22 = new TH1D("h_Q22", ";Q^{2} (GeV^{2});Normalized Counts", 100, 0, 10);
     TH1D* h_W1 = new TH1D("h_W1", ";W (GeV);Normalized Counts", 100, 0.8, 4); 
     TH1D* h_W2 = new TH1D("h_W2", ";W (GeV);Normalized Counts", 100, 0.8, 4);
 
@@ -80,7 +80,7 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_e_p2->SetLabelSize(labelFontSize);
     h_e_p2->SetTitleSize(titleFontSize);
     h_e_p2->Draw("same");
-    h_e_p1->SetMaximum(100); // 100% higher than the maximum
+    h_e_p1->SetMaximum(10); // 100% higher than the maximum
     h_e_p1->SetMinimum(1e-5); // Minimum set to 10e-5
     h_e_p1->SetStats(0); // Remove stat box
     h_e_p2->SetStats(0); // Remove stat box
@@ -101,9 +101,11 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_ratio_e_p->SetLabelSize(labelFontSize);
     h_ratio_e_p->SetTitleSize(titleFontSize);
     h_ratio_e_p->Draw();
-    h_ratio_e_p->SetMaximum(100.0); // 100% higher than the maximum
+    h_ratio_e_p->SetMaximum(10.0); // 100% higher than the maximum
     h_ratio_e_p->SetMinimum(1e-5); // Minimum set to 10e-5
     h_ratio_e_p->SetStats(0); // Remove stat box
+    h_ratio_e_p->GetXaxis()->SetLabelSize(labelFontSize);
+    h_ratio_e_p->GetXaxis()->SetTitleSize(titleFontSize); // Increase x-axis title size
 
 
     // Q^2
@@ -119,10 +121,14 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_Q22->SetLineColor(kRed);
     h_Q22->SetLabelSize(labelFontSize);
     h_Q22->SetTitleSize(titleFontSize);
-    h_Q21->SetMaximum(100.0); // 100% higher than the maximum
+    h_Q21->SetMaximum(10.0); // 100% higher than the maximum
     h_Q21->SetMinimum(1e-5); // Minimum set to 10e-5
     h_Q21->SetStats(0); // Remove stat box
     h_Q22->SetStats(0); // Remove stat box
+    h_Q21->GetXaxis()->SetLabelSize(labelFontSize);
+    h_Q21->GetXaxis()->SetTitleSize(titleFontSize); // Increase x-axis title size
+    h_Q22->GetYaxis()->SetLabelSize(labelFontSize);
+    h_Q22->GetYaxis()->SetTitleSize(titleFontSize); // Increase y-axis title size
 
     c1->cd(4);
     gPad->SetLogy(1);
@@ -135,9 +141,11 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_ratio_Q2->SetLabelSize(labelFontSize);
     h_ratio_Q2->SetTitleSize(titleFontSize);
     h_ratio_Q2->Draw();
-    h_ratio_Q2->SetMaximum(100.0); // 100% higher than the maximum
+    h_ratio_Q2->SetMaximum(10.0); // 100% higher than the maximum
     h_ratio_Q2->SetMinimum(1e-5); // Minimum set to 10e-5
     h_ratio_Q2->SetStats(0); // Remove stat box
+    h_ratio_Q2->GetXaxis()->SetLabelSize(labelFontSize);
+    h_ratio_Q2->GetXaxis()->SetTitleSize(titleFontSize); // Increase x-axis title size
 
     // W
     c1->cd(5);
@@ -152,10 +160,14 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_W2->SetTitleSize(titleFontSize);
     h_W2->Draw("same");
     h_W2->SetLineColor(kRed);
-    h_W1->SetMaximum(100.0); // 100% higher than the maximum
+    h_W1->SetMaximum(10.0); // 100% higher than the maximum
     h_W1->SetMinimum(1e-5); // Minimum set to 10e-5
     h_W1->SetStats(0); // Remove stat box
     h_W2->SetStats(0); // Remove stat box
+    h_W1->GetXaxis()->SetLabelSize(labelFontSize);
+    h_W1->GetXaxis()->SetTitleSize(titleFontSize); // Increase x-axis title size
+    h_W2->GetYaxis()->SetLabelSize(labelFontSize);
+    h_W2->GetYaxis()->SetTitleSize(titleFontSize); // Increase y-axis title size
 
     c1->cd(6);
     gPad->SetLogy(1);
@@ -168,24 +180,26 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_ratio_W->SetLabelSize(labelFontSize);
     h_ratio_W->SetTitleSize(titleFontSize);
     h_ratio_W->Draw();
-    h_ratio_W->SetMaximum(100.0); // 100% higher than the maximum
+    h_ratio_W->SetMaximum(10.0); // 100% higher than the maximum
     h_ratio_W->SetMinimum(1e-5); // Minimum set to 10e-5
     h_ratio_W->SetStats(0); // Remove stat box
+    h_ratio_W->GetXaxis()->SetLabelSize(labelFontSize);
+    h_ratio_W->GetXaxis()->SetTitleSize(titleFontSize); // Increase x-axis title size
 
     // Add legends to the left-side plots
-    TLegend* leg_e_p = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    TLegend* leg_e_p = new TLegend(0.8, 0.7, 0.5, 0.9);;
     leg_e_p->AddEntry(h_e_p1, "run 5038, e^{-}", "l");
     leg_e_p->AddEntry(h_e_p2, "run 5482, e^{+}", "l");
     c1->cd(1);
     leg_e_p->Draw();
 
-    TLegend* leg_Q2 = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    TLegend* leg_Q2 = new TLegend(0.8, 0.7, 0.5, 0.9);;
     leg_Q2->AddEntry(h_Q21, "run 5038, e^{-}", "l");
     leg_Q2->AddEntry(h_Q22, "run 5482, e^{+}", "l");
     c1->cd(3);
     leg_Q2->Draw();
 
-    TLegend* leg_W = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    TLegend* leg_W = new TLegend(0.8, 0.7, 0.5, 0.9);;
     leg_W->AddEntry(h_W1, "run 5038, e^{-}", "l");
     leg_W->AddEntry(h_W2, "run 5482, e^{+}", "l");
     c1->cd(5);
