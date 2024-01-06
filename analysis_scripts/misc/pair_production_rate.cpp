fafc52e7bf0f05a84c525eadc3fa5e5755e758d9
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-05 23:02:48 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/pair_production_rate.cpp
+++ analysis_scripts/misc/pair_production_rate.cpp
@@ -84,6 +84,10 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_e_p1->SetMinimum(1e-5); // Minimum set to 10e-5
     h_e_p1->SetStats(0); // Remove stat box
     h_e_p2->SetStats(0); // Remove stat box
+    h_e_p1->GetXaxis()->SetLabelSize(labelFontSize);
+    h_e_p1->GetXaxis()->SetTitleSize(axisTitleFontSize); // Increase x-axis title size
+    h_e_p1->GetYaxis()->SetLabelSize(labelFontSize);
+    h_e_p1->GetYaxis()->SetTitleSize(axisTitleFontSize); // Increase y-axis title size
 
 
     c1->cd(2);
@@ -169,21 +173,21 @@ void pair_production_rate(const char* file1, const char* file2, const char* outp
     h_ratio_W->SetStats(0); // Remove stat box
 
     // Add legends to the left-side plots
-    TLegend* leg_e_p = new TLegend(0.3, 0.7, 0.5, 0.9);;
-    leg_e_p->AddEntry(h_e_p1, "run 5038, e^-", "l");
-    leg_e_p->AddEntry(h_e_p2, "run 5482, e^+", "l");
+    TLegend* leg_e_p = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    leg_e_p->AddEntry(h_e_p1, "run 5038, e^{-}", "l");
+    leg_e_p->AddEntry(h_e_p2, "run 5482, e^{+}", "l");
     c1->cd(1);
     leg_e_p->Draw();
 
-    TLegend* leg_Q2 = new TLegend(0.3, 0.7, 0.5, 0.9);;
-    leg_Q2->AddEntry(h_Q21, "run 5038, e^-", "l");
-    leg_Q2->AddEntry(h_Q22, "run 5482, e^+", "l");
+    TLegend* leg_Q2 = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    leg_Q2->AddEntry(h_Q21, "run 5038, e^{-}", "l");
+    leg_Q2->AddEntry(h_Q22, "run 5482, e^{+}", "l");
     c1->cd(3);
     leg_Q2->Draw();
 
-    TLegend* leg_W = new TLegend(0.3, 0.7, 0.5, 0.9);;
-    leg_W->AddEntry(h_W1, "run 5038, e^-", "l");
-    leg_W->AddEntry(h_W2, "run 5482, e^+", "l");
+    TLegend* leg_W = new TLegend(0.7, 0.7, 0.5, 0.9);;
+    leg_W->AddEntry(h_W1, "run 5038, e^{-}", "l");
+    leg_W->AddEntry(h_W2, "run 5482, e^{+}", "l");
     c1->cd(5);
     leg_W->Draw();
 
