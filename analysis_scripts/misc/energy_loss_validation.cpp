c5bbcb9a1eeb325f304ae64dbea1071eedf9529c
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-09 19:42:34 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/energy_loss_validation.cpp
+++ analysis_scripts/misc/energy_loss_validation.cpp
@@ -17,16 +17,16 @@ void compareTrees(const char* file1, const char* file2, const char* output,
     const char* output2, double lineValue) {
     // Define the momentum bin edges
     
-    // // pions and kaon
-    // std::vector<double> binEdges = {0.6,1.5,1.8,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.8,3.0,3.2,3.4,3.8,5.0};
+    // pions and kaon
+    std::vector<double> binEdges = {0.6,1.5,1.8,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.8,3.0,3.2,3.4,3.8,5.0};
 
     // // pions and kaon less
     // std::vector<double> binEdges = {0.6,2.6,3.8,5.0};
 
 
-    // proton 
-    std::vector<double> binEdges = 
-        {0,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.8,1.0,1.1,1.2,1.4,1.8,2.4,3.0};
+    // // proton 
+    // std::vector<double> binEdges = 
+    //     {0,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.8,1.0,1.1,1.2,1.4,1.8,2.4,3.0};
     
 
     int nBins = binEdges.size() - 1;
@@ -48,17 +48,17 @@ void compareTrees(const char* file1, const char* file2, const char* output,
         // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.8));
         // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.0, 1.8));
 
-        // // k+ 
-        // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.4));
-        // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.0, 1.4));
+        // k+ 
+        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.4));
+        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.0, 1.4));
 
         // // k- 
         // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 50, 0.0, 1.9));
         // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 50, 0.0, 1.9));
 
-        // proton rho
-        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 0.1, 0.9));
-        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 0.1, 0.9));
+        // // proton rho
+        // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 0.1, 0.9));
+        // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 0.1, 0.9));
 
         // // // proton f2
         // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.2, 1.8));
@@ -223,7 +223,7 @@ void compareTrees(const char* file1, const char* file2, const char* output,
     gr2->Draw("P SAME");
 
     // Set the Y-axis range to be lineValue +/- 0.15
-    double yAxisMin = lineValue - 0.12;
+    double yAxisMin = lineValue - 0.08;
     double yAxisMax = lineValue + 0.04;
     gr1->GetYaxis()->SetRangeUser(yAxisMin, yAxisMax);
     gr2->GetYaxis()->SetRangeUser(yAxisMin, yAxisMax);
