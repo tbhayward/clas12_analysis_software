4500f605e72eafc7d52621fd5b2587c9d4e5d4f7
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-12 13:52:54 -0500

    finalized energy loss corrections; validating

--- analysis_scripts/misc/energy_loss_validation.cpp
+++ analysis_scripts/misc/energy_loss_validation.cpp
@@ -40,13 +40,13 @@ void compareTrees(const char* file1, const char* file2, const char* output,
     // Create histograms for each bin
     std::vector<TH1D*> hist1, hist2;
     for (int i = 0; i < nBins; ++i) {
-        // // pi+ 
-        // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 0.6, 1.2));
-        // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 0.6, 1.2));
+        // pi+ 
+        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 0.6, 1.2));
+        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 0.6, 1.2));
 
-        // pi-
-        hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.8));
-        hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.0, 1.8));
+        // // pi-
+        // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.8));
+        // hist2.push_back(new TH1D(Form("hist2_%d", i), "", 100, 1.0, 1.8));
 
         // // k+ 
         // hist1.push_back(new TH1D(Form("hist1_%d", i), "", 100, 1.0, 1.4));
