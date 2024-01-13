58e111eb70edc3d330a20ceb83fc5923276ba7c0
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-13 13:44:46 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/rgc_preparation.cpp
+++ analysis_scripts/misc/rgc_preparation.cpp
@@ -7,6 +7,16 @@
 
 const std::string output_dir = "output/rgc_ready_for_cooking_plots/";
 
+// Function to create a histogram for a given dataset
+TH1D* createHistogram(TTree* tree, const char* name, const char* title, 
+        const char* variable, const char* cut, double norm) {
+    TH1D* hist = new TH1D(name, title, 80, -4, 4);
+    tree->Draw((std::string(variable) + ">>" + name).c_str(), cut, "goff");
+    hist->Scale(1.0 / norm);
+    hist->SetStats(kFALSE);
+    return hist;
+}
+
 void rgc_preparation() {
     // File paths and tree names
     const char* rga_eX_file = "/volatile/clas12/thayward/rgc_ready_for_cooking/processed_files/rga_ready_for_calibration_eX.root";
@@ -19,32 +29,18 @@ void rgc_preparation() {
     const char* rgc_epX_file = "/volatile/clas12/thayward/rgc_ready_for_cooking/processed_files/rgc_ready_for_calibration_epX.root";
     const char* rgc_epippimX_file = "/volatile/clas12/thayward/rgc_ready_for_cooking/processed_files/rgc_ready_for_calibration_epi+pi-X.root";
 
-    // Loading files
-    TFile* rga_eX = new TFile(rga_eX_file);
-    TFile* rga_epipX = new TFile(rga_epipX_file);
-    TFile* rga_epX = new TFile(rga_epX_file);
-    TFile* rga_epippimX = new TFile(rga_epippimX_file);
+    // Open files and get trees
+    TFile* filesOpened[8];
+    TTree* trees[8];
+    for (int i = 0; i < 8; i++) {
+        filesOpened[i] = new TFile(files[i]);
+        trees[i] = (TTree*)filesOpened[i]->Get("PhysicsEvents");
+    }
 
-    TFile* rgc_eX = new TFile(rgc_eX_file);
-    TFile* rgc_epipX = new TFile(rgc_epipX_file);
-    TFile* rgc_epX = new TFile(rgc_epX_file);
-    TFile* rgc_epippimX = new TFile(rgc_epippimX_file);
-
-    // Extracting trees
-    TTree* tree_rga_eX = (TTree*)rga_eX->Get("PhysicsEvents");
-    TTree* tree_rga_epipX = (TTree*)rga_epipX->Get("PhysicsEvents");
-    TTree* tree_rga_epX = (TTree*)rga_epX->Get("PhysicsEvents");
-    TTree* tree_rga_epippimX = (TTree*)rga_epippimX->Get("PhysicsEvents");
-
-    TTree* tree_rgc_eX = (TTree*)rgc_eX->Get("PhysicsEvents");
-    TTree* tree_rgc_epipX = (TTree*)rgc_epipX->Get("PhysicsEvents");
-    TTree* tree_rgc_epX = (TTree*)rgc_epX->Get("PhysicsEvents");
-    TTree* tree_rgc_epippimX = (TTree*)rgc_epippimX->Get("PhysicsEvents");
-
-    double rga_norm = 159661.55+145813.73;
-    double rgc_pos_norm = 19355.9+19392.53+21683.25+21621.178;
-    double rgc_neg_norm = 21282.264+21217.414+21303.576+21297.766;
-    double rgc_carbon_norm = 8883.014+8834.256;
+    double rga_H2_norm = 159661.55+145813.73;
+    double rgc_pos_NH3_norm = 19355.9+19392.53+21683.25+21621.178;
+    double rgc_neg_NH3_norm = 21282.264+21217.414+21303.576+21297.766;
+    double rgc_C_norm = 8883.014+8834.256;
 
     // Example: Initialize a canvas for plotting for one of the datasets
     TCanvas *c1 = new TCanvas("c1", "Data Analysis", 800, 600);
