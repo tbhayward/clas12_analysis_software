8636b63eaf7042925d0bb4362157179aff5dcd95
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-14 13:09:44 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/energy_loss_validation.cpp
+++ analysis_scripts/misc/energy_loss_validation.cpp
@@ -24,7 +24,7 @@ void compareTrees(const char* file1, const char* file2, const char* output,
     // Define the momentum bin edges
     
     // electrons
-    std::vector<double> binEdges = {5,6.5,7,7.25,7.5,7.75,8,8.25,8.5,8.67,0.8333,9,9.15,9.3,9.5,10.6};
+    std::vector<double> binEdges = {5,6.5,7,7.25,7.5,7.75,8,8.25,8.5,8.67,0.88333,9,9.15,9.3,9.5,10.6};
 
     // // pions and kaon
     // std::vector<double> binEdges = {0.6,1.3,1.8,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.8,3.0,3.2,3.4,3.8,5.0};
