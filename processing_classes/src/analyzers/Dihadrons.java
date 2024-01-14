a67eb5de5f7fad9b2719caaa900fd4fcd94999a9
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-14 16:10:30 -0500

    rgc ready for cooking validation

--- processing_classes/src/analyzers/Dihadrons.java
+++ processing_classes/src/analyzers/Dihadrons.java
@@ -222,6 +222,7 @@ public class Dihadrons {
         Mx1 = lv_Mx1.mass(); // missing mass with p1 observed
         LorentzVector lv_Mx2 = new LorentzVector(lv_q); lv_Mx2.add(lv_target); lv_Mx2.sub(lv_p2);
         Mx2 = lv_Mx2.mass(); // missing mass with p2 observed
+        Mx2 = mass2;
         
         // boost to gamma*-nucleon center of mass frame
         LorentzVector lv_p_gN = new LorentzVector(lv_p); lv_p_gN.boost(gNBoost);
