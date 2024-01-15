a9bc83e41abfa67bf45d6668109b5d8090c3907d
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-15 00:01:43 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/rgc_preparation.cpp
+++ analysis_scripts/misc/rgc_preparation.cpp
@@ -225,7 +225,7 @@ void rgc_preparation() {
         TH1D* xDiffHist = (TH1D*)xHists[i + 4]->Clone(); 
         TH1D* scaledCHist = (TH1D*)xHists[i + 8]->Clone(); 
         // scaledCHist->Scale(-normalization);
-        scaledCHist->Scale(-0.78); 
+        scaledCHist->Scale(-1); 
         xDiffHist->Add(scaledCHist);
         xDiffHist->SetLineColor(kBlack);
         xDiffHist->Draw("same");
