eabc704b2c21a4ac1919cee1301cedbd4539b48e
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-15 20:04:37 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/rgc_preparation.cpp
+++ analysis_scripts/misc/rgc_preparation.cpp
@@ -46,18 +46,18 @@ void rgc_preparation() {
         trees[i] = (TTree*)filesOpened[i]->Get("PhysicsEvents");
     }
 
-    double rga_H2_norm = 443959;
-    // double rga_H2_norm = 53381.99+41401.77;
-    // double rgc_pos_NH3_norm = 19355.9+19392.53+21683.25+21621.178;
-    double rgc_pos_NH3_norm = 45759.5+41050.555;
-    // double rgc_neg_NH3_norm = 21282.264+21217.414+21303.576+21297.766;
-    double rgc_neg_NH3_norm = 44970.203+44984.06;
-    double rgc_NH3_norm = rgc_pos_NH3_norm+rgc_neg_NH3_norm;
-    double rgc_C_norm = 18917.57;
+    // double rga_H2_norm = 443959;
+    // // double rga_H2_norm = 53381.99+41401.77;
+    // // double rgc_pos_NH3_norm = 19355.9+19392.53+21683.25+21621.178;
+    // double rgc_pos_NH3_norm = 45759.5+41050.555;
+    // // double rgc_neg_NH3_norm = 21282.264+21217.414+21303.576+21297.766;
+    // double rgc_neg_NH3_norm = 44970.203+44984.06;
+    // double rgc_NH3_norm = rgc_pos_NH3_norm+rgc_neg_NH3_norm;
+    // double rgc_C_norm = 18917.57;
 
-    // double rga_H2_norm = 159661.55;
-    // double rgc_NH3_norm = 41392.934 + 43299.863;
-    // double rgc_C_norm =  43098.254;
+    double rga_H2_norm = 159661.55;
+    double rgc_NH3_norm = 41392.934 + 43299.863;
+    double rgc_C_norm =  43098.254;
 
     // // Compute normalization factors based on the number of entries under specific conditions
     // double rga_H2_norm = trees[0]->GetEntries();
