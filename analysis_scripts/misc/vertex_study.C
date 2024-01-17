8e3576b67959414f738744c9c7ba5a47e6dbf5f2
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 10:50:56 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -2,20 +2,20 @@
 #include <TTree.h>
 #include <TH1F.h>
 #include <TCanvas.h>
-#include <TLegend.h>
 #include <TStyle.h>
 
 void vertex_study() {
     gStyle->SetOptStat(0); // Turn off the statistics box
 
-    TCanvas* c1 = new TCanvas("c1", "Vertex Position Study", 2000, 1200);
+    TCanvas* c1 = new TCanvas("c1", "Vertex Position Study", 800, 600);
 
-    // Process only one file and one channel
     TString file_path = "/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_eX.root";
     TFile* file = new TFile(file_path);
     if (!file || file->IsZombie()) {
         std::cerr << "Error opening file: " << file_path << std::endl;
         return;
+    } else {
+        std::cout << "Opened file: " << file_path << std::endl;
     }
 
     TTree* tree = (TTree*)file->Get("PhysicsEvents");
@@ -23,16 +23,24 @@ void vertex_study() {
         std::cerr << "Tree PhysicsEvents not found in file: " << file_path << std::endl;
         file->Close();
         return;
+    } else {
+        std::cout << "Found tree PhysicsEvents in file." << std::endl;
     }
 
-    TH1F* hist = new TH1F("hist_eX", "fa18_inb", 100, -15, 10);
+    TH1F* hist = new TH1F("hist_eX", "fa18_inb;v_{z};Counts", 100, -15, 10);
     tree->Draw("vz_e>>hist_eX");
+    
+    int nEntries = hist->GetEntries();
+    std::cout << "Number of entries in histogram: " << nEntries << std::endl;
 
-    hist->SetLineColor(kBlack);
-    hist->Draw();
+    if (nEntries > 0) {
+        hist->SetLineColor(kBlack);
+        hist->Draw();
+    } else {
+        std::cout << "Histogram is empty!" << std::endl;
+    }
 
     file->Close(); // Close the file after use
 
     c1->SaveAs("output/single_histogram.png");
 }
-
