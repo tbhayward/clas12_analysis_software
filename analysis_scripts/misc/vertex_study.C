90463d76715a1ee2bd1e21a9c6a2c9f46869d6e6
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 11:52:01 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -3,7 +3,6 @@
 #include <TH1D.h>
 #include <TCanvas.h>
 #include <TLegend.h>
-#include <TText.h>
 
 void vertex_study() {
     // Create a canvas to draw the histograms
@@ -11,22 +10,18 @@ void vertex_study() {
     c1->SetBottomMargin(0.15);
     c1->SetLeftMargin(0.15);
 
-    // Open the first ROOT file
+    // Open the ROOT files and get the TTrees
     TFile *file1 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_eX.root");
     TTree *tree1 = (TTree*)file1->Get("PhysicsEvents");
-
-    // Open the second ROOT file
     TFile *file2 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_epi-X.root");
     TTree *tree2 = (TTree*)file2->Get("PhysicsEvents");
-
-    // Open the third ROOT file
     TFile *file3 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_ek-X.root");
     TTree *tree3 = (TTree*)file3->Get("PhysicsEvents");
 
     // Create histograms
-    TH1D *h1 = new TH1D("h1", "v_{z} distribution;v_{z};counts", 100, -15, 5);
-    TH1D *h2 = new TH1D("h2", "v_{z} distribution", 100, -15, 5);
-    TH1D *h3 = new TH1D("h3", "v_{z} distribution", 100, -15, 5);
+    TH1D *h1 = new TH1D("h1", "RGA Fa18 Inb;v_{z};Normalized counts", 100, -15, 5);
+    TH1D *h2 = new TH1D("h2", "", 100, -15, 5);
+    TH1D *h3 = new TH1D("h3", "", 100, -15, 5);
 
     // Set histogram colors
     h1->SetLineColor(kBlack);
@@ -38,35 +33,29 @@ void vertex_study() {
     tree2->Draw("vz_p>>h2");
     tree3->Draw("vz_p>>h3");
 
-    // Draw the histograms on the same canvas
-    h1->Draw();
-    h2->Draw("same");
-    h3->Draw("same");
+    // Normalize histograms
+    h1->Scale(1.0 / h1->Integral());
+    h2->Scale(1.0 / h2->Integral());
+    h3->Scale(1.0 / h3->Integral());
 
-    // Customize histogram appearance
-    h1->GetXaxis()->CenterTitle(true);
-    h1->GetYaxis()->CenterTitle(true);
-    h1->GetXaxis()->SetTitleSize(0.05);
-    h1->GetYaxis()->SetTitleSize(0.05);
+    // Draw the histograms on the same canvas
+    h1->Draw("HIST");
+    h2->Draw("HIST same");
+    h3->Draw("HIST same");
 
     // Add a legend
-    TLegend *leg = new TLegend(0.7, 0.7, 0.9, 0.9);
+    TLegend *leg = new TLegend(0.8, 0.7, 0.9, 0.9);
     leg->AddEntry(h1, "e^{-}", "l");
     leg->AddEntry(h2, "#pi^{-}", "l");
     leg->AddEntry(h3, "k^{-}", "l");
     leg->Draw();
-
-    // Add plot label
-    TText *label = new TText(0.2, 0.85, "RGA Fa18 Inb");
-    label->SetTextSize(0.04);
-    label->Draw();
 
     // Remove the stat box from all histograms
     h1->SetStats(0);
     h2->SetStats(0);
     h3->SetStats(0);
 
-    // Save the histogram as an image in the specified output directory
+    // Save the histogram as an image
     c1->SaveAs("output/neg_vertices.png");
 
     // Close the files
