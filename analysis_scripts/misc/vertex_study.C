6e83be3049ffef2c09fec4a73771ea5ae5893a71
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 10:54:51 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -11,34 +11,15 @@ void vertex_study() {
 
     TString file_path = "/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_eX.root";
     TFile* file = new TFile(file_path);
-    if (!file || file->IsZombie()) {
-        std::cerr << "Error opening file: " << file_path << std::endl;
-        return;
-    } else {
-        std::cout << "Opened file: " << file_path << std::endl;
-    }
 
     TTree* tree = (TTree*)file->Get("PhysicsEvents");
-    if (!tree) {
-        std::cerr << "Tree PhysicsEvents not found in file: " << file_path << std::endl;
-        file->Close();
-        return;
-    } else {
-        std::cout << "Found tree PhysicsEvents in file." << std::endl;
-    }
 
     TH1F* hist = new TH1F("hist_eX", "fa18_inb;v_{z};Counts", 100, -15, 10);
     tree->Draw("vz_e>>hist_eX");
-    
-    int nEntries = hist->GetEntries();
-    std::cout << "Number of entries in histogram: " << nEntries << std::endl;
 
-    if (nEntries > 0) {
-        hist->SetLineColor(kBlack);
-        hist->Draw();
-    } else {
-        std::cout << "Histogram is empty!" << std::endl;
-    }
+    hist->SetLineColor(kBlack);
+    hist->SetLineWidth(2);
+    hist->Draw();
 
     file->Close(); // Close the file after use
 
