41aaa8ebff7a129da8a8bf9252f7b8e06e6e40e2
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 11:47:33 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -3,53 +3,70 @@
 #include <TH1D.h>
 #include <TCanvas.h>
 #include <TLegend.h>
+#include <TText.h>
 
 void vertex_study() {
-    // Open the ROOT file
-    TFile *file = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_eX.root");
-    if (file == 0) {
-        // If the file couldn't be opened, print an error and return
-        printf("Error: could not open the file.\n");
-        return;
-    }
+    // Create a canvas to draw the histograms
+    TCanvas *c1 = new TCanvas("c1", "Canvas", 800, 600);
+    c1->SetBottomMargin(0.15);
+    c1->SetLeftMargin(0.15);
 
-    // Get the TTree
-    TTree *tree = (TTree*)file->Get("PhysicsEvents");
-    if (tree == 0) {
-        // If the tree couldn't be found, print an error and return
-        printf("Error: could not find the tree.\n");
-        return;
-    }
+    // Open the first ROOT file
+    TFile *file1 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_eX.root");
+    TTree *tree1 = (TTree*)file1->Get("PhysicsEvents");
 
-    // Create the histogram
+    // Open the second ROOT file
+    TFile *file2 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_epi-X.root");
+    TTree *tree2 = (TTree*)file2->Get("PhysicsEvents");
+
+    // Open the third ROOT file
+    TFile *file3 = TFile::Open("/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_ek-X.root");
+    TTree *tree3 = (TTree*)file3->Get("PhysicsEvents");
+
+    // Create histograms
     TH1D *h1 = new TH1D("h1", "v_{z} distribution;v_{z};counts", 100, -15, 5);
+    TH1D *h2 = new TH1D("h2", "", 100, -15, 5);
+    TH1D *h3 = new TH1D("h3", "", 100, -15, 5);
+
+    // Set histogram colors
     h1->SetLineColor(kBlack);
+    h2->SetLineColor(kRed);
+    h3->SetLineColor(kBlue);
 
-    // Draw the histogram
-    tree->Draw("vz_e>>h1");
+    // Draw the histograms
+    tree1->Draw("vz_e>>h1");
+    tree2->Draw("vz_p>>h2", "same");
+    tree3->Draw("vz_p>>h3", "same");
 
-    // Customize the histogram appearance
+    // Customize histogram appearance
     h1->GetXaxis()->CenterTitle(true);
     h1->GetYaxis()->CenterTitle(true);
     h1->GetXaxis()->SetTitleSize(0.05);
     h1->GetYaxis()->SetTitleSize(0.05);
 
-    // Create a canvas to draw the histogram
-    TCanvas *c1 = new TCanvas("c1", "Canvas", 800, 600);
-    h1->Draw();
-
-    // Remove the stat box
-    h1->SetStats(0);
-
     // Add a legend
-    TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
+    TLegend *leg = new TLegend(0.7, 0.7, 0.9, 0.9);
     leg->AddEntry(h1, "e^{-}", "l");
+    leg->AddEntry(h2, "#pi^{-}", "l");
+    leg->AddEntry(h3, "k^{-}", "l");
     leg->Draw();
 
-    // Save the histogram as an image
-    c1->SaveAs("vz_distribution.png");
+    // Add plot label
+    TText *label = new TText(0.2, 0.85, "RGA Fa18 Inb");
+    label->SetTextSize(0.04);
+    label->Draw();
 
-    // Close the file
-    file->Close();
+    // Remove the stat box from all histograms
+    h1->SetStats(0);
+    h2->SetStats(0);
+    h3->SetStats(0);
+
+    // Save the histogram as an image in the specified output directory
+    c1->SaveAs("output/neg_vertices.png");
+
+    // Close the files
+    file1->Close();
+    file2->Close();
+    file3->Close();
 }
 
