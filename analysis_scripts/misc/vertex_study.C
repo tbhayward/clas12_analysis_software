8bfd8929a0749661a35025592fef88d843d43e41
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 10:16:58 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -8,18 +8,15 @@
 void vertex_study() {
     const char* run_periods[] = {"fa18_inb", "fa18_out", "sp19_inb", "sp19_inb", "fa19_out", "sp20_inb"};
     const char* neg_channels[] = {"eX", "epi-X", "ek-X"};  // Only negative tracks
-
-    gStyle->SetOptStat(0); // Turn off the statistics box
+    const char* pos_channels[] = {"epi+X", "epX"};  // Only positive tracks
+    const char* colors[] = {"black", "blue", "red"};
 
     TCanvas* c1 = new TCanvas("c1", "Vertex Position Study", 2000, 1200);
     c1->Divide(3, 2);
 
     for (int i = 0; i < 6; i++) {
         c1->cd(i+1);
-
         TLegend* leg = new TLegend(0.1, 0.7, 0.3, 0.9);
-        leg->SetBorderSize(0);
-
         for (int j = 0; j < 3; j++) {
             TString file_path = Form("/volatile/clas12/thayward/vertex_studies/rg%c/%s/rg%c_%s_%s.root", 
                                      (i < 3 ? 'a' : 'b'), run_periods[i], (i < 3 ? 'a' : 'b'), run_periods[i], neg_channels[j]);
@@ -36,23 +33,12 @@ void vertex_study() {
             }
             TString hist_name = Form("hist_%d_%d", i, j);
             TH1F* hist = new TH1F(hist_name, run_periods[i], 100, -15, 10);
-            if (!hist) {
-                std::cerr << "Failed to create histogram: " << hist_name << std::endl;
-                file->Close();
-                continue;
-            }
             TString var_name = (j == 0 ? "vz_e" : "vz_p");
-            tree->Draw(Form("%s>>%s", var_name.Data(), hist_name.Data()), "", "goff");
-            if (hist->GetEntries() == 0) {
-                std::cerr << "No entries in histogram: " << hist_name << std::endl;
-                file->Close();
-                continue;
-            }
-            int color = (j == 0) ? kBlack : ((j == 1) ? kRed : kBlue);
-            hist->SetLineColor(color);
-            hist->Draw(j == 0 ? "" : "SAME");
+            tree->Draw(Form("%s>>%s", var_name.Data(), hist_name.Data()));
+            hist->SetLineColor(j + 1);  // Color codes: 1-Black, 2-Red, 3-Green, 4-Blue, etc.
+            if (j == 0) hist->Draw();
+            else hist->Draw("SAME");
             leg->AddEntry(hist, neg_channels[j], "l");
-            file->Close(); // Close the file after use
         }
         leg->Draw();
         gPad->Modified();
