ec38e637a38f5dc1bd687bfa484bffb65cbd9965
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-17 20:28:27 -0500

    rgc ready for cooking validation

--- analysis_scripts/misc/vertex_study.C
+++ analysis_scripts/misc/vertex_study.C
@@ -159,37 +159,37 @@ void vertex_study() {
     TCanvas *c_pos = new TCanvas("c_neg", "Vertex Study", 1200, 800);
     c_pos->Divide(3, 2); // 3 columns, 2 rows
     // RGA Fa18 Inb
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rga/fa18_inb/rga_fa18_inb_epX.root",
         (TPad*)c_pos->cd(1), "RGA Fa18 Inb"
     );
     // RGA Fa18 Out
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rga/fa18_out/rga_fa18_out_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rga/fa18_out/rga_fa18_out_epX.root",
         (TPad*)c_pos->cd(2), "RGA Fa18 Out"
     );
     // RGA Sp19 Inb
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rga/sp19_inb/rga_sp19_inb_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rga/sp19_inb/rga_sp19_inb_epX.root",
         (TPad*)c_pos->cd(3), "RGA Sp19 Inb"
     );
     // RGB Sp19 Inb
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rgb/sp19_inb/rgb_sp19_inb_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rgb/sp19_inb/rgb_sp19_inb_epX.root",
         (TPad*)c_pos->cd(4), "RGB Sp19 Inb"
     );
     // RGB Fa19 Out
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rgb/fa19_out/rgb_fa19_out_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rgb/fa19_out/rgb_fa19_out_epX.root",
         (TPad*)c_pos->cd(5), "RGB Fa19 Out"
     );
     // RGB Sp20 Inb
-    DrawNegHistogramsForPanel(
+    DrawPosHistogramsForPanel(
         "/volatile/clas12/thayward/vertex_studies/rgb/sp20_inb/rgb_sp20_inb_epi+X.root",
         "/volatile/clas12/thayward/vertex_studies/rgb/sp20_inb/rgb_sp20_inb_epX.root",
         (TPad*)c_pos->cd(6), "RGB Sp20 Inb"
