67eded47ba8abb117feaa00d91875da99604a491
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-26 15:25:53 -0500

    rgc ready for cooking validation

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -1013,9 +1013,9 @@ int main(int argc, char *argv[]) {
   cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nC)."<< endl << endl;
 
   createIntegratedKinematicPlots();
-  // createIntegratedKinematicPlotsForBinsAndFits();
-  // createCorrelationPlotsforrunnum();
-  // createCorrelationPlots();
+  createIntegratedKinematicPlotsForBinsAndFits();
+  createCorrelationPlotsforrunnum();
+  createCorrelationPlots();
   currentFits=0;
   dataReader.Restart(); mcReader.Restart();
 
