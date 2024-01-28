cb2ca3b55aab9e3736ca180a92c68922cbdcdbfd
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-28 15:04:06 -0500

    added support for other channels

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.h
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.h
@@ -11,8 +11,8 @@ double asymmetry_value_calculation(double currentVariable, const std::string& pr
 double asymmetry_error_calculation(double currentVariable, const std::string& prefix, 
   double Npp, double Npm, double Nmp, double Nmm, double meanPol, double Ptp, double Ptm, 
   int asymmetry_index);
-double BSA_funcToFit(double* x, double* par);
-double TSA_funcToFit(double* x, double* par);
-double DSA_funcToFit(double* x, double* par);
+double BSA_single_hadron(double* x, double* par);
+double TSA_single_hadron(double* x, double* par);
+double DSA_single_hadron(double* x, double* par);
 
 #endif // ASYMMETRY_FITS_H
