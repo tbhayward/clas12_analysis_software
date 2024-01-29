777847245e6d6393a1d3a5354dfb50e6f02ac87f
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-29 01:00:10 -0500

    added support for other channels

--- analysis_scripts/asymmetry_extraction/B2BDihadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/B2BDihadronKinematicCuts.cpp
@@ -9,8 +9,7 @@ using std::string;
 B2BDihadronKinematicCuts::B2BDihadronKinematicCuts(TTreeReader& reader)
     : BaseKinematicCuts(reader), // Initialize BaseKinematicCuts
       runnum(reader, "runnum"), Q2(reader, "Q2"), W(reader, "W"),
-      x(reader, "x"), y(reader, "y"), z1(reader, "z1"), xF1(reader, "xF1"), 
-      xF2(reader, "xF2"), target_pol(reader, "target_pol") {}
+      x(reader, "x"), y(reader, "y"), target_pol(reader, "target_pol") {}
 
 bool B2BDihadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
     bool goodEvent = false;
@@ -18,12 +17,8 @@ bool B2BDihadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
 
     if (property == "epippX") {
       goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75;
-    } else if (property == "xepippX") {
-      goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75 && *z1 > 0.2 && *xF1 > 0.0 && *xF2 < 0.0;
     } else if (property == "epimpX") {
       goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75;
-    } else if (property == "xepipmpX") {
-      goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75 && *z1 > 0.2 && *xF1 > 0.0 && *xF2 < 0.0;
     } else {
       std::cout << "Property not detected" << std::endl;
     }
