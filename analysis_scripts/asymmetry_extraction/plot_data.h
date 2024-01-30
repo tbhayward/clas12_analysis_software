ecdd1ec7622c8e0685cade6e00cba4f10d424cb9
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-30 03:24:57 -0500

    implementing chi2 fits for b2b case; previous update did inclusive case

--- analysis_scripts/asymmetry_extraction/plot_data.h
+++ analysis_scripts/asymmetry_extraction/plot_data.h
@@ -23,10 +23,10 @@ void createCorrelationPlotsforrunnum();
 void createCorrelationPlots();
 
 template<typename T>
-void FillHistogram(TTreeReader& reader, const std::string& branchName, TH1D* hist, KinematicCuts& kinematicCuts, int fitIndex, bool isMC);
+void FillHistogram(TTreeReader& reader, const std::string& branchName, TH1D* hist, BaseKinematicCuts& kinematicCuts, int fitIndex, bool isMC);
 
 template<typename T1, typename T2>
-void createAndFillHistogram(TTreeReader& reader, TH2D* hist, const std::string& branchX, const std::string& branchY, KinematicCuts& kinematicCuts);
+void createAndFillHistogram(TTreeReader& reader, TH2D* hist, const std::string& branchX, const std::string& branchY, BaseKinematicCuts& kinematicCuts);
 
 
 #endif // PLOT_DATA_H
