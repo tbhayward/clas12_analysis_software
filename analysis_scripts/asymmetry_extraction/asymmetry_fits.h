b91a18c27dfb8040716e48b90c4af1b09b92779c
Timothy Hayward <tbhayward@email.wm.edu>
2024-01-30 03:51:21 -0500

    implementing chi2 fits for b2b case; previous update did inclusive case

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.h
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.h
@@ -14,15 +14,15 @@ double asymmetry_error_calculation(double currentVariable, const std::string& pr
 double Legendre_P(int ell, int m, float theta);
 double BSA_inclusive(double* par);
 double BSA_single_hadron(double* x, double* par);
-double BSA_b2b_dihadron(double* x, double* y, double* par);
-double BSA_dihadron(double* x, double* y, double* z, double* par);
+double BSA_b2b_dihadron(double* x, double* par);
+double BSA_dihadron(double* x, double* par);
 double TSA_inclusive(double* par);
 double TSA_single_hadron(double* x, double* par);
-double TSA_b2b_dihadron(double* x, double* y, double* par);
-double TSA_dihadron(double* x, double* y, double* z, double* par);
+double TSA_b2b_dihadron(double* x, double* par);
+double TSA_dihadron(double* x, double* par);
 double DSA_single_hadron(double* x, double* par);
 double DSA_inclusive(double* par);
-double DSA_b2b_dihadron(double* x, double* y, double* par);
-double DSA_dihadron(double* x, double* y, double* z, double* par);
+double DSA_b2b_dihadron(double* x, double* par);
+double DSA_dihadron(double* x, double* par);
 
 #endif // ASYMMETRY_FITS_H
