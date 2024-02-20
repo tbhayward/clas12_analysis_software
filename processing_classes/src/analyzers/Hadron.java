6e9cb1614c0d8366973862f96da9dc0080e53306
Timothy Hayward <tbhayward@email.wm.edu>
2024-02-20 11:25:58 -0500

    stabilization push to begin building toward systematic plots

--- processing_classes/src/analyzers/Hadron.java
+++ processing_classes/src/analyzers/Hadron.java
@@ -65,7 +65,7 @@ public class Hadron {
         if (variables.Q2()<1) { return false; } 
         if (variables.W()<2) { return false; } 
 //        if (variables.xF()<0.0) { return false; } 
-//        else if (variables.y()>0.75) { return false; } 
+        else if (variables.y()>0.75) { return false; } 
 //        else if (variables.z()<0.2 || variables.z() > 0.95) { return false; } 
 //        else if (variables.p_p()<1.25) { return false; } 
 //        else if (variables.Mx()<1.5) { return false; } 
