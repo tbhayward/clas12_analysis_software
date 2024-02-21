b5f1d8b51ea7a458b345d284458b882f1d84897e
Timothy Hayward <tbhayward@email.wm.edu>
2024-02-21 13:25:25 -0500

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/UU_unfolding.cpp
+++ analysis_scripts/misc/UU_unfolding.cpp
@@ -233,6 +233,7 @@ int main() {
     for (Long64_t i = 0; i < nGenEntries; ++i) {
         tMCGene->GetEntry(i);
         int binIndex = DetermineQ2yBin(Q2Gen, yGen) - 1; // Adjusted for 0-based indexing
+        std::cout << binIndex << std::endl;
         if (binIndex >= 0) {
 
             const auto& currentZEdges = zEdges[binIndex]; 
