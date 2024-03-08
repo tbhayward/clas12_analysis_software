0db640cb9d9db2dca03d5edb1929d2ea5f8acf9a
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-08 16:30:47 -0500

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/acceptance_study.cpp
+++ analysis_scripts/misc/acceptance_study.cpp
@@ -238,7 +238,7 @@ void plotForExclusion(const std::vector<double>& phiVec, double B, double C, int
     // For chi^2 lines in red
     TText *chi2Text1 = pt->AddText(Form("#chi^{2}: A_{UU}^{cos#phi} = %.3f #pm %.3f", fitFuncLimited->GetParameter(1),fitFuncLimited->GetParError(1));
     chi2Text1->SetTextColor(kRed);
-    TText *chi2Text2 = pt->AddText(Form("#chi^{2}: A_{UU}^{cos2#phi} = %.3f #pm %.3f", fitFuncLimited->GetParameter(2),fitFuncLimited->GetParError(2)));
+    TText *chi2Text2 = pt->AddText(Form("#chi^{2}: A_{UU}^{cos2#phi} = %.3f #pm %.3f", fitFuncLimited->GetParameter(2),fitFuncLimited->GetParError(2));
     chi2Text2->SetTextColor(kRed);
     double chi2 = fitFuncLimited->GetChisquare();
     double ndf = fitFuncLimited->GetNDF();
