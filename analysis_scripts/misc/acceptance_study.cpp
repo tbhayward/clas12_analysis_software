d1b87cf135ce500311f891fb1c494ebe1e23f664
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-08 17:36:16 -0500

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/acceptance_study.cpp
+++ analysis_scripts/misc/acceptance_study.cpp
@@ -44,7 +44,7 @@ void negLogLikelihood(Int_t &npar, Double_t *gin, Double_t &f,
     double sum = 0;
     double counts = 0;
     for (int phi = 0; phi < phiVecGlobal.size(); ++phi) {
-        std::cout << binsToExcludeGlobal*2*3.14159/24 << " " << phiVecGlobal[phi] << " " << (24-binsToExcludeGlobal)*2*3.14159/24 << std::endl;
+        // std::cout << binsToExcludeGlobal*2*3.14159/24 << " " << phiVecGlobal[phi] << " " << (24-binsToExcludeGlobal)*2*3.14159/24 << std::endl;
         if (phiVecGlobal[phi] >= binsToExcludeGlobal*2*3.14159/24 && phiVecGlobal[phi] < (24-binsToExcludeGlobal)*2*3.14159/24) {
             counts++;
             sum += log((1 + AUU_cosphi*cos(phiVecGlobal[phi]) + AUU_cos2phi*cos(2*phiVecGlobal[phi])));
@@ -174,7 +174,7 @@ void plotForExclusion(const std::vector<double>& phiVec, double B, double C, int
 	fitFuncFullRange->SetLineColor(kRed);
 
     /****** MLM MINIMIZATION PORTION *******/
-    double arglist[10]; arglist[0] = 1;
+    double arglist[10]; arglist[0] = 50000; arglist[1] = 0.01;
     int ierflg = 0;
     TMinuit minuit(2);
     minuit.SetPrintLevel(-1);
@@ -183,7 +183,7 @@ void plotForExclusion(const std::vector<double>& phiVec, double B, double C, int
     // minuit.DefineParameter(0, "A", maxY, 0.00, 0, 0);
     minuit.DefineParameter(0, "B", B, 0.001, -1, 1);
     minuit.DefineParameter(1, "C", C, 0.001, -1, 1);
-    minuit.Migrad();
+    minuit.mnexcm("MIGRAD", arglist, 2, ierflg);
 
     double fittedA, errA; minuit.GetParameter(0,fittedA,errA);
     std::cout << fittedB << " " << fittedC << std::endl;
