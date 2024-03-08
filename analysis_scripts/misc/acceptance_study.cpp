117ef97f9f60ea17f6f106e2aa8b7c9a6e14aefa
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-08 14:19:53 -0500

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/acceptance_study.cpp
+++ analysis_scripts/misc/acceptance_study.cpp
@@ -225,6 +225,8 @@ void plotForExclusion(const std::vector<double>& phiVec, double B, double C, int
 	// pt->AddText(Form("#chi^{2}/ndf = %.3f", chi2 / ndf));
 	// pt->Draw();
 
+    // // Calculate the start position for TPaveText based on the column, directly here
+    double textStartX = (canvasIndex % 3 == 1) ? 0.15 : 0.0; // Example: 0.15 for left column, adjust 0.12 for middle/right columns
     TPaveText *pt = new TPaveText(textStartX, 0.75, textStartX + 0.45, 1.0, "NDC");
     pt->SetFillColor(0);
     pt->SetTextAlign(12);
