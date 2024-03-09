9791c06cb66d3a3c4535b86e2c00ce1a8a790857
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-08 19:24:26 -0500

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/acceptance_study.cpp
+++ analysis_scripts/misc/acceptance_study.cpp
@@ -167,7 +167,7 @@ void plotForExclusion(const std::vector<double>& phiVec, double B, double C, int
 	fitFuncFullRange->SetLineColor(kRed);
 
     /****** MLM MINIMIZATION PORTION *******/
-    double arglist[10]; arglist[0] = 0.001;
+    double arglist[10]; arglist[0] = 50000; arglist[1] = 0.01;
     int ierflg = 0;
     TMinuit minuit(2);
     minuit.SetPrintLevel(0);
