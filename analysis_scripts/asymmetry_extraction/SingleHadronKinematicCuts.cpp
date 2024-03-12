dc42920747a7dc7a99047106d26138b5b3527a67
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-12 12:22:29 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -18,7 +18,7 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
         string property = binNames[currentFits];
 
         if (property == "xF") {
-            goodEvent = *Q2 > 1 && *W > 2 && *Mx > 1.4 && *y < 0.75;
+            goodEvent = *Q2 > 1 && *W > 2 && *Mx > 1.9 && *y < 0.75;
         }
         else if (property == "Mx") {
             goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75;
