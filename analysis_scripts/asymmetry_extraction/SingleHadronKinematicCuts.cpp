d7701269f0a72972b4ba55962309e991cf4276de
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-18 11:29:33 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -78,30 +78,30 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
                   // electron sector study
                   // // sec 1
                   // goodEvent = goodEvent && (*e_phi < 0.62 || *e_phi > 5.95);
-                  // //sec 2
-                  // goodEvent = goodEvent && (*e_phi > 0.66 || *e_phi < 1.65);
+                  //sec 2
+                  goodEvent = goodEvent && (*e_phi > 0.66 && *e_phi < 1.65);
                   // //sec 3
-                  // goodEvent = goodEvent && (*e_phi > 1.80 || *e_phi < 2.75);
+                  // goodEvent = goodEvent && (*e_phi > 1.80 && *e_phi < 2.75);
                   // //sec 4
-                  // goodEvent = goodEvent && (*e_phi > 2.80 || *e_phi < 3.78);
+                  // goodEvent = goodEvent && (*e_phi > 2.80 && *e_phi < 3.78);
                   // //sec 5
-                  // goodEvent = goodEvent && (*e_phi > 3.85 || *e_phi < 4.80);
+                  // goodEvent = goodEvent && (*e_phi > 3.85 && *e_phi < 4.80);
                   // //sec 6
-                  // goodEvent = goodEvent && (*e_phi > 4.95 || *e_phi < 5.9);
+                  // goodEvent = goodEvent && (*e_phi > 4.95 && *e_phi < 5.90);
 
                   // pion sector study
                   // // sec 1
-                  // goodEvent = goodEvent && (*e_phi < 0.20 || *e_phi > 5.45);
+                  // goodEvent = goodEvent && (*p_phi < 0.20 || *p_phi > 5.45);
                   // //sec 2
-                  // goodEvent = goodEvent && (*e_phi > 0.20 || *e_phi < 1.25);
+                  // goodEvent = goodEvent && (*p_phi > 0.20 && *p_phi < 1.25);
                   // //sec 3
-                  // goodEvent = goodEvent && (*e_phi > 1.25 || *e_phi < 2.30);
+                  // goodEvent = goodEvent && (*p_phi > 1.25 && *p_phi < 2.30);
                   // //sec 4
-                  // goodEvent = goodEvent && (*e_phi > 2.30 || *e_phi < 3.35);
+                  // goodEvent = goodEvent && (*p_phi > 2.30 && *p_phi < 3.35);
                   // //sec 5
-                  // goodEvent = goodEvent && (*e_phi > 3.35 || *e_phi < 4.40);
-                  //sec 6
-                  goodEvent = goodEvent && (*e_phi > 4.40 || *e_phi < 5.45);
+                  // goodEvent = goodEvent && (*p_phi > 3.35 && *p_phi < 4.40);
+                  // //sec 6
+                  // goodEvent = goodEvent && (*p_phi > 4.40 && *p_phi < 5.45);
               } else if (postz == "z5") {
                   goodEvent = *z > 0.55 && *z <= 0.75;
               }
