935e60cf78e9f18106bcea9a9674b619cece46fe
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-18 19:17:11 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -86,10 +86,10 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
                   // goodEvent = goodEvent && (*e_phi > 1.80 && *e_phi < 2.75);
                   // //sec 4
                   // goodEvent = goodEvent && (*e_phi > 2.80 && *e_phi < 3.78);
-                  //sec 5
-                  goodEvent = goodEvent && (*e_phi > 3.85 && *e_phi < 4.80);
-                  // //sec 6
-                  // goodEvent = goodEvent && (*e_phi > 4.95 && *e_phi < 5.90);
+                  // //sec 5
+                  // goodEvent = goodEvent && (*e_phi > 3.85 && *e_phi < 4.80);
+                  //sec 6
+                  goodEvent = goodEvent && (*e_phi > 4.95 && *e_phi < 5.90);
 
                   // pion sector study
                   // // sec 1
