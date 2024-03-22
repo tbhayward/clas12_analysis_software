b9517a7a643ccbc2864716ebb2bd58e5f8e3a532
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-22 11:28:46 -0400

    stabilization push to begin building toward systematic plots

--- processing_scripts/convert_txt_to_root.cpp
+++ processing_scripts/convert_txt_to_root.cpp
@@ -688,6 +688,9 @@ int main(int argc, char *argv[]) {
             p_p >> p_theta >> p_phi >> vz_p >> Q2 >> W >> Mx >> Mx2 >> x >> y >> z >> xF >> 
             pT >> zeta >> eta >> phi >> DepA >> DepB >> DepC >> DepV >> DepW) {
 
+            std::cout << runnum >> " " >> evnum >> " " >> helicity >> " " >> e_p >> " " >> e_theta >> " " >> e_phi >> " " >> vz_e >> " " >> 
+            p_p >> " " >> p_theta >> " " >> p_phi >> " " >> vz_p >> " " >> Q2 >> " " >> W >> " " >> Mx >> " " >> Mx2 >> " " >> x >> " " >> y >> " " >> z >> " " >> xF >> " " >> 
+            pT >> " " >> zeta >> " " >> eta >> " " >> phi >> " " >> DepA >> " " >> DepB >> " " >> DepC >> " " >> DepV >> " " >> DepW >> std::endl;
             beam_pol = getPol(runnum);
             if (runnum < 16000) { target_pol = 0; }
             else { 
@@ -701,7 +704,7 @@ int main(int argc, char *argv[]) {
 
             t = gett(p_p, p_theta); // for SIDIS we calculate t with proton kinematics
             tmin = gettmin(x); 
-            
+
 
             tree->Fill(); // Fill the tree with the read data
         }
