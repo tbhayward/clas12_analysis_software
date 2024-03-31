732110b12858c5e45eae233025e3a552653f1569
Timothy Hayward <tbhayward@email.wm.edu>
2024-03-31 12:04:06 -0400

    stabilization push to begin building toward systematic plots

--- processing_classes/src/analyzers/Inclusive.java
+++ processing_classes/src/analyzers/Inclusive.java
@@ -46,9 +46,7 @@ public class Inclusive {
 //            return false; }
         if (variables.Q2()<1) { return false; } 
         if (variables.W()<2) { return false; } 
-//        if (variables.xF()<0.0) { return false; } 
 //         if (variables.y()>0.75) { return false; } 
-//        else if (variables.Mx()<1.5) { return false; } 
 	return true;
     }
     
