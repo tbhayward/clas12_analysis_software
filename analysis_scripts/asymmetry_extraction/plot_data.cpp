b66b8791690c02129cb1810b91e209aed39e4d8a
Timothy Hayward <tbhayward@email.wm.edu>
2024-05-07 12:51:54 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/plot_data.cpp
+++ analysis_scripts/asymmetry_extraction/plot_data.cpp
@@ -171,8 +171,8 @@ void createIntegratedKinematicPlots() {
         // Add entries to the legend with scientific notation for the number of entries
         dataHist->SetEntries(dataHist->GetEntries());
         mcHist->SetEntries(mcHist->GetEntries());
-        leg->AddEntry(dataHist, (std::string("clasdis (") + std::to_string((int)dataHist->GetEntries()) + " entries)").c_str(), "l");
-        leg->AddEntry(mcHist, (std::string("clasdis+radgen (") + std::to_string((int)mcHist->GetEntries()) + " entries)").c_str(), "l");
+        leg->AddEntry(dataHist, (std::string("data (") + std::to_string((int)dataHist->GetEntries()) + " counts)").c_str(), "l");
+        leg->AddEntry(mcHist, (std::string("mc (") + std::to_string((int)mcHist->GetEntries()) + " counts)").c_str(), "l");
 
         // Set line colors for histograms
         dataHist->SetLineColor(kBlack);
