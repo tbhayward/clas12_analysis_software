34123a62bfbada77f1138a404487422f0a5ea350
Timothy Hayward <tbhayward@email.wm.edu>
2024-05-08 01:03:43 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/plot_data.cpp
+++ analysis_scripts/asymmetry_extraction/plot_data.cpp
@@ -145,12 +145,12 @@ void createIntegratedKinematicPlots() {
         if (dataHist->Integral() != 0) {
             // dataScale = dataHist->Integral();
             dataScale = data_count;
-            // dataHist->Scale(1.0 / dataScale);
+            dataHist->Scale(1.0 / dataScale);
         }
         if (mcHist->Integral() != 0) {
             // mcScale = mcHist->Integral();
             mcScale = mc_count;
-            // mcHist->Scale(1.0 / mcScale);
+            mcHist->Scale(1.0 / mcScale);
         }
 
         // Find the maximum value for y-axis
