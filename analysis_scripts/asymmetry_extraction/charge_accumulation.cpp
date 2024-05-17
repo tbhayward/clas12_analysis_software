ed318afdec0ddac658472f27e8f6b24bfe9673da
Timothy Hayward <tbhayward@email.wm.edu>
2024-05-17 01:03:47 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/charge_accumulation.cpp
+++ analysis_scripts/asymmetry_extraction/charge_accumulation.cpp
@@ -12,7 +12,7 @@ void charge_accumulation(TTreeReader& dataReader, const std::vector<RunInfo>& ru
     while (dataReader.Next()) {
         if (processedRuns.find(*runnum) == processedRuns.end()) {
             processedRuns.insert(*runnum);
-            std::cout << runnum << std::endl;
+            std::cout << *runnum << std::endl;
             // Find run_info for the current run and update cpp, cpm, cmp, cmm
             for (const auto& run_info : run_info_list) {
                 if (run_info.runnum == *runnum) {
