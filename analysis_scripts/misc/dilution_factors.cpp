0b115780229673b4f11a02b8864c9b88f864fb87
Timothy Hayward <tbhayward@email.wm.edu>
2024-05-16 23:54:33 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/misc/dilution_factors.cpp
+++ analysis_scripts/misc/dilution_factors.cpp
@@ -143,13 +143,13 @@ void dilution_factors(const char* nh3_file, const char* c_file) {
     gr_dilution->Draw("AP");
 
     // Fit to a third-degree polynomial
-    TF1 *fit_poly = new TF1("fit_poly", "[0] + [1]*x + [2]*x^2 + [3]*x^3", 0.0, 0.8);
+    TF1 *fit_poly = new TF1("fit_poly", "[0] + [1]*x + [2]*x^2 + [3]*x^3", 0, 0.8);
     gr_dilution->Fit(fit_poly, "R");
     fit_poly->SetLineColor(kRed);
     fit_poly->Draw("SAME");
 
     // Add fit parameters box
-    TPaveText *pt = new TPaveText(0.7, 0.7, 0.9, 0.9, "brNDC");
+    TPaveText *pt = new TPaveText(0.0, 0.7, 0.3, 0.9, "brNDC");
     pt->SetBorderSize(1);
     pt->SetFillStyle(0);
     pt->AddText(Form("p0 = %.3f", fit_poly->GetParameter(0)));
@@ -161,8 +161,8 @@ void dilution_factors(const char* nh3_file, const char* c_file) {
     // Fifth panel: xB histograms scaled by the fit constant
     c1->cd(5);
     gPad->SetLeftMargin(0.15);
-    TH1D *h_xB_nh3 = new TH1D("h_xB_nh3", "x_{B} Distribution; x_{B}; Counts", 100, 0.1, 0.6);
-    TH1D *h_xB_carbon = new TH1D("h_xB_carbon", "x_{B} Distribution; x_{B}; Counts", 100, 0.1, 0.6);
+    TH1D *h_xB_nh3 = new TH1D("h_xB_nh3", "x_{B} Distribution; x_{B}; Counts", 100, 0, 0.7);
+    TH1D *h_xB_carbon = new TH1D("h_xB_carbon", "x_{B} Distribution; x_{B}; Counts", 100, 0, 0.7);
     tree_nh3->Draw("x>>h_xB_nh3");
     tree_carbon->Draw("x>>h_xB_carbon");
 
@@ -203,7 +203,7 @@ void dilution_factors(const char* nh3_file, const char* c_file) {
     gr_dilution_xB->Draw("AP");
 
     // Fit to a third-degree polynomial
-    TF1 *fit_poly_xB = new TF1("fit_poly_xB", "[0] + [1]*x + [2]*x^2 + [3]*x^3", 0.1, 0.6);
+    TF1 *fit_poly_xB = new TF1("fit_poly_xB", "[0] + [1]*x + [2]*x^2 + [3]*x^3", 0, 1);
     gr_dilution_xB->Fit(fit_poly_xB, "R");
     fit_poly_xB->SetLineColor(kRed);
     fit_poly_xB->Draw("SAME");
