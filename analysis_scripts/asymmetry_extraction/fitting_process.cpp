bd44ed46bc57b42ffe909b98c28e05b650cc7bc4
Timothy Hayward <tbhayward@email.wm.edu>
2024-06-11 01:57:16 -0400

    stabilization push to begin building toward systematic plots

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -792,11 +792,11 @@ TH1D* createHistogramForBin_single_hadron(const char* histName, int binIndex,
 
     // Apply kinematic cuts (this function will need to be adapted)
     bool passedKinematicCuts = kinematicCuts->applyCuts(currentFits, false);
+    std::cout << "HEY " << *currentVariable << std::endl;
     // bool passedKinematicCuts = true;
     // Check if the currentVariable is within the desired range
     if (*currentVariable >= varMin && *currentVariable < varMax && passedKinematicCuts) {
       sumVariable += *currentVariable;
-      std::cout << "HEY " << sumVariable << std::endl;
 
       if (*helicity > 0 && *target_pol < 0) { histPosNeg->Fill(*phi); } 
       else if (*helicity < 0 && *target_pol > 0) {  histNegPos->Fill(*phi); }
