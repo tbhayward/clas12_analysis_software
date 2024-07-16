fad0e8eb2abcb2bf46640fa147a30a89c1c9753c
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-16 13:05:54 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -903,8 +903,8 @@ public class analysis_fitter extends GenericKinematicFitter {
 //            && calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
 //            && calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
 //            && vertex_cut(particle_Index, trigger_electron_vz, rec_Bank, run_Bank)    
-            && pcal_fiducial_cut(particle_Index, rec_Bank, cal_Bank)
-//            && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
+//            && pcal_fiducial_cut(particle_Index, rec_Bank, cal_Bank)
+            && dc_fiducial_cut(particle_Index, rec_Bank, track_Bank, traj_Bank, run_Bank)
 //            && nphe_cut(particle_Index, cc_Bank) // legacy cut used in the analysis note to check the effect
                 ;
     }
