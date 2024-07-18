2e05911011cd15ef403b5d5d92c1bab755667b3b
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-18 14:07:19 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -69,7 +69,7 @@ void dilution_factors_epX(const char* nh3_file, const char* c_file) {
     h_Mx_carbon->SetStats(0);
 
     // Second panel: ratio of NH3 to Carbon counts for Mx
-    c1->cd(3);
+    c1->cd(2);
     gPad->SetLeftMargin(0.15);
     TGraphErrors* gr_ratio_Mx = new TGraphErrors();
     for (int i = 1; i <= h_Mx_nh3->GetNbinsX(); ++i) {
@@ -85,7 +85,7 @@ void dilution_factors_epX(const char* nh3_file, const char* c_file) {
     gr_ratio_Mx->GetYaxis()->SetRangeUser(9, 15);
     gr_ratio_Mx->SetTitle("NH_{3} to Carbon Ratio; M_{x} (GeV); Ratio");
     gr_ratio_Mx->SetMarkerStyle(20);
-    gr_ratio_Mx->Draw("AP");
+    // gr_ratio_Mx->Draw("AP");
 
     // // Fit the data from -2 to -0.5 to a constant for Mx
     // TF1* fit_const_Mx = new TF1("fit_const_Mx", "[0]", -2, -0.5);
