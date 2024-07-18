3c07dd0be77414632bdf5a55472887776b91baf4
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-18 13:26:35 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -157,12 +157,12 @@ void dilution_factors_epX(const char* nh3_file, const char* c_file) {
     gr_ratio->Draw("AP");
 
     // Fit the data from -2.5 to -1 to a constant
-    TF1 *fit_const = new TF1("fit_const", "[0]", -2, -0.5);
+    *fit_const = new TF1("fit_const", "[0]", -2, -0.5);
     gr_ratio->Fit(fit_const, "R");
     fit_const->SetLineColor(kRed);
     fit_const->Draw("SAME");
     // Add the dotted-dashed line from -0.5 to 3
-    TF1 *dotted_line = new TF1("dotted_line", "[0]", -0.5, 3);
+    *dotted_line = new TF1("dotted_line", "[0]", -0.5, 3);
     dotted_line->SetParameter(0, fit_const->GetParameter(0)); // Use the same constant value as the fit
     dotted_line->SetLineColor(kRed);
     dotted_line->SetLineStyle(7); // Set line style to dotted-dashed
