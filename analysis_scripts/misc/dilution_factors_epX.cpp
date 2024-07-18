911aa7a7d6752ad2e147f91350008891956c4baa
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-18 11:40:09 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -222,7 +222,6 @@ void dilution_factors_epX(const char* nh3_file, const char* c_file) {
     pt->Draw();
 
     // Add chi2/ndf in the top left
-    TLatex latex;
     latex.SetNDC();
     latex.SetTextSize(0.04);
     latex.DrawLatex(0.15, 0.85, Form("#chi^{2}/NDF = %.2f / %d = %.2f", chi2, ndf, chi2_ndf));
