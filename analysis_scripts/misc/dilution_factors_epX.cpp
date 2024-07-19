d9511dd6e491241ce999bf77d04be8782f785666
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-19 11:24:02 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -101,9 +101,9 @@ std::pair<double, double> scale_normalization(const char* nh3_file, const char* 
     gr_ratio->SetMarkerStyle(20);
     gr_ratio->Draw("AP");
 
-    ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(0);
     // Fit the data from -2.5 to -1 to a constant
     TF1 *fit_const = new TF1("fit_const", "[0]", -2, -0.5);
+    minuit.SetPrintLevel(-1);
     gr_ratio->Fit(fit_const, "R");
     fit_const->SetLineColor(kRed);
     fit_const->Draw("SAME");
