ee6ce53d2464a03f3701dc22a29608b12ec12794
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-19 11:37:17 -0400

    resetting to default kinematic cuts for gross check with G. Matousek; will create branch specific to final cross checked version

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -334,9 +334,9 @@ double one_dimensional(const char* nh3_file, const char* c_file,
     c1->cd(2);
     gPad->SetLeftMargin(0.15);
     TH1D *h_x_nh3 = 
-        new TH1D("h_x_nh3", "P_{T} Distribution; P_{T} (GeV); Counts", 100, 0, 1.0);
+        new TH1D("h_x_nh3", "x_{B} Distribution; x_{B} (GeV); Counts", 100, 0, 1.0);
     TH1D *h_x_carbon = 
-        new TH1D("h_x_carbon", "P_{T} Distribution; P_{T} (GeV); Counts", 100, 0, 1.0);
+        new TH1D("h_x_carbon", "x_{B} Distribution; x_{B} (GeV); Counts", 100, 0, 1.0);
     tree_nh3->Draw("x>>h_x_nh3");
     tree_carbon->Draw("x>>h_x_carbon");
     TH1D *h_x_carbon_scaled = (TH1D*)h_x_carbon->Clone("h_x_carbon_scaled");
@@ -381,7 +381,7 @@ double one_dimensional(const char* nh3_file, const char* c_file,
 
     // Fit to a third-degree polynomial
     TF1 *fit_poly_x = new TF1("fit_poly", "[0] + [1]*x + [2]*x^2 + [3]*x^3", 0, 1.0);
-    gr_dilution_x->Fit(fit_poly, "R");
+    gr_dilution_x->Fit(fit_poly_x, "RQ");
     fit_poly_x->SetLineColor(kRed);
     fit_poly_x->Draw("SAME");
 
