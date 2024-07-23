e6a4dd81f9a07a7592f3472fcef63b1df006c038
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-23 15:14:46 -0400

    switched dilution_factors_epX.root to do the normalization with the counts normalized to accumulated charge

--- analysis_scripts/misc/dilution_factors_epX.cpp
+++ analysis_scripts/misc/dilution_factors_epX.cpp
@@ -113,7 +113,7 @@ std::pair<double, double> scale_normalization(const char* nh3_file, const char* 
         }
     }
     // Set y-axis range from 5 to 15
-    gr_ratio->GetYaxis()->SetRangeUser(9, 15);
+    gr_ratio->GetYaxis()->SetRangeUser(2, 12);
     // Set x-axis range
     gr_ratio->GetXaxis()->SetLimits(-2, 3);
 
@@ -184,7 +184,7 @@ std::pair<double, double> scale_normalization(const char* nh3_file, const char* 
         }
     }
     // Set y-axis range from 5 to 15
-    gr_ratio_xF->GetYaxis()->SetRangeUser(9, 15);
+    gr_ratio_xF->GetYaxis()->SetRangeUser(2, 12);
     // Set x-axis range
     gr_ratio_xF->GetXaxis()->SetLimits(-2, 1);
 
