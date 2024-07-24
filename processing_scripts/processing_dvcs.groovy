4582be338eb8933eda4f4896a7fbe2b85e1851dd
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-24 14:33:47 -0400

    created processing_dvcs.groovy by duplicating processing_dihadron.groovy and removing input pid strings (must be 2212 and 22) and adding exclusivity variables

--- processing_scripts/processing_dvcs.groovy
+++ processing_scripts/processing_dvcs.groovy
@@ -46,6 +46,8 @@ public static void main(String[] args) {
 	file.delete();
 	BufferedWriter writer = new BufferedWriter(new FileWriter(file));
 
+	println("output_file "+output_file);
+
 	// Set the number of files to process based on the provided 4th argument
 	// use the size of the hipo_list if no argument provided
 	int n_files = args.length < 3 || Integer.parseInt(args[2]) > hipo_list.size()
