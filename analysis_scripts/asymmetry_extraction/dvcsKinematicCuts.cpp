7c0e2dffc7eb0cb5fad088dda07a09c33a74ed48
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-24 19:51:50 -0400

    added dvcs variables to hist_configs and format_label_name in asymmetry extraction code, began preparing for chi2 fits of data

--- analysis_scripts/asymmetry_extraction/dvcsKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/dvcsKinematicCuts.cpp
@@ -20,17 +20,17 @@ bool dvcsKinematicCuts::applyCuts(int currentFits, bool isMC) {
 
     goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75; // DIS cuts
     if (property == "dvcsx") {
-        goodEvent = true;
-        // goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
-        //   *Mxgammasquared < 1.25;
+        // goodEvent = true;
+        goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
+          *Mxgammasquared < 1.25;
     } else if (property == "dvcst") {
         goodEvent = true;
-        // goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
-        //   *Mxgammasquared < 1.25;
+        goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
+          *Mxgammasquared < 1.25;
     } else if (property == "dvcsQ2") {
         goodEvent = true;
-        // goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
-        //   *Mxgammasquared < 1.25;
+        goodEvent = *theta_gamma_gamma < 0.6 && *Emiss2 < 0.5 && *pTmiss < 0.125 && 
+          *Mxgammasquared < 1.25;
     } else {
       std::cout << "Property, " << property << ", not detected!" << std::endl;
     }
