d260e4d046f9414d21f181f0ff82aa2be5936f3e
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-25 19:50:41 -0400

    moved Depolarization_A,B,C,V, and W to kinematic_variables class and removed them from inclusive, hadron, dihadron and trihadron classes

--- processing_scripts/processing_trihadrons.groovy
+++ processing_scripts/processing_trihadrons.groovy
@@ -111,7 +111,7 @@ public static void main(String[] args) {
 	StringBuilder batchLines = new StringBuilder();
 
 	int num_events = 0;
-	int max_lines = 1;
+	int max_lines = 1000;
 	int lineCount = 0;
 	for (current_file in 0..<n_files) {
 		// limit to a certain number of files defined by n_files
@@ -379,6 +379,7 @@ public static void main(String[] args) {
 		    }
 		reader.close();
 		}
+
 
 		println(); println(); println("HELLLOOOOO"); println(batchLines);
 		// Write any remaining lines in the batchLines StringBuilder to the file
