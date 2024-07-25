77a734534464f34c076c08bcdbdbb75d506b98e7
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-24 21:03:34 -0400

    added dvcs variables to hist_configs and format_label_name in asymmetry extraction code, began preparing for chi2 fits of data

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -316,7 +316,7 @@ int main(int argc, char *argv[]) {
       case 1: performMLMFits_single_hadron(output_file.c_str(), kinematic_file.c_str(), binNames[i]); break;
       case 2: performMLMFits_b2b_dihadron(output_file.c_str(), kinematic_file.c_str(), binNames[i]); break;
       case 3: cout << "No dihadron MLM fit (yet)." << endl; break;
-      case 4: performMLMFits_dvcs(output_file.c_str(), kinematic_file.c_str(), binNames[i]); break;
+      // case 4: performMLMFits_dvcs(output_file.c_str(), kinematic_file.c_str(), binNames[i]); break;
     }
     cout << endl << "     Completed " << binNames[i] << " MLM fits." << endl;
     cout << endl << endl;
