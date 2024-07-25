c986e0f250e48bfa81802b8af009977ddebf3f9f
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-24 21:47:29 -0400

    added dvcs variables to hist_configs and format_label_name in asymmetry extraction code, began preparing for chi2 fits of data

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -2384,7 +2384,7 @@ void performMLMFits_dvcs(const char* output_file, const char* kinematic_file,
     minuit.DefineParameter(2, "AUL_sin2phi", 0.0, 0.0, -1, 1);
     minuit.DefineParameter(3, "ALL", 0.00, 0.0, -1, 1);
     minuit.DefineParameter(4, "ALL_cosphi", 0.00, 0.0, -1, 1);
-    minuit.DefineParameter(5, "AUU_cosphi", 0.00, 0.01, -1, 1);
+    minuit.DefineParameter(5, "AUU_cosphi", 0.00, 0.0, -1, 1);
     minuit.DefineParameter(6, "AUU_cos2phi", 0.00, 0.00, -1, 1);
 
     // After defining parameters
