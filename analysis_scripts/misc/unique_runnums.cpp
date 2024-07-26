a69ea74319f7d149df34f80d6be760db03bdccec
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-25 20:21:48 -0400

    moved Depolarization_A,B,C,V, and W to kinematic_variables class and removed them from inclusive, hadron, dihadron and trihadron classes

--- analysis_scripts/misc/unique_runnums.cpp
+++ analysis_scripts/misc/unique_runnums.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     // Open the ROOT file
-    TFile *file = TFile::Open("/scratch/thayward/C_test/output.txt");
+    TFile *file = TFile::Open("/scratch/thayward/C_test/output.root");
     if (!file || file->IsZombie()) {
         std::cerr << "Error opening file!" << std::endl;
         return 1;
