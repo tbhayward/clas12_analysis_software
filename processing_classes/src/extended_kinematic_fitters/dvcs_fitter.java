d9e90dde27ea71c69e947ed1b4614d726ede1a6b
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-29 18:56:35 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
@@ -41,8 +41,8 @@ public class dvcs_fitter extends GenericKinematicFitter {
             && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
             && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
             && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)    
-//            && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, rec_Bank, cal_Bank)
-//            && fiducial_cuts.pass1_dc_fiducial_cut(particle_Index,rec_Bank,track_Bank,traj_Bank,run_Bank)
+            && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, rec_Bank, cal_Bank)
+            && fiducial_cuts.pass1_dc_fiducial_cut(particle_Index,rec_Bank,track_Bank,traj_Bank,run_Bank)
             ;
     }
     
