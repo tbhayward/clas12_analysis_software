94cc452b5261f889c696a9b5c3b081c2e25955ff
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-29 14:44:22 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -6,10 +6,11 @@
 package extended_kinematic_fitters;
 
 import org.jlab.clas.physics.GenericKinematicFitter;
+import org.jlab.clas.physics.*;
+import org.jlab.clas.physics.PhysicsEvent;
 import org.jlab.io.base.DataEvent;
 import org.jlab.io.hipo.HipoDataBank;
 
-import org.jlab.clas.physics.*;
 
 //import org.jlab.detector.scalers.DaqScalersSequence;
 
