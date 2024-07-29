0809bd867d055f413ba258d7dbbef1732dec5186
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-29 11:34:10 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -171,7 +171,7 @@ public class analysis_fitter extends GenericKinematicFitter {
                 vz_e = rec_Bank.getFloat("vz",0);
                 
             } else { return physEvent; } // trigger particle was not an electron
-            
+    
             for (int particle_Index = 0; particle_Index < rec_Bank.rows(); particle_Index++) {
                 int pid = rec_Bank.getInt("pid", particle_Index);
                 float px = rec_Bank.getFloat("px", particle_Index);
@@ -201,7 +201,7 @@ public class analysis_fitter extends GenericKinematicFitter {
 //                   double fe = EnergyLoss(run_Bank.getFloat("torus", 0), pid, px, py, pz);
                    double fe = 1;
                    Particle part = new Particle(pid,fe*px,fe*py,fe*pz,vx,vy,vz);
-                   physEvent.addParticle(part);   
+                   physEvent.addParticle(part);
                 }
                 
                 if (Math.abs(pid)==321 && kaon_test(particle_Index, pid, vz, vz_e, rec_Bank, cal_Bank, 
@@ -229,91 +229,36 @@ public class analysis_fitter extends GenericKinematicFitter {
                    physEvent.addParticle(part);   
                }
             }
-
             
-//            // check for >= 2 photons (in order to reconstruct pi0)
-//            int num_EB_photons = 0;
-//            for (int particle_Index = 0; particle_Index < rec_Bank.rows(); particle_Index++) {
-//                int pid = rec_Bank.getInt("pid", particle_Index);
-//                if (pid == 22) num_EB_photons ++;
-//            }
-//            
-//            int num_analysis_photons = 0; // number of photons meeting cuts
-//            LorentzVector lv_gamma_1 = new LorentzVector();
-//            LorentzVector lv_gamma_2 = new LorentzVector();
-//            LorentzVector lv_gamma_3 = new LorentzVector();
-//            LorentzVector lv_gamma_4 = new LorentzVector();
-//            for (int particle_Index = 0; particle_Index < rec_Bank.rows(); particle_Index++) {
-//                int pid = rec_Bank.getInt("pid", particle_Index);
-//                
-//                if (pid!=22 || trigger_electron_vz == -9 || num_EB_photons <2) { continue; }
-//                // requires the particle to be photon by EventBuilder & for an electron to have been assigned to event
-//                
-//                
-//                // load momenta and vertices
-//                float px = rec_Bank.getFloat("px", particle_Index);
-//                float py = rec_Bank.getFloat("py", particle_Index);
-//                float pz = rec_Bank.getFloat("pz", particle_Index);
-//                float vx = rec_Bank.getFloat("vx",particle_Index);
-//                float vy = rec_Bank.getFloat("vy",particle_Index);
-//                float vz = rec_Bank.getFloat("vz",particle_Index);
-//                LorentzVector lv_gamma = new LorentzVector(); lv_gamma.setPxPyPzM(px, py, pz, 0);
-//                if (photon_test(particle_Index, rec_Bank, cal_Bank, lv_e, lv_gamma)) {
-//                   
-//                   Particle part = new Particle(pid,px,py,pz,vx,vy,vz);
-//                   physEvent.addParticle(part);  
-//                   num_analysis_photons++;
-//                   if (num_analysis_photons==1) { lv_gamma_1.setPxPyPzM(px,py,pz,0); }
-//                   else if (num_analysis_photons==2) { lv_gamma_2.setPxPyPzM(px,py,pz,0); }
-//                   else if (num_analysis_photons==3) { lv_gamma_3.setPxPyPzM(px,py,pz,0); }
-//                   else if (num_analysis_photons==4) { lv_gamma_4.setPxPyPzM(px,py,pz,0); }
-//               }
-//            }
-//            
-//            double lower_mgg = 0.11;
-//            double upper_mgg = 0.16; // signal
-////            double lower_mgg = 0.22;
-////            double upper_mgg = 0.45; // background
-//            if (num_analysis_photons>1) {
-//                LorentzVector lv_gamma_1_2 = new LorentzVector(lv_gamma_1); lv_gamma_1_2.add(lv_gamma_2);
-//                if (lv_gamma_1_2.mass() > lower_mgg && lv_gamma_1_2.mass() < upper_mgg) {
-//                    Particle part = new Particle(111,lv_gamma_1_2.px(),lv_gamma_1_2.py(),lv_gamma_1_2.pz(),0,0,0);
-//                    physEvent.addParticle(part);
-//                }
-//                if (num_analysis_photons>2) {
-//                    LorentzVector lv_gamma_1_3 = new LorentzVector(lv_gamma_1); lv_gamma_1_3.add(lv_gamma_3);
-//                    LorentzVector lv_gamma_2_3 = new LorentzVector(lv_gamma_2); lv_gamma_2_3.add(lv_gamma_3);
-//                    if (lv_gamma_1_3.mass() > lower_mgg && lv_gamma_1_3.mass() < upper_mgg) {
-//                        Particle part = new Particle(111,lv_gamma_1_3.px(),lv_gamma_1_3.py(),lv_gamma_1_3.pz(),0,0,0);
-//                        physEvent.addParticle(part);
-//                    }
-//                    if (lv_gamma_2_3.mass() > lower_mgg && lv_gamma_2_3.mass() < upper_mgg) {
-//                        Particle part = new Particle(111,lv_gamma_2_3.px(),lv_gamma_2_3.py(),lv_gamma_2_3.pz(),0,0,0);
-//                        physEvent.addParticle(part);
-//                    }
-//                    if (num_analysis_photons>3) {
-//                        LorentzVector lv_gamma_1_4 = new LorentzVector(lv_gamma_1); lv_gamma_1_4.add(lv_gamma_4);
-//                        LorentzVector lv_gamma_2_4 = new LorentzVector(lv_gamma_2); lv_gamma_2_3.add(lv_gamma_4);
-//                        LorentzVector lv_gamma_3_4 = new LorentzVector(lv_gamma_3); lv_gamma_2_3.add(lv_gamma_4);
-//                        if (lv_gamma_1_4.mass() > lower_mgg && lv_gamma_1_4.mass() < upper_mgg) {
-//                            Particle part = new Particle(111,lv_gamma_1_4.px(),lv_gamma_1_4.py(),lv_gamma_1_4.pz(),
-//                                    0,0,0);
-//                            physEvent.addParticle(part);
-//                        }
-//                        if (lv_gamma_2_4.mass() > lower_mgg && lv_gamma_2_4.mass() < upper_mgg) {
-//                            Particle part = new Particle(111,lv_gamma_2_4.px(),lv_gamma_2_4.py(),lv_gamma_2_4.pz(),
-//                                    0,0,0);
-//                            physEvent.addParticle(part);
-//                        }
-//                        if (lv_gamma_3_4.mass() > lower_mgg && lv_gamma_3_4.mass() < upper_mgg) {
-//                            Particle part = new Particle(111,lv_gamma_3_4.px(),lv_gamma_3_4.py(),lv_gamma_3_4.pz(),
-//                                    0,0,0);
-//                            physEvent.addParticle(part);
-//                        }
-//                    }
-//                }
-//            }
+            int num_gamma = physEvent.countByPid(22); // number of photons in event
             
+            for (int current_p1 = 0; current_p1 < num_gamma; num_gamma++) {
+                for (int current_p2 = 0; current_p2 < num_gamma; num_gamma++) {
+                    parent_hadron_creation parent_hadron_creation = new parent_hadron_creation();
+                    Particle part = parent_hadron_creation.pi0_check(physEvent, current_p1, current_p2);
+                    
+                    // Check if a valid Particle was returned before adding it to the event
+                    if (part != null) {
+                        physEvent.addParticle(part);
+                    }
+                }
+            }
+            
+            int num_pip = physEvent.countByPid(211); // number of pi+ in event
+            int num_pim = physEvent.countByPid(-211); // number of pi- in event
+            int num_pi0 = physEvent.countByPid(111); // number of pi0 in event
+            
+            for (int current_p1 = 0; current_p1 < num_pip; num_pip++) {
+                for (int current_p2 = 0; current_p2 < num_pim; num_pim++) {
+                    parent_hadron_creation parent_hadron_creation = new parent_hadron_creation();
+                    Particle part = parent_hadron_creation.pi0_check(physEvent, current_p1, current_p2);
+                    
+                    // Check if a valid Particle was returned before adding it to the event
+                    if (part != null) {
+                        physEvent.addParticle(part);
+                    }
+                }
+            }
             
             
             return physEvent;
---
+++ processing_classes/src/extended_kinematic_fitters/parent_hadron_creation.java
@@ -0,0 +1,89 @@
+/**
+ *
+ * @author Timothy B. Hayward
+ */
+
+package extended_kinematic_fitters;
+
+import org.jlab.clas.physics.GenericKinematicFitter;
+import org.jlab.clas.physics.Particle;
+import org.jlab.clas.physics.PhysicsEvent;
+import org.jlab.io.base.DataEvent;
+import org.jlab.io.hipo.HipoDataBank;
+
+import org.jlab.clas.physics.*;
+
+public class parent_hadron_creation {
+    
+    Particle pi0_check(PhysicsEvent physEvent, int current_p1, int current_p2) {
+        Particle gamma_1 = physEvent.getParticle("[22,"+current_p1+"]");
+        LorentzVector lv_gamma_1 = new LorentzVector();
+        lv_gamma_1.setPxPyPzM(gamma_1.px(), gamma_1.py(), gamma_1.pz(), 0);
+                    
+        Particle gamma_2 = physEvent.getParticle("[22,"+current_p2+"]");
+        LorentzVector lv_gamma_2 = new LorentzVector();
+        lv_gamma_2.setPxPyPzM(gamma_2.px(), gamma_2.py(), gamma_2.pz(), 0);
+                    
+        LorentzVector lv_pi0_candidate = new LorentzVector(lv_gamma_1); lv_pi0_candidate.add(lv_gamma_2);
+        
+        // candidate is in the mass range for pi0
+        if (0.11 < lv_pi0_candidate.mass() && lv_pi0_candidate.mass() < 0.16) {
+            Particle part = new Particle(111, lv_pi0_candidate.px(), lv_pi0_candidate.py(), 
+                lv_pi0_candidate.pz(), 0, 0, 0);
+            return part;
+        }
+        
+        // candidate is in the sideband of the pi0 mass spectrum, assigned to pid == -111 (negative of pi0 pid)
+        if (0.22 < lv_pi0_candidate.mass() && lv_pi0_candidate.mass() < 0.45) {
+            Particle part = new Particle(-111, lv_pi0_candidate.px(), lv_pi0_candidate.py(), 
+                lv_pi0_candidate.pz(), 0, 0, 0);
+            return part;
+        }
+        
+        return null;
+    }
+    
+    Particle rho0_check(PhysicsEvent physEvent, int current_p1, int current_p2) {
+        Particle pi_1 = physEvent.getParticle("[211,"+current_p1+"]");
+        LorentzVector lv_pi_1 = new LorentzVector();
+        lv_pi_1.setPxPyPzM(pi_1.px(), pi_1.py(), pi_1.pz(), 0);
+                    
+        Particle pi_2 = physEvent.getParticle("[-211,"+current_p1+"]");
+        LorentzVector lv_pi_2 = new LorentzVector();
+        lv_pi_2.setPxPyPzM(pi_2.px(), pi_2.py(), pi_2.pz(), 0);
+                    
+        LorentzVector lv_rho_candidate = new LorentzVector(lv_pi_1); lv_rho_candidate.add(lv_pi_2);
+        
+        // candidate is in the mass range for pi0
+        if (0.65 < lv_rho_candidate.mass() && lv_rho_candidate.mass() < 0.90) {
+            // add rho0 to event
+            Particle part = new Particle(113, lv_rho_candidate.px(), lv_rho_candidate.py(), 
+                lv_rho_candidate.pz(), 0, 0, 0);
+            return part;
+        }
+        
+        return null;
+    }
+    
+    Particle rhop_check(PhysicsEvent physEvent, int current_p1, int current_p2) {
+        Particle pi_1 = physEvent.getParticle("[213,"+current_p1+"]");
+        LorentzVector lv_pi_1 = new LorentzVector();
+        lv_pi_1.setPxPyPzM(pi_1.px(), pi_1.py(), pi_1.pz(), 0);
+                    
+        Particle pi_2 = physEvent.getParticle("[111,"+current_p1+"]");
+        LorentzVector lv_pi_2 = new LorentzVector();
+        lv_pi_2.setPxPyPzM(pi_2.px(), pi_2.py(), pi_2.pz(), 0);
+                    
+        LorentzVector lv_rho_candidate = new LorentzVector(lv_pi_1); lv_rho_candidate.add(lv_pi_2);
+        
+        // candidate is in the mass range for pi0
+        if (0.65 < lv_rho_candidate.mass() && lv_rho_candidate.mass() < 0.90) {
+            // add rho+ to event
+            Particle part = new Particle(213, lv_rho_candidate.px(), lv_rho_candidate.py(), 
+                lv_rho_candidate.pz(), 0, 0, 0);
+            return part;
+        }
+        
+        return null;
+    }
+}
