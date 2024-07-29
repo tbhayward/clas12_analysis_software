33dac0f838d8ea506246ac80385f068fbbde58dc
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-29 12:27:31 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- processing_scripts/processing_single_hadrons.groovy
+++ processing_scripts/processing_single_hadrons.groovy
@@ -100,7 +100,6 @@ public static void main(String[] args) {
 
 		while (reader.hasEvent()) {
 		    ++num_events;
-		    println(num_events);
 		    if (num_events % 1000000 == 0) { // not necessary, just updates output
 		        print("processed: " + num_events + " events. ");
 		    }
