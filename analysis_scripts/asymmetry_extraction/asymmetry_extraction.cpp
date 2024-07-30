ecb01116c8b35a880dc7f6398a435f1b45f8ab3d
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-30 12:12:19 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -280,8 +280,8 @@ int main(int argc, char *argv[]) {
   cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nC)."<< endl << endl;
 
   createIntegratedKinematicPlots();
-  createIntegratedKinematicPlotsForBinsAndFits();
-  // createCorrelationPlotsforrunnum();
+  // createIntegratedKinematicPlotsForBinsAndFits();
+  createCorrelationPlotsforrunnum();
   // createCorrelationPlots();
   // createMisIDRatePlots();
   currentFits=0;
