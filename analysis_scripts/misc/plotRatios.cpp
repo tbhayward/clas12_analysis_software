d71afe0754479b1d85a5a9799e916905b1b4d927
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-30 08:14:45 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- analysis_scripts/misc/plotRatios.cpp
+++ analysis_scripts/misc/plotRatios.cpp
@@ -5,8 +5,6 @@
 #include <TString.h>
 #include <TSystem.h>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 void plotRatios(const char* file1, const char* file2, const char* file3, const char* file4) {
     // Open the ROOT files
@@ -27,109 +25,183 @@ void plotRatios(const char* file1, const char* file2, const char* file3, const c
     TTree *tree4 = (TTree*)f4->Get("PhysicsEvents");
 
     if (!tree1 || !tree2 || !tree3 || !tree4) {
-        std::cerr << "Error: Could not find the tree in one of the files" << std::endl;
+        std::cerr << "Error: Could not find the PhysicsEvents tree in one of the files" << std::endl;
         return;
     }
 
-    // Get the list of branches
-    TObjArray *branches1 = tree1->GetListOfBranches();
-    TObjArray *branches2 = tree2->GetListOfBranches();
-    TObjArray *branches3 = tree3->GetListOfBranches();
-    TObjArray *branches4 = tree4->GetListOfBranches();
+    const int nbins = 50;
+    TH1F *h1_p_p = new TH1F("h1_p_p", "p_p", nbins, 0, 4);
+    TH1F *h2_p_p = new TH1F("h2_p_p", "p_p", nbins, 0, 4);
+    TH1F *h3_p_p = new TH1F("h3_p_p", "p_p", nbins, 0, 4);
+    TH1F *h4_p_p = new TH1F("h4_p_p", "p_p", nbins, 0, 4);
 
-    if (branches1->GetEntries() != branches2->GetEntries() || branches3->GetEntries() != branches4->GetEntries()) {
-        std::cerr << "Error: The trees have different number of branches" << std::endl;
-        return;
+    TH1F *h1_xF = new TH1F("h1_xF", "xF", nbins, -2, 1);
+    TH1F *h2_xF = new TH1F("h2_xF", "xF", nbins, -2, 1);
+    TH1F *h3_xF = new TH1F("h3_xF", "xF", nbins, -2, 1);
+    TH1F *h4_xF = new TH1F("h4_xF", "xF", nbins, -2, 1);
+
+    TH1F *h1_p_theta = new TH1F("h1_p_theta", "p_theta", nbins, 0, 35);
+    TH1F *h2_p_theta = new TH1F("h2_p_theta", "p_theta", nbins, 0, 35);
+    TH1F *h3_p_theta = new TH1F("h3_p_theta", "p_theta", nbins, 0, 35);
+    TH1F *h4_p_theta = new TH1F("h4_p_theta", "p_theta", nbins, 0, 35);
+
+    float p_p1, p_p2, p_p3, p_p4;
+    float xF1, xF2, xF3, xF4;
+    float p_theta1, p_theta2, p_theta3, p_theta4;
+
+    tree1->SetBranchAddress("p_p", &p_p1);
+    tree2->SetBranchAddress("p_p", &p_p2);
+    tree3->SetBranchAddress("p_p", &p_p3);
+    tree4->SetBranchAddress("p_p", &p_p4);
+
+    tree1->SetBranchAddress("xF", &xF1);
+    tree2->SetBranchAddress("xF", &xF2);
+    tree3->SetBranchAddress("xF", &xF3);
+    tree4->SetBranchAddress("xF", &xF4);
+
+    tree1->SetBranchAddress("p_theta", &p_theta1);
+    tree2->SetBranchAddress("p_theta", &p_theta2);
+    tree3->SetBranchAddress("p_theta", &p_theta3);
+    tree4->SetBranchAddress("p_theta", &p_theta4);
+
+    Long64_t nentries1 = tree1->GetEntries();
+    Long64_t nentries2 = tree2->GetEntries();
+    Long64_t nentries3 = tree3->GetEntries();
+    Long64_t nentries4 = tree4->GetEntries();
+
+    // Fill histograms for the first set of files
+    for (Long64_t j = 0; j < nentries1; ++j) {
+        tree1->GetEntry(j);
+        h1_p_p->Fill(p_p1);
+        h1_xF->Fill(xF1);
+        h1_p_theta->Fill(p_theta1 * 180.0 / 3.14159);
     }
 
-    // Loop over branches
-    for (int i = 0; i < branches1->GetEntries(); ++i) {
-        TBranch *branch1 = (TBranch*)branches1->At(i);
-        TBranch *branch2 = (TBranch*)branches2->At(i);
-        TBranch *branch3 = (TBranch*)branches3->At(i);
-        TBranch *branch4 = (TBranch*)branches4->At(i);
-
-        TString branchName1 = branch1->GetName();
-        TString branchName2 = branch2->GetName();
-        TString branchName3 = branch3->GetName();
-        TString branchName4 = branch4->GetName();
-
-        if (branchName1 != branchName2 || branchName3 != branchName4) {
-            std::cerr << "Error: Branch names do not match: " << branchName1 << " vs " << branchName2 
-                      << " or " << branchName3 << " vs " << branchName4 << std::endl;
-            continue;
-        }
-
-        float value1, value2, value3, value4;
-
-        tree1->SetBranchAddress(branchName1, &value1);
-        tree2->SetBranchAddress(branchName2, &value2);
-        tree3->SetBranchAddress(branchName3, &value3);
-        tree4->SetBranchAddress(branchName4, &value4);
-
-        Long64_t nentries1 = tree1->GetEntries();
-        Long64_t nentries2 = tree2->GetEntries();
-        Long64_t nentries3 = tree3->GetEntries();
-        Long64_t nentries4 = tree4->GetEntries();
-
-        // Create histograms for ratios
-        TH1F *h1 = new TH1F(branchName1 + "_ratio1", branchName1 + "_ratio1", 50, 0, 5); // Assuming max ratio range to 5
-        TH1F *h2 = new TH1F(branchName3 + "_ratio2", branchName3 + "_ratio2", 50, 0, 5); // Assuming max ratio range to 5
-
-        // Fill histograms for the first set of files
-        for (Long64_t j = 0; j < nentries1 && j < nentries2; ++j) {
-            tree1->GetEntry(j);
-            tree2->GetEntry(j);
-            if (value2 != 0) { // Avoid division by zero
-                h1->Fill(value1 / value2);
-            }
-        }
-
-        // Fill histograms for the second set of files
-        for (Long64_t j = 0; j < nentries3 && j < nentries4; ++j) {
-            tree3->GetEntry(j);
-            tree4->GetEntry(j);
-            if (value4 != 0) { // Avoid division by zero
-                h2->Fill(value3 / value4);
-            }
-        }
-
-        // Create canvases and draw the histograms
-        TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
-        h1->GetXaxis()->SetTitle(branchName1);
-        h1->GetYaxis()->SetTitle("ratio (NH3/C)");
-        h1->Draw();
-
-        TCanvas *c2 = new TCanvas("c2", "c2", 800, 600);
-        h2->GetXaxis()->SetTitle(branchName3);
-        h2->GetYaxis()->SetTitle("ratio (NH3/C)");
-        h2->Draw();
-
-        // Save the canvases as PNG files
-        TString filename1 = TString::Format("output/%s_ratio1.png", branchName1.Data());
-        TString filename2 = TString::Format("output/%s_ratio2.png", branchName3.Data());
-        c1->SaveAs(filename1);
-        c2->SaveAs(filename2);
-
-        // Clean up
-        delete c1;
-        delete c2;
-        delete h1;
-        delete h2;
+    for (Long64_t j = 0; j < nentries2; ++j) {
+        tree2->GetEntry(j);
+        h2_p_p->Fill(p_p2);
+        h2_xF->Fill(xF2);
+        h2_p_theta->Fill(p_theta2 * 180.0 / 3.14159);
     }
+
+    // Fill histograms for the second set of files
+    for (Long64_t j = 0; j < nentries3; ++j) {
+        tree3->GetEntry(j);
+        h3_p_p->Fill(p_p3);
+        h3_xF->Fill(xF3);
+        h3_p_theta->Fill(p_theta3 * 180.0 / 3.14159);
+    }
+
+    for (Long64_t j = 0; j < nentries4; ++j) {
+        tree4->GetEntry(j);
+        h4_p_p->Fill(p_p4);
+        h4_xF->Fill(xF4);
+        h4_p_theta->Fill(p_theta4 * 180.0 / 3.14159);
+    }
+
+    // Create ratio histograms
+    TH1F *ratio_p_p_1 = (TH1F*)h1_p_p->Clone("ratio_p_p_1");
+    ratio_p_p_1->Divide(h2_p_p);
+
+    TH1F *ratio_p_p_2 = (TH1F*)h3_p_p->Clone("ratio_p_p_2");
+    ratio_p_p_2->Divide(h4_p_p);
+
+    TH1F *ratio_xF_1 = (TH1F*)h1_xF->Clone("ratio_xF_1");
+    ratio_xF_1->Divide(h2_xF);
+
+    TH1F *ratio_xF_2 = (TH1F*)h3_xF->Clone("ratio_xF_2");
+    ratio_xF_2->Divide(h4_xF);
+
+    TH1F *ratio_p_theta_1 = (TH1F*)h1_p_theta->Clone("ratio_p_theta_1");
+    ratio_p_theta_1->Divide(h2_p_theta);
+
+    TH1F *ratio_p_theta_2 = (TH1F*)h3_p_theta->Clone("ratio_p_theta_2");
+    ratio_p_theta_2->Divide(h4_p_theta);
+
+    // Save the histograms and ratios
+    TCanvas *c = new TCanvas("c", "c", 800, 600);
+
+    c->cd();
+    h1_p_p->Draw();
+    c->SaveAs("output/h1_p_p.png");
+    h2_p_p->Draw();
+    c->SaveAs("output/h2_p_p.png");
+    ratio_p_p_1->Draw();
+    c->SaveAs("output/ratio_p_p_1.png");
+
+    c->cd();
+    h3_p_p->Draw();
+    c->SaveAs("output/h3_p_p.png");
+    h4_p_p->Draw();
+    c->SaveAs("output/h4_p_p.png");
+    ratio_p_p_2->Draw();
+    c->SaveAs("output/ratio_p_p_2.png");
+
+    c->cd();
+    h1_xF->Draw();
+    c->SaveAs("output/h1_xF.png");
+    h2_xF->Draw();
+    c->SaveAs("output/h2_xF.png");
+    ratio_xF_1->Draw();
+    c->SaveAs("output/ratio_xF_1.png");
+
+    c->cd();
+    h3_xF->Draw();
+    c->SaveAs("output/h3_xF.png");
+    h4_xF->Draw();
+    c->SaveAs("output/h4_xF.png");
+    ratio_xF_2->Draw();
+    c->SaveAs("output/ratio_xF_2.png");
+
+    c->cd();
+    h1_p_theta->Draw();
+    c->SaveAs("output/h1_p_theta.png");
+    h2_p_theta->Draw();
+    c->SaveAs("output/h2_p_theta.png");
+    ratio_p_theta_1->Draw();
+    c->SaveAs("output/ratio_p_theta_1.png");
+
+    c->cd();
+    h3_p_theta->Draw();
+    c->SaveAs("output/h3_p_theta.png");
+    h4_p_theta->Draw();
+    c->SaveAs("output/h4_p_theta.png");
+    ratio_p_theta_2->Draw();
+    c->SaveAs("output/ratio_p_theta_2.png");
+
+    // Clean up
+    delete c;
+    delete h1_p_p;
+    delete h2_p_p;
+    delete h3_p_p;
+    delete h4_p_p;
+    delete h1_xF;
+    delete h2_xF;
+    delete h3_xF;
+    delete h4_xF;
+    delete h1_p_theta;
+    delete h2_p_theta;
+    delete h3_p_theta;
+    delete h4_p_theta;
+    delete ratio_p_p_1;
+    delete ratio_p_p_2;
+    delete ratio_xF_1;
+    delete ratio_xF_2;
+    delete ratio_p_theta_1;
+    delete ratio_p_theta_2;
 
     f1->Close();
     f2->Close();
     f3->Close();
     f4->Close();
-}
+
+    }
 
 int main(int argc, char **argv) {
     if (argc != 5) {
-        std::cerr << "Usage: " << argv[0] << " file1.root file2.root file3.root file4.root" << std::endl;
+        std::cerr << “Usage: “ << argv[0] << “ file1.root file2.root file3.root file4.root” << std::endl;
         return 1;
     }
-
     plotRatios(argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
