186b92b575314b07441ff912f4c59ec997f634c2
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-30 08:08:08 -0400

    created parent_hadron_creation class inside kinematic fitters package, add pi0, gammagamma background, rho0, rho+ and rho- checks to this class and now have the fitters add those hadrons to the event in the case they pass certain mass requirements, i.e. a pi0 is added to the event if there are two photons whose invariant mass is between 0.11 and 0.16 GeV

--- analysis_scripts/misc/plotRatios.cpp
+++ analysis_scripts/misc/plotRatios.cpp
@@ -6,14 +6,16 @@
 #include <TSystem.h>
 #include <iostream>
 #include <vector>
-#include <stdexcept>
+#include <algorithm>
 
-void plotRatios(const char* file1, const char* file2) {
+void plotRatios(const char* file1, const char* file2, const char* file3, const char* file4) {
     // Open the ROOT files
     TFile *f1 = TFile::Open(file1);
     TFile *f2 = TFile::Open(file2);
+    TFile *f3 = TFile::Open(file3);
+    TFile *f4 = TFile::Open(file4);
 
-    if (!f1 || !f2 || f1->IsZombie() || f2->IsZombie()) {
+    if (!f1 || !f2 || !f3 || !f4 || f1->IsZombie() || f2->IsZombie() || f3->IsZombie() || f4->IsZombie()) {
         std::cerr << "Error opening files" << std::endl;
         return;
     }
@@ -21,8 +23,10 @@ void plotRatios(const char* file1, const char* file2) {
     // Get the trees
     TTree *tree1 = (TTree*)f1->Get("tree");
     TTree *tree2 = (TTree*)f2->Get("tree");
+    TTree *tree3 = (TTree*)f3->Get("tree");
+    TTree *tree4 = (TTree*)f4->Get("tree");
 
-    if (!tree1 || !tree2) {
+    if (!tree1 || !tree2 || !tree3 || !tree4) {
         std::cerr << "Error: Could not find the tree in one of the files" << std::endl;
         return;
     }
@@ -30,8 +34,10 @@ void plotRatios(const char* file1, const char* file2) {
     // Get the list of branches
     TObjArray *branches1 = tree1->GetListOfBranches();
     TObjArray *branches2 = tree2->GetListOfBranches();
+    TObjArray *branches3 = tree3->GetListOfBranches();
+    TObjArray *branches4 = tree4->GetListOfBranches();
 
-    if (branches1->GetEntries() != branches2->GetEntries()) {
+    if (branches1->GetEntries() != branches2->GetEntries() || branches3->GetEntries() != branches4->GetEntries()) {
         std::cerr << "Error: The trees have different number of branches" << std::endl;
         return;
     }
@@ -40,72 +46,90 @@ void plotRatios(const char* file1, const char* file2) {
     for (int i = 0; i < branches1->GetEntries(); ++i) {
         TBranch *branch1 = (TBranch*)branches1->At(i);
         TBranch *branch2 = (TBranch*)branches2->At(i);
+        TBranch *branch3 = (TBranch*)branches3->At(i);
+        TBranch *branch4 = (TBranch*)branches4->At(i);
 
         TString branchName1 = branch1->GetName();
         TString branchName2 = branch2->GetName();
+        TString branchName3 = branch3->GetName();
+        TString branchName4 = branch4->GetName();
 
-        if (branchName1 != branchName2) {
-            std::cerr << "Error: Branch names do not match: " << branchName1 << " vs " << branchName2 << std::endl;
+        if (branchName1 != branchName2 || branchName3 != branchName4) {
+            std::cerr << "Error: Branch names do not match: " << branchName1 << " vs " << branchName2 
+                      << " or " << branchName3 << " vs " << branchName4 << std::endl;
             continue;
         }
 
-        std::vector<float> values1;
-        std::vector<float> values2;
-
-        float value1, value2;
+        float value1, value2, value3, value4;
 
         tree1->SetBranchAddress(branchName1, &value1);
         tree2->SetBranchAddress(branchName2, &value2);
+        tree3->SetBranchAddress(branchName3, &value3);
+        tree4->SetBranchAddress(branchName4, &value4);
 
         Long64_t nentries1 = tree1->GetEntries();
         Long64_t nentries2 = tree2->GetEntries();
+        Long64_t nentries3 = tree3->GetEntries();
+        Long64_t nentries4 = tree4->GetEntries();
 
-        if (nentries1 != nentries2) {
-            std::cerr << "Error: The trees have different number of entries" << std::endl;
-            return;
-        }
+        // Create histograms for ratios
+        TH1F *h1 = new TH1F(branchName1 + "_ratio1", branchName1 + "_ratio1", 50, 0, 5); // Assuming max ratio range to 5
+        TH1F *h2 = new TH1F(branchName3 + "_ratio2", branchName3 + "_ratio2", 50, 0, 5); // Assuming max ratio range to 5
 
-        for (Long64_t j = 0; j < nentries1; ++j) {
+        // Fill histograms for the first set of files
+        for (Long64_t j = 0; j < nentries1 && j < nentries2; ++j) {
             tree1->GetEntry(j);
             tree2->GetEntry(j);
             if (value2 != 0) { // Avoid division by zero
-                values1.push_back(value1 / value2);
-            } else {
-                values1.push_back(0); // or handle it in some other way
+                h1->Fill(value1 / value2);
             }
         }
 
-        // Create histogram for ratios
-        TH1F *h = new TH1F(branchName1, branchName1, 100, 0, *std::max_element(values1.begin(), values1.end()));
-        for (auto ratio : values1) {
-            h->Fill(ratio);
+        // Fill histograms for the second set of files
+        for (Long64_t j = 0; j < nentries3 && j < nentries4; ++j) {
+            tree3->GetEntry(j);
+            tree4->GetEntry(j);
+            if (value4 != 0) { // Avoid division by zero
+                h2->Fill(value3 / value4);
+            }
         }
 
-        // Create a canvas and draw the histogram
-        TCanvas *c = new TCanvas("c", "c", 800, 600);
-        h->GetXaxis()->SetTitle(branchName1);
-        h->GetYaxis()->SetTitle("ratio (NH3/C)");
-        h->Draw();
+        // Create canvases and draw the histograms
+        TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+        h1->GetXaxis()->SetTitle(branchName1);
+        h1->GetYaxis()->SetTitle("ratio (NH3/C)");
+        h1->Draw();
 
-        // Save the canvas as a PNG file
-        TString filename = TString::Format("output/%s.png", branchName1.Data());
-        c->SaveAs(filename);
+        TCanvas *c2 = new TCanvas("c2", "c2", 800, 600);
+        h2->GetXaxis()->SetTitle(branchName3);
+        h2->GetYaxis()->SetTitle("ratio (NH3/C)");
+        h2->Draw();
+
+        // Save the canvases as PNG files
+        TString filename1 = TString::Format("output/%s_ratio1.png", branchName1.Data());
+        TString filename2 = TString::Format("output/%s_ratio2.png", branchName3.Data());
+        c1->SaveAs(filename1);
+        c2->SaveAs(filename2);
 
         // Clean up
-        delete c;
-        delete h;
+        delete c1;
+        delete c2;
+        delete h1;
+        delete h2;
     }
 
     f1->Close();
     f2->Close();
+    f3->Close();
+    f4->Close();
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " file1.root file2.root" << std::endl;
+    if (argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " file1.root file2.root file3.root file4.root" << std::endl;
         return 1;
     }
 
-    plotRatios(argv[1], argv[2]);
+    plotRatios(argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
