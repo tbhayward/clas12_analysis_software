7e4bc816d0f101507ca9809b52a9d4e905290b02
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-31 11:58:15 -0400

    changed processing_calibration to have unique entries for cal layer 1,4,7, track detector 5,6, etc

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -281,7 +281,7 @@ int main(int argc, char *argv[]) {
   cout << "Total unpolarized (carbon) charge: " << total_charge_carbon << " (nC)."<< endl << endl;
 
   // createIntegratedKinematicPlots();
-  // createIntegratedKinematicPlotsForBinsAndFits();
+  createIntegratedKinematicPlotsForBinsAndFits();
   // createCorrelationPlotsforrunnum();
   // createCorrelationPlots();
   // createMisIDRatePlots();
