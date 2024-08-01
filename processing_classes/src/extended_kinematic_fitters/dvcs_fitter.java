55ce1384c31368e05cc447f413d0b143ebff94c5
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-31 21:46:06 -0400

    finished first iteration of processing_calibration, added it to convert_txt_to_root and processing.csh

--- processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
@@ -98,6 +98,7 @@ public class dvcs_fitter extends GenericKinematicFitter {
     public PhysicsEvent getPhysicsEvent(DataEvent event) {
         
         generic_tests generic_tests = new generic_tests();
+        System.out.println(generic_tests.banks_test(event));
         if (generic_tests.banks_test(event)) {
             PhysicsEvent physEvent = new PhysicsEvent();
             // load the hipo banks
