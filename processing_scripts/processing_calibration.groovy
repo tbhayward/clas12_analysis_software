75f59da67691e34947286e9d2c9f7212d558a9b6
Timothy Hayward <tbhayward@email.wm.edu>
2024-07-31 21:59:18 -0400

    finished first iteration of processing_calibration, added it to convert_txt_to_root and processing.csh

--- processing_scripts/processing_calibration.groovy
+++ processing_scripts/processing_calibration.groovy
@@ -160,11 +160,11 @@ class CalibrationScript {
 	}
 
 	static boolean banks_test(DataEvent event) {
-		return event.hasBank("RUN::config");
-        // return event.hasBank("RUN::config") && event.hasBank("REC::Event") && 
-        // 	event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter") && 
-        // 	event.hasBank("REC::Track") && event.hasBank("REC::Traj") && 
-        // 	event.hasBank("REC::Cherenkov");
+		// return event.hasBank("RUN::config");
+        return event.hasBank("RUN::config") && event.hasBank("REC::Event") && 
+        	event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter") && 
+        	event.hasBank("REC::Track") && event.hasBank("REC::Traj") && 
+        	event.hasBank("REC::Cherenkov");
     }
 
     // Method for the main logic
