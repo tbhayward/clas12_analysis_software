461bbb64289011b9fef403b61943ea934ab3ce97
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-02 16:11:34 -0400

    began working on calibration plotting script for ROOT to determine fiducial cuts etc

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -618,17 +618,17 @@ void plot_ft_xy_energy(TTreeReader& dataReader, TTreeReader* mcReader = nullptr)
 	    delete h_mc_masked;
 	}
 
-	// Clean up the dynamically allocated memory
-    delete h_data_sum;
-    delete h_data_count;
-    if (mcReader) {
-        delete h_mc_sum;
-        delete h_mc_count;
-        delete mc_ft_x;
-        delete mc_ft_y;
-        delete mc_ft_energy;
-        delete mc_particle_pid;
-    }
+	// // Clean up the dynamically allocated memory
+    // delete h_data_sum;
+    // delete h_data_count;
+    // if (mcReader) {
+    //     delete h_mc_sum;
+    //     delete h_mc_count;
+    //     delete mc_ft_x;
+    //     delete mc_ft_y;
+    //     delete mc_ft_energy;
+    //     delete mc_particle_pid;
+    // }
 }
 
 void plot_ft_hit_position(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
