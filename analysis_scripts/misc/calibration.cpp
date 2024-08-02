84f0f43e7c04b450f3f5b7f9d294319cc6b9404a
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-02 15:37:56 -0400

    began working on calibration plotting script for ROOT to determine fiducial cuts etc

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -547,7 +547,7 @@ void plot_ft_xy_energy(TTreeReader& dataReader, TTreeReader* mcReader = nullptr)
         {1.60, {-8.42,  9.89}},  // circle 1
         {1.60, {-9.89, -5.33}},  // circle 2
         {2.30, {-6.15, 13.00}},  // circle 3
-        {2.00, {-6.50,  3.70}}   // circle 4
+        {2.00, {3.70,  -6.50}}   // circle 4
     };
 
     for (const auto& hole : holes) {
