4aca991aab54194dd43c0f26f2d05976399e7ef5
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-05 10:21:02 -0400

    began working on calibration plotting script for ROOT to determine fiducial cuts etc

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -2870,7 +2870,7 @@ void dc_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader = 
         int pad = 1;
         std::vector<TH2D*> histograms;  // Store histograms to delete them later
 
-        for (int region = 0; region < 3; region++) {
+        for (const auto& region : regions) {
         	std::cout << "Starting regions" << std::endl;
             std::string x_branch = std::get<0>(region);
             std::string y_branch = std::get<1>(region);
