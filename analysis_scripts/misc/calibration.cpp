d9ee363bc0c2b0f2b543065d1eb5e9be52b83744
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-06 15:30:15 -0400

    began working on cvt variables for calibration.cpp

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3224,14 +3224,14 @@ void plot_chi2pid_cd(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
     for (size_t i = 0; i < pBins.size() - 1; ++i) {
         h_data_beta_bins_pos[i] = new TH1D(("h_data_beta_pos_bin_" + std::to_string(i)).c_str(),
                                            ("#beta: " + std::to_string(pBins[i]) + " < p < " + std::to_string(pBins[i + 1])).c_str(),
-                                           nBins, 0, betaMax);
+                                           nBins, 0.8, 1.2);
         h_data_beta_bins_pos[i]->GetXaxis()->SetTitle("#beta");
         h_data_beta_bins_pos[i]->GetYaxis()->SetTitle("Normalized Counts");
         h_data_beta_bins_pos[i]->SetStats(false);
 
         h_data_beta_bins_neg[i] = new TH1D(("h_data_beta_neg_bin_" + std::to_string(i)).c_str(),
                                            ("#beta: " + std::to_string(pBins[i]) + " < p < " + std::to_string(pBins[i + 1])).c_str(),
-                                           nBins, 0, betaMax);
+                                           nBins, 0.8, 1.2);
         h_data_beta_bins_neg[i]->GetXaxis()->SetTitle("#beta");
         h_data_beta_bins_neg[i]->GetYaxis()->SetTitle("Normalized Counts");
         h_data_beta_bins_neg[i]->SetStats(false);
@@ -3239,7 +3239,7 @@ void plot_chi2pid_cd(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
         if (mcReader) {
             h_mc_beta_bins_pos[i] = new TH1D(("h_mc_beta_pos_bin_" + std::to_string(i)).c_str(),
                                              ("#beta: " + std::to_string(pBins[i]) + " < p < " + std::to_string(pBins[i + 1])).c_str(),
-                                             nBins, 0, betaMax);
+                                             nBins, 0.8, 1.2);
             h_mc_beta_bins_pos[i]->GetXaxis()->SetTitle("#beta");
             h_mc_beta_bins_pos[i]->GetYaxis()->SetTitle("Normalized Counts");
             h_mc_beta_bins_pos[i]->SetLineColor(kRed);
@@ -3247,7 +3247,7 @@ void plot_chi2pid_cd(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
 
             h_mc_beta_bins_neg[i] = new TH1D(("h_mc_beta_neg_bin_" + std::to_string(i)).c_str(),
                                              ("#beta: " + std::to_string(pBins[i]) + " < p < " + std::to_string(pBins[i + 1])).c_str(),
-                                             nBins, 0, betaMax);
+                                             nBins, 0.8, 1.2);
             h_mc_beta_bins_neg[i]->GetXaxis()->SetTitle("#beta");
             h_mc_beta_bins_neg[i]->GetYaxis()->SetTitle("Normalized Counts");
             h_mc_beta_bins_neg[i]->SetLineColor(kRed);
@@ -3464,11 +3464,6 @@ void plot_chi2pid_cd(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
         // Save the 4x4 canvases
         c_data_beta_bins_pos->SaveAs("output/calibration/cvt/chi2pid/beta_vs_p_binned_pos_cd.png");
         c_data_beta_bins_neg->SaveAs("output/calibration/cvt/chi2pid/beta_vs_p_binned_neg_cd.png");
-
-        if (mcReader) {
-            c_mc_beta_bins_pos->SaveAs("output/calibration/cvt/chi2pid/mc_beta_vs_p_binned_pos_cd.png");
-            c_mc_beta_bins_neg->SaveAs("output/calibration/cvt/chi2pid/mc_beta_vs_p_binned_neg_cd.png");
-        }
 
         // Clean up
         delete c;
