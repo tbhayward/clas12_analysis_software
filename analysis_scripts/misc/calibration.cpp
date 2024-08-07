b95c9a195e23008d942e0185c1621c0109a44526
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-07 15:44:04 -0400

    created dilution_factors_epX_full_calculation.cpp that does the full dilution factor calculation involving all target types following Sebastian Kuhn's outline

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3454,8 +3454,9 @@ void plot_chi2pid_cd(TTreeReader& dataReader, TTreeReader* mcReader = nullptr) {
             }
             TLegend* legend_pos = new TLegend(0.7, 0.7, 0.9, 0.9);
             // legend_pos->AddEntry(h_data_beta_bins_pos[bin], "Data", "l");
-            legend_pos->AddEntry(h_data_beta_bins_pos[bin], "pass-1", "l");
-            if (mcReader) legend_pos->AddEntry(h_mc_beta_bins_pos[bin], "MC", "l");
+            // if (mcReader) legend_pos->AddEntry(h_mc_beta_bins_pos[bin], "MC", "l");
+            legend_pos->AddEntry(h_data_beta_bins_pos[bin], "NH_{3} pass-1", "l");
+            if (mcReader) legend_pos->AddEntry(h_mc_beta_bins_pos[bin], "C pass-1", "l");
             legend_pos->Draw();
 
             c_data_beta_bins_neg->cd(bin + 1);
