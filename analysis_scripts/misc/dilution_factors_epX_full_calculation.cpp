20bf2fcb8734e62ed1359c4549a05c4f2d7f6f1f
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-07 15:08:45 -0400

    created dilution_factors_epX_full_calculation.cpp that does the full dilution factor calculation involving all target types following Sebastian Kuhn's outline

--- analysis_scripts/misc/dilution_factors_epX_full_calculation.cpp
+++ analysis_scripts/misc/dilution_factors_epX_full_calculation.cpp
@@ -41,25 +41,24 @@ double calculate_dilution_factor(double nA, double nC, double nCH, double nMT, d
 }
 
 void plot_dilution_factor(const char* variable_name, const char* x_title, double x_min, double x_max, int n_bins, 
-                          TTree* nh3, TTree* c, TTree* ch, TTree* he, TTree* empty, TCanvas* canvas, int pad, 
-                          std::map<std::string, std::string>& fit_results) {
-    // Create histograms for each target
-    TH1D* h_nh3 = new TH1D(Form("h_%s_nh3", variable_name), "", n_bins, x_min, x_max);
-    TH1D* h_c = new TH1D(Form("h_%s_c", variable_name), "", n_bins, x_min, x_max);
-    TH1D* h_ch = new TH1D(Form("h_%s_ch", variable_name), "", n_bins, x_min, x_max);
-    TH1D* h_he = new TH1D(Form("h_%s_he", variable_name), "", n_bins, x_min, x_max);
-    TH1D* h_empty = new TH1D(Form("h_%s_empty", variable_name), "", n_bins, x_min, x_max);
+                          TTree* nh3, TTree* c, TTree* ch, TTree* he, TTree* empty, TCanvas* canvas, int pad) {
+    canvas->cd(pad);
+    gPad->SetLeftMargin(0.15);
 
-    // Fill histograms
-    nh3->Draw(Form("%s>>h_%s_nh3", variable_name, variable_name), "Mx > 1.4");
-    c->Draw(Form("%s>>h_%s_c", variable_name, variable_name), "Mx > 1.4");
-    ch->Draw(Form("%s>>h_%s_ch", variable_name, variable_name), "Mx > 1.4");
-    he->Draw(Form("%s>>h_%s_he", variable_name, variable_name), "Mx > 1.4");
-    empty->Draw(Form("%s>>h_%s_empty", variable_name, variable_name), "Mx > 1.4");
+    TH1D *h_nh3 = new TH1D(Form("h_%s_nh3", variable_name), "", n_bins, x_min, x_max);
+    TH1D *h_c = new TH1D(Form("h_%s_c", variable_name), "", n_bins, x_min, x_max);
+    TH1D *h_ch = new TH1D(Form("h_%s_ch", variable_name), "", n_bins, x_min, x_max);
+    TH1D *h_he = new TH1D(Form("h_%s_he", variable_name), "", n_bins, x_min, x_max);
+    TH1D *h_empty = new TH1D(Form("h_%s_empty", variable_name), "", n_bins, x_min, x_max);
 
-    // Calculate dilution factor for each bin
-    TGraphErrors* gr_dilution = new TGraphErrors();
-    for (int i = 1; i <= h_nh3->GetNbinsX(); ++i) {
+    nh3->Draw(Form("%s>>h_%s_nh3", variable_name, variable_name));
+    c->Draw(Form("%s>>h_%s_c", variable_name, variable_name));
+    ch->Draw(Form("%s>>h_%s_ch", variable_name, variable_name));
+    he->Draw(Form("%s>>h_%s_he", variable_name, variable_name));
+    empty->Draw(Form("%s>>h_%s_empty", variable_name, variable_name));
+
+    TGraphErrors *gr_dilution = new TGraphErrors();
+    for (int i = 1; i <= n_bins; ++i) {
         double nA = h_nh3->GetBinContent(i);
         double nC = h_c->GetBinContent(i);
         double nCH = h_ch->GetBinContent(i);
@@ -67,37 +66,25 @@ void plot_dilution_factor(const char* variable_name, const char* x_title, double
         double nf = h_empty->GetBinContent(i);
 
         double dilution = calculate_dilution_factor(nA, nC, nCH, nMT, nf);
+        double error = 0.01; // Placeholder error
+
         gr_dilution->SetPoint(i - 1, h_nh3->GetBinCenter(i), dilution);
-        gr_dilution->SetPointError(i - 1, 0, 0.01); // Placeholder for the error
+        gr_dilution->SetPointError(i - 1, 0, error);
     }
 
-    // Draw on the canvas
-    canvas->cd(pad);
-    gPad->SetLeftMargin(0.15);
     gr_dilution->SetTitle(Form(";%s;D_{f}", x_title));
     gr_dilution->SetMarkerStyle(20);
     gr_dilution->Draw("AP");
     gr_dilution->GetXaxis()->SetRangeUser(x_min, x_max);
     gr_dilution->GetYaxis()->SetRangeUser(0.10, 0.30);
 
-    // Fit to a fourth-degree polynomial
-    TF1* fit_func = new TF1("fit_func", "[0] + [1]*x + [2]*x^2 + [3]*x^3 + [4]*x^4", x_min, x_max);
+    // Fit and plot
+    TF1 *fit_func = new TF1("fit_func", "[0] + [1]*x + [2]*x^2", x_min, x_max);
     gr_dilution->Fit(fit_func, "RQ");
     fit_func->SetLineColor(kRed);
     fit_func->Draw("SAME");
 
-    // Retrieve fit parameters
-    double p0 = fit_func->GetParameter(0);
-    double p1 = fit_func->GetParameter(1);
-    double p2 = fit_func->GetParameter(2);
-    double p3 = fit_func->GetParameter(3);
-    double p4 = fit_func->GetParameter(4);
-
-    // Store the fit result in the map
-    fit_results[variable_name] = Form("%f+%f*currentVariable+%f*std::pow(currentVariable,2)+%f*std::pow(currentVariable,3)+%f*std::pow(currentVariable,4)", 
-                                      p0, p1, p2, p3, p4);
-
-    // Optional: Add chi2/ndf in the top left
+    // Print fit parameters and chi2/ndf
     double chi2 = fit_func->GetChisquare();
     int ndf = fit_func->GetNDF();
     TLatex latex;
@@ -105,7 +92,17 @@ void plot_dilution_factor(const char* variable_name, const char* x_title, double
     latex.SetTextSize(0.04);
     latex.DrawLatex(0.20, 0.15, Form("#chi^{2}/NDF = %.2f / %d = %.2f", chi2, ndf, chi2 / ndf));
 
-    // Clean up
+    // Add fit parameters box
+    TPaveText *pt = new TPaveText(0.5, 0.7, 0.9, 0.9, "brNDC");
+    pt->SetBorderSize(1);
+    pt->SetFillStyle(1001);
+    pt->SetFillColor(kWhite);
+    pt->AddText(Form("p0 = %.3f +/- %.3f", fit_func->GetParameter(0), fit_func->GetParError(0)));
+    pt->AddText(Form("p1 = %.3f +/- %.3f", fit_func->GetParameter(1), fit_func->GetParError(1)));
+    pt->AddText(Form("p2 = %.3f +/- %.3f", fit_func->GetParameter(2), fit_func->GetParError(2)));
+    pt->Draw();
+
+    // Clean up histograms
     delete h_nh3;
     delete h_c;
     delete h_ch;
@@ -127,42 +124,32 @@ std::pair<TF1*, TGraphErrors*> fit_and_plot_dilution(const char* variable_name, 
 
 void one_dimensional(TFile* nh3_file, TFile* c_file, TFile* ch_file, TFile* he_file, TFile* empty_file) {
     // Get the PhysicsEvents trees
-    TTree* nh3_tree = (TTree*)nh3_file->Get("PhysicsEvents");
-    TTree* c_tree = (TTree*)c_file->Get("PhysicsEvents");
-    TTree* ch_tree = (TTree*)ch_file->Get("PhysicsEvents");
-    TTree* he_tree = (TTree*)he_file->Get("PhysicsEvents");
-    TTree* empty_tree = (TTree*)empty_file->Get("PhysicsEvents");
+    TTree* nh3 = (TTree*)nh3_file->Get("PhysicsEvents");
+    TTree* c = (TTree*)c_file->Get("PhysicsEvents");
+    TTree* ch = (TTree*)ch_file->Get("PhysicsEvents");
+    TTree* he = (TTree*)he_file->Get("PhysicsEvents");
+    TTree* empty = (TTree*)empty_file->Get("PhysicsEvents");
 
     // Create a canvas and divide it into 1 row and 3 columns
-    TCanvas* c1 = new TCanvas("c1", "Dilution Factor Analysis", 1600, 600);
+    TCanvas *c1 = new TCanvas("c1", "Dilution Factor Analysis", 1600, 600);
     c1->Divide(3, 1);
 
-    // Create a map to store the fit results
-    std::map<std::string, std::string> fit_results;
+    // Fit and plot for x-Bjorken
+    auto fit_x = fit_and_plot_dilution("x", "x_{B} (GeV)", 0.06, 0.6, 50, nh3, c, ch, he, empty, c1, 1);
 
-    // Plot for x-Bjorken
-    plot_dilution_factor("x", "x_{B} (GeV)", 0.06, 0.6, 50, nh3_tree, c_tree, ch_tree, he_tree, empty_tree, c1, 1, fit_results);
+    // Fit and plot for transverse momentum
+    auto fit_pT = fit_and_plot_dilution("pT", "P_{T} (GeV)", 0, 1.0, 50, nh3, c, ch, he, empty, c1, 2);
 
-    // Plot for transverse momentum
-    plot_dilution_factor("pT", "P_{T} (GeV)", 0, 1.0, 50, nh3_tree, c_tree, ch_tree, he_tree, empty_tree, c1, 2, fit_results);
-
-    // Plot for x-Feynman
-    plot_dilution_factor("xF", "x_{F} (GeV)", -0.8, 0.5, 50, nh3_tree, c_tree, ch_tree, he_tree, empty_tree, c1, 3, fit_results);
+    // Fit and plot for x-Feynman
+    auto fit_xF = fit_and_plot_dilution("xF", "x_{F} (GeV)", -0.8, 0.5, 50, nh3, c, ch, he, empty, c1, 3);
 
     // Save the canvas as a PNG file
-    c1->SaveAs("output/one_dimensional_placeholder.png");
+    c1->SaveAs("output/one_dimensional.png");
 
-    // Print the fit results in the desired format
-    std::cout << "\n\n";
-    if (fit_results.find("x") != fit_results.end()) {
-        std::cout << "if (prefix == \"x\") { return " << fit_results["x"] << "; }\n";
-    }
-    if (fit_results.find("pT") != fit_results.end()) {
-        std::cout << "if (prefix == \"pT\") { return " << fit_results["pT"] << "; }\n";
-    }
-    if (fit_results.find("xF") != fit_results.end()) {
-        std::cout << "if (prefix == \"xF\") { return " << fit_results["xF"] << "; }\n";
-    }
+    // Print the fit functions for each variable
+    std::cout << "Fit for x_Bjorken: " << fit_x.first->GetExpFormula("p") << std::endl;
+    std::cout << "Fit for P_T: " << fit_pT.first->GetExpFormula("p") << std::endl;
+    std::cout << "Fit for x_Feynman: " << fit_xF.first->GetExpFormula("p") << std::endl;
 
     // Clean up
     delete c1;
