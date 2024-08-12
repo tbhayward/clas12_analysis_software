e8e094913c1faf3503a1187c345451ae5c3bce80
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-12 15:26:58 -0400

    began writing epX_plots.cpp

--- analysis_scripts/misc/epX_plots.cpp
+++ analysis_scripts/misc/epX_plots.cpp
@@ -436,6 +436,13 @@ void plotQ2yz_pT(
                     graph->Draw("P SAME");
                 }
             }
+            
+            // Fix the right margin for the rightmost plots to ensure the vertical axis is shown
+            if (q2Index == Q2_prefixes[row].size() - 1) {
+                gPad->SetRightMargin(0.15);
+            } else {
+                gPad->SetRightMargin(0.01);  // Ensure other plots have minimal right margin
+            }
         }
     }
 
