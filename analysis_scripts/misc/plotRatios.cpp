07799bd915cfc7e7d7cc075a1d337f371bb6b784
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-12 20:22:33 -0400

    working on epX_plots

--- analysis_scripts/misc/plotRatios.cpp
+++ analysis_scripts/misc/plotRatios.cpp
@@ -53,7 +53,7 @@ void plotRatios(const char* file1, const char* file2, const char* file3, const c
     TH1F *h3_Mx = new TH1F("h3_Mx", "Mx", nbins, -3, 4);
     TH1F *h4_Mx = new TH1F("h4_Mx", "Mx", nbins, -3, 4);
 
-    int nbins2 = 75;
+    int nbins2 = 150;
     TH1F *h1_vz_p = new TH1F("h1_vz_p", "vz_p", nbins2, -8, 2);
     TH1F *h2_vz_p = new TH1F("h2_vz_p", "vz_p", nbins2, -8, 2);
     TH1F *h3_vz_p = new TH1F("h3_vz_p", "vz_p", nbins2, -8, 2);
@@ -212,8 +212,8 @@ void plotRatios(const char* file1, const char* file2, const char* file3, const c
 
         g1->Draw("AP");
         g2->Draw("P SAME");
-        // g1->GetYaxis()->SetRangeUser(0, 5); // Enforce the range again
-        g1->GetYaxis()->SetRangeUser(1, 2); // Enforce the range again
+        g1->GetYaxis()->SetRangeUser(0, 5); // Enforce the range again
+        // g1->GetYaxis()->SetRangeUser(0.5, 2); // Enforce the range again
         c->Update(); // Update the canvas after setting the range
 
         TLegend* legend = new TLegend(0.7, 0.8, 0.9, 0.9);
