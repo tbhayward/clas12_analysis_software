facc82e454e0869578caffed9d11327bef324815
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-14 09:23:06 -0400

    returning to calibration.cpp script

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3589,14 +3589,14 @@ int main(int argc, char** argv) {
     // dataReader.Restart();
     // if (mcReader) mcReader->Restart();
     
-    plot_ft_xy_energy(dataReader, mcReader);
-    dataReader.Restart();
-    if (mcReader) mcReader->Restart();
-    plot_ft_hit_position(dataReader, mcReader);
-
+    // plot_ft_xy_energy(dataReader, mcReader);
     // dataReader.Restart();
     // if (mcReader) mcReader->Restart();
-    // plot_cal_hit_position(dataReader, mcReader);
+    // plot_ft_hit_position(dataReader, mcReader);
+
+    dataReader.Restart();
+    if (mcReader) mcReader->Restart();
+    plot_cal_hit_position(dataReader, mcReader);
 
     // dataReader.Restart();
     // if (mcReader) mcReader->Restart();
