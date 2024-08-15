9a1b711b9885e174afa783dd2e5b33a4c712b3e5
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-15 16:05:09 -0400

    further features to calibration.cpp working on dc fiducial cuts

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3873,7 +3873,7 @@ void plot_cvt_hit_position(TTreeReader& dataReader, TTreeReader* mcReader = null
 
         dataReader.Restart();
         while (dataReader.Next()) {
-            if (*particle_pid == pid) {
+            if (*particle_pid == pid && *traj_x_12 != -9999 && *traj_y_12 != -9999 && *traj_z_12 != -9999 && ) {
                 double theta_CVT_value = calculate_theta(*traj_x_12, *traj_y_12, *traj_z_12);
                 double phi_CVT_value = calculate_phi(*traj_x_12, *traj_y_12);
                 theta_CVT_data.push_back(theta_CVT_value);
@@ -3884,7 +3884,7 @@ void plot_cvt_hit_position(TTreeReader& dataReader, TTreeReader* mcReader = null
         if (mcReader) {
             mcReader->Restart();
             while (mcReader->Next()) {
-                if (**mc_particle_pid == pid) {
+                if (**mc_particle_pid == pid && *mc_traj_x_12 != -9999 && *mc_traj_y_12 != -9999 && *mc_traj_z_12 != -9999 && ) {
                     double mc_theta_CVT_value = calculate_theta(**mc_traj_x_12, **mc_traj_y_12, **mc_traj_z_12);
                     double mc_phi_CVT_value = calculate_phi(**mc_traj_x_12, **mc_traj_y_12);
                     theta_CVT_mc.push_back(mc_theta_CVT_value);
@@ -3894,72 +3894,7 @@ void plot_cvt_hit_position(TTreeReader& dataReader, TTreeReader* mcReader = null
         }
 
         // Create and fill histograms for theta_CVT vs theta and theta_CVT vs phi_CVT
-        TH2D* h_theta_vs_theta_data_before = new TH2D("h_theta_vs_theta_data_before", ("#theta_{CVT} vs #theta Before Cuts (Data, " + particle_name + ")").c_str(), nBins, 0, 180, nBins, 0, 180);
-        h_theta_vs_theta_data_before->GetXaxis()->SetTitle("#theta");
-        h_theta_vs_theta_data_before->GetYaxis()->SetTitle("#theta_{CVT}");
-
-        TH2D* h_phi_vs_theta_CVT_data_before = new TH2D("h_phi_vs_theta_CVT_data_before", ("#phi_{CVT} vs #theta_{CVT} Before Cuts (Data, " + particle_name + ")").c_str(), nBins, 0, 360, nBins, 0, 180);
-        h_phi_vs_theta_CVT_data_before->GetXaxis()->SetTitle("#phi_{CVT}");
-        h_phi_vs_theta_CVT_data_before->GetYaxis()->SetTitle("#theta_{CVT}");
-
-        TH2D* h_theta_vs_theta_data_after = new TH2D("h_theta_vs_theta_data_after", ("#theta_{CVT} vs #theta After Cuts (Data, " + particle_name + ")").c_str(), nBins, 0, 180, nBins, 0, 180);
-        h_theta_vs_theta_data_after->GetXaxis()->SetTitle("#theta");
-        h_theta_vs_theta_data_after->GetYaxis()->SetTitle("#theta_{CVT}");
-
-        TH2D* h_phi_vs_theta_CVT_data_after = new TH2D("h_phi_vs_theta_CVT_data_after", ("#phi_{CVT} vs #theta_{CVT} After Cuts (Data, " + particle_name + ")").c_str(), nBins, 0, 360, nBins, 0, 180);
-        h_phi_vs_theta_CVT_data_after->GetXaxis()->SetTitle("#phi_{CVT}");
-        h_phi_vs_theta_CVT_data_after->GetYaxis()->SetTitle("#theta_{CVT}");
-
-        dataReader.Restart();
-        while (dataReader.Next()) {
-            if (*particle_pid == pid) {
-                double theta_CVT_value = calculate_theta(*traj_x_12, *traj_y_12, *traj_z_12);
-                double phi_CVT_value = calculate_phi(*traj_x_12, *traj_y_12);
-                h_theta_vs_theta_data_before->Fill(*theta, theta_CVT_value);
-                h_phi_vs_theta_CVT_data_before->Fill(phi_CVT_value, theta_CVT_value);
-                if (cvt_fiducial(*traj_edge_1, *traj_edge_3, *traj_edge_5, *traj_edge_7, *traj_edge_12, pid)) {
-                    h_theta_vs_theta_data_after->Fill(*theta, theta_CVT_value);
-                    h_phi_vs_theta_CVT_data_after->Fill(phi_CVT_value, theta_CVT_value);
-                }
-            }
-        }
-
-        TH2D* h_theta_vs_theta_mc_before = nullptr;
-        TH2D* h_phi_vs_theta_CVT_mc_before = nullptr;
-        TH2D* h_theta_vs_theta_mc_after = nullptr;
-        TH2D* h_phi_vs_theta_CVT_mc_after = nullptr;
-
-        if (mcReader) {
-            h_theta_vs_theta_mc_before = new TH2D("h_theta_vs_theta_mc_before", ("#theta_{CVT} vs #theta Before Cuts (MC, " + particle_name + ")").c_str(), nBins, 0, 180, nBins, 0, 180);
-            h_theta_vs_theta_mc_before->GetXaxis()->SetTitle("#theta");
-            h_theta_vs_theta_mc_before->GetYaxis()->SetTitle("#theta_{CVT}");
-
-            h_phi_vs_theta_CVT_mc_before = new TH2D("h_phi_vs_theta_CVT_mc_before", ("#phi_{CVT} vs #theta_{CVT} Before Cuts (MC, " + particle_name + ")").c_str(), nBins, 0, 360, nBins, 0, 180);
-            h_phi_vs_theta_CVT_mc_before->GetXaxis()->SetTitle("#phi_{CVT}");
-            h_phi_vs_theta_CVT_mc_before->GetYaxis()->SetTitle("#theta_{CVT}");
-
-            h_theta_vs_theta_mc_after = new TH2D("h_theta_vs_theta_mc_after", ("#theta_{CVT} vs #theta After Cuts (MC, " + particle_name + ")").c_str(), nBins, 0, 180, nBins, 0, 180);
-            h_theta_vs_theta_mc_after->GetXaxis()->SetTitle("#theta");
-            h_theta_vs_theta_mc_after->GetYaxis()->SetTitle("#theta_{CVT}");
-
-            h_phi_vs_theta_CVT_mc_after = new TH2D("h_phi_vs_theta_CVT_mc_after", ("#phi_{CVT} vs #theta_{CVT} After Cuts (MC, " + particle_name + ")").c_str(), nBins, 0, 360, nBins, 0, 180);
-            h_phi_vs_theta_CVT_mc_after->GetXaxis()->SetTitle("#phi_{CVT}");
-            h_phi_vs_theta_CVT_mc_after->GetYaxis()->SetTitle("#theta_{CVT}");
-
-            mcReader->Restart();
-            while (mcReader->Next()) {
-                if (**mc_particle_pid == pid) {
-                    double theta_CVT_value = calculate_theta(**mc_traj_x_12, **mc_traj_y_12, **mc_traj_z_12);
-                    double phi_CVT_value = calculate_phi(**mc_traj_x_12, **mc_traj_y_12);
-                    h_theta_vs_theta_mc_before->Fill(**mc_theta, theta_CVT_value);
-                    h_phi_vs_theta_CVT_mc_before->Fill(phi_CVT_value, theta_CVT_value);
-                    if (cvt_fiducial(**mc_traj_edge_1, **mc_traj_edge_3, **mc_traj_edge_5, **mc_traj_edge_7, **mc_traj_edge_12, pid)) {
-                        h_theta_vs_theta_mc_after->Fill(**mc_theta, theta_CVT_value);
-                        h_phi_vs_theta_CVT_mc_after->Fill(phi_CVT_value, theta_CVT_value);
-                    }
-                }
-            }
-        }
+        TH2D* h_theta 
 
         // Create canvases for theta_CVT vs theta and phi_CVT vs theta_CVT plots (data and MC)
         TCanvas* c_theta_vs_theta_data = new TCanvas(("c_theta_vs_theta_data_" + particle_name).c_str(), ("#theta_{CVT} vs #theta and #phi_{CVT} vs #theta_{CVT} (Data, " + particle_name + ")").c_str(), 1200, 1200);
