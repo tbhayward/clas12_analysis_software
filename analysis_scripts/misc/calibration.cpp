b02a3d272860e4707e42c6e3b5598667dd578e1e
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-15 12:15:24 -0400

    further features to calibration.cpp working on dc fiducial cuts

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -2910,11 +2910,11 @@ void dc_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader = 
 
     // Array of particle types (photons and electrons) and their corresponding PIDs
     std::vector<std::tuple<int, std::string>> particle_types = {
-        {11, "electron"},
-        {-211, "pim"},
-        {211, "pip"},
-        {321, "kp"},
-        {-321, "km"},
+        // {11, "electron"},
+        // {-211, "pim"},
+        // {211, "pip"},
+        // {321, "kp"},
+        // {-321, "km"},
         {2212, "proton"}
     };
 
