4f619fca75535bc6644b179bd29fc38944af2aca
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-16 08:56:19 -0400

    further features to calibration.cpp working on dc fiducial cuts

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3795,7 +3795,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
         // Fill data histograms
         dataReader.Restart();
         while (dataReader.Next()) {
-            if (*particle_pid == pid && *track_ndf_5 > 0 && *track_chi2_5 < 1000) {
+            if (*particle_pid == pid && *track_ndf_5 > 0 && *track_chi2_5 < 10000) {
                 double chi2_ndf = *track_chi2_5 / *track_ndf_5;
 
                 for (size_t i = 0; i < layers.size(); ++i) {
@@ -3813,7 +3813,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
         if (mcReader) {
             mcReader->Restart();
             while (mcReader->Next()) {
-                if (**mc_particle_pid == pid && **mc_track_ndf_5 > 0 && **mc_track_chi2_5 < 1000) {
+                if (**mc_particle_pid == pid && **mc_track_ndf_5 > 0 && **mc_track_chi2_5 < 10000) {
                     double mc_chi2_ndf = **mc_track_chi2_5 / **mc_track_ndf_5;
 
                     for (size_t i = 0; i < mc_layers.size(); ++i) {
@@ -3870,7 +3870,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
             c_layer_data->cd(i + 1);
             h_median_chi2_ndf_data[i]->SetStats(false);
             h_median_chi2_ndf_data[i]->SetLineColor(kBlack);
-            h_median_chi2_ndf_data[i]->Draw("HIST");
+            h_median_chi2_ndf_data[i]->Draw("P");
         }
 
         c_layer_data->SaveAs(("output/calibration/cvt/determination/median_chi2_per_ndf_vs_edge_data_" + particle_name + ".png").c_str());
@@ -3883,7 +3883,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
                 c_layer_mc->cd(i + 1);
                 h_median_chi2_ndf_mc[i]->SetStats(false);
                 h_median_chi2_ndf_mc[i]->SetLineColor(kRed);
-                h_median_chi2_ndf_mc[i]->Draw("HIST");
+                h_median_chi2_ndf_mc[i]->Draw("P");
             }
 
             c_layer_mc->SaveAs(("output/calibration/cvt/determination/median_chi2_per_ndf_vs_edge_mc_" + particle_name + ".png").c_str());
