ca4327d561888eaae01d86ecd0076a0d58324971
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-16 09:07:29 -0400

    further features to calibration.cpp working on dc fiducial cuts

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -3870,6 +3870,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
             c_layer_data->cd(i + 1);
             h_median_chi2_ndf_data[i]->SetStats(false);
             h_median_chi2_ndf_data[i]->SetLineColor(kBlack);
+            h_median_chi2_ndf_data[i]->SetMarkerSize(1.5);
             h_median_chi2_ndf_data[i]->Draw("P0");
         }
 
@@ -3883,6 +3884,7 @@ void cvt_fiducial_determination(TTreeReader& dataReader, TTreeReader* mcReader =
                 c_layer_mc->cd(i + 1);
                 h_median_chi2_ndf_mc[i]->SetStats(false);
                 h_median_chi2_ndf_mc[i]->SetLineColor(kRed);
+                h_median_chi2_ndf_mc[i]->SetMarkerSize(1.5);
                 h_median_chi2_ndf_mc[i]->Draw("P0 SAME");
             }
 
