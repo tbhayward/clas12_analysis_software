788b772b5828e19052842e0337afecd00e45266d
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-19 18:09:56 -0400

    set up runnum epX check

--- processing_classes/src/analyzers/Hadron.java
+++ processing_classes/src/analyzers/Hadron.java
@@ -57,7 +57,7 @@ public class Hadron {
         if (variables.helicity == 0 && variables.runnum != 11) {
             return false;
         }
-        if (variables.runnum == 16213 || variables.runnum == 16317 || variables.runnum == 16742 || variables.runnum == 16306) {
+        if (variables.runnum == 16213 || variables.runnum == 16234 || variables.runnum == 16317 || variables.runnum == 16742 || variables.runnum == 16753 || variables.runnum == 16306) {
             return false;
         }
         if (variables.runnum >= 16658 && variables.runnum <= 16695) {
