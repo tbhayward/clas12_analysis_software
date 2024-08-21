7b40cb2042238936bb6fbbffa83a58924f8c10e8
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-20 21:05:30 -0400

    set up runnum epX check

--- analysis_scripts/misc/trim_multidimensional_epX.cpp
+++ analysis_scripts/misc/trim_multidimensional_epX.cpp
@@ -159,7 +159,7 @@ void process_file(const char* input_filename) {
             *vz_e + *p_p + *p_theta + *p_phi + *vz_p + *Q2 + *W + *Mx + 
             *Mx2 + *x + *y + *t + *tmin + *z + *xF + *pT + *zeta + *eta + *phi + *DepA +
             *DepB + *DepC + *DepV + *DepW;
-        if (*Mx > 0 && *vz_e > -10  && *vz_e < 1 && *vz_p > -10 && *vz_p < 1) {
+        if (*Mx < 0 && *vz_e < -10  && *vz_e > 1 && *vz_p < -10 && *vz_p > 1) {
             continue;
         }
         if (*Mx > 1.35) {
