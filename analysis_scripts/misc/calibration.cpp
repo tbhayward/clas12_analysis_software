40fcf40206c548f40dcf3466d1f24d15efc5c10c
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-22 10:55:40 -0400

    began energy loss calculations in calibration.cpp

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -4418,6 +4418,7 @@ bool is_cd_track(double track_sector_6) {
 
 // Function to create energy loss distributions
 void energy_loss_distributions(TTreeReader& mcReader) {
+    std::cout << "HELLO WORLD" << std::endl;
     // Define 2D histograms for FD and CD
     int nBinsX = 50, nBinsY = 50;
     double xMin = 0, xMax = 2;
