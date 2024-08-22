d9608498d0cc420983a89e72c2f2188ab5118f86
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-22 10:37:35 -0400

    began energy loss calculations in calibration.cpp

--- processing_scripts/convert_txt_to_root.cpp
+++ processing_scripts/convert_txt_to_root.cpp
@@ -1141,7 +1141,6 @@ int main(int argc, char *argv[]) {
 
     // calibration script
     if (hadron_count == 5 && is_mc == 0) {
-        std::cout << mc_matching_pid << " " << mc_parent_pid << std::endl;
         while (infile >> config_run >> config_event >> event_helicity >> particle_pid >>
                 particle_px >> particle_py >> particle_pz >> p >> theta >> phi >> 
                 particle_vx >> particle_vy >> particle_vz >> particle_beta >> 
@@ -1158,7 +1157,7 @@ int main(int argc, char *argv[]) {
                 traj_z_5 >> traj_edge_5 >> traj_x_7 >> traj_y_7 >> traj_z_7 >> traj_edge_7 >> 
                 traj_x_12 >> traj_y_12 >> traj_z_12 >> traj_edge_12 >> ft_energy >> ft_x >> 
                 ft_y >> ft_z >> ft_radius) {
-
+            std::cout << mc_matching_pid << " " << mc_parent_pid << std::endl;
             tree->Fill(); // Fill the tree with the read data
         }
     }
