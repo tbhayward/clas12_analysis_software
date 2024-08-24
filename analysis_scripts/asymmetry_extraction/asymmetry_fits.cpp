998910abf53b2f1b6b1ee13bf50dc377e16912db
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-24 16:12:29 -0400

    created calculate_dilution_factors.cpp in the asymmetry_extraction program to calculate dilution factors on the fly based on our input bins

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
@@ -277,6 +277,7 @@ double asymmetry_value_calculation(double currentVariable, const std::pair<doubl
   int asymmetry_index) {
   // double Df = dilution_factor(Q2, xB, z, pT, prefix); // dilution factor
   double Df = dilutionFactor.first;
+  Df = 1;
   // return the asymmetry value 
   switch (asymmetry_index) {
     case 0: // beam-spin asymmetry
