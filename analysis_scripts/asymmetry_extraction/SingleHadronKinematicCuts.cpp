08c6d592cff475a880d9361db36cc0a6105c6150
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-26 10:41:06 -0400

    began setting up new Q2x multidimensional plots in epX_plots

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -39,12 +39,12 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
     if (property == "Q2multi1") {
       goodEvent = *vz_e > -10 && *vz_e < 1 && *vz_p > -10 && *vz_p < 1 && *Q2 > 1 && *W > 2 && *Mx > 1.35 && *y < 0.75;
       goodEvent = goodEvent && *x > 0.06 && *x < 0.60 && *pT > 0 && *pT < 1.2 && *xF > -1 && *xF < 1;
-      goodEvent = goodEvent && *x > 0.12 && *x < 0.22;
+      // goodEvent = goodEvent && *x > 0.12 && *x < 0.22;
     }
     if (property == "Q2multi2") {
       goodEvent = *vz_e > -10 && *vz_e < 1 && *vz_p > -10 && *vz_p < 1 && *Q2 > 1 && *W > 2 && *Mx > 1.35 && *y < 0.75;
       goodEvent = goodEvent && *x > 0.06 && *x < 0.60 && *pT > 0 && *pT < 1.2 && *xF > -1 && *xF < 1;
-      goodEvent = goodEvent && *x > 0.22 && *x < 0.32;
+      // goodEvent = goodEvent && *x > 0.22 && *x < 0.32;
     }
 
     if (*Q2 > 1 && *W > 2 && *Mx > 1.35 && *y < 0.75 && !checked) {
