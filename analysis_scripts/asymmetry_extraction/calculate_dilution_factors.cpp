cd89f8172e349d42efb0634a56492b7f6c714f79
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-27 17:30:16 -0400

    switched fit label names to ccdb and sqlite for comparison for RGC Fa22, changed bins

--- analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
+++ analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
@@ -38,29 +38,71 @@ const double xCH_split = 0.055;
 const double xHe_split = 0.115;
 const double xf_split = 0.140;
 
-double calculate_dilution_error(double nA, double nC, double nCH, double nMT, double nf) {
-    double term1 = 0.734694 * nA * nC * pow((nA - nMT), 2) * 
-                   pow((- 0.554976 * nCH - 0.0373109 * nf + 0.592287 * nMT), 2);
+// double calculate_dilution_error(double nA, double nC, double nCH, double nMT, double nf) {
+//     double term1 = 0.734694 * nA * nC * pow((nA - nMT), 2) * 
+//                    pow((- 0.554976 * nCH - 0.0373109 * nf + 0.592287 * nMT), 2);
 
-    double term2 = 0.456108 * nA * pow((nA - nMT), 2) * 
-                   pow((0.704358 * nC + 0.295642 * nf - nMT), 2) * nMT;
+//     double term2 = 0.456108 * nA * pow((nA - nMT), 2) * 
+//                    pow((0.704358 * nC + 0.295642 * nf - nMT), 2) * nMT;
 
-    double term3 = 0.0281176 * nA * nf * pow((nA - nMT), 2) * 
-                   pow((0.190722 * nC - 1.19072 * nCH + nMT), 2);
+//     double term3 = 0.0281176 * nA * nf * pow((nA - nMT), 2) * 
+//                    pow((0.190722 * nC - 1.19072 * nCH + nMT), 2);
 
-    double term4 = 0.0135714 * pow((nC - 1.16667 * nCH + 0.341297 * nf - 0.17463 * nMT), 2) * 
-                   pow(nMT, 2) * pow((nC - 5.25 * nCH + 0.0667755 * nf + 4.18322 * nMT), 2);
+//     double term4 = 0.0135714 * pow((nC - 1.16667 * nCH + 0.341297 * nf - 0.17463 * nMT), 2) * 
+//                    pow(nMT, 2) * pow((nC - 5.25 * nCH + 0.0667755 * nf + 4.18322 * nMT), 2);
 
-    double term5 = 0.022405 * nA * nMT * 
-                   pow((0.778288 * pow(nC, 2) + 4.76701 * pow(nCH, 2) - 1.45518 * nCH * nf + 0.0177374 * pow(nf, 2) + 
-                        nC * (-4.99401 * nCH + 0.317598 * nf - 0.271825 * nMT) + 
-                        nA * (3.39167 * nC - 4.51192 * nCH + 1.12025 * nf) + 
-                        1.42708 * nCH * nMT - 0.0181513 * nf * nMT - 0.568553 * pow(nMT, 2)), 2);
+//     double term5 = 0.022405 * nA * nMT * 
+//                    pow((0.778288 * pow(nC, 2) + 4.76701 * pow(nCH, 2) - 1.45518 * nCH * nf + 0.0177374 * pow(nf, 2) + 
+//                         nC * (-4.99401 * nCH + 0.317598 * nf - 0.271825 * nMT) + 
+//                         nA * (3.39167 * nC - 4.51192 * nCH + 1.12025 * nf) + 
+//                         1.42708 * nCH * nMT - 0.0181513 * nf * nMT - 0.568553 * pow(nMT, 2)), 2);
 
-    double denominator = pow(nA, 3) * 
-                         pow((0.135913 * nC - 0.713541 * nCH + 0.00907563 * nf + 0.568553 * nMT), 4);
+//     double denominator = pow(nA, 3) * 
+//                          pow((0.135913 * nC - 0.713541 * nCH + 0.00907563 * nf + 0.568553 * nMT), 4);
 
-    double sigma_df = 0.713541 * sqrt((term1 + term2 + term3 + term4 + term5 )/ denominator);
+//     double sigma_df = 0.713541 * sqrt((term1 + term2 + term3 + term4 + term5 )/ denominator);
+
+//     return sigma_df;
+// }
+
+double calculate_dilution_error(double nA, double nC, double nCH, double nMT, double nf, 
+                                double xA, double xC, double xCH, double xHe, double xf) {
+    // First part of the expression
+    double term1 = 3988.9 * nA * nf * pow(xC, 2) * pow(xCH, 2) * pow(xf, 2) * pow(xHe, 2) *
+                  pow((-1.0 * nMT * xA + nA * xHe), 2) *
+                  pow((1.0 * nMT * xC * xCH - 1.19072 * nCH * xC * xf + 0.190722 * nC * xCH * xHe), 2);
+
+    // Second part of the expression
+    double term2 = 64705.7 * nA * nCH * pow(xC, 2) * pow(xCH, 2) * pow(xf, 2) * pow(xHe, 2) *
+                  pow((-1.0 * nMT * xA + nA * xHe), 2) *
+                  pow((1.0 * nMT * xC * xf - 0.295642 * nf * xC * xHe - 0.704359 * nC * xf * xHe), 2);
+
+    // Third part of the expression
+    double term3 = 8.5849 * nA * nC * pow(xCH, 2) * pow(xf, 2) * pow(xHe, 2) *
+                  pow((-1.0 * nMT * xA + nA * xHe), 2) *
+                  pow((65.2613 * nMT * xC * xCH * xf + (-4.11113 * nf * xC * xCH - 61.1502 * nCH * xC * xf) * xHe), 2);
+
+    // Fourth part of the expression
+    double term4 = 1027.46 * pow(nMT, 2) * pow(xA, 2) *
+                  pow((1.0 * nMT * xC * xCH * xf + (-1.9544 * nf * xC * xCH + 6.68077 * nCH * xC * xf - 5.72638 * nC * xCH * xf) * xHe), 2) *
+                  pow((1.0 * nMT * xC * xCH * xf + (0.0159627 * nf * xC * xCH - 1.25501 * nCH * xC * xf + 0.239051 * nC * xCH * xf) * xHe), 2);
+
+    // Fifth part of the expression
+    double term5 = 0.261803 * nA * nMT *
+                  pow((62.6461 * pow(nMT, 2) * xA * pow(xC, 2) * pow(xCH, 2) * pow(xf, 2) +
+                       nMT * xA * xC * xCH * xf * (2.0 * nf * xC * xCH - 157.243 * nCH * xC * xf + 29.9512 * nC * xCH * xf) * xHe +
+                       (-1.9544 * pow(nf, 2) * xA * pow(xC, 2) * pow(xCH, 2) +
+                        nf * xC * xCH * (160.339 * nCH * xA * xC - 34.9946 * nC * xA * xCH - 123.435 * nA * xC * xCH) * xf +
+                        (-525.254 * pow(nCH, 2) * xA * pow(xC, 2) +
+                         nCH * xC * (550.266 * nC * xA + 497.146 * nA * xC) * xCH +
+                         nC * (-85.756 * nC * xA - 373.711 * nA * xC) * pow(xCH, 2)) * xf)) *
+                      pow(xHe, 2),2);
+
+    // Denominator of the expression
+    double denominator = pow(nA, 3) * pow(xHe, 2) * pow((62.6461 * nMT * xC * xCH * xf + 1.0 * nf * xC * xCH * xHe - 78.6217 * nCH * xC * xf * xHe + 14.9756 * nC * xCH * xf * xHe), 4);
+
+    // Final error calculation
+    double sigma_df = 23.0 * sqrt((term1 + term2 + term3 + term4 + term5) / denominator);
 
     return sigma_df;
 }
