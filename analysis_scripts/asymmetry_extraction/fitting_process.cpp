0e6e7422859115fa9d1af531510203ee97a013b1
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-27 16:50:52 -0400

    switched fit label names to ccdb and sqlite for comparison for RGC Fa22, changed bins

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -805,7 +805,6 @@ TH1D* createHistogramForBin_single_hadron(const char* histName, int binIndex,
     if (*currentVariable >= varMin && *currentVariable < varMax && passedKinematicCuts) {
       sumVariable += *currentVariable;
 
-      *target_pol = 0;
       if (*helicity > 0 && *target_pol < 0) { histPosNeg->Fill(*phi); } 
       else if (*helicity < 0 && *target_pol > 0) {  histNegPos->Fill(*phi); }
 
