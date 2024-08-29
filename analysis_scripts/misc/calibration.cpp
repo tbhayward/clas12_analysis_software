f6322f4f136bd6c94c89e039a9fb06b0a146a6b8
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-29 15:35:37 -0400

    working on calibration scripts

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -6450,7 +6450,7 @@ void plot_energy_loss_corrections_fd(TTreeReader& mcReader, const std::string& d
 
 // Main function to call both energy loss distribution functions
 void energy_loss(TTreeReader& mcReader, const std::string& dataset) {
-    energy_loss_distributions(mcReader, dataset);
+    // energy_loss_distributions(mcReader, dataset);
 
     // mcReader.Restart();
     // energy_loss_fd_distributions(mcReader, dataset);
@@ -6470,8 +6470,8 @@ void energy_loss(TTreeReader& mcReader, const std::string& dataset) {
     // mcReader.Restart();
     // plot_energy_loss_corrections_fd(mcReader, dataset);
 
-    // mcReader.Restart();
-    // energy_loss_distributions_delta_p_cd(mcReader, dataset);
+    mcReader.Restart();
+    energy_loss_distributions_delta_p_cd(mcReader, dataset);
 }
                            
 void create_directories() {
