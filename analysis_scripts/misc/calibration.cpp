fdc4eafd915ed9174f1862add93c55e18e1ff5bc
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-28 22:59:57 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -5108,8 +5108,8 @@ void energy_loss_distributions_delta_p(TTreeReader& mcReader, const std::string&
     //     mcReader.Next();
     while (mcReader.Next()) {
         double delta_p = *mc_p - *p;
-        std::cout << *traj_x_6 << std::endl;
         double theta_dc_1 = calculate_theta(*traj_x_6, *traj_y_6, *traj_z_6);
+        std::cout << theta_dc_1 << " " << *traj_x_6 << std::endl;
 
         // Check if the current particle type is one of interest and if the track is below the curve
         // if (histograms.find(*pid) != histograms.end() && !is_above_theta_dc_curve(*p, theta_dc_1)) {
