85b897751ac6b272d5c3d2c89798c91191d3a768
Timothy Hayward <tbhayward@email.wm.edu>
2024-08-31 16:10:28 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -6655,7 +6655,7 @@ void plot_energy_loss_corrections_fd(TTreeReader& mcReader, const std::string& d
 
 // Main function to call both energy loss distribution functions
 void energy_loss(TTreeReader& mcReader, const std::string& dataset) {
-    // energy_loss_distributions(mcReader, dataset);
+    energy_loss_distributions(mcReader, dataset);
 
     // mcReader.Restart();
     // energy_loss_fd_distributions(mcReader, dataset);
@@ -6663,14 +6663,14 @@ void energy_loss(TTreeReader& mcReader, const std::string& dataset) {
     // mcReader.Restart();
     // energy_loss_fd_distributions_theta_dc(mcReader, dataset);
 
-    // mcReader.Restart();
-    // energy_loss_distributions_delta_p_fd(mcReader, dataset);
+    mcReader.Restart();
+    energy_loss_distributions_delta_p_fd(mcReader, dataset);
 
-    // mcReader.Restart();
-    // energy_loss_distributions_delta_theta_fd(mcReader, dataset);
+    mcReader.Restart();
+    energy_loss_distributions_delta_theta_fd(mcReader, dataset);
 
-    // mcReader.Restart();
-    // energy_loss_distributions_delta_phi_fd(mcReader, dataset);
+    mcReader.Restart();
+    energy_loss_distributions_delta_phi_fd(mcReader, dataset);
 
     mcReader.Restart();
     plot_energy_loss_corrections_fd(mcReader, dataset);
@@ -6803,8 +6803,8 @@ int main(int argc, char** argv) {
 
     dataReader.Restart();
     if (mcReader) mcReader->Restart();
-    // if (mcReader) energy_loss(*mcReader, "rga_fa18_inb"); 
-    if (mcReader) energy_loss(*mcReader, "rga_fa18_out");  
+    if (mcReader) energy_loss(*mcReader, "rga_fa18_inb"); 
+    // if (mcReader) energy_loss(*mcReader, "rga_fa18_out");  
     // if (mcReader) energy_loss(*mcReader, "rga_sp19_inb"); 
     // if (mcReader) energy_loss(*mcReader, "rgc_su22_inb");   
 
