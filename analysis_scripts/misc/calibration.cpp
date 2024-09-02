7622858807d5288a78ad28fdcd03a8cffe48fedd
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-01 20:25:35 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -6011,7 +6011,7 @@ void energy_loss_distributions_delta_p_cd(TTreeReader& mcReader, const std::stri
             prof_deltap->GetXaxis()->SetRangeUser(minXValue, maxXValue);
 
             // Fit the profiles with appropriate functions
-            fit_deltap[i] = new TF1(("fit_deltap_" + std::to_string(i)).c_str(), "[0] + [1]*x + [2]*x^2", minXValue, maxXValue);
+            fit_deltap[i] = new TF1(("fit_deltap_" + std::to_string(i)).c_str(), "[0] + [1]/x + [2]/x^2", minXValue, maxXValue);
 
             prof_deltap->Fit(fit_deltap[i], "Q"); // Silent fit
 
