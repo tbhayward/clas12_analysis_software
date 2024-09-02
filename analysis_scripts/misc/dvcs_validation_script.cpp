1b4403f23d03a0f2b557144cd97f186a567fff58
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-02 10:40:20 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/dvcs_validation_script.cpp
+++ analysis_scripts/misc/dvcs_validation_script.cpp
@@ -208,7 +208,7 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
     legend12->Draw();
 
     // Save the canvas as a PDF
-    c1->SaveAs("output/dvcs_energy_loss_validation.pdf");
+    c1->SaveAs("output/dvcs_"+titleSuffix+"_energy_loss_validation.pdf");
 
     // Clean up
     delete c1;
