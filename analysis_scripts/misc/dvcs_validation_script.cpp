c07dbd18d9557884cc601aafc41f470ea12ed8bd
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-04 14:23:23 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/dvcs_validation_script.cpp
+++ analysis_scripts/misc/dvcs_validation_script.cpp
@@ -166,8 +166,10 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
     h2[0]->SetStats(0);
     h2[0]->Draw("E SAME");
 
-    TF1 *fit1_int = new TF1("fit1_integrated", "gaus(0) + pol1(3)", -0.3, 0.3);
-    TF1 *fit2_int = new TF1("fit2_integrated", "gaus(0) + pol1(3)", -0.3, 0.3);
+    // TF1 *fit1_int = new TF1("fit1_integrated", "gaus(0) + pol1(3)", -0.3, 0.3);
+    // TF1 *fit2_int = new TF1("fit2_integrated", "gaus(0) + pol1(3)", -0.3, 0.3);
+    TF1 *fit1_int = new TF1("fit1_integrated", "gaus(0)", -0.3, 0.3);
+    TF1 *fit2_int = new TF1("fit2_integrated", "gaus(0)", -0.3, 0.3);
 
     fit1_int->SetParameters(0.8 * maxVal1, 0, 0.2);
     fit1_int->SetParLimits(1, -0.15, 0.15);
@@ -220,8 +222,10 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
         h2[i]->SetStats(0);
         h2[i]->Draw("E SAME");
 
-        TF1 *fit1 = new TF1(Form("fit1_%d", i), "gaus(0) + pol1(3)", -0.3, 0.3);
-        TF1 *fit2 = new TF1(Form("fit2_%d", i), "gaus(0) + pol1(3)", -0.3, 0.3);
+        // TF1 *fit1 = new TF1(Form("fit1_%d", i), "gaus(0) + pol1(3)", -0.3, 0.3);
+        // TF1 *fit2 = new TF1(Form("fit2_%d", i), "gaus(0) + pol1(3)", -0.3, 0.3);
+        TF1 *fit1 = new TF1(Form("fit1_%d", i), "gaus(0)", -0.3, 0.3);
+        TF1 *fit2 = new TF1(Form("fit2_%d", i), "gaus(0)", -0.3, 0.3);
 
         fit1->SetParameters(0.8 * maxVal1, 0, 0.2);
         fit1->SetParLimits(1, -0.15, 0.15);
