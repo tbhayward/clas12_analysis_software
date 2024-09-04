add5ee60a896d6e6eaa15baf59dc6118651537f7
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-04 15:01:14 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/dvcs_validation_script.cpp
+++ analysis_scripts/misc/dvcs_validation_script.cpp
@@ -304,7 +304,6 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
     delete f2;
 } 
 
-
 void plot_rho0_energy_loss_validation(const char* file1, const char* file2, const char* titleSuffix) {
     // Open the ROOT files
     TFile *f1 = new TFile(file1);
@@ -535,12 +534,12 @@ void plot_elastic_energy_loss_validation(const char* file1, const char* file2, c
     Double_t theta_mean[nBins] = {0.0};
 
     // Create histograms for each theta bin with 50 bins
-    h1[0] = new TH1D("h1_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 100, -0.1, 0.1);
-    h2[0] = new TH1D("h2_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 100, -0.1, 0.1);
+    h1[0] = new TH1D("h1_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 100, -0.1, 1.1);
+    h2[0] = new TH1D("h2_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 100, -0.1, 1.1);
 
     for (int i = 0; i < nBins; ++i) {
-        h1[i + 1] = new TH1D(Form("h1_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 100, -0.1, 0.1);
-        h2[i + 1] = new TH1D(Form("h2_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 100, -0.1, 0.1);
+        h1[i + 1] = new TH1D(Form("h1_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 100, -0.1, 1.1);
+        h2[i + 1] = new TH1D(Form("h2_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 100, -0.1, 1.1);
     }
 
     // Fill the histograms and calculate theta means
