6fe1c151e5eaa3b54fca718fb2e41aec38ec011f
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-04 13:52:34 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/dvcs_validation_script.cpp
+++ analysis_scripts/misc/dvcs_validation_script.cpp
@@ -79,7 +79,7 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
         Double_t thetaDeg1 = p1_theta1 * (180.0 / TMath::Pi()); // Convert to degrees
         if (thetaDeg1 >= 5 && thetaDeg1 < 65 
             && eta2_1 < 0 
-            // && t1_1 > -2 
+            && t1_1 > -2 
             // && theta_gamma_gamma_1 < 0.6 
             // && Emiss2_1 < 0.5 && pTmiss_1 < 0.125
                 ) {
@@ -88,7 +88,7 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
         for (int i = 0; i < nBins; ++i) {
             if (thetaDeg1 >= thetaBins[i] && thetaDeg1 < thetaBins[i + 1] 
                 && eta2_1 < 0 
-                // && t1_1 > -2 
+                && t1_1 > -2 
                 // && theta_gamma_gamma_1 < 0.6 
                 // && Emiss2_1 < 0.5 && pTmiss_1 < 0.125
                 ) {
@@ -105,7 +105,7 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
         Double_t thetaDeg2 = p1_theta2 * (180.0 / TMath::Pi()); // Convert to degrees
         if (thetaDeg2 >= 5 && thetaDeg2 < 65 
             && eta2_2 < 0 
-            // && t1_2 > -2 
+            && t1_2 > -2 
             // && theta_gamma_gamma_2 < 0.6 
             // && Emiss2_2 < 0.5 && pTmiss_2 < 0.125
                 ) {
@@ -114,7 +114,7 @@ void plot_dvcs_energy_loss_validation(const char* file1, const char* file2, cons
         for (int i = 0; i < nBins; ++i) {
             if (thetaDeg2 >= thetaBins[i] && thetaDeg2 < thetaBins[i + 1] 
                 && eta2_2 < 0 
-                // && t1_2 > -2 
+                && t1_2 > -2 
                 // && theta_gamma_gamma_2 < 0.6 
                 // && Emiss2_2 < 0.5 && pTmiss_2 < 0.125
                 ) {
