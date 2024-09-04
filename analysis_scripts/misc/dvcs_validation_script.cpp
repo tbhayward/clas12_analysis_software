84f587bdd6c76ed8fd990f4195ca32b36c8dba52
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-04 16:21:32 -0400

    resume working on calibration.cpp

--- analysis_scripts/misc/dvcs_validation_script.cpp
+++ analysis_scripts/misc/dvcs_validation_script.cpp
@@ -538,12 +538,12 @@ void plot_elastic_energy_loss_validation(const char* file1, const char* file2, c
     Double_t theta_mean[nBins] = {0.0};
 
     // Create histograms for each theta bin with 50 bins
-    h1[0] = new TH1D("h1_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 25, -0.005, 0.01);
-    h2[0] = new TH1D("h2_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 25, -0.005, 0.01);
+    h1[0] = new TH1D("h1_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 25, -0.0075, 0.0075);
+    h2[0] = new TH1D("h2_integrated", Form("Integrated #theta [5, 65] %s", titleSuffix), 25, -0.0075, 0.0075);
 
     for (int i = 0; i < nBins; ++i) {
-        h1[i + 1] = new TH1D(Form("h1_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 25, -0.005, 0.01);
-        h2[i + 1] = new TH1D(Form("h2_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 25, -0.005, 0.01);
+        h1[i + 1] = new TH1D(Form("h1_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 25, -0.0075, 0.0075);
+        h2[i + 1] = new TH1D(Form("h2_%d", i), Form("#theta [%.0f, %.0f] %s", thetaBins[i], thetaBins[i + 1], titleSuffix), 25, -0.0075, 0.0075);
     }
 
     // Fill the histograms and calculate theta means
@@ -613,8 +613,8 @@ void plot_elastic_energy_loss_validation(const char* file1, const char* file2, c
     h2[0]->Draw("E SAME");
 
     // Define the Gaussian + quadratic background fit functions
-    TF1 *fit1_int = new TF1("fit1_integrated", "gaus(0) + pol1(3)", -0.005, 0.01);
-    TF1 *fit2_int = new TF1("fit2_integrated", "gaus(0) + pol1(3)", -0.005, 0.01);
+    TF1 *fit1_int = new TF1("fit1_integrated", "gaus(0) + pol1(3)", -0.0075, 0.0075);
+    TF1 *fit2_int = new TF1("fit2_integrated", "gaus(0) + pol1(3)", -0.0075, 0.0075);
 
     // Set the initial guesses and parameter limits for the fit
     fit1_int->SetParameters(0.5 * maxVal1, 0, 0.0025);  // Initial guesses: amplitude, mu, sigma
@@ -670,8 +670,8 @@ void plot_elastic_energy_loss_validation(const char* file1, const char* file2, c
         h2[i]->Draw("E SAME");
 
         // Define the Gaussian + quadratic background fit functions for each bin
-        TF1 *fit1 = new TF1(Form("fit1_%d", i), "gaus(0) + pol1(3)", -0.005, 0.01);
-        TF1 *fit2 = new TF1(Form("fit2_%d", i), "gaus(0) + pol1(3)", -0.005, 0.01);
+        TF1 *fit1 = new TF1(Form("fit1_%d", i), "gaus(0) + pol1(3)", -0.0075, 0.0075);
+        TF1 *fit2 = new TF1(Form("fit2_%d", i), "gaus(0) + pol1(3)", -0.0075, 0.0075);
 
         // Set the initial guesses and parameter limits for the fit
         fit1->SetParameters(0.5 * maxVal1, 0, 0.0025);  // Initial guesses: amplitude, mu, sigma
