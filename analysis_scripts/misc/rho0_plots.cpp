e30ddc4ab78d2511bd67987ac7da98c290767335
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-13 19:36:17 -0400

    working on calibration.cpp; added in pcal enerty deposition plot

--- analysis_scripts/misc/rho0_plots.cpp
+++ analysis_scripts/misc/rho0_plots.cpp
@@ -181,17 +181,6 @@ void printVector(const std::vector<double>& vec, const std::string& vecName) {
 }
 
 
-// Helper function to trim whitespace from a string
-std::string trim(const std::string& str) {
-    std::string result = str;
-    result.erase(result.begin(), std::find_if(result.begin(), result.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }));
-    result.erase(std::find_if(result.rbegin(), result.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), result.end());
-    return result;
-}
 
 // Helper function to compare strings and print character-by-character differences
 void compareKeys(const std::string& generatedKey, const std::string& mapKey) {
