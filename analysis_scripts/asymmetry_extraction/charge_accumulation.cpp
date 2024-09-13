4d8e85f0c2071f7590b50f472f0445c89aea262a
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-13 12:42:27 -0400

    adjusted single hadron kinematic cuts to work dynamically even if a dihadron or trihadron file is passed

--- analysis_scripts/asymmetry_extraction/charge_accumulation.cpp
+++ analysis_scripts/asymmetry_extraction/charge_accumulation.cpp
@@ -9,7 +9,6 @@
 void charge_accumulation(TTreeReader& dataReader, const std::vector<RunInfo>& run_info_list) {
     std::set<int> processedRuns; // To keep track of processed runs
     TTreeReaderValue<int> runnum(dataReader, "runnum"); // For retrieving the runnum from the data
-    std::cout << "HELLO WORLD BEFORE WHILE LOOP" << std::endl;
     while (dataReader.Next()) {
         if (processedRuns.find(*runnum) == processedRuns.end()) {
             processedRuns.insert(*runnum);
