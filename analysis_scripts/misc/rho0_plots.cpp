c186ac3d35a64769879c69ea0f2d9bf5eb16b04c
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-14 00:20:26 -0400

    working on calibration.cpp; added in pcal enerty deposition plot

--- analysis_scripts/misc/rho0_plots.cpp
+++ analysis_scripts/misc/rho0_plots.cpp
@@ -371,14 +371,14 @@ void plotCombinationDependence(
 
         if (i == 0) {
             // Left subplot: reduce the right margin to minimize space between plots
-            gPad->SetLeftMargin(0.1);
+            gPad->SetLeftMargin(0.05);
             gPad->SetRightMargin(0.05);  // Decrease right margin
-            gPad->SetBottomMargin(0.15);
+            gPad->SetBottomMargin(0.05);
         } else {
             // Right subplot: reduce the left margin to minimize space between plots
             gPad->SetLeftMargin(0.05);  // Decrease left margin
-            gPad->SetRightMargin(0.1);  // Standard right margin
-            gPad->SetBottomMargin(0.15);
+            gPad->SetRightMargin(0.05);  // Standard right margin
+            gPad->SetBottomMargin(0.05);
         }
     }
 
