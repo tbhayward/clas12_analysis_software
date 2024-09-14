11b6dcd036a65763062155df10e2343210c3b493
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-14 00:28:13 -0400

    working on calibration.cpp; added in pcal enerty deposition plot

--- analysis_scripts/misc/rho0_plots.cpp
+++ analysis_scripts/misc/rho0_plots.cpp
@@ -388,7 +388,7 @@ void plotCombinationDependence(
     // Define the two suffixes and corresponding y-axis labels
     std::vector<std::string> suffixes = {"ALUsinphi", "ALL"};
     std::vector<std::string> yLabels = {
-        "F_{LU}^{sin#phi}/F_{UU}",
+        "F_{LU}^{sin#phi_{#pi^{+}}}/F_{UU}",
         "F_{LL}/F_{UU}"
     };
 
@@ -528,7 +528,7 @@ int main(int argc, char *argv[]) {
         "P_{T} (GeV)", 
         {0.0, 1.1}, 
         {0.0, 0.035},  // y range for ALUsinphi
-        {0.15, 0.35},  // y range for ALL
+        {0.2, 0.4},  // y range for ALL
         "output/rho0_plots/PT_epipluspiminus_combination_dependence_plots.png", 
         {
             "e'#pi^{+}X, M_{x (#pi^{+})} > 1.5 (GeV)",  // Baseline (black)
