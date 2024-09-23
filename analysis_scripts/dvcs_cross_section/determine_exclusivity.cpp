f7d7451edd052c33c7f149c5ec152d150b881a91
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 09:14:34 -0400

    created initial dvcs_cross_section_extract.cpp and corresponding MAKEFILE, just does simple imports for now

--- analysis_scripts/dvcs_cross_section/determine_exclusivity.cpp
+++ analysis_scripts/dvcs_cross_section/determine_exclusivity.cpp
@@ -10,7 +10,7 @@
 void determine_exclusivity(TTreeReader& dataReader, TTreeReader& mcReader, const std::string& outputDir) {
     // Create a 2x4 canvas
     TCanvas* canvas = new TCanvas("exclusivity_canvas", "Exclusivity Plots", 1600, 800);
-    canvas->Divide(2, 4);
+    canvas->Divide(4, 2);  // 2 rows, 4 columns
 
     std::vector<std::string> variables = {"open_angle_ep2", "Mx2_2", "theta_gamma_gamma", "placeholder", "Emiss2", "Mx2", "Mx2_1", "pTmiss"};
 
@@ -22,32 +22,40 @@ void determine_exclusivity(TTreeReader& dataReader, TTreeReader& mcReader, const
         // Retrieve histogram bin settings
         HistConfig config = histConfigs[variables[i]];
 
+        // Create histogram names
+        std::string hist_data_name = "data_" + variables[i];
+        std::string hist_mc_name = "mc_" + variables[i];
+
         // Create histograms for data and MC
-        TH1D* hist_data = new TH1D(hist_data_name.c_str(), hist_data_name.c_str(), config.bins, config.min, config.max);
-        TH1D* hist_mc = new TH1D(hist_mc_name.c_str(), hist_mc_name.c_str(), config.bins, config.min, config.max);
-        
+        TH1D* hist_data = new TH1D(hist_data_name.c_str(), formatLabelName(variables[i]).c_str(), config.bins, config.min, config.max);
+        TH1D* hist_mc = new TH1D(hist_mc_name.c_str(), formatLabelName(variables[i]).c_str(), config.bins, config.min, config.max);
+
         // Fill histograms using TTreeReader values
         TTreeReaderValue<double> dataVar(dataReader, variables[i].c_str());
         TTreeReaderValue<double> mcVar(mcReader, variables[i].c_str());
 
         while (dataReader.Next()) {
-            hData->Fill(*dataVar);
+            hist_data->Fill(*dataVar);
         }
         while (mcReader.Next()) {
-            hMC->Fill(*mcVar);
+            hist_mc->Fill(*mcVar);
         }
+
+        // Normalize histograms
+        hist_data->Scale(1.0 / hist_data->Integral());
+        hist_mc->Scale(1.0 / hist_mc->Integral());
 
         // Draw the histograms
         canvas->cd(i + 1);
-        hData->SetLineColor(kBlue);
-        hMC->SetLineColor(kRed);
-        hData->Draw("HIST");
-        hMC->Draw("HIST SAME");
+        hist_data->SetLineColor(kBlue);
+        hist_mc->SetLineColor(kRed);
+        hist_data->Draw("HIST");
+        hist_mc->Draw("HIST SAME");
 
         // Add a legend
         TLegend* legend = new TLegend(0.7, 0.7, 0.9, 0.9);
-        legend->AddEntry(hData, "Data", "l");
-        legend->AddEntry(hMC, "MC", "l");
+        legend->AddEntry(hist_data, ("Data (" + std::to_string(hist_data->GetEntries()) + ")").c_str(), "l");
+        legend->AddEntry(hist_mc, ("MC (" + std::to_string(hist_mc->GetEntries()) + ")").c_str(), "l");
         legend->Draw();
     }
 
