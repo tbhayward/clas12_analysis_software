d86620ea9110400784edec88f539ed0558f1bf09
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 18:13:58 -0400

    added beam energy option to input arguments of all processing files

--- analysis_scripts/dvcs_cross_section/kinematic_cuts.cpp
+++ analysis_scripts/dvcs_cross_section/kinematic_cuts.cpp
@@ -3,11 +3,11 @@
 // Function that applies the kinematic cuts
 bool apply_kinematic_cuts(double t, double open_angle_ep2, double Emiss2, double Mx2_1, double pTmiss) {
     // Applying the specified cuts
-    if (t >= 1) return false;    
+    if (-t >= 1) return false;    
     if (open_angle_ep2 <= 10) return false; 
     if (Emiss2 >= 1) return false;        
     if (Mx2_1 >= 0.25) return false;        
-    if (pTmiss >= 0.12) return false;       
+    if (pTmiss >= 0.10) return false;       
 
     return true;  // Event passes all cuts
 }
