d2b223b0de2c6dff32af81028b5ea33abb589792
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 10:33:48 -0400

    created hist configs and format label name like as in asymmetry_extraction and then began working on determine exclusivity function

--- analysis_scripts/dvcs_cross_section/histConfigs.cpp
+++ analysis_scripts/dvcs_cross_section/histConfigs.cpp
@@ -3,12 +3,12 @@
 
 // Define the histogram binning configurations
 std::map<std::string, HistConfig> histConfigs = {
-    {"open_angle_ep2", {50, 0, 60}},   // Adjusted range
-    {"Mx2_2", {50, -1, 4}},
-    {"theta_gamma_gamma", {50, 0, 7}}, 
-    {"placeholder", {50, 0, 0}}, // Placeholder for future use
-    {"Emiss2", {50, -1, 2}},
-    {"Mx2", {50, -0.06, 0.06}},
-    {"Mx2_1", {50, -1, 1.5}},
-    {"pTmiss", {50, 0, 0.7}}
+    {"open_angle_ep2", {100, 0, 60}},   // Adjusted range
+    {"Mx2_2", {100, -1, 4}},
+    {"theta_gamma_gamma", {100, 0, 7}}, 
+    {"placeholder", {100, 0, 0}}, // Placeholder for future use
+    {"Emiss2", {100, -1, 2}},
+    {"Mx2", {100, -0.06, 0.06}},
+    {"Mx2_1", {100, -1, 1.5}},
+    {"pTmiss", {100, 0, 0.7}}
 };
