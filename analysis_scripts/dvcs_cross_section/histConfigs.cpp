f2f92ec75bc0d1ffdf730df71fa759b36b997cbf
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 11:16:15 -0400

    created hist configs and format label name like as in asymmetry_extraction and then began working on determine exclusivity function

--- analysis_scripts/dvcs_cross_section/histConfigs.cpp
+++ analysis_scripts/dvcs_cross_section/histConfigs.cpp
@@ -8,7 +8,7 @@ std::map<std::string, HistConfig> histConfigs = {
     {"theta_gamma_gamma", {100, 0, 7}}, 
     {"placeholder", {100, 0, 0}}, // Placeholder for future use
     {"Emiss2", {100, -1, 2}},
-    {"Mx2", {100, -0.06, 0.06}},
+    {"Mx2", {100, -0.05, 0.05}},
     {"Mx2_1", {100, -1, 1.5}},
     {"pTmiss", {100, 0, 0.7}}
 };
