e1df6a10d928a301e78630930e5e36dec177d134
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 08:59:07 -0400

    created initial dvcs_cross_section_extract.cpp and corresponding MAKEFILE, just does simple imports for now

--- analysis_scripts/dvcs_cross_section/histConfigs.cpp
+++ analysis_scripts/dvcs_cross_section/histConfigs.cpp
@@ -8,7 +8,7 @@ std::map<std::string, HistConfig> histConfigs = {
     {"theta_gamma_gamma", {100, 0, 8}}, 
     {"placeholder", {100, 0, 0}}, // Placeholder for future use
     {"Emiss2", {100, -1, 2}},
-    {"Mx2", {100, -, 1}},
+    {"Mx2", {100, -1, 1}},
     {"Mx2_1", {100, -1, 2}},
     {"pTmiss", {100, 0, 1}}
 };
