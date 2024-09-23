d01c964391ef54cf3ef5b4b960e89c7c855497fb
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 09:40:04 -0400

    created hist configs and format label name like as in asymmetry_extraction and then began working on determine exclusivity function

--- analysis_scripts/dvcs_cross_section/determine_exclusivity.h
+++ analysis_scripts/dvcs_cross_section/determine_exclusivity.h
@@ -2,7 +2,8 @@
 #define DETERMINE_EXCLUSIVITY_H
 
 #include <TTreeReader.h>
+#include <string>
 
-void determine_exclusivity(TTreeReader& dataReader, TTreeReader& mcReader, const std::string& outputDir);
+void determine_exclusivity(TTreeReader& dataReader, TTreeReader& mcReader, const std::string& outputDir, const std::string& plotTitle);
 
 #endif
