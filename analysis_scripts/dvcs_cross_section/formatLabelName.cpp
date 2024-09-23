47ad660e26a014f30103429f191e8491d21f9e85
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 15:27:29 -0400

    added beam energy option to input arguments of all processing files

--- analysis_scripts/dvcs_cross_section/formatLabelName.cpp
+++ analysis_scripts/dvcs_cross_section/formatLabelName.cpp
@@ -5,14 +5,14 @@ std::string formatLabelName(const std::string& original, const std::string& anal
     // Define common labels for both DVCS and eppi0
     std::map<std::string, std::string> commonLabels = {
         {"open_angle_ep2", "#theta_{e'#gamma} (deg)"},
-        {"Mx2_2", "M M_{e'#gamma}^{2} (GeV^{2})"},
+        {"Mx2_2", "Miss M_{e'#gamma}^{2} (GeV^{2})"},
         {"theta_gamma_gamma", "#theta_{#gamma(det)#gamma(rec)}"},
         {"theta_pi0_pi0", "#theta_{#pi^{0}(det)#pi^{0}(rec)}"},
         {"xF", "x_{F}"},
-        {"Emiss2", "M E_{e'p'#gamma} (GeV)"},
-        {"Mx2", "M M_{e'p'#gamma}^{2} (GeV^{2})"},
-        {"Mx2_1", "M M_{e'p'}^{2} (GeV^{2})"},
-        {"pTmiss", "M P_{T(e'p'#gamma)} (GeV)"}
+        {"Emiss2", "Miss E_{e'p'#gamma} (GeV)"},
+        {"Mx2", "Miss M_{e'p'#gamma}^{2} (GeV^{2})"},
+        {"Mx2_1", "Miss M_{e'p'}^{2} (GeV^{2})"},
+        {"pTmiss", "Miss P_{T(e'p'#gamma)} (GeV)"}
     };
 
     // Determine if we are dealing with DVCS or eppi0
