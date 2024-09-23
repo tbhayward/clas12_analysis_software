d6238a493fa97eb58bb685bdf5b36c291e345bc1
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 16:19:41 -0400

    added beam energy option to input arguments of all processing files

--- processing_classes/src/analyzers/ThreeParticles.java
+++ processing_classes/src/analyzers/ThreeParticles.java
@@ -194,7 +194,7 @@ public class ThreeParticles {
         if (generic_tests.forward_tagger_cut(p2_rec_index, rec_Bank)) {
             detector2 = 0; // Forward Tagger
         } else if (generic_tests.forward_detector_cut(p2_rec_index, rec_Bank)) {
-            detector2 = 1; // Forward Detector
+            detector2 = 11; // Forward Detector
         } else if (generic_tests.central_detector_cut(p2_rec_index, rec_Bank)) {
             detector2 = 2; // Central Detector
         }
