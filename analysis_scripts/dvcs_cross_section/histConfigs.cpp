124e11f1a89b7ee776f4a0615cc39777cffc0b45
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-23 22:03:36 -0400

    added beam energy option to input arguments of all processing files

--- analysis_scripts/dvcs_cross_section/histConfigs.cpp
+++ analysis_scripts/dvcs_cross_section/histConfigs.cpp
@@ -5,7 +5,7 @@
 std::map<std::string, HistConfig> histConfigs = {
     {"open_angle_ep2", {100, 0, 60}},   // Adjusted range
     {"Mx2_2", {100, 0, 4}},
-    {"theta_gamma_gamma", {100, 0, 5}}, 
+    {"theta_gamma_gamma", {100, 0, 3}}, 
     {"theta_pi0_pi0", {100, 0, 5}}, 
     {"xF", {100, -0.4, 0.2}}, // Placeholder for future use
     {"Emiss2", {100, -1, 2}},
