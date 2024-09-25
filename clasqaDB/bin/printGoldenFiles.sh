0b0f3993659acb2b9d1f4df8a6dea6af4eb09437
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-25 09:55:35 -0400

    removed clasqaDB from repository since it's being sourced on the farm

--- clasqaDB/bin/printGoldenFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# prints list of golden files
-if [ -z "$QADB" ]; then
-  echo "ERROR: you must source env.sh first"; exit
-fi
-run-groovy ${QADB}/util/printGoldenFiles.groovy
--- clasqaDB/bin/printGoldenRuns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# prints list of golden runs
-if [ -z "$QADB" ]; then
-  echo "ERROR: you must source env.sh first"; exit
-fi
-run-groovy ${QADB}/util/printGoldenRuns.groovy
