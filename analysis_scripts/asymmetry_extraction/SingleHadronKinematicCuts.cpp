3acee8e5f34e71501be87712ca80e8c9981bef13
Timothy Hayward <tbhayward@email.wm.edu>
2024-09-30 21:26:11 +0400

    added q2 vs epsilon

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -34,7 +34,7 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
     if (property == "epsilonStudyUpper") {
       // goodEvent == *Q2 > 1 && *W > 2 && *y < 0.80 && *Mx2 > 2.25 && *x > 0.30 && *x < 0.34 && *z > 0.25 && *z < 0.35 && *pT > 0.2 && *pT < 0.3; 
       // goodEvent = *Q2 > 1 && *W > 2 && *y < 0.80 && *Mx2 > 2.25 && *x > 0.12 && *x < 0.22 && *z > 0.55 && *z < 0.65 && *pT > 0.15 && *pT < 0.25; 
-      goodEvent = *Q2 > 1 && *W > 2 && *y < 0.80 && *Mx2 > 2.25 && *x > 0.12 && *x < 0.18;
+      goodEvent = *Q2 > 1 && *W > 2 && *y < 0.80 && *x > 0.12 && *x < 0.18;
       return goodEvent;
     }
 
