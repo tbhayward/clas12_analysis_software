c987a6ac4432a0b4e53b156740705fa6b0e8b4b5
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-03 09:42:47 +0400

    added q2 vs epsilon

--- processing_classes/src/analyzers/TwoParticles.java
+++ processing_classes/src/analyzers/TwoParticles.java
@@ -315,34 +315,45 @@ public class TwoParticles {
 //        System.out.println("  P^-   : " + P_minus);
 //        System.out.println("  P^⊥   : " + P_perp);
 //        System.out.println();
-//
-//// Print Lorentz vector components of lv_q_gN (Photon in gamma*-nucleon frame)
-//        System.out.println("Lorentz Vector lv_q_gN (Photon in gamma*-nucleon frame):");
-//        System.out.println("  Energy (E)  : " + lv_q_gN.e());
-//        System.out.println("  Px          : " + lv_q_gN.px());
-//        System.out.println("  Py          : " + lv_q_gN.py());
-//        System.out.println("  Pz          : " + lv_q_gN.pz());
-//        System.out.println("  |P| (3D mag): " + lv_q_gN.vect().mag());
-//        System.out.println();
-//        
-//        System.out.println("Q2 = "+Q2+", xB = "+x);
-//        System.out.println();
-//        System.out.println("lck inner product = "+lck.lightConeInnerProduct(lv_p_gN,lv_q_gN)+", calculated inner product = "+(Q2*Ph_plus/(2*x*P_plus)-x*P_plus*Ph_minus));
-//        System.out.println(lck.lightConeInnerProduct(lv_target_gN,lv_q_gN));
-//        System.out.println();
-//
-//// Print the results for xi and xi2
-//        System.out.println("Results for xi2 (approximation) and xi1 (Lorentz invariant):");
-//        System.out.println("  xi2 (P_h^+/P^+)         : " + xi);
-//        System.out.println("  xi1 (Lorentz invariant): " + xi2);
-//        System.out.println();
-//
-//// Extra checks: Compare the two methods
-//        double ratio = Math.abs((xi2 - xi) / xi);
-//        System.out.println("Relative difference between xi2 and xi1: " + ratio);
-//        System.out.println("============================================");
+
+// Print Lorentz vector components of lv_q_gN (Photon in gamma*-nucleon frame)
+        System.out.println("Lorentz Vector lv_q_gN (Photon in gamma*-nucleon frame):");
+        System.out.println("  Energy (E)  : " + lv_q_gN.e());
+        System.out.println("  Px          : " + lv_q_gN.px());
+        System.out.println("  Py          : " + lv_q_gN.py());
+        System.out.println("  Pz          : " + lv_q_gN.pz());
+        System.out.println("  |P| (3D mag): " + lv_q_gN.vect().mag());
+        System.out.println();
         
-        System.out.print(2*Ph_minus*P_plus*x/Q2+",");
+                System.out.println("Light-cone components of lv_q_gN:");
+        double q_plus = (lv_q_gN.e() + lv_q_gN.pz()) / Math.sqrt(2);  // P_h^+
+        double q_minus = (lv_q_gN.e() - lv_q_gN.pz()) / Math.sqrt(2); // P_h^-
+        double q_perp = Math.sqrt(Math.pow(lv_q_gN.px(), 2) + Math.pow(lv_q_gN.py(), 2)); // P_h^perp
+        System.out.println("  P^+   : " + q_plus);
+        System.out.println("  P^-   : " + q_minus);
+        System.out.println("  P^⊥   : " + q_perp);
+        System.out.println();
+        
+        System.out.println("Q2 = "+Q2+", xB = "+x);
+        System.out.println();
+        System.out.println(-x*P_plus+" "+q_plus);
+        System.out.println();
+        System.out.println("lck inner product = "+lck.lightConeInnerProduct(lv_p_gN,lv_q_gN)+", calculated inner product = "+(Q2*Ph_plus/(2*x*P_plus)-x*P_plus*Ph_minus));
+        System.out.println(lck.lightConeInnerProduct(lv_target_gN,lv_q_gN));
+        System.out.println();
+
+// Print the results for xi and xi2
+        System.out.println("Results for xi2 (approximation) and xi1 (Lorentz invariant):");
+        System.out.println("  xi2 (P_h^+/P^+)         : " + xi);
+        System.out.println("  xi1 (Lorentz invariant): " + xi2);
+        System.out.println();
+
+// Extra checks: Compare the two methods
+        double ratio = Math.abs((xi2 - xi) / xi);
+        System.out.println("Relative difference between xi2 and xi1: " + ratio);
+        System.out.println("============================================");
+        
+//        System.out.print(2*Ph_minus*P_plus*x/Q2+",");
     
         p_gN_pz = lv_p_gN.vect().dot(lv_q_gN.vect()) / lv_q_gN.vect().mag();
         p_Breit_pz = lv_p_Breit.vect().dot(lv_q_Breit.vect()) / lv_q_Breit.vect().mag();
