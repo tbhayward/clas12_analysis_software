c5c766f083eaeeccfb171ef5d14a60c81455690e
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-04 09:16:40 +0400

    added q2 vs epsilon

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -270,8 +270,7 @@ int main(int argc, char *argv[]) {
         // If cpp == 1, fill dilutionFactors with zeros
         dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {0.0, 0.0});
     }
-    // dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {1.0, 0.0});
-
+    
     for (int asymmetry = 0; asymmetry < 3; ++asymmetry) {
         if (asymmetry > 0 && cpp == 1) {
             cout << "Skipping TSA and DSA for unpolarized target data." << endl;
