02068da130d20a04a6e71f680550a610af2fca92
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-08 10:15:05 -0400

    working on plotting function for dvcs

--- analysis_scripts/dvcs_cross_section/plot_dvcs_data_mc_comparison.cpp
+++ analysis_scripts/dvcs_cross_section/plot_dvcs_data_mc_comparison.cpp
@@ -201,10 +201,10 @@ void plot_dvcs_data_mc_comparison(const std::string& output_dir, const std::stri
         for (int idx = 0; idx < n_Q2t_bins; ++idx) {
             const auto& bin = bin_boundaries[relevant_bins[idx]];
 
-            if (*xB_data >= bin.xB_low && *xB_data <= bin.xB_high &&
+            if ((*xB_data >= bin.xB_low && *xB_data <= bin.xB_high &&
                 *Q2_data >= bin.Q2_low && *Q2_data <= bin.Q2_high &&
                 std::abs(*t1_data) >= bin.t_low && std::abs(*t1_data) <= bin.t_high) &&
-                apply_kinematic_cuts(*t1_data, *open_angle_ep2_data, **theta_neutral_neutral_data, *Emiss2_data, *Mx2_1_data, *pTmiss_data) {
+                apply_kinematic_cuts(*t1_data, *open_angle_ep2_data, **theta_neutral_neutral_data, *Emiss2_data, *Mx2_1_data, *pTmiss_data)) {
 
                 h_data_histograms[idx]->Fill(phi_deg);
                 break;
@@ -219,10 +219,10 @@ void plot_dvcs_data_mc_comparison(const std::string& output_dir, const std::stri
         for (int idx = 0; idx < n_Q2t_bins; ++idx) {
             const auto& bin = bin_boundaries[relevant_bins[idx]];
 
-            if (*xB_mc_gen >= bin.xB_low && *xB_mc_gen <= bin.xB_high &&
+            if ((*xB_mc_gen >= bin.xB_low && *xB_mc_gen <= bin.xB_high &&
                 *Q2_mc_gen >= bin.Q2_low && *Q2_mc_gen <= bin.Q2_high &&
                 std::abs(*t1_mc_gen) >= bin.t_low && std::abs(*t1_mc_gen) <= bin.t_high) &&
-                apply_kinematic_cuts(*t1_mc_gen, *open_angle_ep2_mc_gen, **theta_neutral_neutral_mc_gen, *Emiss2_mc_gen, *Mx2_1_mc_gen, *pTmiss_mc_gen) {
+                apply_kinematic_cuts(*t1_mc_gen, *open_angle_ep2_mc_gen, **theta_neutral_neutral_mc_gen, *Emiss2_mc_gen, *Mx2_1_mc_gen, *pTmiss_mc_gen)) {
 
                 h_mc_gen_histograms[idx]->Fill(phi_mc_gen_deg);
                 break;
@@ -237,10 +237,10 @@ void plot_dvcs_data_mc_comparison(const std::string& output_dir, const std::stri
         for (int idx = 0; idx < n_Q2t_bins; ++idx) {
             const auto& bin = bin_boundaries[relevant_bins[idx]];
 
-            if (*xB_mc_rec >= bin.xB_low && *xB_mc_rec <= bin.xB_high &&
+            if ((*xB_mc_rec >= bin.xB_low && *xB_mc_rec <= bin.xB_high &&
                 *Q2_mc_rec >= bin.Q2_low && *Q2_mc_rec <= bin.Q2_high &&
                 std::abs(*t1_mc_rec) >= bin.t_low && std::abs(*t1_mc_rec) <= bin.t_high) &&
-                apply_kinematic_cuts(*t1_mc_rec, *open_angle_ep2_mc_rec, **theta_neutral_neutral_mc_rec, *Emiss2_mc_rec, *Mx2_1_mc_rec, *pTmiss_mc_rec) {
+                apply_kinematic_cuts(*t1_mc_rec, *open_angle_ep2_mc_rec, **theta_neutral_neutral_mc_rec, *Emiss2_mc_rec, *Mx2_1_mc_rec, *pTmiss_mc_rec)) {
 
                 h_mc_rec_histograms[idx]->Fill(phi_mc_rec_deg);
                 break;
