28193e06720ac3948d9d8fded97ae556e65b5e3b
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-09 12:38:12 -0400

    added bin number to read bin boundaries function that looks at the integrated csv

--- analysis_scripts/dvcs_cross_section/plot_unfolding.h
+++ analysis_scripts/dvcs_cross_section/plot_unfolding.h
@@ -1,16 +1,26 @@
 #ifndef PLOT_UNFOLDING_H
 #define PLOT_UNFOLDING_H
 
+#include <TH1D.h>
+#include <TCanvas.h>
+#include <TTreeReader.h>
+#include <cmath>
 #include <string>
 #include <vector>
 #include "bin_boundaries.h"
-#include <TTreeReader.h>
+#include "kinematic_cuts.h"
+#include "bin_helpers.h"
 
-// Function to plot unfolded distributions and acceptance
+// Constant to convert radians to degrees
+constexpr double RAD_TO_DEG = 180.0 / M_PI;
+
+// Function prototype for plot_unfolding
 void plot_unfolding(const std::string& output_dir, 
                     const std::string& analysisType, 
                     int xB_bin,
                     const std::vector<BinBoundary>& bin_boundaries, 
-                    TTreeReader& data_reader);
+                    TTreeReader& data_reader, 
+                    TTreeReader& mc_gen_reader, 
+                    TTreeReader& mc_rec_reader);
 
-#endif  // PLOT_UNFOLDING_H
+#endif // PLOT_UNFOLDING_H
