5c888e968634f0b8e6fcc8bb8951b7983b8e79cf
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-10 11:55:27 -0400

    added bin number to read bin boundaries function that looks at the integrated csv

--- analysis_scripts/dvcs_cross_section/write_csv.h
+++ analysis_scripts/dvcs_cross_section/write_csv.h
@@ -3,9 +3,9 @@
 
 #include <string>
 #include <vector>
-#include "plot_unfolding.h"  // Assuming the new structure YieldData is defined here
+#include "plot_unfolding.h"
 
-// Function to write yield data to a CSV file
-void write_csv(std::vector<YieldData> &yields, const std::string &filename);
+// Function to write unfolding data to a CSV file
+void write_csv(const std::string& filename, const std::vector<UnfoldingData>& unfolding_data);
 
 #endif // WRITE_CSV_H
