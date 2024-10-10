8103815ca08cf8880db7b5932f26604bb514717f
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-10 12:57:13 -0400

    added bin number to read bin boundaries function that looks at the integrated csv

--- analysis_scripts/dvcs_cross_section/write_csv.cpp
+++ analysis_scripts/dvcs_cross_section/write_csv.cpp
@@ -27,9 +27,9 @@ void write_csv(const std::string& filename, const std::vector<UnfoldingData>& un
     // Write the header
     file << "Bin,xB_min,xB_max,xB_avg,Q2_min,Q2_max,Q2_avg,t_min,t_max,t_avg,"
          << "phi_min,phi_max,"
-         << "raw_yield_FD_FD_Fa18Inb,raw_yield_CD_FD_Fa18Inb,raw_yield_CD_FT_Fa18Inb,raw_yield_combined_Fa18Inb,acceptance_Fa18Inb,unfolded_yield_Fa18Inb,"
-         << "raw_yield_FD_FD_Fa18Out,raw_yield_CD_FD_Fa18Out,raw_yield_CD_FT_Fa18Out,raw_yield_combined_Fa18Out,acceptance_Fa18Out,unfolded_yield_Fa18Out,"
-         << "raw_yield_FD_FD_Sp19Inb,raw_yield_CD_FD_Sp19Inb,raw_yield_CD_FT_Sp19Inb,raw_yield_combined_Sp19Inb,acceptance_Sp19Inb,unfolded_yield_Sp19Inb"
+         << "raw_yield_Fa18Inb,acceptance_Fa18Inb,unfolded_yield_Fa18Inb,"
+         << "raw_yield_Fa18Out,acceptance_Fa18Out,unfolded_yield_Fa18Out,"
+         << "raw_yield_Sp19Inb,acceptance_Sp19Inb,unfolded_yield_Sp19Inb"
          << std::endl;
 
     // Loop through the unfolding data and write each entry to the file
@@ -43,26 +43,17 @@ void write_csv(const std::string& filename, const std::vector<UnfoldingData>& un
                  << data.phi_min[i] << "," << data.phi_max[i] << ",";
 
             // Write raw yields, acceptance, and unfolded yields for Fa18Inb
-            file << data.raw_yields_FD_FD_Fa18Inb[i] << "," 
-                 << data.raw_yields_CD_FD_Fa18Inb[i] << "," 
-                 << data.raw_yields_CD_FT_Fa18Inb[i] << "," 
-                 << data.raw_yield_combined_Fa18Inb[i] << ","
+            file << data.raw_yields_Fa18Inb[i] << "," 
                  << data.acceptance_Fa18Inb[i] << "," 
                  << data.unfolded_yields_Fa18Inb[i] << ",";
 
             // Write raw yields, acceptance, and unfolded yields for Fa18Out
-            file << data.raw_yields_FD_FD_Fa18Out[i] << "," 
-                 << data.raw_yields_CD_FD_Fa18Out[i] << "," 
-                 << data.raw_yields_CD_FT_Fa18Out[i] << "," 
-                 << data.raw_yield_combined_Fa18Out[i] << ","
+            file << data.raw_yields_Fa18Out[i] << "," 
                  << data.acceptance_Fa18Out[i] << "," 
                  << data.unfolded_yields_Fa18Out[i] << ",";
 
             // Write raw yields, acceptance, and unfolded yields for Sp19Inb
-            file << data.raw_yields_FD_FD_Sp19Inb[i] << "," 
-                 << data.raw_yields_CD_FD_Sp19Inb[i] << "," 
-                 << data.raw_yields_CD_FT_Sp19Inb[i] << "," 
-                 << data.raw_yield_combined_Sp19Inb[i] << ","
+            file << data.raw_yields_Sp19Inb[i] << "," 
                  << data.acceptance_Sp19Inb[i] << "," 
                  << data.unfolded_yields_Sp19Inb[i];
 
