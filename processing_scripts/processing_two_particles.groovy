4afdce9b03e6890aecd15be975826c3e8737dd52
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-15 08:51:45 -0400

    turned eventbuilder on in two particle processing script

--- processing_scripts/processing_two_particles.groovy
+++ processing_scripts/processing_two_particles.groovy
@@ -151,7 +151,6 @@ public static void main(String[] args) {
 	    	// 	qa.OkForAsymmetry(runnum, evnum));
 
 		    if (process_event) {
-		    	println("HEY WE'RE IN");
 		    	num_hadrons++;
 
 		        // get # of particles w/ pid1
