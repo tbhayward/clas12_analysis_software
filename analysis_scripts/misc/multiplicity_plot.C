1c9336eba46b3f3d1186a13445427a7b692d430a
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-15 13:01:22 -0400

    turned eventbuilder on in two particle processing script

--- analysis_scripts/misc/multiplicity_plot.C
+++ analysis_scripts/misc/multiplicity_plot.C
@@ -29,58 +29,93 @@ void multiplicity_plot(const char* filename)
     tree->SetBranchAddress("num_neg", &num_neg);
     tree->SetBranchAddress("num_neutral", &num_neutral);
 
-    // Collect unique run numbers
-    std::set<int> runnum_set;
+    // Define data structures to accumulate sums per run number
+    struct Accumulator {
+        double sum_pos = 0;
+        double sum_pos2 = 0;
+        double sum_neg = 0;
+        double sum_neg2 = 0;
+        double sum_neutral = 0;
+        double sum_neutral2 = 0;
+        int count = 0;
+    };
+
+    std::map<int, Accumulator> run_data;
+
+    // Loop over the tree once
     Long64_t nentries = tree->GetEntries();
     for (Long64_t i = 0; i < nentries; i++) {
         tree->GetEntry(i);
-        runnum_set.insert(runnum);
+
+        Accumulator &acc = run_data[runnum];
+        acc.sum_pos += num_pos;
+        acc.sum_pos2 += num_pos * num_pos;
+        acc.sum_neg += num_neg;
+        acc.sum_neg2 += num_neg * num_neg;
+        acc.sum_neutral += num_neutral;
+        acc.sum_neutral2 += num_neutral * num_neutral;
+        acc.count += 1;
     }
 
-    // Sort run numbers
-    std::vector<int> runnums(runnum_set.begin(), runnum_set.end());
-    std::sort(runnums.begin(), runnums.end());
-
-    // Vectors to store mean values and errors
+    // Prepare vectors for plotting
     std::vector<double> runnum_vals, mean_pos, mean_neg, mean_neutral;
     std::vector<double> err_pos, err_neg, err_neutral;
 
     // Calculate means and errors for each run number
-    for (size_t i = 0; i < runnums.size(); i++) {
-        int curr_runnum = runnums[i];
-        TString selection = Form("runnum == %d", curr_runnum);
+    for (const auto& kv : run_data) {
+        int curr_runnum = kv.first;
+        const Accumulator &acc = kv.second;
+        int N = acc.count;
 
-        // Histograms for calculations
-        TH1D *h_pos = new TH1D("h_pos", "", 100, -0.5, 99.5);
-        TH1D *h_neg = new TH1D("h_neg", "", 100, -0.5, 99.5);
-        TH1D *h_neutral = new TH1D("h_neutral", "", 100, -0.5, 99.5);
+        double mean_p = acc.sum_pos / N;
+        double mean_n = acc.sum_neg / N;
+        double mean_neu = acc.sum_neutral / N;
 
-        // Fill histograms
-        tree->Draw("num_pos>>h_pos", selection);
-        tree->Draw("num_neg>>h_neg", selection);
-        tree->Draw("num_neutral>>h_neutral", selection);
+        // Variance calculation
+        double var_p = (acc.sum_pos2 / N) - (mean_p * mean_p);
+        double var_n = (acc.sum_neg2 / N) - (mean_n * mean_n);
+        double var_neu = (acc.sum_neutral2 / N) - (mean_neu * mean_neu);
 
-        // Store mean values and errors
+        // Statistical uncertainty (standard error)
+        double err_p = sqrt(var_p / N);
+        double err_n = sqrt(var_n / N);
+        double err_neu = sqrt(var_neu / N);
+
         runnum_vals.push_back(curr_runnum);
-        mean_pos.push_back(h_pos->GetMean());
-        err_pos.push_back(h_pos->GetMeanError());
+        mean_pos.push_back(mean_p);
+        mean_neg.push_back(mean_n);
+        mean_neutral.push_back(mean_neu);
 
-        mean_neg.push_back(h_neg->GetMean());
-        err_neg.push_back(h_neg->GetMeanError());
+        err_pos.push_back(err_p);
+        err_neg.push_back(err_n);
+        err_neutral.push_back(err_neu);
+    }
 
-        mean_neutral.push_back(h_neutral->GetMean());
-        err_neutral.push_back(h_neutral->GetMeanError());
+    // Sort data by run number
+    std::vector<size_t> indices(runnum_vals.size());
+    std::iota(indices.begin(), indices.end(), 0);
 
-        // Clean up
-        delete h_pos;
-        delete h_neg;
-        delete h_neutral;
+    std::sort(indices.begin(), indices.end(),
+        [&runnum_vals](size_t i1, size_t i2) { return runnum_vals[i1] < runnum_vals[i2]; });
+
+    // Create sorted vectors
+    std::vector<double> runnum_vals_sorted, mean_pos_sorted, mean_neg_sorted, mean_neutral_sorted;
+    std::vector<double> err_pos_sorted, err_neg_sorted, err_neutral_sorted;
+
+    for (size_t idx : indices) {
+        runnum_vals_sorted.push_back(runnum_vals[idx]);
+        mean_pos_sorted.push_back(mean_pos[idx]);
+        mean_neg_sorted.push_back(mean_neg[idx]);
+        mean_neutral_sorted.push_back(mean_neutral[idx]);
+        err_pos_sorted.push_back(err_pos[idx]);
+        err_neg_sorted.push_back(err_neg[idx]);
+        err_neutral_sorted.push_back(err_neutral[idx]);
     }
 
     // Create TGraphErrors for plotting
-    TGraphErrors *gr_pos = new TGraphErrors(runnum_vals.size(), &runnum_vals[0], &mean_pos[0], 0, &err_pos[0]);
-    TGraphErrors *gr_neg = new TGraphErrors(runnum_vals.size(), &runnum_vals[0], &mean_neg[0], 0, &err_neg[0]);
-    TGraphErrors *gr_neutral = new TGraphErrors(runnum_vals.size(), &runnum_vals[0], &mean_neutral[0], 0, &err_neutral[0]);
+    TGraphErrors *gr_pos = new TGraphErrors(runnum_vals_sorted.size(), &runnum_vals_sorted[0], &mean_pos_sorted[0], 0, &err_pos_sorted[0]);
+    TGraphErrors *gr_neg = new TGraphErrors(runnum_vals_sorted.size(), &runnum_vals_sorted[0], &mean_neg_sorted[0], 0, &err_neg_sorted[0]);
+    TGraphErrors *gr_neutral = new TGraphErrors(runnum_vals_sorted.size(), &runnum_vals_sorted[0], &mean_neutral_sorted[0], 0, &err_neutral_sorted[0]);
 
     // Customize graph appearance
     gr_pos->SetMarkerStyle(20);
