db6c239dc55c830b76b5d429c51f9b4676fd106b
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-16 11:24:47 -0400

    added bin number to read bin boundaries function that looks at the integrated csv

--- processing_classes/src/extended_kinematic_fitters/generic_tests.java
+++ processing_classes/src/extended_kinematic_fitters/generic_tests.java
@@ -11,11 +11,10 @@ import org.jlab.io.hipo.HipoDataBank;
 public class generic_tests {
     
     public boolean banks_test(DataEvent event) {
-        System.out.println("HEY");
         String[] bankNames = 
             {"RUN::config","REC::Event","REC::Particle","REC::Calorimeter","REC::Track","REC::Traj","REC::Cherenkov"};
         for (String bankName : bankNames) {
-            if (!event.hasBank(bankName)) { return false; }
+            if (!event.hasBank(bankName)) { System.out.println(bankName); return false; }
         }
         return true;
     }
