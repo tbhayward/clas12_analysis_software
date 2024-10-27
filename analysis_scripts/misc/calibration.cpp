95cf7d6c39091596b71c0bc1988c880531a6847d
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-27 09:05:18 -0400

    testing FSR study

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -8728,7 +8728,7 @@ int main(int argc, char** argv) {
 
     //// PLOTS ////
 
-    std::string dataset = "rga_fa18_out";
+    std::string dataset = "rga_fa18_inb";
 
     // plot_htcc_nphe(dataReader, mcReader, dataset);
     // plot_ltcc_nphe(dataReader, mcReader, dataset);
