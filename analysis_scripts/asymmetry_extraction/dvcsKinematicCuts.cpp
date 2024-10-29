6ff77af971303301b9868f7ded66333f124a3840
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-29 18:47:46 -0400

    updated dc determination plots in calibration.cpp to have theta bins to check for dependendence, now testing no rad, ISR and FSR simulation on RGC epX fits

--- analysis_scripts/asymmetry_extraction/dvcsKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/dvcsKinematicCuts.cpp
@@ -18,7 +18,7 @@ bool dvcsKinematicCuts::applyCuts(int currentFits, bool isMC) {
     bool goodEvent = false;
     string property = binNames[currentFits];
 
-    goodEvent = *Q2 > 1 && *W > 2 && *eta2 < 0 && *t1 > -1 && pTmiss < 0.15 && Emiss2 < 1 && theta_gamma_gamma < 0.7 && open_angle_ep2 > 5;
+    goodEvent = *Q2 > 1 && *W > 2 && *eta2 < 0 && *t1 > -1 && *pTmiss < 0.15 && *Emiss2 < 1 && *theta_gamma_gamma < 0.7 && *open_angle_ep2 > 5;
     goodEvent = true;
     
     if (isMC || (*runnum < 16042 || *runnum > 17811)) {
