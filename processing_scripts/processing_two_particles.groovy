328e276188dc68833609ac7ed7a37cb178603a01
Timothy Hayward <tbhayward@email.wm.edu>
2024-10-31 10:11:41 -0400

    updated dc determination plots in calibration.cpp to have theta bins to check for dependendence, now testing no rad, ISR and FSR simulation on RGC epX fits

--- processing_scripts/processing_two_particles.groovy
+++ processing_scripts/processing_two_particles.groovy
@@ -115,7 +115,8 @@ public static void main(String[] args) {
 	qa.checkForDefect('ChargeNegative')
 	qa.checkForDefect('ChargeUnknown')
 	qa.checkForDefect('PossiblyNoBeam')
-	[ // list of runs with `Misc` that should be allowed, generally empty target etc for dilution factor calculations
+	[ // list of runs with `Misc` that should be allowed, 
+	 // for RGC generally empty target etc for dilution factor calculations
 	 	5046, 5047, 5051, 5128, 5129, 5130, 5158, 5159,
   		5160, 5163, 5165, 5166, 5167, 5168, 5169, 5180,
   		5181, 5182, 5183, 5400, 5448, 5495, 5496, 5505,
