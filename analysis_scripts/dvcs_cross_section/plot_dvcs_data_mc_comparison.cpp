1ee9ecd6f6f85ba764b6fb7f320302537fd6a22f
Timothy Hayward <tbhayward@email.wm.edu>
2024-11-11 12:04:55 -0500

    updated dvcs extraction to now function with the phi bins defined by sangbaek

--- analysis_scripts/dvcs_cross_section/plot_dvcs_data_mc_comparison.cpp
+++ analysis_scripts/dvcs_cross_section/plot_dvcs_data_mc_comparison.cpp
@@ -190,12 +190,12 @@ void plot_dvcs_data_mc_comparison(const std::string& output_dir,
             const auto& idx_list = group.second;
 
             const auto& bin_example = bin_boundaries[idx_list[0]];  // Use first bin as representative
-
+            std::cout << "MADE IT PASSED KINEMATIC CUTS" << std::endl;
             if (xB >= bin_example.xB_low && xB <= bin_example.xB_high &&
                 Q2 >= bin_example.Q2_low && Q2 <= bin_example.Q2_high &&
                 t_abs >= bin_example.t_low && t_abs <= bin_example.t_high &&
                 apply_kinematic_cuts(*t1_data, *open_angle_ep2_data, *theta_neutral_neutral_data, *Emiss2_data, *Mx2_1_data, *pTmiss_data)) {
-                std::cout << "MADE IT PASSED KINEMATIC CUTS" << std::endl;
+                
                 // Now find the phi bin within this group
                 std::vector<BinCounts>& bin_counts_vector = bin_counts_map[key];
 
