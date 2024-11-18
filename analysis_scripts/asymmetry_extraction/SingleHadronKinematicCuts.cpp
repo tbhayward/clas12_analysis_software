9c628b807f056a4983ccae8de43b5d6813367059
Timothy Hayward <tbhayward@email.wm.edu>
2024-11-18 14:29:31 -0500

    realized the unfolded yields from Sangbaek's file need to be the acceptance corrected yields not the signal yields, adjusted read csv file functions accordingly and now testing plotting comparison

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -22,7 +22,7 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
 
     if (*fiducial_status != 2) return false; // fiducial cuts
     if (*Q2 < 1) return false;
-    if (*W < 2) return false;
+    // if (*W < 2) return false;
     if (*y > 0.8) return false;
     if (property == "W") {
       goodEvent = true;
