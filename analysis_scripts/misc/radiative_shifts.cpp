72ec4aa46e80738dd9e29f17c63621341e9d6c29
Timothy Hayward <tbhayward@email.wm.edu>
2024-11-17 20:16:55 -0500

    began working on radiative_shift.cpp

--- analysis_scripts/misc/radiative_shifts.cpp
+++ analysis_scripts/misc/radiative_shifts.cpp
@@ -123,20 +123,21 @@ int main(int argc, char** argv) {
     tree2->SetBranchAddress(branch_name, &branch_value);
     tree2->SetBranchAddress("Q2", &Q2);
     tree2->SetBranchAddress("y", &y);
+    tree2->SetBranchAddress("W", &W);
     tree2->SetBranchAddress("runnum", &runnum);
     tree2->SetBranchAddress("evnum", &evnum);
 
     Long64_t nEntries2 = tree2->GetEntries();
     for (Long64_t i = 0; i < nEntries2; ++i) {
         tree2->GetEntry(i);
-        if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX) {
+        // if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX) {
             if (branch_value >= range_low && branch_value <= range_high) {
                 matching_event_pairs1.emplace(runnum, evnum);
             }
             if (has_second_region && branch_value >= range_low2 && branch_value <= range_high2) {
                 matching_event_pairs2.emplace(runnum, evnum);
             }
-        }
+        // }
     }
 
     // Step 2: Record positions of matching events in file1 starting from ratio_lower_bound
@@ -148,11 +149,12 @@ int main(int argc, char** argv) {
     tree1->SetBranchAddress(branch_name, &branch_value);
     tree1->SetBranchAddress("Q2", &Q2);
     tree1->SetBranchAddress("y", &y);
+    tree1->SetBranchAddress("W", &W);
 
     Long64_t nEntries1 = tree1->GetEntries();
     for (Long64_t i = 0; i < nEntries1; ++i) {
         tree1->GetEntry(i);
-        if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX && branch_value >= ratio_lower_bound) {
+        if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX && W >= W_MIN && W <= W_MAX && branch_value >= ratio_lower_bound) {
             if (matching_event_pairs1.find({runnum, evnum}) != matching_event_pairs1.end()) {
                 hist3->Fill(branch_value);
             }
