ba7c9fde2763c26f89554135c98f6e02f5a0f89c
Timothy Hayward <tbhayward@email.wm.edu>
2024-11-17 20:25:30 -0500

    began working on radiative_shift.cpp

--- analysis_scripts/misc/radiative_shifts.cpp
+++ analysis_scripts/misc/radiative_shifts.cpp
@@ -129,14 +129,14 @@ int main(int argc, char** argv) {
     Long64_t nEntries2 = tree2->GetEntries();
     for (Long64_t i = 0; i < nEntries2; ++i) {
         tree2->GetEntry(i);
-        if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX) {
+        // if (Q2 >= Q2_MIN && Q2 <= Q2_MAX && y >= y_MIN && y <= y_MAX) {
             if (branch_value >= range_low && branch_value <= range_high) {
                 matching_event_pairs1.emplace(runnum, evnum);
             }
             if (has_second_region && branch_value >= range_low2 && branch_value <= range_high2) {
                 matching_event_pairs2.emplace(runnum, evnum);
             }
-        }
+        // }
     }
 
     // Step 2: Record positions of matching events in file1 starting from ratio_lower_bound
