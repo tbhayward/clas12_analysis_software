367a7a9454e852627517e2cd04715703c9db80e5
Timothy Hayward <tbhayward@email.wm.edu>
2024-11-20 11:53:18 -0500

    added energy conservation to beamEnergy.java isRadiative calculation

--- analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/SingleHadronKinematicCuts.cpp
@@ -25,7 +25,6 @@ bool SingleHadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
     // if (*W < 2) return false;
     // if (*y > 0.8) return false;
     if (property == "W" || property == "x") {
-        std::cout << "HELLO WORLD" << std::endl;
       goodEvent = true;
       return goodEvent;
     }
