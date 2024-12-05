3142f3a19d4e2810faca12236c6a96f0393c1d53
Timothy Hayward <tbhayward@email.wm.edu>
2024-12-04 20:42:37 -0500

    adjusted kinematic cuts to work as 3 sigma range, updated write csv to record the cross section

--- analysis_scripts/dvcs_cross_section/dvcs_cross_section_extraction.cpp
+++ analysis_scripts/dvcs_cross_section/dvcs_cross_section_extraction.cpp
@@ -339,7 +339,7 @@ int main(int argc, char* argv[]) {
     // write_csv("output/unfolding_data.csv", all_unfolding_data);
 
     // Call plot_comparison
-    plot_comparison(binning_file, "output/unfolding_data.csv");
+    // plot_comparison(binning_file, "output/unfolding_data.csv");
     plot_cross_section_comparison(binning_file, "output/unfolding_data.csv");
 
     std::cout << "Program complete. Additional functionality to be added later." << std::endl << std::endl;
