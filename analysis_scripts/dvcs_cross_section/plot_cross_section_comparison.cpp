7aee765ec353baad3d57de655fe40745058ce384
Timothy Hayward <tbhayward@email.wm.edu>
2024-12-05 11:15:37 -0500

    adjusted kinematic cuts to work as 3 sigma range, updated write csv to record the cross section

--- analysis_scripts/dvcs_cross_section/plot_cross_section_comparison.cpp
+++ analysis_scripts/dvcs_cross_section/plot_cross_section_comparison.cpp
@@ -221,7 +221,7 @@ CrossSectionData read_second_csv(const std::string &filename) {
         // entry.sys_uncertainty = 0;
 
         entry.cross_section=0.98*entry.cross_section/0.80/0.93;
-        entry.stat_uncertainty=2*0.98*entry.stat_uncertainty/0.80/0.93;
+        entry.stat_uncertainty=0.98*entry.stat_uncertainty/0.80/0.93;
         entry.sys_uncertainty=0.204*entry.cross_section*3;
 
         data.push_back(entry);
