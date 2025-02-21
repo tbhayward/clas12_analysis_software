9388053580807bfc8ad22d32d2cb1ce4d257f016
Timothy Hayward <tbhayward@email.wm.edu>
2025-02-21 18:08:55 -0500

    fixed Frad calculation to invert the value, dvcsgen gives Frad/Fborn but we want Fborn/Frad

--- analysis_scripts/asymmetry_extraction/B2BDihadronKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/B2BDihadronKinematicCuts.cpp
@@ -21,34 +21,9 @@ bool B2BDihadronKinematicCuts::applyCuts(int currentFits, bool isMC) {
     if (*p1_p < 1.2 || *xF1 < 0 || *Mx2_1 < 3.24) return false;
     return true;
 
-    if (property == "PTPT") {
-        goodEvent = goodEvent;
-    } else if (property == "b2bchannel") {
-      goodEvent = goodEvent;
-    } else if (property == "b2bchannelMxStudy") {
-      goodEvent = goodEvent; // && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2;
-    } else if (property == "b2bchannelMx1Study") {
-      goodEvent = goodEvent; // && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2;
-    } else if (property == "b2bchannelMx2Study") {
-      goodEvent = goodEvent; // && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2;
-    } else if (property == "b2bchannelxF1Study") {
-      goodEvent = goodEvent && *Mx > 0.95 && *Mx1 > 1.8 && *Mx2 > 1.4;
-    } else if (property == "b2bchannelxF2Study") {
-      goodEvent = goodEvent && *Mx > 0.95 && *Mx1 > 1.8 && *Mx2 > 1.4;
-    } else if (property == "b2banalysis") {
-      goodEvent = goodEvent && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2 && *Mx > 0.95 && 
-        *Mx1 > 1.8 && *Mx2 > 1.4;
-    } else if (property == "b2banalysisx") {
-      goodEvent = goodEvent && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2 && *Mx > 0.95 && 
-        *Mx1 > 1.8 && *Mx2 > 1.4;
-    } else if (property == "b2banalysispTpT") {
-      // goodEvent = goodEvent && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2 && *Mx > 0.95 && 
-      //   *Mx1 > 1.8 && *Mx2 > 1.4;
-      goodEvent = goodEvent && *xF1 > 0 && *xF2 < 0 && *z1 > 0.2 && *Mx > 1.2 && 
-        *Mx1 > 1.5 && *Mx2 > 1.2;
-    } else {
-      std::cout << "Property, " << property << ", not detected!" << std::endl;
-    }
+    // else {
+    //   std::cout << "Property, " << property << ", not detected!" << std::endl;
+    // }
     
     if (isMC || (*runnum < 16042 || *runnum > 17811)) {
       return goodEvent;
