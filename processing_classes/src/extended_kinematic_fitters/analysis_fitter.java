289bab967b941798b5808cfce9ded55a12813dbe
Timothy Hayward <tbhayward@email.wm.edu>
2025-04-02 17:19:06 -0400

    reset to having pid and fiducial cuts on after creation of latifa branch

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -30,14 +30,14 @@ public class analysis_fitter extends GenericKinematicFitter {
         pid_cuts pid_cuts = new pid_cuts();
 
         return true
-//                && p > 2.0 // higher cut ultimately enforced when we cut on y, this speeds processing
-//                && generic_tests.forward_detector_cut(particle_Index, rec_Bank)
-//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-//                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, 1)
-//                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
-//                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
-//                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 1, run_Bank, rec_Bank, cal_Bank)
-//                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
+                && p > 2.0 // higher cut ultimately enforced when we cut on y, this speeds processing
+                && generic_tests.forward_detector_cut(particle_Index, rec_Bank)
+                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, 1)
+                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
+                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank)
+                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 1, run_Bank, rec_Bank, cal_Bank)
+                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
                 ;
     }
 
@@ -56,21 +56,21 @@ public class analysis_fitter extends GenericKinematicFitter {
         boolean passesCentralDetector = generic_tests.central_detector_cut(particle_Index, rec_Bank);
 
         return true
-                //                && p > 1.20
-                //            && p < 5.00 
-//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-//                && (passesForwardDetector // dedicated PID cuts for forward
-//                        //                        ? pid_cuts.charged_hadron_pass2_chi2pid_cut(particle_Index, rec_Bank)
-//                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
-//                        : true)
-//                && (passesCentralDetector // generic |chi2pid| < 3.5 for cd
-//                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
-//                        : true) //                && (passesForwardDetector
-                //                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
-                //                        : true)
-                //                && (passesCentralDetector
-                //                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
-                //                        : true)
+                                && p > 1.20
+                            && p < 5.00 
+                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+                && (passesForwardDetector // dedicated PID cuts for forward
+                        //                        ? pid_cuts.charged_hadron_pass2_chi2pid_cut(particle_Index, rec_Bank)
+                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
+                        : true)
+                && (passesCentralDetector // generic |chi2pid| < 3.5 for cd
+                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
+                        : true) //                && (passesForwardDetector
+//                                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
+//                                        : true)
+//                                && (passesCentralDetector
+//                                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
+//                                        : true)
                 ;
     }
 
@@ -126,25 +126,25 @@ public class analysis_fitter extends GenericKinematicFitter {
         boolean passesCentralDetector = generic_tests.central_detector_cut(particle_Index, rec_Bank);
 
         return true
-//                && p > 0.4
-//                && (passesCentralDetector ? p > 0.3 : true)
-//                && (passesForwardDetector && (torus > 0) ? p > 0.42 : true)
-//                && (passesForwardDetector && (torus < 0) ? p > 0.50 : true)
-//                && p < 1.2 // this bound is enforced at p < 1.14 by -t < 1, done here to speed up processing
-//                && generic_tests.theta_calculation(px, py, pz) < 64.23
-//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-//                && (passesForwardDetector
-//                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
-//                        : true)
-//                && (passesCentralDetector
-//                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
-//                        : true)
-//                && (passesForwardDetector // dedicated PID cuts for forward
-//                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
-//                        : true)
-//                && (passesCentralDetector
-//                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
-//                        : true) //            && charged_hadron_chi2pid_cut(particle_Index, rec_Bank)
+                && p > 0.4
+                && (passesCentralDetector ? p > 0.3 : true)
+                && (passesForwardDetector && (torus > 0) ? p > 0.42 : true)
+                && (passesForwardDetector && (torus < 0) ? p > 0.50 : true)
+                && p < 1.2 // this bound is enforced at p < 1.14 by -t < 1, done here to speed up processing
+                && generic_tests.theta_calculation(px, py, pz) < 64.23
+                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+                && (passesForwardDetector
+                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
+                        : true)
+                && (passesCentralDetector
+                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank)
+                        : true)
+                && (passesForwardDetector // dedicated PID cuts for forward
+                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
+                        : true)
+                && (passesCentralDetector
+                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
+                        : true) //            && charged_hadron_chi2pid_cut(particle_Index, rec_Bank)
                 ;
     }
 
@@ -166,12 +166,12 @@ public class analysis_fitter extends GenericKinematicFitter {
         boolean passesForwardTagger = generic_tests.forward_tagger_cut(particle_Index, rec_Bank);
 
         return true
-//                && (num_photon == 0 ? p > 2.0 : p > 0.5)
-//                && (passesForwardDetector || passesForwardTagger)
-//                && (passesForwardDetector
-//                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 1, run_Bank, rec_Bank, cal_Bank)
-//                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
-//                && pid_cuts.beta_cut(particle_Index, rec_Bank)
+                && (num_photon == 0 ? p > 2.0 : p > 0.5)
+                && (passesForwardDetector || passesForwardTagger)
+                && (passesForwardDetector
+                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 1, run_Bank, rec_Bank, cal_Bank)
+                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
+                && pid_cuts.beta_cut(particle_Index, rec_Bank)
             ;
     }
 
