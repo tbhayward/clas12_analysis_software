0d4a9bab64e91aa0d0a6a7e113bad26dd94fffcf
Timothy Hayward <tbhayward@email.wm.edu>
2025-04-11 10:20:16 -0400

    added new processing_mc_dvcs to processing.csh and convert_txt_to_root files, testing begins

--- analysis_scripts/rga_dvcs_efficiency/efficiency_analysis.py
+++ analysis_scripts/rga_dvcs_efficiency/efficiency_analysis.py
@@ -344,7 +344,7 @@ def plot_data_normalized_efficiencies(output_dir):
             50: 2.1047e6
         },
         "rga_sp19_inb": {
-            # 5: 30860.979,
+            5: 30860.979,
             10: 1.02766e6,
             50: 3.57217e6
         }
@@ -495,7 +495,7 @@ def plot_data_normalized_efficiencies(output_dir):
         plt.xlabel("Current (nA)")
         plt.ylabel("Normalized Efficiency")
         plt.xlim(-5, 60)
-        plt.ylim(0.0, 1.5)  # Set y-axis limits as requested.
+        # plt.ylim(0.0, 1.5)  # Set y-axis limits as requested.
         plt.legend(loc='upper right')
         plt.title(f"Normalized Efficiency for {title_map.get(run_prefix, run_prefix)}")
         
