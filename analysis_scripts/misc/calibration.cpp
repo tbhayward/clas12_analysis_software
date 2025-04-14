506b771e61e405bb6560347a79908b6fd3be5aff
Timothy Hayward <tbhayward@email.wm.edu>
2025-04-14 11:12:43 -0400

    added new processing_mc_dvcs to processing.csh and convert_txt_to_root files, testing begins

--- analysis_scripts/misc/calibration.cpp
+++ analysis_scripts/misc/calibration.cpp
@@ -8936,8 +8936,8 @@ int main(int argc, char** argv) {
 
     //// PLOTS ////
 
-    std::string dataset = "rga_fa18_inb";
-    // std::string dataset = "rga_fa18_out";
+    // std::string dataset = "rga_fa18_inb";
+    std::string dataset = "rga_fa18_out";
     // std::string dataset = "rga_sp19_inb";
 
     // plot_htcc_nphe(dataReader, mcReader, dataset);
@@ -9006,8 +9006,8 @@ int main(int argc, char** argv) {
 
     dataReader.Restart();
     if (mcReader) mcReader->Restart();
-    if (mcReader) energy_loss(*mcReader, "rga_fa18_inb"); 
-    // if (mcReader) energy_loss(*mcReader, "rga_fa18_out");  
+    // if (mcReader) energy_loss(*mcReader, "rga_fa18_inb"); 
+    if (mcReader) energy_loss(*mcReader, "rga_fa18_out");  
     // if (mcReader) energy_loss(*mcReader, "rga_sp19_inb"); 
     // if (mcReader) energy_loss(*mcReader, "rgc_su22_inb");   
 
