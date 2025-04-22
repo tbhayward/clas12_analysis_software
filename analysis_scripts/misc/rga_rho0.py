991d37c85ea515b6e8f013a5fc49f8dc3c99b362
Timothy Hayward <tbhayward@email.wm.edu>
2025-04-22 14:40:11 -0400

    created rga rho0 ALU script under analysis/misc

--- analysis_scripts/misc/rga_rho0.py
+++ analysis_scripts/misc/rga_rho0.py
@@ -13,8 +13,8 @@ def sin_func(phi, A):
 # 1) Define your files and labels
 files = {
     "RGA Fa18 Inb":  "/work/clas12/thayward/CLAS12_SIDIS/processed_data/pass2/data/epi+pi-X/rga_fa18_inb_epi+pi-X.root",
-    # "RGA Fa18 Out":  "/work/clas12/thayward/CLAS12_SIDIS/processed_data/pass2/data/epi+pi-X/rga_fa18_out_epi+pi-X.root",
-    # "RGA Sp19 Inb":  "/work/clas12/thayward/CLAS12_SIDIS/processed_data/pass2/data/epi+pi-X/rga_sp19_inb_epi+pi-X.root"
+    "RGA Fa18 Out":  "/work/clas12/thayward/CLAS12_SIDIS/processed_data/pass2/data/epi+pi-X/rga_fa18_out_epi+pi-X.root",
+    "RGA Sp19 Inb":  "/work/clas12/thayward/CLAS12_SIDIS/processed_data/pass2/data/epi+pi-X/rga_sp19_inb_epi+pi-X.root"
 }
 
 # 2) Set up bins in cos(theta)
@@ -32,7 +32,7 @@ results = {}
 for label, path in files.items():
     tree = uproot.open(path)["PhysicsEvents"]
     data = tree.arrays(
-        ["helicity","beam_pol","DepA","DepW","theta","phi","W","Q2","z","Mx","Mh"],
+        ["helicity","beam_pol","DepA","DepW","theta","phi","W","Q2","z","Mx"],
         library="np"
     )
 
@@ -41,9 +41,7 @@ for label, path in files.items():
         (data["W"]  > 2) &
         (data["Q2"] > 2) &
         (data["z"]  > 0.9) &
-        (data["Mx"] < 1.05) &
-        (data["Mh"] > 0.65) &
-        (data["Mh"] < 0.85)
+        (data["Mx"] < 1.05)
     )
     helicity = data["helicity"][mask]
     beam_pol  = data["beam_pol"][mask]
@@ -55,87 +53,71 @@ for label, path in files.items():
     cos_th = np.cos(theta)
 
     # Prepare arrays
-    amps_unw = np.zeros(len(cos_centers))
-    errs_unw = np.zeros(len(cos_centers))
-    amps_w   = np.zeros(len(cos_centers))
-    errs_w   = np.zeros(len(cos_centers))
+    amps_unw     = np.zeros(len(cos_centers))
+    errs_unw     = np.zeros(len(cos_centers))
+    mean_bp      = np.zeros(len(cos_centers))
+    mean_DepA    = np.zeros(len(cos_centers))
+    mean_DepW    = np.zeros(len(cos_centers))
 
     # 6) Loop over cos(theta) bins
     for i in range(len(cos_centers)):
         sel_cos = (cos_th >= cos_bins[i]) & (cos_th < cos_bins[i+1])
         if not np.any(sel_cos):
             amps_unw[i], errs_unw[i] = 0.0, 0.0
-            amps_w[i],   errs_w[i]   = 0.0, 0.0
-            continue
-        #endif
-
-        # Lists for phi-dependent asymmetry
-        A_phi_unw, err_phi_unw = [], []
-        A_phi_w,   err_phi_w   = [], []
-
-        # 7) Loop over phi bins
-        for j in range(len(phi_centers)):
-            sel_phi = sel_cos & (phi >= phi_bins[j]) & (phi < phi_bins[j+1])
-
-            # Unweighted asymmetry
-            hj = helicity[sel_phi]
-            n_p = np.sum(hj > 0)
-            n_m = np.sum(hj < 0)
-            N   = n_p + n_m
-            if N > 0:
-                A_j   = (n_p - n_m) / N
-                err_j = np.sqrt((1 - A_j**2) / N)
-            else:
-                A_j, err_j = 0.0, 0.0
+            mean_bp[i] = np.nan
+            mean_DepA[i] = np.nan
+            mean_DepW[i] = np.nan
             #endif
-            A_phi_unw.append(A_j)
-            err_phi_unw.append(err_j)
+        else:
+            # track means
+            mean_bp[i]   = np.mean(beam_pol[sel_cos])
+            mean_DepA[i] = np.mean(DepA[sel_cos])
+            mean_DepW[i] = np.mean(DepW[sel_cos])
+            
+            # prepare lists for phi-dependent asymmetry
+            A_phi, err_phi = [], []
+            
+            # 7) Loop over phi bins
+            for j in range(len(phi_centers)):
+                sel_phi = sel_cos & (phi >= phi_bins[j]) & (phi < phi_bins[j+1])
+                
+                hj = helicity[sel_phi]
+                n_p = np.sum(hj > 0)
+                n_m = np.sum(hj < 0)
+                N   = n_p + n_m
+                if N > 0:
+                    A_j   = (n_p - n_m) / N
+                    err_j = np.sqrt((1 - A_j**2) / N)
+                else:
+                    A_j, err_j = 0.0, 0.0
+                #endif
+                A_phi.append(A_j)
+                err_phi.append(err_j)
+            #endfor
 
-            # Weighted asymmetry
-            wj = DepA[sel_phi] / (beam_pol[sel_phi] * DepW[sel_phi])
-            w_p   = wj[hj > 0]
-            w_m   = wj[hj < 0]
-            sp, sm   = np.sum(w_p), np.sum(w_m)
-            sp2, sm2 = np.sum(w_p**2), np.sum(w_m**2)
-            Nw = sp + sm
-            if Nw > 0:
-                A_w   = (sp - sm) / Nw
-                N_eff = Nw**2 / (sp2 + sm2)
-                err_w = np.sqrt((1 - A_w**2) / N_eff)
-            else:
-                A_w, err_w = 0.0, 0.0
+            # 8) Fit unweighted A(phi) → A·sin(phi)
+            try:
+                popt, pcov    = curve_fit(
+                    sin_func, phi_centers, A_phi,
+                    sigma=err_phi, absolute_sigma=True
+                )
+                amps_unw[i]  = popt[0]
+                errs_unw[i]  = np.sqrt(pcov[0,0])
+            except:
+                amps_unw[i], errs_unw[i] = 0.0, 0.0
             #endif
-            A_phi_w.append(A_w)
-            err_phi_w.append(err_w)
-        #endfor
-
-        # 8) Fit unweighted A(phi) → A·sin(phi)
-        try:
-            popt_u, pcov_u     = curve_fit(sin_func, phi_centers, A_phi_unw,
-                                            sigma=err_phi_unw, absolute_sigma=True)
-            amps_unw[i]        = popt_u[0]
-            errs_unw[i]        = np.sqrt(pcov_u[0,0])
-        except:
-            amps_unw[i], errs_unw[i] = 0.0, 0.0
         #endif
-
-        # 9) Fit weighted A(phi)
-        try:
-            popt_w, pcov_w   = curve_fit(sin_func, phi_centers, A_phi_w,
-                                          sigma=err_phi_w, absolute_sigma=True)
-            amps_w[i]        = popt_w[0]
-            errs_w[i]        = np.sqrt(pcov_w[0,0])
-        except:
-            amps_w[i], errs_w[i] = 0.0, 0.0
-        #endif
-
     #endfor
+
+    # 9) Apply DepA/DepW & beam_pol scaling
+    amps_corr = amps_unw * mean_DepA / (mean_bp * mean_DepW)
+    errs_corr = errs_unw * mean_DepA / (mean_bp * mean_DepW)
 
     results[label] = {
         "amp_unw": amps_unw,
         "err_unw": errs_unw,
-        "amp_w":   amps_w,
-        "err_w":   errs_w
+        "amp_w":   amps_corr,
+        "err_w":   errs_corr
     }
 #endfor
 
@@ -158,7 +140,7 @@ ax.text(0.02, 0.98,
 plt.savefig("output/rga_rho0_first_step.pdf")
 plt.close()
 
-# 12) Weighted (final amplitude) plot
+# 12) Corrected (final) amplitude plot
 fig, ax = plt.subplots()
 for label, res in results.items():
     ax.errorbar(cos_centers, res["amp_w"], yerr=res["err_w"],
