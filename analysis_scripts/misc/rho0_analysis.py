7424d209af30f1afe278bfebbd42f79e4addd185
Timothy Hayward <tbhayward@email.wm.edu>
2025-04-23 11:46:45 -0400

    created rga rho0 ALU script under analysis/misc

--- analysis_scripts/misc/rho0_analysis.py
+++ analysis_scripts/misc/rho0_analysis.py
@@ -70,17 +70,17 @@ def kinematic_cuts(ev, use_missing_mass_cuts=False, beam_energy=10.6):
     delta_th = np.abs(p3_theta - theta_calc)
 
     mask = (
-        (Q2    > 2.0) &
-        (W     > 2.0) &
-        (y     < 0.75) &
-        (z_rho > 0.9) &
-        (delta_th < 0.05)
+        # (Q2    > 2.0) &
+        # (W     > 2.0) &
+        # (y     < 0.75) &
+        # (z_rho > 0.9) &
+        # (delta_th < 0.05)
     )
     if use_missing_mass_cuts:
         mask &= (
-            (np.abs(Mx2) < 0.01) &
-            (Mx2_2      > 3.24) &
-            (Mx2_3      > 1.8225)
+            # (np.abs(Mx2) < 0.01) &
+            # (Mx2_2      > 3.24) &
+            # (Mx2_3      > 1.8225)
         )
     return mask
 
