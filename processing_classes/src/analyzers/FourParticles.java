4f03aff97619abaa20f5e581811b53fcf050a313
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-08 11:26:17 -0400

    created corrections_study.py for misc

--- processing_classes/src/analyzers/FourParticles.java
+++ processing_classes/src/analyzers/FourParticles.java
@@ -205,6 +205,8 @@ public class FourParticles {
             detector3 = 2; // Central Detector
         }
         
+        System.out.println(detector1+" "+detector2+" "+detector3);
+        
 
         // Set up Lorentz vectors
         // beam electron
