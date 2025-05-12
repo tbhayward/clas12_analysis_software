6d8807d271b4379afa8380f6c66ca093d199ac3d
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-12 18:12:50 -0400

    created corrections_study.py for misc

--- processing_scripts/processing_two_particles.groovy
+++ processing_scripts/processing_two_particles.groovy
@@ -111,8 +111,8 @@ public static void main(String[] args) {
 	// GenericKinematicFitter fitter = new event_builder_fitter(10.6041);  
 	
 	// set filter for final states
-	// EventFilter filter = new EventFilter("11:"+p1_Str+":X+:X-:Xn");
-	EventFilter filter = new EventFilter("11:"+p1_Str);
+	EventFilter filter = new EventFilter("11:"+p1_Str+":X+:X-:Xn");
+	// EventFilter filter = new EventFilter("11:"+p1_Str);
 	
 	// instantiate QADB
 	QADB qa = new QADB()
