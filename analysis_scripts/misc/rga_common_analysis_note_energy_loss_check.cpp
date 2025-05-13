a27c769341b12896e2e19b428892cb1e22789806
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-13 18:27:16 -0400

    created corrections_study.py for misc

--- analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
+++ analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
@@ -512,7 +512,7 @@ void plot_two_pions(
     // 2) Branches: missing mass² and proton θ
     Double_t Mx2[nFiles], p1_theta[nFiles];
     for (int i = 0; i < nFiles; ++i) {
-        tree[i]->SetBranchAddress("Mx2_12",   &Mx2[i]);
+        tree[i]->SetBranchAddress("Mx2_13",   &Mx2[i]);
         tree[i]->SetBranchAddress("p1_theta",&p1_theta[i]);
     }
 
@@ -714,7 +714,7 @@ void plot_two_pions(
     gr[0]->GetXaxis()->SetTitle("#theta (deg)");
     gr[0]->GetYaxis()->SetTitle("#mu (GeV^{2})");
     gr[0]->GetXaxis()->SetLimits(thetaBins[0], thetaBins[nBins]);
-    gr[0]->GetYaxis()->SetRangeUser(-0.02, 0.04);
+    gr[0]->GetYaxis()->SetRangeUser(-0.01, 0.04);
 
     TLegend* leg12 = new TLegend(0.6,0.75,0.9,0.9);
     leg12->SetTextSize(0.03);
