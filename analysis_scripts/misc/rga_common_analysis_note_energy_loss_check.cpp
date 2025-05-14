89b7fe1c3354f7a5fa73e47b16a92f3a5ac96a86
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-14 09:40:47 -0400

    created corrections_study.py for misc

--- analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
+++ analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
@@ -1002,13 +1002,13 @@ int main(int argc, char** argv) {
     //     argv[1], argv[2], argv[3], argv[4], argv[5]
     // );
 
-    plot_eppi0_energy_loss_validation(
-        argv[1], argv[2], argv[3], argv[4], argv[5]
-    );
-
-    // plot_mx2_comparison_elastic(
+    // plot_eppi0_energy_loss_validation(
     //     argv[1], argv[2], argv[3], argv[4], argv[5]
     // );
+
+    plot_mx2_comparison_elastic(
+        argv[1], argv[2], argv[3], argv[4], argv[5]
+    );
 
     // plot_two_pions(
     //     argv[1], argv[2], argv[3], argv[4], argv[5]
