a861d4d158b8aa049e43c519c5db370e83ab6a39
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-19 11:22:08 -0400

    created corrections_study.py for misc

--- analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
+++ analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
@@ -1625,8 +1625,12 @@ int main(int argc, char** argv) {
 
     /////////
 
-    plot_dvcs_sebastian_energy_loss_validation(
-        argv[1], argv[2], argv[3], argv[4]
+    // plot_dvcs_sebastian_energy_loss_validation(
+    //     argv[1], argv[2], argv[3], argv[4]
+    // );
+
+    plot_eppi0_sebastian_energy_loss_validation(
+        argv[1], argv[2], argv[3], argv[4], false
     );
 
     // plot_eppi0_sebastian_energy_loss_validation(
