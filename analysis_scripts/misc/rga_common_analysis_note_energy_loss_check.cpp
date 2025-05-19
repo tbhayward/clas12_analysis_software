9ae98fdc1aa7838969fa7b3aabb7fdeb03844302
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-19 11:15:17 -0400

    created corrections_study.py for misc

--- analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
+++ analysis_scripts/misc/rga_common_analysis_note_energy_loss_check.cpp
@@ -1562,13 +1562,13 @@ void plot_eppi0_sebastian_energy_loss_validation(
 
     const double pi0Mass = 0.135;
     // zero line
-    TLine* zero = new TLine(thetaBins[0], pi0Mass*pi0Mass, thetaBins[nBins], pi0Mass*pi0Mass);
+    TLine* zero = new TLine(thetaBins[0], pi0Mass*pi0Mass, 70, pi0Mass*pi0Mass);
     zero->SetLineColor(kGray);
     zero->SetLineStyle(2);
     zero->Draw("SAME");
 
     // π0‐mass line
-    TLine* pi0line = new TLine(thetaBins[0], pi0Mass, thetaBins[nBins], pi0Mass);
+    TLine* pi0line = new TLine(thetaBins[0], pi0Mass, 70, pi0Mass);
     pi0line->SetLineColor(kGray);
     pi0line->SetLineStyle(2);
     pi0line->Draw("SAME");
@@ -1629,13 +1629,11 @@ int main(int argc, char** argv) {
     //     argv[1], argv[2], argv[3], argv[4]
     // );
 
-    // plot_eppi0_sebastian_energy_loss_validation(
-    //     argv[1], argv[2], argv[3], argv[4], false 
-    // );
+    plo 
 
-    plot_eppi0_sebastian_energy_loss_validation(
-        argv[1], argv[2], argv[3], argv[4], true
-    );
+    // plot_eppi0_sebastian_energy_loss_validation(
+    //     argv[1], argv[2], argv[3], argv[4], true
+    // );
 
 
     return 0;
