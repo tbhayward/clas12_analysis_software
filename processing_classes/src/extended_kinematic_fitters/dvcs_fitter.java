dd4630995bf8655d75ca909514e3d697f6e0176a
Timothy Hayward <tbhayward@email.wm.edu>
2025-05-23 10:14:42 -0400

    created corrections_study.py for misc

--- processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
@@ -203,7 +203,7 @@ public class dvcs_fitter extends GenericKinematicFitter {
                 if (pid == 22 && photon_test(particle_Index, run_Bank, rec_Bank, cal_Bank, ft_Bank, lv_e)) {
 
                     float[] momentum = {px, py, pz};
-//                    energy_loss_corrections.sebastian_photon_energy_loss_corrections(particle_Index, momentum, rec_Bank, run_Bank);
+                    energy_loss_corrections.sebastian_photon_energy_loss_corrections(particle_Index, momentum, rec_Bank, run_Bank);
 //
                     px = momentum[0];
                     py = momentum[1];
