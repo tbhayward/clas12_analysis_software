bcdd16b61b891a92a06cce1a764e53d108952f94
Timothy Hayward <tbhayward@email.wm.edu>
2025-06-02 18:50:37 -0400

    created corrections_study.py for misc

--- analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_extraction.cpp
@@ -253,23 +253,23 @@ int main(int argc, char *argv[]) {
   for (size_t i = 0; i < allBins.size(); ++i) {
     cout << "-- Beginning kinematic fits." << endl;
 
-    if (cpp != 1) {
-        // Calculate the dilution factors for the current bin
-        dilutionFactors = calculate_dilution_factors();
+    // if (cpp != 1) {
+    //     // Calculate the dilution factors for the current bin
+    //     dilutionFactors = calculate_dilution_factors();
 
-        // Print out the dilution factors and their uncertainties
-        cout << "Dilution Factors for Bin Set " << i + 1 << ":" << endl;
-        for (size_t j = 0; j < dilutionFactors.size(); ++j) {
-            cout << "Bin " << j + 1 << ": "
-                 << "Dilution Factor = " << dilutionFactors[j].first
-                 << ", Uncertainty = " << dilutionFactors[j].second
-                 << endl;
-        }
-    } else {
-        // If cpp == 1, fill dilutionFactors with zeros
-        dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {0.0, 0.0});
-    }
-    // dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {1.0, 0.0});
+    //     // Print out the dilution factors and their uncertainties
+    //     cout << "Dilution Factors for Bin Set " << i + 1 << ":" << endl;
+    //     for (size_t j = 0; j < dilutionFactors.size(); ++j) {
+    //         cout << "Bin " << j + 1 << ": "
+    //              << "Dilution Factor = " << dilutionFactors[j].first
+    //              << ", Uncertainty = " << dilutionFactors[j].second
+    //              << endl;
+    //     }
+    // } else {
+    //     // If cpp == 1, fill dilutionFactors with zeros
+    //     dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {0.0, 0.0});
+    // }
+    dilutionFactors = std::vector<std::pair<double, double>>(allBins[i].size() - 1, {1.0, 0.0});
 
     // minimum necessary change in charge values needed for 0 AUL offset
     // cpp = 1.06509e+06;
