19182e3971bbae43cd6c6c26ea0c8993f45d892e
Timothy Hayward <tbhayward@email.wm.edu>
2025-06-03 00:48:15 -0400

    created corrections_study.py for misc

--- analysis_scripts/misc/plot_rgc_epni+_asymmetry.py
+++ analysis_scripts/misc/plot_rgc_epni+_asymmetry.py
@@ -244,7 +244,7 @@ for p in ["Su22", "Fa22", "Sp23"]:
 # end for
 
 ax1.set_xlim(0, 0.7)
-ax1.set_ylim(-0.2, 0.3)
+ax1.set_ylim(-0.3, 0.3)
 ax1.set_xlabel(r"$x_{B}$", fontsize=label_fontsize)
 ax1.set_ylabel(r"$F_{LU}^{\sin\phi}/F_{UU}$", fontsize=label_fontsize)
 ax1.axhline(0, color="black", linestyle="--", linewidth=1.2)
@@ -296,7 +296,7 @@ for p in ["Su22", "Fa22", "Sp23"]:
 # end for
 
 ax2.set_xlim(0, 0.7)
-ax2.set_ylim(-0.2, 0.3)
+ax2.set_ylim(-0.3, 0.3)
 ax2.set_xlabel(r"$x_{B}$", fontsize=label_fontsize)
 ax2.set_ylabel(r"$F_{UL}^{\sin\,n\phi}/F_{UU}$", fontsize=label_fontsize)
 ax2.axhline(0, color="black", linestyle="--", linewidth=1.2)
