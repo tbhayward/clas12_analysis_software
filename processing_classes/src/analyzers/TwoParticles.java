39c6be8b67a15fb7f5e32cb1257e7b68add7edd1
Timothy Hayward <tbhayward@email.wm.edu>
2025-06-18 13:05:38 -0400

    added missing mass squared as input to fermi motion smearing

--- processing_classes/src/analyzers/TwoParticles.java
+++ processing_classes/src/analyzers/TwoParticles.java
@@ -186,8 +186,10 @@ public class TwoParticles {
         
         /* TOGGLE ON OR OFF IF FERMI MOTION DESIRED */
         // Simulate Fermi motion
-        org.jlab.clas.physics.Vector3 fermiP = momentum_corrections.sampleFermiMomentum();
+        org.jlab.clas.physics.Vector3 fermiP = momentum_corrections.sampleFermiMomentum(Mx2);
         lv_target.setPxPyPzM(fermiP.x(), fermiP.y(), fermiP.z(), kinematic_variables.particle_mass(2212));
+        Mx = kinematic_variables.Mx(lv_q, lv_target, lv_p);
+        Mx2 = kinematic_variables.Mx2(lv_q, lv_target, lv_p); // missing mass squared
 
         // kinematics of electron
         e_px = lv_e.px();
