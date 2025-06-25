54e2aad7592d9c7f0b880002ea54747090894bff
Timothy Hayward <tbhayward@email.wm.edu>
2025-06-25 10:10:15 -0400

    created processing_dvcs_calibration.groovy in order to do usual calibration study but imposing exclusivity cuts and requiring the epgamma channel

--- processing_scripts/processing_dvcs_calibration.groovy
+++ processing_scripts/processing_dvcs_calibration.groovy
@@ -309,7 +309,7 @@ class CalibrationScript {
 	        		// supply runnum and boolean for radiative simulation or not
 					BeamEnergy Eb = new BeamEnergy(research_Event, config_run, false);
 					// Use the input beam energy if runnum == 11, otherwise use Eb.Eb()
-					double b_energy = (runnum == 11) ? beam_energy : Eb.Eb();
+					double b_energy = (config_run == 11) ? beam_energy : Eb.Eb();
 		            ThreeParticles variables = new ThreeParticles(event, research_Event, 
 						2212, 0, 22, 0, b_energy);
 		            // this is my class for defining all relevant kinematic variables
