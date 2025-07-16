5f7d1acf11f4587485d9f59782260a9089b988aa
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-16 00:25:20 -0400

    added appropriately calculated beam polarizations from Moeller runs for RGC Fa22 and RGC Sp23 to convert_txt_to_root.cpp

--- analysis_scripts/misc/rgc_electron_htcc.py
+++ analysis_scripts/misc/rgc_electron_htcc.py
@@ -85,7 +85,7 @@ def main():
                 (sector6 != -9999) &
                 (vz >= vz_cuts[label][0]) &
                 (vz <= vz_cuts[label][1]) &
-                (p > 2.0) &
+                # (p > 2.0) &
                 (nphe != -9999) &
                 fid
             )
