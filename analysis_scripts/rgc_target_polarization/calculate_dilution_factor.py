827c8741bf0106194f4a60ee948412c49b2a76d1
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-17 14:18:39 -0400

    wrote calculate_dilution_factor.py and added to rgc polarization determination script

--- analysis_scripts/rgc_target_polarization/calculate_dilution_factor.py
+++ analysis_scripts/rgc_target_polarization/calculate_dilution_factor.py
@@ -30,33 +30,81 @@ def calculate_dilution_error(nNH3, nC, nCH2, nHe, nET, xNH3, xC, xCH2, xHe, xET)
     """
     Compute sigma_Df with debug output of each term.
     """
-    a = -1.0 * nHe * xNH3 + nNH3 * xHe
-    term1 = 5438.81 * nNH3 * nET * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
-            (1.0 * nHe * xC * xCH2 - 1.19655 * nCH2 * xC * xHe + 0.196547 * nC * xCH2 * xHe)**2
-    term2 = 85044.9 * nNH3 * nCH2 * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
-            (1.0 * nHe * xC * xET - 0.302592 * nET * xC * xHe - 0.697408 * nC * xET * xHe)**2
-    term3 = 47470.0 * nNH3 * nC * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
-            (1.0 * nHe * xCH2 * xET - 0.0665285 * nET * xCH2 * xHe - 0.933472 * nCH2 * xET * xHe)**2
-    term4 = 1371.83 * nHe**2 * xNH3**2 * \
-            (1.0 * nHe * xC * xCH2 * xET + (-1.97748 * nET * xC * xCH2 + 6.62306 * nCH2 * xC * xET - 5.64558 * nC * xCH2 * xET) * xHe)**2 * \
-            (1.0 * nHe * xC * xCH2 * xET + (0.0136533 * nET * xC * xCH2 - 1.25054 * nCH2 * xC * xET + 0.236883 * nC * xCH2 * xET) * xHe)**2
-    t5_p1 = 73.2426 * nHe**2 * xNH3 * xC**2 * xCH2**2 * xET**2
-    t5_p2 = nHe * xC * xCH2 * xET * \
-            (2.0 * nET * xNH3 * xC * xCH2 + (-183.185 * nCH2 * xNH3 * xC + 34.6998 * nC * xNH3 * xCH2 + 1.42109e-14 * nNH3 * xC * xCH2) * xET)
-    t5_p3 = (-1.97748 * nET**2 * xNH3 * xC**2 * xCH2**2) + \
-             nET * xC * xCH2 * (187.746 * nCH2 * xNH3 * xC - 39.9547 * nC * xNH3 * xCH2 - 145.836 * nNH3 * xC * xCH2) * xET + \
-             (-606.623 * nCH2**2 * xNH3 * xC**2 + nCH2 * xC * (632.002 * nC * xNH3 + 576.683 * nNH3 * xC) * xCH2 + \
-             nC * (-97.9502 * nC * xNH3 - 430.847 * nNH3 * xC) * xCH2**2) * xET**2
-    term5 = 0.255725 * nNH3 * nHe * (t5_p1 + t5_p2 * xHe + t5_p3 * xHe**2)**2
-    denom = nNH3**3 * xHe**2 * \
-            (73.2426 * nHe * xC * xCH2 * xET + 1.0 * nET * xC * xCH2 * xHe - 91.5925 * nCH2 * xC * xET * xHe + 17.3499 * nC * xCH2 * xET * xHe)**4
-    total = term1 + term2 + term3 + term4 + term5
-    print(f"[Debug Error] sum_terms={total:.4e}, denom={denom:.4e}")
-    var = total/denom if denom else 0.0
-    if var < 0:
-        print(f"[Warning] Negative var={var:.4e}, clamping to 0.")
-        var = 0.0
-    sigma_df = 27.3473 * math.sqrt(var)
+    # a = -1.0 * nHe * xNH3 + nNH3 * xHe
+    # term1 = 5438.81 * nNH3 * nET * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
+    #         (1.0 * nHe * xC * xCH2 - 1.19655 * nCH2 * xC * xHe + 0.196547 * nC * xCH2 * xHe)**2
+    # term2 = 85044.9 * nNH3 * nCH2 * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
+    #         (1.0 * nHe * xC * xET - 0.302592 * nET * xC * xHe - 0.697408 * nC * xET * xHe)**2
+    # term3 = 47470.0 * nNH3 * nC * xC**2 * xCH2**2 * xET**2 * xHe**2 * a**2 * \
+    #         (1.0 * nHe * xCH2 * xET - 0.0665285 * nET * xCH2 * xHe - 0.933472 * nCH2 * xET * xHe)**2
+    # term4 = 1371.83 * nHe**2 * xNH3**2 * \
+    #         (1.0 * nHe * xC * xCH2 * xET + (-1.97748 * nET * xC * xCH2 + 6.62306 * nCH2 * xC * xET - 5.64558 * nC * xCH2 * xET) * xHe)**2 * \
+    #         (1.0 * nHe * xC * xCH2 * xET + (0.0136533 * nET * xC * xCH2 - 1.25054 * nCH2 * xC * xET + 0.236883 * nC * xCH2 * xET) * xHe)**2
+    # t5_p1 = 73.2426 * nHe**2 * xNH3 * xC**2 * xCH2**2 * xET**2
+    # t5_p2 = nHe * xC * xCH2 * xET * \
+    #         (2.0 * nET * xNH3 * xC * xCH2 + (-183.185 * nCH2 * xNH3 * xC + 34.6998 * nC * xNH3 * xCH2 + 1.42109e-14 * nNH3 * xC * xCH2) * xET)
+    # t5_p3 = (-1.97748 * nET**2 * xNH3 * xC**2 * xCH2**2) + \
+    #          nET * xC * xCH2 * (187.746 * nCH2 * xNH3 * xC - 39.9547 * nC * xNH3 * xCH2 - 145.836 * nNH3 * xC * xCH2) * xET + \
+    #          (-606.623 * nCH2**2 * xNH3 * xC**2 + nCH2 * xC * (632.002 * nC * xNH3 + 576.683 * nNH3 * xC) * xCH2 + \
+    #          nC * (-97.9502 * nC * xNH3 - 430.847 * nNH3 * xC) * xCH2**2) * xET**2
+    # term5 = 0.255725 * nNH3 * nHe * (t5_p1 + t5_p2 * xHe + t5_p3 * xHe**2)**2
+    # denom = nNH3**3 * xHe**2 * \
+    #         (73.2426 * nHe * xC * xCH2 * xET + 1.0 * nET * xC * xCH2 * xHe - 91.5925 * nCH2 * xC * xET * xHe + 17.3499 * nC * xCH2 * xET * xHe)**4
+    # total = term1 + term2 + term3 + term4 + term5
+    # print(f"[Debug Error] sum_terms={total:.4e}, denom={denom:.4e}")
+    # var = total/denom if denom else 0.0
+    # if var < 0:
+    #     print(f"[Warning] Negative var={var:.4e}, clamping to 0.")
+    #     var = 0.0
+    # sigma_df = 27.3473 * math.sqrt(var)
+    # First part of the expression (term1)
+    double term1 = 5438.81 * nNH3 * nET * pow(xC, 2) * pow(xCH2, 2) * pow(xET, 2) * pow(xHe, 2)
+                   * pow((-1.0 * nHe * xNH3 + nNH3 * xHe), 2)
+                   * pow((1.0 * nHe * xC * xCH2 - 1.19655 * nCH2 * xC * xHe + 0.196547 * nC * xCH2 * xHe), 2);
+
+    # Second part of the expression (term2)
+    double term2 = 85044.9 * nNH3 * nCH2 * pow(xC, 2) * pow(xCH2, 2) * pow(xET, 2) * pow(xHe, 2)
+                   * pow((-1.0 * nHe * xNH3 + nNH3 * xHe), 2)
+                   * pow((1.0 * nHe * xC * xET - 0.302592 * nET * xC * xHe - 0.697408 * nC * xET * xHe), 2);
+
+    # Third part of the expression (term3)
+    double term3 = 47470.0 * nNH3 * nC * pow(xC, 2) * pow(xCH2, 2) * pow(xET, 2) * pow(xHe, 2)
+                   * pow((-1.0 * nHe * xNH3 + nNH3 * xHe), 2)
+                   * pow((1.0 * nHe * xCH2 * xET - 0.0665285 * nET * xCH2 * xHe - 0.933472 * nCH2 * xET * xHe), 2);
+
+    # Fourth part of the expression (term4)
+    double term4 = 1371.83 * pow(nHe, 2) * pow(xNH3, 2)
+                   * pow((1.0 * nHe * xC * xCH2 * xET + (-1.97748 * nET * xC * xCH2 + 6.62306 * nCH2 * xC * xET - 5.64558 * nC * xCH2 * xET) * xHe), 2)
+                   * pow((1.0 * nHe * xC * xCH2 * xET + (0.0136533 * nET * xC * xCH2 - 1.25054 * nCH2 * xC * xET + 0.236883 * nC * xCH2 * xET) * xHe), 2);
+
+    # Fifth part of the expression (term5)
+    # First part of the numerator
+    double term5_numerator_part1 = 73.2426 * pow(nHe, 2) * xNH3 * pow(xC, 2) * pow(xCH2, 2) * pow(xET, 2);
+
+    # Second part of the numerator (to be multiplied by xHe)
+    double term5_numerator_part2 = nHe * xC * xCH2 * xET
+                                   * (2.0 * nET * xNH3 * xC * xCH2 + (-183.185 * nCH2 * xNH3 * xC + 34.6998 * nC * xNH3 * xCH2 + 1.42109e-14 * nNH3 * xC * xCH2) * xET);
+
+    # Third part of the numerator (to be multiplied by xHe^2)
+    double term5_numerator_part3 = (-1.97748 * pow(nET, 2) * xNH3 * pow(xC, 2) * pow(xCH2, 2))
+                                   + nET * xC * xCH2 * (187.746 * nCH2 * xNH3 * xC - 39.9547 * nC * xNH3 * xCH2 - 145.836 * nNH3 * xC * xCH2) * xET
+                                   + (-606.623 * pow(nCH2, 2) * xNH3 * pow(xC, 2)
+                                      + nCH2 * xC * (632.002 * nC * xNH3 + 576.683 * nNH3 * xC) * xCH2
+                                      + nC * (-97.9502 * nC * xNH3 - 430.847 * nNH3 * xC) * pow(xCH2, 2)
+                                     ) * pow(xET, 2);
+
+    # The full numerator
+    double term5_numerator = term5_numerator_part1 + term5_numerator_part2 * xHe + term5_numerator_part3 * pow(xHe, 2);
+
+    double term5 = 0.255725 * nNH3 * nHe * pow(term5_numerator, 2);
+
+    # DenominNH3tor of the expression
+    double denominNH3tor = pow(nNH3, 3) * pow(xHe, 2)
+                         * pow((73.2426 * nHe * xC * xCH2 * xET + 1.0 * nET * xC * xCH2 * xHe - 91.5925 * nCH2 * xC * xET * xHe + 17.3499 * nC * xCH2 * xET * xHe), 4);
+
+    # FinNH3l error calculation
+    double sigma_df = 27.3473 * sqrt((term1 + term2 + term3 + term4 + term5) / denominNH3tor);
+
     print(f"[Error] sigma_Df={sigma_df:.4e}")
     return sigma_df
 
