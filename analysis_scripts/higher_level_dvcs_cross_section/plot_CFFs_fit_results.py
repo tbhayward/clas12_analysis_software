95d49cdc49eaf83e7d71101d6248cd90067c9839
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-23 17:10:46 -0400

    updated comments in code for increased clarity

--- analysis_scripts/higher_level_dvcs_cross_section/plot_CFFs_fit_results.py
+++ analysis_scripts/higher_level_dvcs_cross_section/plot_CFFs_fit_results.py
@@ -167,7 +167,7 @@ from matplotlib.lines import Line2D
 
 legend_elements = [
     Line2D([0], [0], color='tab:blue', linestyle='-', lw=2.5, label='Original'),
-    Line2D([0], [0], color='tab:red',  linestyle='--',lw=2.5, label='Fit median'),
+    Line2D([0], [0], color='tab:red',  linestyle='--',lw=2.5, label='RGA pass-1'),
     plt.Rectangle((0,0),1,1,fc='tab:red',alpha=0.2,ec=None,label='95% CI')
 ]
 
