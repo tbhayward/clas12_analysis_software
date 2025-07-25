6a4e431f97b9575c23b369907dc0be2476842d22
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-25 11:16:47 -0400

    added rgk preliminary BSA to imports, adjusted fitting script to allow for input parameter specifying which input to use

--- analysis_scripts/higher_level_dvcs_cross_section/plot_CFFs_fit_results.py
+++ analysis_scripts/higher_level_dvcs_cross_section/plot_CFFs_fit_results.py
@@ -162,7 +162,9 @@ t_range  = np.linspace(0.0, 1.0, 200)
 from matplotlib.lines import Line2D
 legend_elems = [
     Line2D([0],[0], color='tab:blue', linestyle='-', lw=2.5, label='Original'),
-    Line2D([0],[0], color='tab:red',  linestyle='--',lw=2.5, label='Fit'),
+    # Line2D([0],[0], color='tab:red',  linestyle='--',lw=2.5, label='Fit'),
+    Line2D([0],[0], color='tab:red',  linestyle='--',lw=2.5, label='RGA pass-1'),
+    Line2D([0],[0], color='tab:red',  linestyle='--',lw=2.5, label='RGK preliminary'),
     plt.Rectangle((0,0),1,1, fc='tab:red',alpha=0.2, ec=None, label='95% CI'),
 ]
 tex_map = {"H":"H","Ht":r"\tilde H","E":"E","Et":r"\tilde E"}
