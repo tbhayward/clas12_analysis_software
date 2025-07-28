23b8dfe7a8640f4b28a3daf5838b69dbb20d5741
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-28 15:16:41 -0400

    radically changed the parameterization of ImH including a new Q2 term and different formulae

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -324,7 +324,7 @@ int main(int argc, char** argv) {
             else if (nm == "alpha1_H")  {init = alpha1_H; step = 0.00; }
             else if (nm == "b_H")       { init = b_H; lo = 0.0; }
             else if (nm == "M2_H")     { init = M2_H;    lo = 0.0; hi = 2.0; }
-            else if (nm == "P_H")      { init = P_H;     lo = 0.0; hi = 5.0; }
+            else if (nm == "P_H")      { init = P_H; step = 0.00;    lo = 0.0; hi = 5.0; }
 
             else if (nm == "r_Ht")      init = r_Ht;
             else if (nm == "alpha0_Ht") { init = alpha0_Ht; lo = 0.2; hi = 1.0; }
