10de254196573fad8bd07034c0acfe7564de84e2
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-27 20:24:25 -0400

    adjusted fitting procedure to only access the Im CFFs from the BSA data by approximating the denominator as only the BH2 term and not the DVCS2 or interference terms which accessed the Re part, and fit only these imaginary parts by comparing amplitude of BSA A \sin\phi terms to the model's interfernce sine coefficient

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -336,15 +336,15 @@ int main(int argc, char** argv){
             if(nm.rfind("M2_",0)==0 || nm.rfind("r_",0)==0) lo=0.0;
             minu.DefineParameter(i, nm.c_str(), init, step, lo, hi);
 
-            if(nm=="renormImag" 
-               // || nm.rfind("alpha0_",0)==0 
-               // || nm.rfind("alpha1_",0)==0 
-               // || nm.rfind("n_",0)==0 
-               // || nm.rfind("P_",0)==0 
-               )
-            {
-                minu.FixParameter(i);
-            }
+            // if(nm=="renormImag" 
+            //    // || nm.rfind("alpha0_",0)==0 
+            //    // || nm.rfind("alpha1_",0)==0 
+            //    // || nm.rfind("n_",0)==0 
+            //    // || nm.rfind("P_",0)==0 
+            //    )
+            // {
+            //     minu.FixParameter(i);
+            // }
         }
         std::cout<<" Stage1: fitting r_*, b_*, M2_* (others fixed)...\n";
         minu.Migrad(); minu.Command("HESSE");
