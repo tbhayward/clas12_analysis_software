41ac791bdab9fa4ebff3f78156b5ed5f5187936a
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-29 09:27:52 -0400

    fixed typo in new rga_prl_bsa.txt

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -327,16 +327,16 @@ int main(int argc, char** argv) {
         else if(nm=="alpha1_H") { init=alpha1_H; }
         else if(nm=="b_H")      { init=b_H;   }
         else if(nm=="M2_H")     { init=M2_H; lo=0.0; hi=1.0; }
-        else if(nm=="P_H")      { init=P_H;   step = 0;}
+        else if(nm=="P_H")      { init=P_H;   }
         else if(nm=="r_Ht")       init=r_Ht;
         else if(nm=="alpha0_Ht")  { init=alpha0_Ht; step = 0; lo=0.0; hi=1.0; }
         else if(nm=="alpha1_Ht")  { init=alpha1_Ht; step = 0; }
         else if(nm=="b_Ht")       { init=b_Ht;    }
         else if(nm=="M2_Ht")      { init=M2_Ht;  lo=0.0; hi=1.0; }
-        else if(nm=="P_Ht")       { init=P_Ht;   step = 0; }
+        else if(nm=="P_Ht")       { init=P_Ht;   step = 0;}
         else if(nm=="r_E")        init=r_E;
-        else if(nm=="alpha0_E")   { init=alpha0_E; step = 0;  }
-        else if(nm=="alpha1_E")   {init=alpha1_E; step = 0; }
+        else if(nm=="alpha0_E")   { init=alpha0_E; }
+        else if(nm=="alpha1_E")   {init=alpha1_E; }
         else if(nm=="b_E")        init=b_E;
         else if(nm=="M2_E")       { init=M2_E;   }
         else if(nm=="P_E")        { init=P_E;  step = 0; }
