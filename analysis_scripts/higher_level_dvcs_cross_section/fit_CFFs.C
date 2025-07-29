fb34f5819856f75a63a0d8234e62c8d0d587ceb9
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-29 09:28:18 -0400

    fixed typo in new rga_prl_bsa.txt

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -329,8 +329,8 @@ int main(int argc, char** argv) {
         else if(nm=="M2_H")     { init=M2_H; lo=0.0; hi=1.0; }
         else if(nm=="P_H")      { init=P_H;   }
         else if(nm=="r_Ht")       init=r_Ht;
-        else if(nm=="alpha0_Ht")  { init=alpha0_Ht; step = 0; lo=0.0; hi=1.0; }
-        else if(nm=="alpha1_Ht")  { init=alpha1_Ht; step = 0; }
+        else if(nm=="alpha0_Ht")  { init=alpha0_Ht; lo=0.0; hi=1.0; }
+        else if(nm=="alpha1_Ht")  { init=alpha1_Ht; }
         else if(nm=="b_Ht")       { init=b_Ht;    }
         else if(nm=="M2_Ht")      { init=M2_Ht;  lo=0.0; hi=1.0; }
         else if(nm=="P_Ht")       { init=P_Ht;   step = 0;}
