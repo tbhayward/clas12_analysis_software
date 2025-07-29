31fbf415630c79154b89d9cb3133953c9028831e
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-28 20:45:19 -0400

    radically changed the parameterization of ImH including a new Q2 term and different formulae

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -325,13 +325,13 @@ int main(int argc, char** argv) {
         if(nm=="r_H")         { init=r_H; lo=0.0; hi=5.0; }
         else if(nm=="alpha0_H"){ init=alpha0_H; lo=0.0; hi=1.0; }
         else if(nm=="alpha1_H") init=alpha1_H;
-        else if(nm=="b_H")      { init=b_H;  step = 0; }
+        else if(nm=="b_H")      { init=b_H;   }
         else if(nm=="M2_H")     { init=M2_H; lo=0.0; hi=1.0; }
         else if(nm=="P_H")      { init=P_H;   step = 0; }
         else if(nm=="r_Ht")       init=r_Ht;
         else if(nm=="alpha0_Ht")  { init=alpha0_Ht; lo=0.0; hi=1.0; }
         else if(nm=="alpha1_Ht")  init=alpha1_Ht;
-        else if(nm=="b_Ht")       { init=b_Ht;   step = 0; }
+        else if(nm=="b_Ht")       { init=b_Ht;    }
         else if(nm=="M2_Ht")      { init=M2_Ht;  lo=0.0; hi=1.0; }
         else if(nm=="P_Ht")       { init=P_Ht;   step = 0; }
         else if(nm=="r_E")        init=r_E;
