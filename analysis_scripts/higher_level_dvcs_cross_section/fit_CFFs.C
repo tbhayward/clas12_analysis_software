2cf66dc02ebb3c3d0f3a88fc621e61e8d9bfc7be
Timothy Hayward <tbhayward@email.wm.edu>
2025-07-30 14:26:03 -0400

    added additional function in rga_bsa.py that will save text files of the data points

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -366,7 +366,7 @@ int main(int argc, char** argv) {
         if      (nm=="r_H")       init=r_H;
         else if (nm=="alpha0_H")  {init=alpha0_H; low = 0.2; hi = 0.8; }
         else if (nm=="alpha1_H")  {init=alpha1_H; low = 0.2; hi 1.2; }
-        else if (nm=="b_H")       {init=b_H; lo = 0; }
+        else if (nm=="b_H")       {init=b_H; lo = 0.3; hi = 3; }
         else if (nm=="M2_H")      {init=M2_H; lo = 0; }
         else if (nm=="P_H")       {init=P_H; step=0; }
         else if (nm=="r_Ht")      init=r_Ht;
