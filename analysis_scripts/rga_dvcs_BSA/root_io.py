b0ddc4070fd24e793cf18c5b2db3a17a2b061cb0
Timothy Hayward <tbhayward@email.wm.edu>
2025-08-05 18:54:18 -0400

    moved previous cross section extraction to deprecated directory, started new modern program for simultaneous cross section and asymmetry extraction, created main.cpp, load_files.cpp and relevant makefile

--- analysis_scripts/rga_dvcs_BSA/root_io.py
+++ analysis_scripts/rga_dvcs_BSA/root_io.py
@@ -19,10 +19,18 @@ def load_root_files(period):
         #     "data": "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/data/dvcs/rga_fa18_inb_epgamma.root",
         #     "mc":   "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_inb_10604MeV.root"
         # },
+        # "DVCS_Fa18_inb": {
+        #     "data": "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj11.root",
+        #     "mc":   "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj13.root"
+        # },
         "DVCS_Fa18_inb": {
-            "data": "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj11.root",
-            "mc":   "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj13.root"
+            "data": "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_pass2.root",
+            "mc":   "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj11.root"
         },
+        # "DVCS_Fa18_inb": {
+        #     "data": "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_pass2.root",
+        #     "mc":   "/volatile/clas12/thayward/rgk_dc_study/dvcs/dvcs_cj13.root"
+        # },
         "DVCS_Fa18_out": {
             "data": "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/data/dvcs/rga_fa18_out_epgamma.root",
             "mc":   "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_out_10604MeV.root"
