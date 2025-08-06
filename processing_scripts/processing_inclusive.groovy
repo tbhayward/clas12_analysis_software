5d718c82a5dfcfb0c12eb34ce0cc54bf02b627c8
Timothy Hayward <tbhayward@email.wm.edu>
2025-08-06 10:01:26 -0400

    updated to qadb 3.3.0 which includes rgc fa22 and rgc sp23

--- processing_scripts/processing_inclusive.groovy
+++ processing_scripts/processing_inclusive.groovy
@@ -89,7 +89,7 @@ public static void main(String[] args) {
 	EventFilter filter = new EventFilter("11:X+:X-:Xn"); 
 	
 	// setup QA database
-	QADB qa = new QADB();
+	QADB qa = new QADB("latest");
 	qa.checkForDefect('TotalOutlier')    
 	qa.checkForDefect('TerminalOutlier')
 	qa.checkForDefect('MarginalOutlier')
