2817287d6c6fde3840336b86cfc9978921d7b0aa
Timothy Hayward <tbhayward@email.wm.edu>
2025-08-07 12:49:56 -0400

    updated calculate_dilution_factor.cpp in the asymmetry extraction program to use the new thermal contracted values

--- analysis_scripts/misc/sum_charge_by_period.py
+++ analysis_scripts/misc/sum_charge_by_period.py
@@ -38,7 +38,8 @@ def parse_csv(path):
             except (IndexError, ValueError):
                 continue
 
-            data[current_period][current_species] += (pos + neg)
+            # data[current_period][current_species] += (pos + neg)
+            data[current_period][current_species] += (parts[1])
 
     return data, periods, species_list
 
