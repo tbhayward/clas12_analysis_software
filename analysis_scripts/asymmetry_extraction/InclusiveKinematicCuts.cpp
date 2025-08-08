3db890f24554dd5b677ad4c43b35580c8f896dc3
Timothy Hayward <tbhayward@email.wm.edu>
2025-08-08 10:01:17 -0400

    updated calculate_dilution_factor.cpp in the asymmetry extraction program to use the new thermal contracted values

--- analysis_scripts/asymmetry_extraction/InclusiveKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/InclusiveKinematicCuts.cpp
@@ -9,7 +9,7 @@ using std::string;
 InclusiveKinematicCuts::InclusiveKinematicCuts(TTreeReader& reader)
     : BaseKinematicCuts(reader), // Initialize BaseKinematicCuts
       runnum(reader, "runnum"), 
-      Q2(reader, "Q2"), W(reader, "W"), 
+      Q2(reader, "Q2"), W(reader, "W"), fiducial_status(reader, "fiducial_status"),
       x(reader, "x"), y(reader, "y"), e_phi(reader, "e_phi"), target_pol(reader, "target_pol") {}
 
 bool InclusiveKinematicCuts::applyCuts(int currentFits, bool isMC) {
@@ -17,7 +17,7 @@ bool InclusiveKinematicCuts::applyCuts(int currentFits, bool isMC) {
     string property = binNames[currentFits];
 
 
-    // if (*fiducial_status != 1) return false; // fiducial cuts
+    if (*fiducial_status != 111) return false; // fiducial cuts
 
     if (property == "eX") {
         goodEvent = *Q2 > 1 && *W > 2 && *y < 0.75;
