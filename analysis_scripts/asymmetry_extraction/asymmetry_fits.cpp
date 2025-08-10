35f10463f9c9a5168329d10be8150e05c536ff86
Timothy Hayward <tbhayward@email.wm.edu>
2025-08-09 21:02:55 -0400

    added missing runnums to clas12_run_info.csv

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
@@ -300,14 +300,14 @@ double asymmetry_error_calculation(double currentVariable,
   int asymmetry_index) {
   double Df = dilutionFactors[currentBin].first;
   double sigmaDf = dilutionFactors[currentBin].second;
-  // Su22
-  double sigmaPb = 0.0086;
-  double sigmaPtp = 0.0368;
-  double sigmaPtm = 0.03542;
-  // // Fa22
-  // double sigmaPb = 0.0045;
-  // double sigmaPtp = 0.0272;
-  // double sigmaPtm = 0.0404;
+  // // Su22
+  // double sigmaPb = 0.0086;
+  // double sigmaPtp = 0.0368;
+  // double sigmaPtm = 0.03542;
+  // Fa22
+  double sigmaPb = 0.0045;
+  double sigmaPtp = 0.0272;
+  double sigmaPtm = 0.0404;
   // // Sp23
   // double sigmaPb = 0.0061;
   // double sigmaPtp = 0.0404;
