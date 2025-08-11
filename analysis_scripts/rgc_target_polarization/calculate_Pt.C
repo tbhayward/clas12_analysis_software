2c61d888e99e59894aa96cf6408bb2229c8de853
Timothy B. Hayward <haywardt@mit.edu>
2025-08-11 19:50:09 -0400

    created rgc mc electron diagonal cut study python script

--- analysis_scripts/rgc_target_polarization/calculate_Pt.C
+++ analysis_scripts/rgc_target_polarization/calculate_Pt.C
@@ -18,7 +18,7 @@
 
 // ---------- CONFIGURATION ----------
 // 0 = all three periods, 1 = RGC_Su22 only, 2 = RGC_Fa22 only, 3 = RGC_Sp23 only
-const int runMode = 0;
+const int runMode = 1;
 // testRun: 0 means process all runs, >0 will restrict to that single run number
 const int testRun = 0;  // set to your run of interest, or 0 to do all
 
