78152c75f9e6839750cf9c133f01396c64e92b25
Timothy B. Hayward <haywardt@mit.edu>
2025-08-12 10:47:10 -0400

    reformatted rgk BSAs from Josh to also include his systematic uncertainty

--- analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
+++ analysis_scripts/higher_level_dvcs_cross_section/fit_CFFs.C
@@ -151,9 +151,9 @@ void LoadData(){
             if(line.empty()||line[0]=='#') continue;
             std::istringstream iss(line);
             DataPoint d; iss>>d.phi>>d.Q2>>d.xB>>d.t>>d.Eb>>d.A>>d.sigA;
-            if(-d.t>1) continue;
-            std::cout << -d.t << std::endl;
-            if(-d.t<0) continue;
+            if(d.t>1) continue;
+            if(d.t<0) continue;
+            std::cout << d.t << std::endl;
             if(gConstraint==1){
               if((-d.t/d.Q2)>=0.2) continue;
               if((-d.t)    >=1.0) continue;
