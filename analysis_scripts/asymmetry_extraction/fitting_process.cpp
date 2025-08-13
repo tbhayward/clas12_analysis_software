c536cc20bfb754616623b4f9c214a1d0b4075584
Timothy B. Hayward <haywardt@mit.edu>
2025-08-13 13:12:20 -0400

    tried to add in general exclusive channel to asymmetry extraction. The idea is to simultaneously fit BSA, TSA and DSA with the unpolarized terms constrainted by all of them in the chi2 minimization. requires extensive testing.

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -372,6 +372,7 @@ void negLogLikelihood_single_hadron(Int_t &npar, Double_t *gin, Double_t &f,
 
   // Initial definitions (move outside the loop)
   double dilution_factor = dilutionFactors[currentBin].first;
+  double Df = dilution_factor
   double sigmaDf = dilutionFactors[currentBin].second;
   // // Su22
   // double sigmaPb = 0.0086;
