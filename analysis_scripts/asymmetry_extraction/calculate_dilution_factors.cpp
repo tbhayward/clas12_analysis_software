304904e775fddee4ea42ad8cfdfc8f944c68572e
Timothy B. Hayward <haywardt@mit.edu>
2025-08-13 13:27:04 -0400

    added eppi0 and general exclusive kinematic cuts to calculate dilution factor

--- analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
+++ analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
@@ -27,6 +27,8 @@
 #include "B2BDihadronKinematicCuts.h"
 #include "DihadronKinematicCuts.h"
 #include "dvcsKinematicCuts.h"
+#include "eppi0KinematicCuts.h"
+#include "GeneralExclusiveKinematicsCuts.h"
 #include "formatLabelName.h"
 #include "readChi2Fits.h"
 #include "histConfigs.h"
