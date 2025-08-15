641cd7e0b5952fb74c9f7fb264f1d08f44c5881c
Timothy B. Hayward <haywardt@mit.edu>
2025-08-15 11:17:52 -0400

    added eppi0 and general exclusive kinematic cuts to calculate dilution factor

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -3560,8 +3560,8 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
                                       const char* kinematicPlot_file,
                                       const string& prefix) {
   // Prepare output streams for parameter arrays
-  std::ostringstream sALUoff, sAULoff, sALU, sAUL, sAUL2, sALL, sALLc, sAUUc, sAUUc2;
-  for (auto* s : {&sALUoff,&sAULoff,&sALU,&sAUL,&sAUL2,&sALL,&sALLc,&sAUUc,&sAUUc2})
+  std::ostringstream sALUoff, sAULoff, sALU, sAUL, sAUL2, sALL, sALLc, sAUUc, sAUUc2, sAUUs;
+  for (auto* s : {&sALUoff,&sAULoff,&sALU,&sAUL,&sAUL2,&sALL,&sALLc,&sAUUc,&sAUUc2,&sAUUs})
     (*s) << std::fixed << std::setprecision(9);
 
   sALUoff << prefix << "GEchi2FitsALUoffset = {";
@@ -3573,6 +3573,7 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
   sALLc   << prefix << "GEchi2FitsALLcosphi = {";
   sAUUc   << prefix << "GEchi2FitsAUUcosphi = {";
   sAUUc2  << prefix << "GEchi2FitsAUUcos2phi = {";
+  sAUUs  << prefix << "GEchi2FitsAUUsinphi = {";
 
   // Kinematic LaTeX and list
   std::ostringstream kinLatex;
