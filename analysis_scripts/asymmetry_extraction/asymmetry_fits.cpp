b306238f167801d5046513c75091ce047851dd3f
Timothy B. Hayward <haywardt@mit.edu>
2025-08-17 09:24:37 -0400

    added eppi0 and general exclusive kinematic cuts to calculate dilution factor

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
@@ -37,10 +37,10 @@ double BSA_single_hadron(double* x, double* par) {
   // Retrieve the phi variable from the input x array
   double phi = x[0];
   // Calculate and return the value of the function for the given phi and parameters 
-  // return ALU_offset + ALU_sinphi*sin(phi);
-  AUU_cosphi = -0.184162428;
-  AUU_cos2phi = -0.553694788;
-  return ALU_offset + ALU_sinphi*sin(phi)/(1+AUU_cosphi*cos(phi)+AUU_cos2phi*cos(2*phi));
+  return ALU_offset + ALU_sinphi*sin(phi);
+  // AUU_cosphi = -0.184162428;
+  // AUU_cos2phi = -0.553694788;
+  // return ALU_offset + ALU_sinphi*sin(phi)/(1+AUU_cosphi*cos(phi)+AUU_cos2phi*cos(2*phi));
 }
 
 double BSA_dvcs(double* x, double* par) {
