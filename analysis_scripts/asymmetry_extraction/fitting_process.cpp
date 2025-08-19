92eae4d2f575602d5471b88434cc39d269c69397
Timothy B. Hayward <haywardt@mit.edu>
2025-08-18 20:38:58 -0400

    created depolarization factor calculation for enpi+ rgc

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -3738,13 +3738,13 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
     // name, init, step, low, high
     minuit.DefineParameter(0,  "ALU_offset",      0.00,        0.01, -1.0, 1.0);
     minuit.DefineParameter(1,  "AUL_offset",      0.00,        0.01, -1.0, 1.0);
-    minuit.DefineParameter(2,  "F_LU_sin/F_UU",   0.00,        0.01, -1.0, 1.0); 
-    minuit.DefineParameter(3,  "F_UL_sin/F_UU",   0.00,        0.01, -1.0, 1.0);
-    minuit.DefineParameter(4,  "F_UL_sin2/F_UU",  0.00,        0.01, -1.0, 1.0);
-    minuit.DefineParameter(5,  "F_LL/F_UU",       0.00,        0.01, -1.0, 1.0);
-    minuit.DefineParameter(6,  "F_LL_cos/F_UU",   0.00,        0.01, -1.0, 1.0);
-    minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,        0.01, -1.0, 1.0); 
-    minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,        0.01, -1.0, 1.0);  
+    minuit.DefineParameter(2,  "F_LU_sin/F_UU",   0.00,        0.001, -1.0, 1.0); 
+    minuit.DefineParameter(3,  "F_UL_sin/F_UU",   0.00,        0.001, -1.0, 1.0);
+    minuit.DefineParameter(4,  "F_UL_sin2/F_UU",  0.00,        0.001, -1.0, 1.0);
+    minuit.DefineParameter(5,  "F_LL/F_UU",       0.00,        0.001, -1.0, 1.0);
+    minuit.DefineParameter(6,  "F_LL_cos/F_UU",   0.00,        0.001, -1.0, 1.0);
+    minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,        0.001, -0.5, 0.5); 
+    minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,        0.001, -0.5, 0.5);  
     minuit.DefineParameter(9,  "F_UU_sin/F_UU",   0.00,        0.00, -1.0, 1.0);
     minuit.DefineParameter(10, "F_UU_sin2/F_UU",  0.00,        0.00, -1.0, 1.0);
 
