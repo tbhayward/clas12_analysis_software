4d28992e030b3a1aa7219358c58cbd0f9f00b61d
Timothy B. Hayward <haywardt@mit.edu>
2025-08-19 12:50:01 -0400

    created depolarization factor calculation for enpi+ rgc

--- analysis_scripts/misc/plot_rga_rgc_comparison.py
+++ analysis_scripts/misc/plot_rga_rgc_comparison.py
@@ -91,7 +91,7 @@ def main():
     # --------------------------
     # Save
     # --------------------------
-    outdir = os.path.join('enpi+')
+    outdir = os.path.join('output', 'enpi+')
     if not os.path.isdir(outdir):
         os.makedirs(outdir, exist_ok=True)
     #endif
