225fabcbb97b111fd2b9cd4a760ac060de710843
Timothy B. Hayward <haywardt@mit.edu>
2025-08-20 16:16:17 -0400

    created plotting script for 2d histograms of sinthetagamma vs Q2, xB and -t

--- analysis_scripts/asymmetry_extraction/GeneralExclusiveKinematicCuts.cpp
+++ analysis_scripts/asymmetry_extraction/GeneralExclusiveKinematicCuts.cpp
@@ -328,30 +328,26 @@ bool GeneralExclusiveKinematicCuts::applyCuts(int currentFits, bool isMC)
         return PassesDynamicMx2(*x, *t, *Mx2);
     }
 
-    if (property == "enpiLowxBLowt") {
+    if (property == "enpiLowxB") {
         bool goodEvent = (*fiducial_status >= 111) &&
-                         (*x > 0.10 && *x < 0.25) && 
+                         (*x > 0.10 && *x < 0.25);
+        if (!goodEvent) return false;
+        return PassesDynamicMx2(*x, *t, *Mx2);
+    }
+    if (property == "enpiHighxB") {
+        bool goodEvent = (*fiducial_status >= 111) &&
+                         (*x > 0.45 && *x < 0.60);
+        if (!goodEvent) return false;
+        return PassesDynamicMx2(*x, *t, *Mx2);
+    }
+    if (property == "enpiLowt") {
+        bool goodEvent = (*fiducial_status >= 111) &&
                          (-*t > 0.05 && -*t < 0.25);
         if (!goodEvent) return false;
         return PassesDynamicMx2(*x, *t, *Mx2);
     }
-    if (property == "enpiLowxBHight") {
+    if (property == "enpiHight") {
         bool goodEvent = (*fiducial_status >= 111) &&
-                         (*x > 0.10 && *x < 0.25) && 
-                         (-*t > 0.8 && -*t < 1.0);
-        if (!goodEvent) return false;
-        return PassesDynamicMx2(*x, *t, *Mx2);
-    }
-    if (property == "enpiHighxBLowt") {
-        bool goodEvent = (*fiducial_status >= 111) &&
-                         (*x > 0.45 && *x < 0.60) && 
-                         (-*t > 0.05 && -*t < 0.25);
-        if (!goodEvent) return false;
-        return PassesDynamicMx2(*x, *t, *Mx2);
-    }
-    if (property == "enpiHighxBHight") {
-        bool goodEvent = (*fiducial_status >= 111) &&
-                         (*x > 0.45 && *x < 0.60) && 
                          (-*t > 0.8 && -*t < 1.0);
         if (!goodEvent) return false;
         return PassesDynamicMx2(*x, *t, *Mx2);
