c26638f46fb8699d01595988326996a109265791
Timothy B. Hayward <haywardt@mit.edu>
2025-08-20 14:17:53 -0400

    created plotting script for 2d histograms of sinthetagamma vs Q2, xB and -t

--- analysis_scripts/misc/plot_rgc_sinthetagamma_enpi.py
+++ analysis_scripts/misc/plot_rgc_sinthetagamma_enpi.py
@@ -46,10 +46,10 @@ T_LIM   = (0.0, 1.30)     # for -t
 SG_LIM  = (0.0, 0.50)     # sin(theta_gamma)
 
 # 2D bin counts (same as earlier for X axes; choose 40 bins for sinθγ)
-NB_Q2_2D = 50
-NB_X_2D  = 50
-NB_T_2D  = 50
-NB_SG    = 50
+NB_Q2_2D = 75
+NB_X_2D  = 75
+NB_T_2D  = 75
+NB_SG    = 75
 
 # Labels (units on axes only)
 LAB_Q2 = r"$Q^{2}$ (GeV$^{2}$)"
