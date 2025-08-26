1dd0ba8e017a08a85281fcfbc53852a24ef1baf2
Timothy B. Hayward <haywardt@mit.edu>
2025-08-26 14:25:25 -0400

    added ISR FSR Mx2

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -3534,7 +3534,7 @@ createHistogramForBin_GeneralExclusive(const char* histBaseName, int binIndex, c
   const double varMax = allBins[currentFits][binIndex + 1];
 
   // φ-binning
-  const int nPhiBins = 9;
+  const int nPhiBins = 12;
   const double phiMin = 0.0;
   const double phiMax = 2.0*TMath::Pi();
 
