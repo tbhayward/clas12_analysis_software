8524551099645c1e9dfbb78f3f3d51f2b9b56ebe
Timothy B. Hayward <haywardt@mit.edu>
2025-08-28 12:34:13 -0400

    made various changes to enable ISR to simulate changing of q vector direction, added in ISRThetaKernel which contains a CDF giving the likelihood of the ISR having any paticular theta angle, adjusted BeamEnergy.java to accept angles and pass them along, adjusted TwoParticles.java to build the correct beam vector if ISR happened, adjusted processing_two_particles.groovy to read these all in. After testing we will need to transfer this to other processing files.

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -4147,6 +4147,17 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
     minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.01,  -1.0,  1.0);
     minuit.DefineParameter(10, "A_T_LL",          0.00,  0.01,  -1.0,  1.0);
 
+    minuit.FixParameter(0);
+    minuit.FixParameter(1);
+    // minuit.FixParameter(2);
+    minuit.FixParameter(3);
+    minuit.FixParameter(4);
+    minuit.FixParameter(5);
+    minuit.FixParameter(6);
+    // minuit.FixParameter(7);
+    // minuit.FixParameter(8);
+    minuit.FixParameter(9);
+    minuit.FixParameter(10);
     if (!g_fit_enable_TUL || g_ge_ctx.sTG_wstd <= 1e-4) minuit.FixParameter(9);
     if (!g_fit_enable_TLL || g_ge_ctx.sTG_wstd <= 1e-4) minuit.FixParameter(10);
 
