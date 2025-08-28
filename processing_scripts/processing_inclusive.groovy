78ae96b5eb611c9a100a668a259aa4a4ec89cd00
Timothy B. Hayward <haywardt@mit.edu>
2025-08-28 15:56:46 -0400

    updated theta kernels for ISR to have patch for low angle ineffciency when detection occurs around electron

--- processing_scripts/processing_inclusive.groovy
+++ processing_scripts/processing_inclusive.groovy
@@ -131,7 +131,7 @@ public static void main(String[] args) {
             // --- Toggle here ---
             // false -> baseline (no inverse-ISR)
             // true  -> enable inverse-ISR sampling (subtract R from q inside Inclusive)
-            BeamEnergy Eb = new BeamEnergy(research_Event, runnum, /*isRadiative=*/true)
+            BeamEnergy Eb = new BeamEnergy(research_Event, runnum, /*isRadiative=*/false)
 
             double Ebeam = (runnum == 11) ? beam_energy : Eb.Eb()
 
