83cfb11e0ba8802420520c70a7bb526724e89b7e
Timothy B. Hayward <haywardt@mit.edu>
2025-08-27 22:19:57 -0400

    made various changes to enable ISR to simulate changing of q vector direction, added in ISRThetaKernel which contains a CDF giving the likelihood of the ISR having any paticular theta angle, adjusted BeamEnergy.java to accept angles and pass them along, adjusted TwoParticles.java to build the correct beam vector if ISR happened, adjusted processing_two_particles.groovy to read these all in. After testing we will need to transfer this to other processing files.

--- analysis_scripts/misc/ISR_FSR_validation_studies.py
+++ analysis_scripts/misc/ISR_FSR_validation_studies.py
@@ -196,23 +196,24 @@ def main():
     # --- top row: R_p, R_theta, R_phi ---
     plot_hist_points(ax_Rp,   Rp,     bins=60,  rng=None,                  xlabel=r"$R_p$ (GeV)",        logy=True)
     plot_hist_points(ax_Rth,  Rtheta, bins=50,  rng=None,                  xlabel=r"$R_{\theta}$ (deg)", logy=False)
-    # --- R_phi with standardized x-limits from 0.8*min to 1.2*max of non-empty bins ---
+    # --- top row: R_p, R_theta, R_phi ---
+    plot_hist_points(ax_Rp,   Rp,     bins=60, rng=None,                   xlabel=r"$R_p$ (GeV)",        logy=True)
+    plot_hist_points(ax_Rth,  Rtheta, bins=50, rng=None,                   xlabel=r"$R_{\theta}$ (deg)", logy=False)
+
+    # R_phi: force x in [0, 360], y = [0.5*min_nonzero, 1.5*max]
     if Rphi.size:
-        bins_Rphi = np.linspace(0, 360, 61)  # 60 bins
-        plot_hist_points(ax_Rph, Rphi, bins=bins_Rphi, rng=None,
+        bins_Rphi = np.linspace(0.0, 360.0, 61)  # 60 bins
+        plot_hist_points(ax_Rph, Rphi, bins=bins_Rphi, rng=(0.0, 360.0),
                          xlabel=r"$R_{\phi}$ (deg)", logy=False)
 
-        span = nonzero_hist_range(Rphi, bins_Rphi)
-        if span is not None:
-            lo, hi = span
-            # pad by 0.5x / 1.5x then clamp to [0, 360]
-            left  = max(0.0, 0.5 * lo)
-            right = min(360.0, 1.5 * hi)
-            if right <= left:  # numeric guard
-                left, right = 0.0, 360.0
-            ax_Rph.set_xlim(left, right)
-        else:
-            ax_Rph.set_xlim(0.0, 360.0)
+        n_phi, _ = np.histogram(Rphi, bins=bins_Rphi)
+        nz = n_phi[n_phi > 0]
+        if nz.size:
+            ymin = 0.5 * nz.min()
+            ymax = 1.5 * n_phi.max()
+            if ymax <= ymin:
+                ymax = ymin + 1.0  # tiny guard
+            ax_Rph.set_ylim(ymin, ymax)
     else:
         ax_Rph.text(0.5, 0.5, "No $R_{\\phi}$ info", ha="center", va="center", transform=ax_Rph.transAxes)
         ax_Rph.set_axis_off()
