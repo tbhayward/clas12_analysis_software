6527cfe2653d298386a0048c66df90db5cfa8ff5
Timothy B. Hayward <haywardt@mit.edu>
2025-08-28 21:41:56 -0400

    updated theta kernels for ISR to have patch for low angle ineffciency when detection occurs around electron

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -4150,13 +4150,13 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
 
     // minuit.FixParameter(0);
     // minuit.FixParameter(1);
-    // minuit.FixParameter(2);
+    minuit.FixParameter(2);
     // minuit.FixParameter(3);
     // minuit.FixParameter(4);
     // minuit.FixParameter(5);
     // minuit.FixParameter(6);
-    // minuit.FixParameter(7);
-    // minuit.FixParameter(8);
+    minuit.FixParameter(7);
+    minuit.FixParameter(8);
     // minuit.FixParameter(9);
     // minuit.FixParameter(10);
     if (!g_fit_enable_TUL || g_ge_ctx.sTG_wstd <= 1e-4) minuit.FixParameter(9);
