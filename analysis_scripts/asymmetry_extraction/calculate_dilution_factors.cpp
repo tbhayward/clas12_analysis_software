4662255bf51c7f613d768e8187eef8308f204486
Timothy B. Hayward <haywardt@mit.edu>
2025-09-11 13:55:30 -0400

    reset for ISR FSR study

--- analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
+++ analysis_scripts/asymmetry_extraction/calculate_dilution_factors.cpp
@@ -38,7 +38,7 @@
 #include "fitting_process.h"
 
 // Select dataset: 1 = RGC Su22, 2 = RGC Fa22, 3 = RGC Sp23
-constexpr int data_set = 7;
+constexpr int data_set = 5;
 
 struct DataSetConfig {
     std::string name;
@@ -108,11 +108,11 @@ const std::vector<DataSetConfig> dataSetConfigs = {
     {
         "RGC_Fa22_enpi+_ISR_FSR_0",
         0.5839, 0.1992, 0.1802, 0.0301, 0.0066,
-        "/volatile/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_NH3_epi+_ISR_FSR_0_2.root",
-        "/volatile/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_C_epi+_ISR_FSR_0_2.root",
-        "/volatile/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_CH2_epi+_ISR_FSR_0_2.root",
-        "/volatile/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_He_epi+_ISR_FSR_0_2.root",
-        "/volatile/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_ET_epi+_ISR_FSR_0_2.root",
+        "/work/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_NH3_epi+_ISR_FSR_0_2.root",
+        "/work/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_C_epi+_ISR_FSR_0_2.root",
+        "/work/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_CH2_epi+_ISR_FSR_0_2.root",
+        "/work/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_He_epi+_ISR_FSR_0_2.root",
+        "/work/clas12/thayward/ISR_FSR_study/rgc_fa22_inb_ET_epi+_ISR_FSR_0_2.root",
     },
 };
 
