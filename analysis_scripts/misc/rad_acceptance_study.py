13cf140bcc714752b82d47435443f1c3baf5201c
Timothy B. Hayward <haywardt@mit.edu>
2025-10-06 12:57:06 -0400

    update

--- analysis_scripts/misc/rad_acceptance_study.py
+++ analysis_scripts/misc/rad_acceptance_study.py
@@ -70,20 +70,25 @@ ANNOT_FONTSIZE = 7  # tiny, unobtrusive
 # GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_sp18_out_rad.root"
 # REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_sp18_out_rad.root"
 
-GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_fa18_inb_10604MeV.root"
-REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_inb_10604MeV.root"
-GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_fa18_inb_rad.root"
-REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_fa18_inb_rad.root"
+# GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_fa18_inb_10604MeV.root"
+# REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_inb_10604MeV.root"
+# GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_fa18_inb_rad.root"
+# REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_fa18_inb_rad.root"
 
-# GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_fa18_out_10604MeV.root"
-# REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_out_10604MeV.root"
-# GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_fa18_out_rad.root"
-# REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_fa18_out_rad.root"
+GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_fa18_out_10604MeV.root"
+REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_out_10604MeV.root"
+GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_fa18_out_rad.root"
+REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_fa18_out_rad.root"
 
 # GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_sp19_inb_10200MeV.root"
 # REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_sp19_inb_10200MeV.root"
 # GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_sp19_inb_rad.root"
 # REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_sp19_inb_rad.root"
+
+# GEN_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/gen_dvcsgen_rga_fa18_out_10604MeV.root"
+# REC_BORN_PATH = "/work/clas12/thayward/CLAS12_exclusive/dvcs/data/pass2/mc/dvcsgen/rec_dvcsgen_rga_fa18_out_10604MeV.root"
+# GEN_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/gen_fa18_out_rad.root"
+# REC_RAD_PATH  = "/volatile/clas12/thayward/temp_rad/rec_fa18_out_rad.root"
 
 TTREE_NAME = "PhysicsEvents"
 
