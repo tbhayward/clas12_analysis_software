9a63a9e32c27f0c6efd3fbc3eb30fc25c321ee81
Timothy B. Hayward <haywardt@mit.edu>
2025-10-16 16:56:44 -0400

    no longer (accidentally) dropped mc with helicity =0

--- analysis_scripts/dvcs_cross_section/total_counts.h
+++ analysis_scripts/dvcs_cross_section/total_counts.h
@@ -11,6 +11,7 @@ void compute_total_counts(
     const std::vector<std::string>& topologies,
     const std::vector<Binning>& binning_scheme,
     const std::map<std::string, TTree*>& dvcsDataTrees,
-    const std::string& combined_cuts_json,  // "output/jsons/combined_cuts.json"
-    const std::string& out_json_path        // "output/jsons/total_counts.json"
+    const std::string& combined_cuts_json,
+    const std::string& out_json_path,
+    const std::string& out_root_dir  // e.g. "output"
 );
