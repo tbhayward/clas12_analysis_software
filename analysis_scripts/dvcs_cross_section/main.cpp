035ac8cb4dba1ce2fdfeb259fab70bd8276588a6
Timothy B. Hayward <haywardt@mit.edu>
2025-11-03 13:53:39 -0500

    added model calling capability via small python script for KM15 in gepard and via calling VGG (and base BH) from their fortran installations in C++ script

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -217,7 +217,7 @@ int main(int argc, char* argv[]) {
 
     // Example: unpolarized predictions (xB,Q2,t,phi,Ebeam)
     ModelPaths paths; // leave empty to use env/defaults
-    double xB = 0.11; double Q2 = 1.6; double tpos = 0.20; double phi_deg = 180;
+    double xB = 0.11; double Q2 = 1.6; double tpos = 0.20; double phi_deg = 1;
     double xs_vgg  = vgg_xs(xB, Q2, tpos, phi_deg, 10.604, Helicity::Unpol, paths, /*globalfit=*/false);
     double xs_bh   = vgg_bh_only(xB, Q2, tpos, phi_deg, 10.604, paths, /*globalfit=*/false);
     double xs_km15 = km15_xs(xB, Q2, tpos, phi_deg, 10.604, Helicity::Unpol, paths);
