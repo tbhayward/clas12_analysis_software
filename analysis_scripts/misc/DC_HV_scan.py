24365daa6ea1364f8736824d83818a54d24d2342
Timothy B. Hayward <haywardt@mit.edu>
2025-12-02 09:48:47 -0500

    updated cross check program to now do bin centering correction comparison

--- analysis_scripts/misc/DC_HV_scan.py
+++ analysis_scripts/misc/DC_HV_scan.py
@@ -112,7 +112,7 @@ def hv_sort_key(path):
 file_paths = sorted(file_paths, key=hv_sort_key)
 
 # Common histogram settings for Mx (inclusive) - defined early for diagnostics
-MX_MIN, MX_MAX = 0.88, 1.02
+MX_MIN, MX_MAX = 0.94, 1.02
 MX_NBINS = 80
 
 # Universal selection toggles
