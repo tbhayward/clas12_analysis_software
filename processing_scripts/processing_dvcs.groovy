5407913264096cccee22d500b71cc5b2125ad07e
Timothy B. Hayward <haywardt@mit.edu>
2025-12-17 07:18:17 -0500

    created second class TwoParticles_Fermi with target momentum not at rest but drawn from distribution of Nitrogen fermi motion, updated processing_two_particles_mc.groovy to use this class for reconstructed data

--- processing_scripts/processing_dvcs.groovy
+++ processing_scripts/processing_dvcs.groovy
@@ -96,9 +96,9 @@ public static void main(String[] args) {
 	double Emiss2, theta_gamma_gamma, pTmiss;
 
 	// load my kinematic fitter/PID
-	GenericKinematicFitter fitter = new dvcs_fitter(10.6041); 
+	// GenericKinematicFitter fitter = new dvcs_fitter(10.6041); 
 	// GenericKinematicFitter fitter = new monte_carlo_fitter(10.6041);
-	// GenericKinematicFitter fitter = new event_builder_fitter(10.6041); 
+	GenericKinematicFitter fitter = new event_builder_fitter(10.6041); 
 	
 	// set filter for final states
 	EventFilter filter = new EventFilter("11:2212:22:Xn"); 
@@ -110,7 +110,7 @@ public static void main(String[] args) {
 	qa.checkForDefect('MarginalOutlier')
 	qa.checkForDefect('SectorLoss')
 	// qa.checkForDefect('LowLiveTime')
-	// qa.checkForDefect('Misc')
+	qa.checkForDefect('Misc')
 	qa.checkForDefect('ChargeHigh')
 	qa.checkForDefect('ChargeNegative')
 	qa.checkForDefect('ChargeUnknown')
