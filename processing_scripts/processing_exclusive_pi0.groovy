6e0d3b7af581194126fb7ad786bdba1aeb8563b0
Timothy B. Hayward <haywardt@mit.edu>
2025-12-17 09:21:18 -0500

    updated run 5247 charge total after sector loss cut added in

--- processing_scripts/processing_exclusive_pi0.groovy
+++ processing_scripts/processing_exclusive_pi0.groovy
@@ -187,6 +187,7 @@ public static void main(String[] args) {
 		     	(runnum > 3306 && runnum < 3817) || // RGA Sp18 Inb
 		     	(runnum > 4003 && runnum < 5020) || // RGA Sp18 Inb
 		    	qa.pass(runnum, evnum));
+		    if (runnum == 5247) process_event = false; // sector 4 loss, should be removed by qa but maybe early events need it too?
 	    	if (runnum > 17768) process_event = false; // outbending RGC Sp23
 	    	if (runnum == 17331 || runnum == 16987 || runnum == 17079 || runnum == 17190 || runnum == 17639) process_event = false; // low live time
 	    	if (runnum == 16850 || runnum == 16851 || runnum == 16852 || runnum == 16855 || runnum == 16879) process_event = false; // luminosity scans
