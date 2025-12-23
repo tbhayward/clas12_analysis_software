70e494f7e4f8f1df2d7934e3561743d349009c99
Timothy B. Hayward <haywardt@mit.edu>
2025-12-23 11:45:43 -0500

    removed a lot of runs from processing script for rga sp18 inb that had very low dvcs yields per charge

--- processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/analysis_fitter.java
@@ -32,12 +32,12 @@ public class analysis_fitter extends GenericKinematicFitter {
         return true
                 && p > 2.0 // higher cut ultimately enforced when we cut on y, this speeds processing
                 && generic_tests.forward_detector_cut(particle_Index, rec_Bank)
-                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-//                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, run_Bank)
-                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
-                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank, run_Bank)
-                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
-                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
+//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+////                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, run_Bank)
+//                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
+//                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank, run_Bank)
+//                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
+//                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
                 ;
     }
 
@@ -131,14 +131,14 @@ public class analysis_fitter extends GenericKinematicFitter {
                                 && (passesForwardDetector && (torus > 0) ? p > 0.42 : true)
                                 && (passesForwardDetector && (torus < 0) ? p > 0.50 : true)
                                 && p < 1.2 // this bound is enforced at p < 1.14 by -t < 1, done here to speed up processing
-                                && generic_tests.theta_calculation(px, py, pz) < 64.23
-                                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-                && (passesForwardDetector
-                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
-                        : true)
-                && (passesCentralDetector
-                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank, 2)
-                        : true) //               
+//                                && generic_tests.theta_calculation(px, py, pz) < 64.23
+//                                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+//                && (passesForwardDetector
+//                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
+//                        : true)
+//                && (passesCentralDetector
+//                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank, 2)
+//                        : true) //               
 //                && (passesForwardDetector // dedicated PID cuts for forward
 //                                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
 //                                        : true)
@@ -168,10 +168,10 @@ public class analysis_fitter extends GenericKinematicFitter {
         return true
                 && (num_photon == 0 ? p > 2.0 : p > 0.4)
                 && (passesForwardDetector || passesForwardTagger)
-                && (passesForwardDetector
-                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
-                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
-                && pid_cuts.beta_cut(particle_Index, rec_Bank)
+//                && (passesForwardDetector
+//                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
+//                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
+//                && pid_cuts.beta_cut(particle_Index, rec_Bank)
                 ;
     }
 
