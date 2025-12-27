f1a002cee90e296653cc5c1f7ed8dcf2363c6587
Timothy B. Hayward <haywardt@mit.edu>
2025-12-27 10:01:19 -0500

    fixed missing dataTree input for pi0 background subtraction calculation

--- processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/dvcs_fitter.java
@@ -33,12 +33,12 @@ public class dvcs_fitter extends GenericKinematicFitter {
         return true
                 && p > 2.0
                 && generic_tests.forward_detector_cut(particle_Index, rec_Bank)
-//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-////                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, run_Bank)
-//                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
-//                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank, run_Bank)
-//                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
-//                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
+                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+//                && pid_cuts.calorimeter_energy_cut(particle_Index, cal_Bank, run_Bank)
+                && pid_cuts.calorimeter_sampling_fraction_cut(particle_Index, p, run_Bank, cal_Bank)
+                && pid_cuts.calorimeter_diagonal_cut(particle_Index, p, cal_Bank, run_Bank)
+                && fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
+                && fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
                 ;
     }
 
@@ -65,14 +65,14 @@ public class dvcs_fitter extends GenericKinematicFitter {
                 && (passesForwardDetector && (torus > 0) ? p > 0.42 : true)
                 && (passesForwardDetector && (torus < 0) ? p > 0.50 : true)
                 && p < 1.2 // this bound is enforced at p < 1.14 by -t < 1, done here to speed up processing
-//                && generic_tests.theta_calculation(px, py, pz) < 64.23
-//                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
-//                && (passesForwardDetector
-//                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
-//                        : true)
-//                && (passesCentralDetector
-//                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank, 2)
-//                        : true)
+                && generic_tests.theta_calculation(px, py, pz) < 64.23
+                && generic_tests.vertex_cut(particle_Index, rec_Bank, run_Bank)
+                && (passesForwardDetector
+                        ? fiducial_cuts.dc_fiducial_cut(particle_Index, rec_Bank, traj_Bank, run_Bank)
+                        : true)
+                && (passesCentralDetector
+                        ? fiducial_cuts.cvt_fiducial_cut(particle_Index, rec_Bank, traj_Bank, 2)
+                        : true)
 //                && (passesForwardDetector // dedicated PID cuts for forward
 //                        ? pid_cuts.charged_hadron_chi2pid_cut(particle_Index, rec_Bank, run_Bank)
 //                        : true)
@@ -102,10 +102,10 @@ public class dvcs_fitter extends GenericKinematicFitter {
         return true
                 && p > 2.00
                 && (passesForwardDetector || passesForwardTagger)
-//                && (passesForwardDetector
-//                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
-//                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
-//                && pid_cuts.beta_cut(particle_Index, rec_Bank)
+                && (passesForwardDetector
+                        ? fiducial_cuts.pcal_fiducial_cut(particle_Index, 2, run_Bank, rec_Bank, cal_Bank)
+                        : fiducial_cuts.forward_tagger_fiducial_cut(particle_Index, rec_Bank, ft_Bank))
+                && pid_cuts.beta_cut(particle_Index, rec_Bank)
                 ;
     }
 
