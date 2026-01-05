96873633258e292b0d6e5152f7ae3bd906f718c6
Timothy B. Hayward <haywardt@mit.edu>
2026-01-05 14:20:43 -0500

    fixed missing dataTree input for pi0 background subtraction calculation

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -354,14 +354,26 @@ int main(int argc, char* argv[]) {
     {
         const std::string csv_main = "output/csvs/dvcs_pass2_analysis.csv";
 
+        // // 10.6 GeV combined group (unpolarized) normalization study
+        // if (!print_bh_normalization_study(csv_main, "Fa18 Inb", "unpol")) {
+        //     std::cerr << "[main] FATAL: overall normalization study failed for 10.6 GeV.\n";
+        //     return 1;
+        // }
+
+        // // 10.6 GeV combined group (unpolarized) normalization study
+        // if (!print_bh_normalization_study(csv_main, "Fa18 Out", "unpol")) {
+        //     std::cerr << "[main] FATAL: overall normalization study failed for 10.6 GeV.\n";
+        //     return 1;
+        // }
+
         // 10.6 GeV combined group (unpolarized) normalization study
-        if (!print_bh_normalization_study(csv_main, "Fa18 Inb", "unpol")) {
+        if (!print_bh_normalization_study(csv_main, "Sp18 Inb", "unpol")) {
             std::cerr << "[main] FATAL: overall normalization study failed for 10.6 GeV.\n";
             return 1;
         }
 
         // 10.6 GeV combined group (unpolarized) normalization study
-        if (!print_bh_normalization_study(csv_main, "Fa18 Out", "unpol")) {
+        if (!print_bh_normalization_study(csv_main, "Sp18 Out", "unpol")) {
             std::cerr << "[main] FATAL: overall normalization study failed for 10.6 GeV.\n";
             return 1;
         }
