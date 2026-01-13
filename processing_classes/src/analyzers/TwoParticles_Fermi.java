1673155e4a165b39db9be41410c7c464d5ded756
Timothy B. Hayward <haywardt@mit.edu>
2026-01-13 14:03:16 -0500

    message

--- processing_classes/src/analyzers/TwoParticles_Fermi.java
+++ processing_classes/src/analyzers/TwoParticles_Fermi.java
@@ -200,8 +200,9 @@ public class TwoParticles_Fermi {
         
         /* TOGGLE ON OR OFF IF FERMI MOTION DESIRED */
         // Simulate Fermi motion
-        System.out.println(Mx2);
+        System.out.println("TP_FERMI preMx2=" + Mx2);
         org.jlab.clas.physics.Vector3 fermiP = momentum_corrections.sampleFermiMomentum(Mx2);
+        System.out.println("TP_FERMI afterSample blankline");
         System.out.println();
         lv_target.setPxPyPzM(fermiP.x(),fermiP.y(),fermiP.z(),kinematic_variables.particle_mass(2212));
         Mx  = kinematic_variables.Mx (lv_q, lv_target, lv_p);
