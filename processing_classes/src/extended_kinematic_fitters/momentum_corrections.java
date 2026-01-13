8ef0366d9702590998952e8bdec4d6895b29345b
Timothy B. Hayward <haywardt@mit.edu>
2026-01-13 14:37:01 -0500

    message

--- processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
+++ processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
@@ -761,7 +761,7 @@ public class momentum_corrections {
         // smear with probability = 1 – D_f(mx2)
         double pSmear = 1.0 - dilutionFactor(mx2);
         double random = rand.nextDouble();
-        System.out.println(pSmear+" "+random);
+        System.out.println("pSmear = "+pSmear+", random = "+random);
         if (random < pSmear) {
             // leave at rest
             return new Vector3(0.0, 0.0, 0.0);
