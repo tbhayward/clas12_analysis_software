4719df5d99ec8297cf89e6bba819c054dd15379c
Timothy B. Hayward <haywardt@mit.edu>
2026-02-17 10:28:56 -0500

    updated asymmetry_extraction with dilution factor for eppi-

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -3881,9 +3881,9 @@ static void plotHistogramAndFit_GeneralExclusive(
   };
 
   // ---------------- Draw the three panels -------------------------------------
-  addPointsAndCurve(1, hALU, yALU, "A_{LU}", fillBSA, -0.20, 0.20);
-  addPointsAndCurve(2, hAUL, yAUL, "A_{UL}", fillTSA, -0.20, 0.20);
-  addPointsAndCurve(3, hALL, yALL, "A_{LL}", fillDSA, -0.10, 0.60);
+  addPointsAndCurve(1, hALU, yALU, "A_{LU}", fillBSA, -0.30, 0.30);
+  addPointsAndCurve(2, hAUL, yAUL, "A_{UL}", fillTSA, -0.30, 0.30);
+  addPointsAndCurve(3, hALL, yALL, "A_{LL}", fillDSA, -0.60, 0.60);
 
   // ---------------- Title and save --------------------------------------------
   const double vminB = allBins[currentFits][binIndex];
@@ -4180,10 +4180,14 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
     minuit.DefineParameter(4,  "F_UL_sin2/F_UU",  0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(5,  "F_LL/F_UU",       0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(6,  "F_LL_cos/F_UU",   0.00,  0.001,  -1.0,  1.0);
-    minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(10, "A_T_LL",          0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(10, "A_T_LL",          0.00,  0.01,  -1.0,  1.0);
+    minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.00,  -1.0,  1.0);
+    minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.00,  -1.0,  1.0);
+    minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.00,  -1.0,  1.0);
+    minuit.DefineParameter(10, "A_T_LL",          0.00,  0.00,  -1.0,  1.0);
 
     // minuit.FixParameter(0);
     // minuit.FixParameter(1);
