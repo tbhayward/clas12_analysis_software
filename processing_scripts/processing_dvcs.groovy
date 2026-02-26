741b7573cfa84fac58248dec2488df623dec536c
Timothy B. Hayward <haywardt@mit.edu>
2026-02-26 12:37:53 -0500

    added pass-1 paper plots to dvcs analysis

--- processing_scripts/processing_dvcs.groovy
+++ processing_scripts/processing_dvcs.groovy
@@ -101,8 +101,7 @@ public static void main(String[] args) {
 	// GenericKinematicFitter fitter = new event_builder_fitter(10.6041); 
 	
 	// set filter for final states
-	// EventFilter filter = new EventFilter("11:2212:22:Xn"); 
-	EventFilter filter = new EventFilter("11:2212:11:Xn"); 
+	EventFilter filter = new EventFilter("11:2212:22:Xn"); 
 	
 	// setup QA database
 	QADB qa = new QADB("latest");
