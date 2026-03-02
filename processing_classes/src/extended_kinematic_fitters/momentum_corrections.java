8ef605f406dfdcadbf24d83134fc746a4aa7a104
Timothy B. Hayward <haywardt@mit.edu>
2026-03-02 18:21:44 -0500

    added user provided override to three particles processing

--- processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
+++ processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
@@ -761,7 +761,7 @@ public class momentum_corrections {
         // smear with probability = 1 – D_f(mx2)
         double pSmear = dilutionFactor(mx2); 
 //        pSmear = 0;
-//        pSmear = 1;
+        pSmear = 1;
         if (rand.nextDouble() > pSmear) {
             // leave at rest
             return new Vector3(0.0, 0.0, 0.0);
