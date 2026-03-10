c3614d97c4773f7ac595d319da3d1cac24958bd9
Timothy B. Hayward <haywardt@mit.edu>
2026-03-10 15:22:41 -0400

    added user provided override to three particles processing

--- analysis_scripts/misc/fermi_motion_size.py
+++ analysis_scripts/misc/fermi_motion_size.py
@@ -60,15 +60,15 @@ def get_beam_energy(run):
 # input files
 # ------------------------------------------------
 
-# files = [
-# "/volatile/clas12/users/mkerr/rgc/p_pim/ND3/fall22_nd3_p_pim.root",
-# "/volatile/clas12/users/mkerr/rgc/p_pim/ND3/summer22_nd3_p_pim.root",
-# "/volatile/clas12/users/mkerr/rgc/p_pim/ND3/spring23_nd3_p_pim.root"
-# ]
-
 files = [
-"/volatile/clas12/users/mkerr/rgb/rgb_sidisdvcs_p_pim.root",
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/fall22_nd3_p_pim.root",
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/summer22_nd3_p_pim.root",
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/spring23_nd3_p_pim.root"
 ]
+
+# files = [
+# "/volatile/clas12/users/mkerr/rgb/rgb_sidisdvcs_p_pim.root",
+# ]
 
 tree_name = "PhysicsEvents"
 
