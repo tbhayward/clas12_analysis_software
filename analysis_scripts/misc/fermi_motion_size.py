7732d0b595503d98b4d8137bfb0db4eb773e359d
Timothy B. Hayward <haywardt@mit.edu>
2026-03-10 13:38:48 -0400

    added user provided override to three particles processing

--- analysis_scripts/misc/fermi_motion_size.py
+++ analysis_scripts/misc/fermi_motion_size.py
@@ -1,6 +1,5 @@
 import ROOT
 import math
-import random
 import os
 
 ROOT.gROOT.SetBatch(True)
@@ -18,9 +17,9 @@ M_n  = 0.939565
 f = ROOT.TFile.Open(input_file)
 t = f.Get(tree_name)
 
-# -----------------------------------------
-# histograms (DATA)
-# -----------------------------------------
+# ----------------------------------------
+# histograms
+# ----------------------------------------
 
 h_density = ROOT.TH2D(
 "h_density",
@@ -29,25 +28,35 @@ h_density = ROOT.TH2D(
 60,0.3,1.5
 )
 
-h_t_data = ROOT.TH2D(
-"h_t_data",
-"|t_{mes}-t_{bar}| (data); |p_{F}| (GeV); p'_{p} (GeV)",
+h_dxB = ROOT.TH2D(
+"h_dxB",
+"|x_{B}^{mes}-x_{B}^{true}|; |p_{F}| (GeV); p'_{p} (GeV)",
 60,0,0.3,
 60,0.3,1.5
 )
 
-h_mx2_data = ROOT.TH2D(
-"h_mx2_data",
-"|#Delta Mx^{2}| (data); |p_{F}| (GeV); p'_{p} (GeV)",
+h_dt = ROOT.TH2D(
+"h_dt",
+"|t_{mes}-t_{true}|; |p_{F}| (GeV); p'_{p} (GeV)",
 60,0,0.3,
 60,0.3,1.5
 )
 
-print("Running data loop")
+h_dmx2 = ROOT.TH2D(
+"h_dmx2",
+"|Mx2_{mes}-Mx2_{true}|; |p_{F}| (GeV); p'_{p} (GeV)",
+60,0,0.3,
+60,0.3,1.5
+)
+
+print("Looping over events")
 
 for ev in t:
 
+    # ----------------------------------
     # electron
+    # ----------------------------------
+
     e_p = ev.e_p
     th  = ev.e_theta
     ph  = ev.e_phi
@@ -58,7 +67,10 @@ for ev in t:
 
     Ee = math.sqrt(e_p*e_p + m_e*m_e)
 
+    # ----------------------------------
     # pion
+    # ----------------------------------
+
     p1 = ev.p1_p
     th = ev.p1_theta
     ph = ev.p1_phi
@@ -69,7 +81,10 @@ for ev in t:
 
     Epi = math.sqrt(p1*p1 + m_pi*m_pi)
 
+    # ----------------------------------
     # proton
+    # ----------------------------------
+
     p2 = ev.p2_p
     th = ev.p2_theta
     ph = ev.p2_phi
@@ -80,113 +95,92 @@ for ev in t:
 
     Ep = math.sqrt(p2*p2 + M_p*M_p)
 
+    # ----------------------------------
     # virtual photon
+    # ----------------------------------
+
     qx = -ex
     qy = -ey
     qz = beamE - ez
     qE = beamE - Ee
 
-    # exclusivity cut
-    mx2 = (qE + M_p - Epi)**2 - ((qx - pix)**2 + (qy - piy)**2 + (qz - piz)**2)
+    # ----------------------------------
+    # mesonic missing mass cut
+    # ----------------------------------
 
-    if mx2 >= 1.07:
+    mx2_mes = (qE + M_p - Epi)**2 - ((qx - pix)**2 + (qy - piy)**2 + (qz - piz)**2)
+
+    if mx2_mes >= 1.07:
         continue
     #endif
 
-    # estimate Fermi momentum
-    pFx = ppx + pix - qx
-    pFy = ppy + piy - qy
-    pFz = ppz + piz - qz
+    # ----------------------------------
+    # reconstructed neutron momentum
+    # ----------------------------------
 
-    pF = math.sqrt(pFx*pFx + pFy*pFy + pFz*pFz)
+    pnx = ppx + pix - qx
+    pny = ppy + piy - qy
+    pnz = ppz + piz - qz
+
+    pF = math.sqrt(pnx*pnx + pny*pny + pnz*pnz)
 
     if pF > 0.3 or p2 < 0.3 or p2 > 1.5:
         continue
     #endif
 
-    h_density.Fill(pF,p2)
+    En = math.sqrt(pF*pF + M_n*M_n)
 
-    # t definitions
+    # ----------------------------------
+    # Q2 and nu
+    # ----------------------------------
+
+    Q2 = -(qE*qE - qx*qx - qy*qy - qz*qz)
+    nu = qE
+
+    # ----------------------------------
+    # xB
+    # ----------------------------------
+
+    xB_mes = Q2/(2*M_n*nu)
+    xB_true = Q2/(2*(En*nu - (pnx*qx+pny*qy+pnz*qz)))
+
+    dxB = abs(xB_mes-xB_true)
+
+    # ----------------------------------
+    # t
+    # ----------------------------------
+
     dE = qE - Epi
     dx = qx - pix
     dy = qy - piy
     dz = qz - piz
 
-    t_mes = dE*dE - (dx*dx + dy*dy + dz*dz)
+    t_mes = dE*dE - (dx*dx+dy*dy+dz*dz)
 
-    t_bar = M_n*M_n + M_p*M_p - 2*M_n*Ep
+    t_true = (Ep-En)**2 - ((ppx-pnx)**2 + (ppy-pny)**2 + (ppz-pnz)**2)
 
-    dt = abs(t_mes - t_bar)
+    dt = abs(t_mes-t_true)
 
-    h_t_data.Fill(pF,p2,dt)
+    # ----------------------------------
+    # Mx2 true
+    # ----------------------------------
 
-    # approximate Mx2 shift
-    dmx2 = abs(2*(pFx*ppx + pFy*ppy + pFz*ppz))
+    mx2_true = (qE + En - Epi)**2 - ((qx + pnx - pix)**2 +
+                                    (qy + pny - piy)**2 +
+                                    (qz + pnz - piz)**2)
 
-    h_mx2_data.Fill(pF,p2,dmx2)
+    dmx2 = abs(mx2_mes-mx2_true)
 
-#endfor
+    # ----------------------------------
+    # fill histograms
+    # ----------------------------------
 
-# -----------------------------------------
-# theoretical shift maps
-# -----------------------------------------
-
-h_blank = ROOT.TH2D(
-"h_blank",
-"Phase space reference; |p_{F}| (GeV); p'_{p} (GeV)",
-60,0,0.3,
-60,0.3,1.5
-)
-
-h_t_theory = ROOT.TH2D(
-"h_t_theory",
-"Expected |#Delta t|; |p_{F}| (GeV); p'_{p} (GeV)",
-60,0,0.3,
-60,0.3,1.5
-)
-
-h_mx2_theory = ROOT.TH2D(
-"h_mx2_theory",
-"Expected |#Delta Mx^{2}|; |p_{F}| (GeV); p'_{p} (GeV)",
-60,0,0.3,
-60,0.3,1.5
-)
-
-samples = 200
-
-print("Scanning theoretical phase space")
-
-for i in range(60):
-
-    pF = (i+0.5)*0.3/60
-
-    for j in range(60):
-
-        pp = 0.3 + (j+0.5)*(1.2/60)
-
-        avg_t = 0
-        avg_mx2 = 0
-
-        for s in range(samples):
-
-            costh = random.uniform(-1,1)
-
-            avg_t += abs(2*pF*pp*costh)
-            avg_mx2 += abs(2*pF*pp*costh)
-
-        #endfor
-
-        avg_t /= samples
-        avg_mx2 /= samples
-
-        h_t_theory.SetBinContent(i+1,j+1,avg_t)
-        h_mx2_theory.SetBinContent(i+1,j+1,avg_mx2)
+    h_density.Fill(pF,p2)
+    h_dxB.Fill(pF,p2,dxB)
+    h_dt.Fill(pF,p2,dt)
+    h_dmx2.Fill(pF,p2,dmx2)
 
 #endfor
-
-# -----------------------------------------
-# plotting
-# -----------------------------------------
 
 ROOT.gStyle.SetOptStat(0)
 
@@ -197,22 +191,20 @@ c.cd(1)
 h_density.Draw("COLZ")
 
 c.cd(2)
-h_t_data.Draw("COLZ")
+h_density.Draw("COLZ")
 
 c.cd(3)
-h_mx2_data.Draw("COLZ")
+h_density.Draw("COLZ")
 
 c.cd(4)
-h_blank.Draw("COLZ")
+h_dxB.Draw("COLZ")
 
 c.cd(5)
-h_t_theory.Draw("COLZ")
+h_dt.Draw("COLZ")
 
 c.cd(6)
-h_mx2_theory.Draw("COLZ")
+h_dmx2.Draw("COLZ")
 
 os.makedirs("output",exist_ok=True)
 
-c.SaveAs("output/fermi_motion_diagnostics.png")
-
-print("Saved output/fermi_motion_diagnostics.png")
+c.SaveAs("output/fermi_motion_truth_comparison.png")
