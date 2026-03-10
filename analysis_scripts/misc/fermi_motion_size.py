75176aae7576d2e50cb2bff1b364ba47828e79bd
Timothy B. Hayward <haywardt@mit.edu>
2026-03-10 14:37:43 -0400

    added user provided override to three particles processing

--- analysis_scripts/misc/fermi_motion_size.py
+++ analysis_scripts/misc/fermi_motion_size.py
@@ -57,51 +57,55 @@ def get_beam_energy(run):
     return Eb
 
 # ------------------------------------------------
-# input file
+# input files
 # ------------------------------------------------
 
-input_file = "/volatile/clas12/users/mkerr/rgc/p_pim/ND3/fall22_nd3_p_pim.root"
-tree_name  = "PhysicsEvents"
+files = [
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/fall22_nd3_p_pim.root",
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/summer22_nd3_p_pim.root",
+"/volatile/clas12/users/mkerr/rgc/p_pim/ND3/spring23_nd3_p_pim.root"
+]
 
-f = ROOT.TFile.Open(input_file)
-t = f.Get(tree_name)
+tree_name = "PhysicsEvents"
 
 # ------------------------------------------------
 # histograms
 # ------------------------------------------------
 
-nb_pf = 20
-nb_pp = 20
+nb_pf = 60
+nb_pp = 60
 
 h_density = ROOT.TH2D(
 "h_density",
 "Event density; |p_{F}| (GeV); p' (GeV)",
 nb_pf,0,0.3,
-nb_pp,0.3,1.5
+nb_pp,0.3,1.2
 )
 
 h_dxB = ROOT.TH2D(
 "h_dxB",
 "<|#Delta x_{B}|>; |p_{F}| (GeV); p' (GeV)",
 nb_pf,0,0.3,
-nb_pp,0.3,1.5
+nb_pp,0.3,1.2
 )
 
 h_dt = ROOT.TH2D(
 "h_dt",
 "<|#Delta t|>; |p_{F}| (GeV); p' (GeV)",
 nb_pf,0,0.3,
-nb_pp,0.3,1.5
+nb_pp,0.3,1.2
 )
 
 h_dmx2 = ROOT.TH2D(
 "h_dmx2",
 "<|#Delta Mx^{2}|>; |p_{F}| (GeV); p' (GeV)",
 nb_pf,0,0.3,
-nb_pp,0.3,1.5
+nb_pp,0.3,1.2
 )
 
+# ------------------------------------------------
 # accumulation arrays
+# ------------------------------------------------
 
 sum_dxB = [[0]*nb_pp for _ in range(nb_pf)]
 sum_dt  = [[0]*nb_pp for _ in range(nb_pf)]
@@ -116,110 +120,124 @@ print("Scanning events")
 
 event_counter = 0
 
-for ev in t:
+for file in files:
 
-    event_counter += 1
+    print("Opening",file)
+
+    f = ROOT.TFile.Open(file)
+    t = f.Get(tree_name)
+
+    for ev in t:
+
+        event_counter += 1
+
+        if event_counter > max_events:
+            break
+        #endif
+
+        run = ev.runnum
+        Eb = get_beam_energy(run)
+
+        # electron
+        e_p = ev.e_p
+        th  = ev.e_theta
+        ph  = ev.e_phi
+
+        ex = e_p*math.sin(th)*math.cos(ph)
+        ey = e_p*math.sin(th)*math.sin(ph)
+        ez = e_p*math.cos(th)
+
+        Ee = math.sqrt(e_p*e_p + m_e*m_e)
+
+        # proton
+        p1 = ev.p1_p
+        th = ev.p1_theta
+        ph = ev.p1_phi
+
+        ppx = p1*math.sin(th)*math.cos(ph)
+        ppy = p1*math.sin(th)*math.sin(ph)
+        ppz = p1*math.cos(th)
+
+        Ep = math.sqrt(p1*p1 + M_p*M_p)
+
+        # pion
+        p2 = ev.p2_p
+        th = ev.p2_theta
+        ph = ev.p2_phi
+
+        pix = p2*math.sin(th)*math.cos(ph)
+        piy = p2*math.sin(th)*math.sin(ph)
+        piz = p2*math.cos(th)
+
+        Epi = math.sqrt(p2*p2 + m_pi*m_pi)
+
+        # virtual photon
+        qx = -ex
+        qy = -ey
+        qz = Eb - ez
+        qE = Eb - Ee
+
+        Q2 = -(qE*qE - qx*qx - qy*qy - qz*qz)
+        nu = qE
+
+        # mesonic Mx2
+        mx2_mes = (qE + M_p - Epi)**2 - ((qx-pix)**2+(qy-piy)**2+(qz-piz)**2)
+
+        if mx2_mes >= 1.07:
+            continue
+        #endif
+
+        # neutron momentum
+        pnx = ppx + pix - qx
+        pny = ppy + piy - qy
+        pnz = ppz + piz - qz
+
+        pF = math.sqrt(pnx*pnx + pny*pny + pnz*pnz)
+
+        En = math.sqrt(M_n*M_n + pF*pF)
+
+        # xB
+        dot_pq = En*nu - (pnx*qx + pny*qy + pnz*qz)
+
+        xB_mes = Q2/(2*M_n*nu)
+        xB_true = Q2/(2*dot_pq)
+
+        # t
+        t_mes = (qE - Epi)**2 - ((qx-pix)**2+(qy-piy)**2+(qz-piz)**2)
+
+        t_true = (Ep-En)**2 - ((ppx-pnx)**2+(ppy-pny)**2+(ppz-pnz)**2)
+
+        # true Mx2
+        mx2_true = (qE + En - Epi)**2 - ((qx+pnx-pix)**2+(qy+pny-piy)**2+(qz+pnz-piz)**2)
+
+        dxB = abs(xB_mes - xB_true)
+        dt  = abs(t_mes - t_true)
+        dmx2 = abs(mx2_mes - mx2_true)
+
+        # bin indices
+        i = int(pF/0.3 * nb_pf)
+        j = int((p1-0.3)/0.9 * nb_pp)
+
+        if 0 <= i < nb_pf and 0 <= j < nb_pp:
+
+            h_density.Fill(pF,p1)
+
+            sum_dxB[i][j] += dxB
+            sum_dt[i][j] += dt
+            sum_dmx2[i][j] += dmx2
+            counts[i][j] += 1
+
+        #endif
+
+    #endfor
+
     if event_counter > max_events:
         break
     #endif
 
-    run = ev.runnum
-    Eb = get_beam_energy(run)
-
-    # electron
-    e_p = ev.e_p
-    th  = ev.e_theta
-    ph  = ev.e_phi
-
-    ex = e_p*math.sin(th)*math.cos(ph)
-    ey = e_p*math.sin(th)*math.sin(ph)
-    ez = e_p*math.cos(th)
-
-    Ee = math.sqrt(e_p*e_p + m_e*m_e)
-
-    # proton
-    p1 = ev.p1_p
-    th = ev.p1_theta
-    ph = ev.p1_phi
-
-    ppx = p1*math.sin(th)*math.cos(ph)
-    ppy = p1*math.sin(th)*math.sin(ph)
-    ppz = p1*math.cos(th)
-
-    Ep = math.sqrt(p1*p1 + M_p*M_p)
-
-    # pion
-    p2 = ev.p2_p
-    th = ev.p2_theta
-    ph = ev.p2_phi
-
-    pix = p2*math.sin(th)*math.cos(ph)
-    piy = p2*math.sin(th)*math.sin(ph)
-    piz = p2*math.cos(th)
-
-    Epi = math.sqrt(p2*p2 + m_pi*m_pi)
-
-    # virtual photon
-    qx = -ex
-    qy = -ey
-    qz = Eb - ez
-    qE = Eb - Ee
-
-    Q2 = -(qE*qE - qx*qx - qy*qy - qz*qz)
-    nu = qE
-
-    # mesonic missing mass
-    mx2_mes = (qE + M_p - Epi)**2 - ((qx-pix)**2+(qy-piy)**2+(qz-piz)**2)
-
-    if mx2_mes >= 1.07:
-        continue
-    #endif
-
-    # reconstructed neutron momentum
-    pnx = ppx + pix - qx
-    pny = ppy + piy - qy
-    pnz = ppz + piz - qz
-
-    pF = math.sqrt(pnx*pnx + pny*pny + pnz*pnz)
-
-    # neutron energy
-    En = math.sqrt(M_n*M_n + pF*pF)
-
-    # true xB
-    dot_pq = En*nu - (pnx*qx + pny*qy + pnz*qz)
-
-    xB_mes = Q2/(2*M_n*nu)
-    xB_true = Q2/(2*dot_pq)
-
-    # t definitions
-    t_mes = (qE - Epi)**2 - ((qx-pix)**2+(qy-piy)**2+(qz-piz)**2)
-
-    t_true = (Ep-En)**2 - ((ppx-pnx)**2+(ppy-pny)**2+(ppz-pnz)**2)
-
-    # true Mx2
-    mx2_true = (qE + En - Epi)**2 - ((qx+pnx-pix)**2+(qy+pny-piy)**2+(qz+pnz-piz)**2)
-
-    # shifts
-    dxB = abs(xB_mes - xB_true)
-    dt  = abs(t_mes - t_true)
-    dmx2 = abs(mx2_mes - mx2_true)
-
-    # bin indices
-    i = int(pF/0.3 * nb_pf)
-    j = int((p1-0.3)/1.2 * nb_pp)
-
-    if 0 <= i < nb_pf and 0 <= j < nb_pp:
-
-        h_density.Fill(pF,p1)
-
-        sum_dxB[i][j] += dxB
-        sum_dt[i][j] += dt
-        sum_dmx2[i][j] += dmx2
-        counts[i][j] += 1
-
-    #endif
-
 #endfor
+
+print("Events processed:",event_counter)
 
 # ------------------------------------------------
 # compute averages
@@ -235,7 +253,7 @@ for i in range(nb_pf):
             h_dmx2.SetBinContent(i+1,j+1,sum_dmx2[i][j]/counts[i][j])
 
 # ------------------------------------------------
-# plot
+# plotting
 # ------------------------------------------------
 
 ROOT.gStyle.SetOptStat(0)
