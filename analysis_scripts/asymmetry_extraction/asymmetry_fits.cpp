c9714fa182658b43e58f59b20030594a3dd8eb18
Timothy B. Hayward <haywardt@mit.edu>
2026-04-13 14:54:50 -0400

    initial commit

--- analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
+++ analysis_scripts/asymmetry_extraction/asymmetry_fits.cpp
@@ -38,8 +38,6 @@ double BSA_single_hadron(double* x, double* par) {
   double phi = x[0];
   // Calculate and return the value of the function for the given phi and parameters 
   // return ALU_offset + ALU_sinphi*sin(phi);
-  // AUU_cosphi = -0.184162428;
-  // AUU_cos2phi = -0.553694788;
   return ALU_offset + ALU_sinphi*sin(phi)/(1+AUU_cosphi*cos(phi)+AUU_cos2phi*cos(2*phi));
 }
 
