e9ea2bc922bd705a12e7a787cfdcb39d6dc76872
Timothy B. Hayward <haywardt@mit.edu>
2026-04-14 11:18:34 -0400

    initial commit

--- analysis_scripts/misc/DVCS_per_run.py
+++ analysis_scripts/misc/DVCS_per_run.py
@@ -486,7 +486,7 @@ for period_label, root_path in period_files:
     plt.grid(True, alpha=0.3)
 
     # Force the same y-axis range on all plots
-    plt.ylim(0.02, 0.04)
+    plt.ylim(0.01, 0.03)
 
     plt.xticks(rotation=45)
     plt.legend(title="Beam current (nA)", fontsize=10)
