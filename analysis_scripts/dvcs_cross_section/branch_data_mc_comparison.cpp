deb8df2602a1b4076e2c2a9420b5ad52ec13c0e9
Timothy B. Hayward <haywardt@mit.edu>
2026-04-21 12:42:47 -0400

    added data mc comparison to dvcs analysis

--- analysis_scripts/dvcs_cross_section/branch_data_mc_comparison.cpp
+++ analysis_scripts/dvcs_cross_section/branch_data_mc_comparison.cpp
@@ -36,7 +36,7 @@
 
 namespace {
 
-static constexpr bool kSkipExclusivityCuts = false;
+static constexpr bool kSkipExclusivityCuts = true;
 static constexpr int kNClasdSectors = 6;
 
 struct PeriodDef {
