516c7aed75d5848c5d0ebcdb19fe774713c82945
Timothy B. Hayward <haywardt@mit.edu>
2026-04-22 18:43:47 -0400

    added data mc comparison to dvcs analysis

--- processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
+++ processing_classes/src/extended_kinematic_fitters/momentum_corrections.java
@@ -785,7 +785,7 @@ public class momentum_corrections {
         double px = pMag * sinT * Math.cos(phi);
         double py = pMag * sinT * Math.sin(phi);
         double pz = pMag * cosT;
-        System.out.println(px+" "+py+" "+pz);
+//        System.out.println(px+" "+py+" "+pz);
         return new Vector3(px, py, pz);
     }
 
