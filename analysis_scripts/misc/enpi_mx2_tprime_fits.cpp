f5fe3b41c2f4379474491b794d30f4634e4c39fc
Timothy B. Hayward <haywardt@mit.edu>
2026-04-21 20:33:18 -0400

    added data mc comparison to dvcs analysis

--- analysis_scripts/misc/enpi_mx2_tprime_fits.cpp
+++ analysis_scripts/misc/enpi_mx2_tprime_fits.cpp
@@ -65,7 +65,7 @@ static const std::string kTreeName = "PhysicsEvents";
 static const std::string kOutputDir = "output/enpi+_Mx2_fits";
 
 static constexpr double kMx2Min = 0.70;
-static constexpr double kMx2Max = 1.20;
+static constexpr double kMx2Max = 1.30;
 static constexpr int kNMx2Bins = 15;
 
 static constexpr double kMuMin = 0.85;
