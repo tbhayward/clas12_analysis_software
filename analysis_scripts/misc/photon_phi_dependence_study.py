d7ced90a0f125037071a3515d28858627b394b7f
Timothy B. Hayward <haywardt@mit.edu>
2026-04-27 12:39:25 -0400

    added data mc comparison to dvcs analysis

--- analysis_scripts/misc/photon_phi_dependence_study.py
+++ analysis_scripts/misc/photon_phi_dependence_study.py
@@ -17,7 +17,7 @@ input_file_name = sys.argv[1]
 tree_name = "PhysicsEvents"
 output_dir = "output/photon_phi_dependence_study"
 output_file_name = os.path.join(output_dir, "2D_phi_vs_theta.pdf")
-max_entries = 100000
+max_entries = 1000000
 
 os.makedirs(output_dir, exist_ok=True)
 
@@ -64,6 +64,32 @@ hist_p = ROOT.TH2D(
     140, 0.0, 35.0
 )
 
+sector_hists = []
+sector_colors = [
+    ROOT.kBlack,
+    ROOT.kRed + 1,
+    ROOT.kBlue + 1,
+    ROOT.kGreen + 2,
+    ROOT.kMagenta + 1,
+    ROOT.kOrange + 7,
+]
+
+for sector_index in range(6):
+    sector = sector_index + 1
+
+    hist = ROOT.TH1D(
+        "hist_p_theta_sector_{}".format(sector),
+        ";#theta_{#gamma} (deg);Counts",
+        140, 0.0, 35.0
+    )
+
+    hist.SetLineColor(sector_colors[sector_index])
+    hist.SetMarkerColor(sector_colors[sector_index])
+    hist.SetLineWidth(2)
+
+    sector_hists.append(hist)
+#endfor
+
 def wrap_phi_deg(phi_deg):
     while phi_deg < 0.0:
         phi_deg += 360.0
@@ -74,6 +100,36 @@ def wrap_phi_deg(phi_deg):
     #endwhile
 
     return phi_deg
+#enddef
+
+def get_clas12_sector_from_phi_deg(phi_deg):
+    phi = wrap_phi_deg(phi_deg)
+
+    if (phi >= 330.0 and phi < 360.0) or (phi >= 0.0 and phi < 30.0):
+        return 1
+    #endif
+
+    if phi >= 30.0 and phi < 90.0:
+        return 2
+    #endif
+
+    if phi >= 90.0 and phi < 150.0:
+        return 3
+    #endif
+
+    if phi >= 150.0 and phi < 210.0:
+        return 4
+    #endif
+
+    if phi >= 210.0 and phi < 270.0:
+        return 5
+    #endif
+
+    if phi >= 270.0 and phi < 330.0:
+        return 6
+    #endif
+
+    return 0
 #enddef
 
 n_entries = tree.GetEntries()
@@ -97,10 +153,15 @@ for i in range(n_entries_to_process):
 
     hist_e.Fill(e_phi_deg, e_theta_deg)
     hist_p.Fill(p_phi_deg, p_theta_deg)
+
+    sector = get_clas12_sector_from_phi_deg(p_phi_deg)
+    if sector >= 1 and sector <= 6:
+        sector_hists[sector - 1].Fill(p_theta_deg)
+    #endif
 #endfor
 
-canvas = ROOT.TCanvas("canvas", "canvas", 1400, 600)
-canvas.Divide(2, 1)
+canvas = ROOT.TCanvas("canvas", "canvas", 2100, 600)
+canvas.Divide(3, 1)
 
 canvas.cd(1)
 ROOT.gPad.SetLeftMargin(0.13)
@@ -113,6 +174,40 @@ ROOT.gPad.SetLeftMargin(0.13)
 ROOT.gPad.SetRightMargin(0.16)
 ROOT.gPad.SetBottomMargin(0.13)
 hist_p.Draw("COLZ")
+
+canvas.cd(3)
+ROOT.gPad.SetLeftMargin(0.14)
+ROOT.gPad.SetRightMargin(0.05)
+ROOT.gPad.SetBottomMargin(0.13)
+
+max_sector_count = 0.0
+for hist in sector_hists:
+    if hist.GetMaximum() > max_sector_count:
+        max_sector_count = hist.GetMaximum()
+    #endif
+#endfor
+
+if max_sector_count <= 0.0:
+    max_sector_count = 1.0
+#endif
+
+sector_hists[0].SetMaximum(1.20 * max_sector_count)
+sector_hists[0].Draw("HIST")
+
+legend = ROOT.TLegend(0.58, 0.60, 0.92, 0.88)
+legend.SetBorderSize(1)
+legend.SetFillStyle(1001)
+legend.SetFillColor(ROOT.kWhite)
+legend.SetTextSize(0.035)
+
+legend.AddEntry(sector_hists[0], "Sector 1", "l")
+
+for sector_index in range(1, 6):
+    sector_hists[sector_index].Draw("HIST SAME")
+    legend.AddEntry(sector_hists[sector_index], "Sector {}".format(sector_index + 1), "l")
+#endfor
+
+legend.Draw()
 
 canvas.SaveAs(output_file_name)
 
