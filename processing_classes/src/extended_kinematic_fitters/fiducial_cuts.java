018c2e041793dd5ba2d183cd388a5daf5850805e
Timothy B. Hayward <haywardt@mit.edu>
2026-04-28 10:59:20 -0400

    removed commented out runs that were being skipped in processing eppi0

--- processing_classes/src/extended_kinematic_fitters/fiducial_cuts.java
+++ processing_classes/src/extended_kinematic_fitters/fiducial_cuts.java
@@ -126,7 +126,7 @@ public class fiducial_cuts {
             return true;
         }
 
-        if (runnum >= 3030 && runnum <= 6783) { // only explicitly checked for RGA
+//        if (runnum >= 3030 && runnum <= 6783) { // only explicitly checked for RGA
             // RGA, RGK, RGB Sp19
             switch (sector) {
                 case 1:
@@ -171,20 +171,20 @@ public class fiducial_cuts {
                 default:
                     break;
             }
-        }
+//        }
 
-        if (runnum >= 6616 && runnum <= 6783) {
-            // RGA Sp19 !!only!!, maybe applies to RGB Sp19 but not checked
-            switch (sector) {
-                case 2:
-                    if (lv_1 > 31.5 && lv_1 < 49.5) {
-                        return false;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+////        if (runnum >= 6616 && runnum <= 6783) {
+//            // RGA Sp19 !!only!!, maybe applies to RGB Sp19 but not checked
+//            switch (sector) {
+//                case 2:
+//                    if (lv_1 > 31.5 && lv_1 < 49.5) {
+//                        return false;
+//                    }
+//                    break;
+//                default:
+//                    break;
+//            }
+////        }
 
         return true;
     }
