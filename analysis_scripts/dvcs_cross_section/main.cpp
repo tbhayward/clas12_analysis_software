5f196cc81b531d63f76300de5489d9c7fa786057
Timothy B. Hayward <haywardt@mit.edu>
2026-05-13 11:43:45 -0400

    full update of aaogen normalization method in python script and full dvcs cross section code, no longer uses event weighting but uses given cross section in output of generator

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -169,7 +169,7 @@ int main(int argc, char* argv[]) {
         norm_opts.combined_cuts_json = "output/jsons/combined_cuts.json";
         norm_opts.normalization_json_path = "imports/eppi0_aao_normalization_inputs.json";
         norm_opts.output_dir = "output/data_mc_normalization";
-        norm_opts.override_to_unity = true;
+        norm_opts.override_to_unity = false;
         norm_opts.max_workers = 5;
 
         // Set this to true to disable the eppi0 AAOGEN normalization study.
