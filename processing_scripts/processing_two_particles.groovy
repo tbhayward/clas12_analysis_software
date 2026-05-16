145467537f8da547853a5aafbe1444ef51d17b22
Timothy B. Hayward <haywardt@mit.edu>
2026-05-16 10:39:06 -0400

    radiative on for two particles

--- processing_scripts/processing_two_particles.groovy
+++ processing_scripts/processing_two_particles.groovy
@@ -177,7 +177,7 @@ public static void main(String[] args) {
             // --- Toggle here ---
             // false -> baseline (no inverse-ISR)
             // true  -> apply inverse-ISR (subtract R from q inside TwoParticles)
-            BeamEnergy Eb = new BeamEnergy(research_Event, runnum, /*isRadiative=*/true)
+            BeamEnergy Eb = new BeamEnergy(research_Event, runnum, /*isRadiative=*/false)
 
             double Ebeam = (runnum == 11) ? beam_energy : Eb.Eb()
 
