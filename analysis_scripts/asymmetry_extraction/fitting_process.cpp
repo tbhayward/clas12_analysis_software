80a9014a4adf8da05a04c8d70aa5375f9d4dbcb1
Timothy B. Hayward <haywardt@mit.edu>
2026-05-19 18:34:02 -0400

    lowered minimum e_p in dvcs_fitter and analysis_fitter to 1.3

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -4174,7 +4174,7 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
 
     minuit.DefineParameter(0,  "ALU_offset",      0.00,  0.01,  -0.1,  0.1);
     minuit.DefineParameter(1,  "AUL_offset",      0.00,  0.01,  -0.1,  0.1);
-    minuit.DefineParameter(2,  "F_LU_sin/F_UU",   0.00,  0.001,  -0.2,  0.4);
+    minuit.DefineParameter(2,  "F_LU_sin/F_UU",   0.00,  0.001,  -0.1,  0.22);
     minuit.DefineParameter(3,  "F_UL_sin/F_UU",   0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(4,  "F_UL_sin2/F_UU",  0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(5,  "F_LL/F_UU",       0.00,  0.001,  -1.0,  1.0);
