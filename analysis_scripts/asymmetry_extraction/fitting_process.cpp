862f4cc2f97c251ae3e876b74e32a058108a6ae8
Timothy B. Hayward <haywardt@mit.edu>
2026-05-19 10:27:25 -0400

    lowered minimum e_p in dvcs_fitter and analysis_fitter to 1.3

--- analysis_scripts/asymmetry_extraction/fitting_process.cpp
+++ analysis_scripts/asymmetry_extraction/fitting_process.cpp
@@ -4179,10 +4179,10 @@ void performChi2Fits_GeneralExclusive(const char* output_file,
     minuit.DefineParameter(4,  "F_UL_sin2/F_UU",  0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(5,  "F_LL/F_UU",       0.00,  0.001,  -1.0,  1.0);
     minuit.DefineParameter(6,  "F_LL_cos/F_UU",   0.00,  0.001,  -1.0,  1.0);
-    minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.01,  -1.0,  1.0);
-    minuit.DefineParameter(10, "A_T_LL",          0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.01,  -1.0,  1.0);
+    // minuit.DefineParameter(10, "A_T_LL",          0.00,  0.01,  -1.0,  1.0);
     // minuit.DefineParameter(7,  "F_UU_cos/F_UU",   0.00,  0.00,  -1.0,  1.0);
     // minuit.DefineParameter(8,  "F_UU_cos2/F_UU",  0.00,  0.00,  -1.0,  1.0);
     // minuit.DefineParameter(9,  "A_T_UL",          0.00,  0.00,  -1.0,  1.0);
