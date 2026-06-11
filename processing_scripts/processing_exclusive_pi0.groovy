fa3d74faaf3e923965c8f299c759f754e28ffeec
Timothy B. Hayward <haywardt@mit.edu>
2026-06-11 18:26:59 -0400

    refactor of dvcs analysis to allow for global selection of: topology, electron FD sector, photon FD sector, proton FD sector and proton CD sector

--- processing_scripts/processing_exclusive_pi0.groovy
+++ processing_scripts/processing_exclusive_pi0.groovy
@@ -271,7 +271,6 @@ public static void main(String[] args) {
 
 		    			// supply runnum and boolean for radiative simulation or not
 						BeamEnergy Eb = new BeamEnergy(research_Event, runnum, false);
-						println(BeamEnergy);
 						// Use the input beam energy if runnum == 11, otherwise use Eb.Eb()
 						double energy = (runnum == 11) ? beam_energy : Eb.Eb();
 			            ThreeParticles variables = new ThreeParticles(event, research_Event, 
