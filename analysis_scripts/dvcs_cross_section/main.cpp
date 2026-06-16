214b3b58df462e669d307f372fe93622327cd02a
Timothy B. Hayward <haywardt@mit.edu>
2026-06-16 14:39:05 -0400

    refactor of dvcs analysis to allow for global selection of: topology, electron FD sector, photon FD sector, proton FD sector and proton CD sector

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char* argv[]) {
 
     // Photon FD sector study. This automatically keeps only CD-FD and FD-FD events.
     global_cfg.enable_photon_fd_sector_filter = true;
-    global_cfg.photon_fd_sector = 1;
+    global_cfg.photon_fd_sector = 2;
 
     // Existing optional propagator/ycol mirror cut.
     global_cfg.enable_dvcsgen_ycol_cut = false;
