ddd8fc967d0c006e5f0a808457353788f10cd8df
Timothy B. Hayward <haywardt@mit.edu>
2026-06-29 09:45:53 -0400

    add pass-1 tag restriction to processing beam charge

--- processing_scripts/processing_beamCharge.groovy
+++ processing_scripts/processing_beamCharge.groovy
@@ -1,115 +1,165 @@
 /*
  * author Timothy B. Hayward
- * 
+ *
+ * Read only HIPO tag-1 events and extract RUN::scaler / HEL::scaler charge.
  */
 
 import java.io.File;
-import org.jlab.io.hipo.*;
-import org.jlab.io.base.DataEvent;
-import org.jlab.clas.physics.*;
-import org.jlab.clas12.physics.*;
-
-// import from hayward_coatjava_extensions
-import extended_kinematic_fitters.*; 
-import analyzers.*;
-
-// filetype for gathering files in directory
 import groovy.io.FileType;
 
+import org.jlab.jnp.hipo4.io.HipoReader;
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.data.SchemaFactory;
 
 public class processing_beamCharge {
 
-	public static void main(String[] args) {
-		// ~~~~~~~~~~~~~~~~ set up input paramaeters ~~~~~~~~~~~~~~~~ //
+    public static void main(String[] args) {
 
-		// Check if an argument is provided
-		if (!args) {
-		    // Print an error message and exit the program if the input directory is not specified
-		    println("ERROR: Please enter a hipo file directory as the first argument");
-		    System.exit(0);
-		}
-		// If the input directory is provided, iterate through each file recursively
-		def hipo_list = []
-		(args[0] as File).eachFileRecurse(FileType.FILES) 
-			{ if (it.name.endsWith('.hipo')) hipo_list << it }
+        // ~~~~~~~~~~~~~~~~ set up input parameters ~~~~~~~~~~~~~~~~ //
 
-		// Set the number of files to process based on the provided 4th argument
-		// use the size of the hipo_list if no argument provided
-		int n_files = args.length < 2 || Integer.parseInt(args[1]) > hipo_list.size()
-		    ? hipo_list.size() : Integer.parseInt(args[1]);
-		if (args.length < 2 || Integer.parseInt(args[1]) > hipo_list.size()) {
-		    // Print warnings and information if the number of files is not specified or too large
-		    println("WARNING: Number of files not specified or number too large.")
-		    println("Setting # of files to be equal to number of files in the directory.");
-		    println("There are $hipo_list.size files.");
-		}
+        if (!args || args.length < 1) {
+            println("ERROR: Please enter a hipo file directory as the first argument");
+            System.exit(0);
+        }
 
-		int num_events = 0;
-		int current_file = 0;
-		String beamChargeList = '';
-		float beamChargeMax = 0;
-		float runScalerCharge = 0;
-		float posHelbeamChargeTotal = 0;
-		float negHelbeamChargeTotal = 0;
-		float noHelbeamChargeTotal = 0;
-		int runnum;
-		while (current_file < n_files) {
-			def beamCharge = 0;
-			runnum = 0;
-			println(); println(); println("Opening file "+Integer.toString(current_file+1)
-				+" out of "+n_files); println(); println();
-			// limit to a certain number of files defined by n_files
+        def hipo_list = [];
 
-			HipoDataSource reader = new HipoDataSource();
+        (args[0] as File).eachFileRecurse(FileType.FILES) {
+            if (it.name.endsWith(".hipo")) {
+                hipo_list << it;
+            }
+        }
 
-			reader.open(hipo_list[current_file]); // open next hipo file
-			current_file++;
-			HipoDataEvent event = reader.getNextEvent(); 
+        hipo_list = hipo_list.sort { it.getAbsolutePath() };
 
+        int n_files = args.length < 2 || Integer.parseInt(args[1]) > hipo_list.size()
+            ? hipo_list.size()
+            : Integer.parseInt(args[1]);
 
-			while(reader.hasEvent()==true){
-				num_events++; 
-				if (num_events%1000000 == 0) { // not necessary, just updates output
-					print("processed: "+num_events+" events, max beamCharge of current ");
-					print("run = "+beamChargeMax+" nC.\n");
-				}
+        if (args.length < 2 || Integer.parseInt(args[1]) > hipo_list.size()) {
+            println("WARNING: Number of files not specified or number too large.");
+            println("Setting # of files to be equal to number of files in the directory.");
+            println("There are ${hipo_list.size()} files.");
+        }
 
-				if (event.hasBank("RUN::scaler")) {
-					runScalerCharge = event.getBank("RUN::scaler").getFloat("fcupgated",0);
-				}
+        // HIPO event tag to process.
+        // Tag 1 is usually the scaler / configuration stream.
+        int hipo_tag = 1;
 
-    			if (event.hasBank("HEL::scaler")) {
-    				if (event.getBank("HEL::scaler").getInt("helicity",0) == 1) {
-    					float posbeamCharge = event.getBank("HEL::scaler").getFloat("fcupgated",0);
-    					posHelbeamChargeTotal+=posbeamCharge;
-					} else if (event.getBank("HEL::scaler").getInt("helicity",0) == -1) {
-						float negbeamCharge = event.getBank("HEL::scaler").getFloat("fcupgated",0);
-    					negHelbeamChargeTotal+=negbeamCharge;
-					} else if (event.getBank("HEL::scaler").getInt("helicity",0) == 0) {
-						float undfbeamCharge = event.getBank("HEL::scaler").getFloat("fcupgated",0);
-    					noHelbeamChargeTotal+=undfbeamCharge;
-					}
-    			}
+        int num_events = 0;
+        int current_file = 0;
 
-				// get run and event numbers
-				event = reader.getNextEvent();
-				if (runnum==0) {
-					runnum = event.getBank("RUN::config").getInt('run',0);
-				} 
+        String beamChargeList = "";
 
-			}
-			beamChargeList+=runnum.toString()+","+(runScalerCharge).toString()+",";
-			beamChargeList+=posHelbeamChargeTotal.toString()+","
-			beamChargeList+=negHelbeamChargeTotal.toString()+","
-			beamChargeList+=noHelbeamChargeTotal.toString()+",0,0\n";
-			println(); println(); println();
-			print(beamChargeList);
-			println();
-			posHelbeamChargeTotal = 0;
-			negHelbeamChargeTotal = 0;
-			noHelbeamChargeTotal = 0;
-			runScalerCharge = 0;
-		}
+        while (current_file < n_files) {
 
-	}
+            File inputFile = hipo_list[current_file];
+
+            println();
+            println();
+            println("Opening file " + Integer.toString(current_file + 1) + " out of " + n_files);
+            println("File: " + inputFile.getAbsolutePath());
+            println("Reading only HIPO tag-" + hipo_tag + " events");
+            println();
+            println();
+
+            float beamChargeMax = 0.0f;
+            float runScalerCharge = 0.0f;
+            float posHelbeamChargeTotal = 0.0f;
+            float negHelbeamChargeTotal = 0.0f;
+            float noHelbeamChargeTotal = 0.0f;
+
+            int runnum = 0;
+
+            HipoReader reader = new HipoReader();
+
+            /*
+             * This is the important line.
+             *
+             * It must be called before reader.open(...).
+             * Only events with HIPO tag 1 will be returned by reader.nextEvent(event).
+             */
+            reader.setTags(hipo_tag);
+
+            reader.open(inputFile.getAbsolutePath());
+
+            SchemaFactory schema = reader.getSchemaFactory();
+
+            Bank runConfigBank = new Bank(schema.getSchema("RUN::config"));
+            Bank runScalerBank = new Bank(schema.getSchema("RUN::scaler"));
+            Bank helScalerBank = new Bank(schema.getSchema("HEL::scaler"));
+
+            Event event = new Event();
+
+            while (reader.hasNext()) {
+
+                reader.nextEvent(event);
+                num_events++;
+
+                event.read(runConfigBank);
+                event.read(runScalerBank);
+                event.read(helScalerBank);
+
+                if (runConfigBank.getRows() > 0 && runnum == 0) {
+                    runnum = runConfigBank.getInt("run", 0);
+                }
+
+                if (runScalerBank.getRows() > 0) {
+
+                    /*
+                     * RUN::scaler fcupgated is usually cumulative in the scaler stream.
+                     * Therefore, for a run/file total, keeping the largest value is safer
+                     * than summing over scaler rows/events.
+                     */
+                    float thisRunScalerCharge = runScalerBank.getFloat("fcupgated", 0);
+
+                    if (thisRunScalerCharge > runScalerCharge) {
+                        runScalerCharge = thisRunScalerCharge;
+                    }
+
+                    if (thisRunScalerCharge > beamChargeMax) {
+                        beamChargeMax = thisRunScalerCharge;
+                    }
+                }
+
+                if (helScalerBank.getRows() > 0) {
+
+                    for (int row = 0; row < helScalerBank.getRows(); row++) {
+
+                        int helicity = helScalerBank.getInt("helicity", row);
+                        float helBeamCharge = helScalerBank.getFloat("fcupgated", row);
+
+                        if (helicity == 1) {
+                            posHelbeamChargeTotal += helBeamCharge;
+                        } else if (helicity == -1) {
+                            negHelbeamChargeTotal += helBeamCharge;
+                        } else if (helicity == 0) {
+                            noHelbeamChargeTotal += helBeamCharge;
+                        }
+                    }
+                }
+
+                if (num_events % 1000000 == 0) {
+                    print("processed: " + num_events + " tag-" + hipo_tag + " events, ");
+                    print("max beamCharge of current run/file = " + beamChargeMax + " nC.\n");
+                }
+            }
+
+            reader.close();
+
+            beamChargeList += runnum.toString() + "," + runScalerCharge.toString() + ",";
+            beamChargeList += posHelbeamChargeTotal.toString() + ",";
+            beamChargeList += negHelbeamChargeTotal.toString() + ",";
+            beamChargeList += noHelbeamChargeTotal.toString() + ",0,0\n";
+
+            println();
+            println();
+            println();
+            print(beamChargeList);
+            println();
+
+            current_file++;
+        }
+    }
 }
