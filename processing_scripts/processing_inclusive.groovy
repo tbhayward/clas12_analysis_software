6a7d0b86ad3045226829024f5c27ba51e0cb03ee
Timothy B. Hayward <haywardt@mit.edu>
2026-07-01 13:13:48 -0400

    add pass-1 tag restriction to processing beam charge

--- processing_scripts/processing_inclusive.groovy
+++ processing_scripts/processing_inclusive.groovy
@@ -86,8 +86,8 @@ public static void main(String[] args) {
     EventFilter filter = new EventFilter("11:X+:X-:Xn")
 
     // setup QA database
-    QADB qa = new QADB("latest")
-    qa.checkForDefect('TotalOutlier')
+    QADB qa = new QADB("latest");
+    qa.checkForDefect('TotalOutlier')    
     qa.checkForDefect('TerminalOutlier')
     qa.checkForDefect('MarginalOutlier')
     qa.checkForDefect('SectorLoss')
@@ -96,19 +96,16 @@ public static void main(String[] args) {
     qa.checkForDefect('ChargeHigh')
     qa.checkForDefect('ChargeNegative')
     qa.checkForDefect('ChargeUnknown')
-
-    [
-        5418, 5419,
-        5443,
-        5444,
-        6616,
-        6736, 6737, 6738, 6739, 6740, 6741,
-        6742, 6743, 6744, 6746, 6747, 6748,
-        6749, 6750, 6751, 6753, 6754, 6755,
-        6756, 6757
-    ].each { run ->
-        qa.allowMiscBit(run)
-    }
+    [ // list of runs with `Misc` that should be allowed
+        // 5418, 5419, // RGA Fa18 Inb 5nA run
+        // 5443, // RGA Fa18 Out 5nA run
+        // 5444, // RGA Fa18 Out 20nA run
+        // 6616, // RGA Sp19 Inb 5nA run
+        6736, 6737, 6738, 6739, 6740, 6741, 
+            6742, 6743, 6744, 6746, 6747, 6748, 
+            6749, 6750, 6751, 6753, 6754, 6755, 
+            6756, 6757 // RGA runs FADC failure sector 6
+    ].each{ run -> qa.allowMiscBit(run) }
 
     StringBuilder batchLines = new StringBuilder()
 
