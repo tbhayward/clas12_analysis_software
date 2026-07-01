c9a79d7decb2ae1c850b060439ef66d6c30f994f
Timothy B. Hayward <haywardt@mit.edu>
2026-07-01 10:08:12 -0400

    add pass-1 tag restriction to processing beam charge

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]) {
     // and editing required_detector1/required_detector2.
     global_cfg.enable_topology_filter = true;
     global_cfg.required_detector1 = 2;  // 1 FD proton, 2 CD proton
-    global_cfg.required_detector2 = 1;  // 0 FT photon, 1 FD photon
+    global_cfg.required_detector2 = 0;  // 0 FT photon, 1 FD photon
 
     // Electron FD sector study. Electron is always FD.
     global_cfg.enable_electron_fd_sector_filter = false;
