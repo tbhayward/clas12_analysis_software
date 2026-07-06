9c8bc7446d7d1f018df98de9378bfb600fbdbcff
Timothy B. Hayward <haywardt@mit.edu>
2026-07-06 09:52:14 -0400

    addition of auxiliary fiducial cuts with stringent limits on polar angles, FT photon momentum minimum and requirement all FD tracks be in different sectors

--- analysis_scripts/dvcs_cross_section/main.cpp
+++ analysis_scripts/dvcs_cross_section/main.cpp
@@ -72,15 +72,15 @@ int main(int argc, char* argv[]) {
     global_cfg.electron_fd_sector = 1;
 
     // Proton FD sector study. This automatically keeps only FD-FD events.
-    global_cfg.enable_proton_fd_sector_filter = true;
-    global_cfg.proton_fd_sector = 6;
+    global_cfg.enable_proton_fd_sector_filter = false;
+    global_cfg.proton_fd_sector = 1;
 
     // Proton CD sector study. This automatically keeps only CD-FD and CD-FT events.
     global_cfg.enable_proton_cd_sector_filter = false;
     global_cfg.proton_cd_sector = 1;
 
     // Photon FD sector study. This automatically keeps only CD-FD and FD-FD events.
-    global_cfg.enable_photon_fd_sector_filter = false;
+    global_cfg.enable_photon_fd_sector_filter = true;
     global_cfg.photon_fd_sector = 1;
 
     // Auxiliary fiducial cuts. Enable this single switch to apply the additional
